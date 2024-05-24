263d29f1f7d665c38d0a9af13495eaca27783070
saiashish9 <saiashish3760@gmail.com>
2024-05-24 15:25:49 +0530

    k

--- src/screens/problem/index.js
+++ src/screens/problem/index.js
@@ -418,7 +418,7 @@ const Problem = () => {
                 </TabOptionsContainer>
                 <Editor
                   width="100%"
-                  height="85vh"
+                  height="63vh"
                   theme="vs-dark"
                   defaultLanguage="java"
                   userSelect={false}
