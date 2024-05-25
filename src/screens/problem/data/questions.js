c82699f6de6edabbbcd6f02a05eb80f437dc10d4
saiashish9 <saiashish3760@gmail.com>
2024-05-25 13:18:02 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -79,6 +79,160 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  470: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <strong>API</strong> <code>rand7()</code> that generates a
+        uniform random integer in the range <code>[1, 7]</code>, write a
+        function <code>rand10()</code> that generates a uniform random integer
+        in the range <code>[1, 10]</code>. You can only call the API{" "}
+        <code>rand7()</code>, and you shouldn't call any other API. Please{" "}
+        <strong>do not</strong> use a language's built-in random API.
+      </p>
+
+      <p>
+        Each test case will have one <strong>internal</strong> argument{" "}
+        <code>n</code>, the number of times that your implemented function{" "}
+        <code>rand10()</code> will be called while testing. Note that this is{" "}
+        <strong>not an argument</strong> passed to <code>rand10()</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> n = 1 <br />
+        <strong>Output:</strong> [2]
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> n = 2 <br /> <strong>Output:</strong> [2,8]
+      </pre>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> n = 3 <br /> <strong>Output:</strong> [3,8,10]
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong>
+      </p>
+
+      <ul>
+        <li>
+          What is the{" "}
+          <a
+            href="https://en.wikipedia.org/wiki/Expected_value"
+            target="_blank"
+            rel="noreferrer"
+          >
+            expected value
+          </a>{" "}
+          for the number of calls to <code>rand7()</code> function?
+        </li>
+        <li>
+          Could you minimize the number of calls to <code>rand7()</code>?
+        </li>
+      </ul>
+    </div>
+  ),
+  478: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the radius and the position of the center of a circle, implement
+        the function <code>randPoint</code> which generates a uniform random
+        point inside the circle.
+      </p>
+
+      <p>
+        Implement the <code>Solution</code> class:
+      </p>
+
+      <ul>
+        <li>
+          <code>Solution(double radius, double x_center, double y_center)</code>{" "}
+          initializes the object with the radius of the circle{" "}
+          <code>radius</code> and the position of the center{" "}
+          <code>(x_center, y_center)</code>.
+        </li>
+        <li>
+          <code>randPoint()</code> returns a random point inside the circle. A
+          point on the circumference of the circle is considered to be in the
+          circle. The answer is returned as an array <code>[x, y]</code>.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong>
+        <br />
+        ["Solution", "randPoint", "randPoint", "randPoint"]
+        <br />
+        [[1.0, 0.0, 0.0], [], [], []]
+        <br />
+        <strong>Output:</strong>
+        <br />
+        [null, [-0.02493, -0.38077], [0.82314, 0.38945], [0.36572, 0.17248]]
+        <br />
+        <strong>Explanation:</strong>
+        <br />
+        Solution solution = new Solution(1.0, 0.0, 0.0);
+        <br />
+        solution.randPoint(); // return [-0.02493, -0.38077]
+        <br />
+        solution.randPoint(); // return [0.82314, 0.38945]
+        <br />
+        solution.randPoint(); // return [0.36572, 0.17248]
+        <br />
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            0 &lt;&nbsp;radius &lt;= 10<sup>8</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>7</sup> &lt;= x_center, y_center &lt;= 10<sup>7</sup>
+          </code>
+        </li>
+        <li>
+          At most{" "}
+          <code>
+            3 * 10<sup>4</sup>
+          </code>{" "}
+          calls will be made to <code>randPoint</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
