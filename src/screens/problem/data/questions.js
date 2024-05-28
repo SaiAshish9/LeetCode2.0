0f3ce3e19ff75fdb960e6c5d4afe56fbda94f32c
saiashish9 <saiashish3760@gmail.com>
2024-05-28 17:30:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -233,33 +233,65 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
-  164:(<div class="elfjS" data-track-load="description_content"><p>Given an integer array <code>nums</code>, return <em>the maximum difference between two successive elements in its sorted form</em>. If the array contains less than two elements, return <code>0</code>.</p>
+  164: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code>, return{" "}
+        <em>
+          the maximum difference between two successive elements in its sorted
+          form
+        </em>
+        . If the array contains less than two elements, return <code>0</code>.
+      </p>
 
-  <p>You must write an algorithm that runs in linear time and uses linear extra space.</p>
-  
-  <p>&nbsp;</p>
-  <p><strong class="example">Example 1:</strong></p>
-  
-  <pre><strong>Input:</strong> nums = [3,6,9,1]<br/>
-  <strong>Output:</strong> 3<br/>
-  <strong>Explanation:</strong> The sorted form of the array is [1,3,6,9], either (3,6) or (6,9) has the maximum difference 3.
-  </pre>
-  
-  <p><strong class="example">Example 2:</strong></p>
-  
-  <pre><strong>Input:</strong> nums = [10] <br/>
-  <strong>Output:</strong> 0<br/>
-  <strong>Explanation:</strong> The array contains less than 2 elements, therefore return 0.
-  </pre>
-  
-  <p>&nbsp;</p>
-  <p><strong>Constraints:</strong></p>
-  
-  <ul>
-    <li><code>1 &lt;= nums.length &lt;= 10<sup>5</sup></code></li>
-    <li><code>0 &lt;= nums[i] &lt;= 10<sup>9</sup></code></li>
-  </ul>
-  </div>)
+      <p>
+        You must write an algorithm that runs in linear time and uses linear
+        extra space.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [3,6,9,1]
+        <br />
+        <strong>Output:</strong> 3<br />
+        <strong>Explanation:</strong> The sorted form of the array is [1,3,6,9],
+        either (3,6) or (6,9) has the maximum difference 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [10] <br />
+        <strong>Output:</strong> 0<br />
+        <strong>Explanation:</strong> The array contains less than 2 elements,
+        therefore return 0.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= nums[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
