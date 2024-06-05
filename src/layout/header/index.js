1028fd11dfa2ee67ccd67bb00c1c67fd7a43c7ae
saiashish9 <saiashish3760@gmail.com>
2024-06-06 02:54:38 +0530

    k

--- src/layout/header/index.js
+++ src/layout/header/index.js
@@ -74,6 +74,7 @@ const Navbar = () => {
         <NavItemContainer>
           <StyledImage
             alt="img"
+            onClick={() => navigate("/")}
             src={
               !pathname.includes("/tag")
                 ? "https://leetcode.com/_next/static/images/logo-dark-c96c407d175e36c81e236fcfdd682a0b.png"
