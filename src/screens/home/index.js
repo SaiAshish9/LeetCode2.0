24f9bb3e3f4402bd566ce3b51fd917a57461670d
saiashish9 <saiashish3760@gmail.com>
2024-06-05 23:16:59 +0530

    k

--- src/screens/home/index.js
+++ src/screens/home/index.js
@@ -18,10 +18,21 @@ import {
 } from "./components";
 
 import "react-circular-progressbar/dist/styles.css";
+import { Helmet } from "react-helmet";
 
 const HomeScreen = () => {
   return (
     <HomeComponent>
+      <Helmet>
+        <meta property="og:title" content="LeetCode2.0" />
+        <meta property="og:description" content="3K+ Optimal Solutions" />
+        <meta
+          property="og:image"
+          content="https://raw.githubusercontent.com/SaiAshish9/LeetCode2.0_Assets/main/LeetCode2.0.png"
+        />
+        <meta property="og:url" content="https://leetcodev2.vercel.app/" />
+        <meta property="og:type" content="website" />
+      </Helmet>
       <Content>
         <TopLeftContainer>
           <Banner />
