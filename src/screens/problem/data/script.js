9d1c6c5c798b4751c94ea841a9b1222ecc85accc
saiashish9 <saiashish3760@gmail.com>
2024-06-12 19:51:36 +0530

    k

---
+++ src/screens/problem/data/script.js
@@ -0,0 +1,16 @@
+const tags = require("./tags.json");
+
+let index = 0;
+
+tags.forEach((x, i) => {
+  if (x.tag.toLowerCase().includes("shell")) {
+    index = i;
+  }
+});
+
+console.log({
+  count: tags
+    .slice(index)
+    .map((x) => +x.count)
+    .reduce((a, b) => a + b, 0),
+});
