6cc7b464b17f2c9ade2a30c7e85d78aa8b0e28e6
saiashish9 <saiashish3760@gmail.com>
2024-06-13 01:30:21 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -3028,6 +3028,112 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  215: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code> and an integer <code>k</code>,
+        return <em>the</em>{" "}
+        <code>
+          k<sup>th</sup>
+        </code>{" "}
+        <em>largest element in the array</em>.
+      </p>
+
+      <p>
+        Note that it is the{" "}
+        <code>
+          k<sup>th</sup>
+        </code>{" "}
+        largest element in the sorted order, not the{" "}
+        <code>
+          k<sup>th</sup>
+        </code>{" "}
+        distinct element.
+      </p>
+
+      <p>Can you solve it without sorting?</p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> nums = [3,2,1,5,6,4], k = 2 <br />
+        <strong>Output:</strong> 5
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> nums = [3,2,3,1,2,4,5,5,6], k = 4 <br />
+        <strong>Output:</strong> 4
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= k &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= nums[i] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
+  324: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code>, reorder it such that{" "}
+        <code>nums[0] &lt; nums[1] &gt; nums[2] &lt; nums[3]...</code>.
+      </p>
+      <p>You may assume the input array always has a valid answer.</p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> nums = [1,5,1,1,6,4] <br />
+        <strong>Output:</strong> [1,6,1,5,1,4] <br />
+        <strong>Explanation:</strong> [1,4,1,5,1,6] is also accepted.
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> nums = [1,3,2,2,3,1]
+        <br />
+        <strong>Output:</strong> [2,3,1,3,1,2]
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 5 * 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>0 &lt;= nums[i] &lt;= 5000</code>
+        </li>
+        <li>
+          It is guaranteed that there will be an answer for the given input{" "}
+          <code>nums</code>.
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <strong>Follow Up:</strong> Can you do it in <code>O(n)</code> time and/or{" "}
+      <strong>in-place</strong> with <code>O(1)</code> extra space?
+    </div>
+  ),
 };
 
 export default QUESTIONS;
