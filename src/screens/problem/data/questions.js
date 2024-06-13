3ff5627583d33f6ab613121132e821403bc50ef5
saiashish9 <saiashish3760@gmail.com>
2024-06-13 16:53:50 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -5778,6 +5778,90 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  23: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an array of <code>k</code> linked-lists <code>lists</code>
+        , each linked-list is sorted in ascending order.
+      </p>
+
+      <p>
+        <em>
+          Merge all the linked-lists into one sorted linked-list and return it.
+        </em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> lists = [[1,4,5],[1,3,4],[2,6]] <br />
+        <strong>Output:</strong> [1,1,2,3,4,4,5,6] <br />
+        <strong>Explanation:</strong> The linked-lists are: <br />
+        [ <br />
+        &nbsp; 1-&gt;4-&gt;5, <br />
+        &nbsp; 1-&gt;3-&gt;4, <br />
+        &nbsp; 2-&gt;6 <br />
+        ] <br />
+        merging them into one sorted list: <br />
+        1-&gt;1-&gt;2-&gt;3-&gt;4-&gt;4-&gt;5-&gt;6 <br />
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> lists = [] <br />
+        <strong>Output:</strong> [] <br />
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> lists = [[]] <br />
+        <strong>Output:</strong> []
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>k == lists.length</code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= k &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>0 &lt;= lists[i].length &lt;= 500</code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= lists[i][j] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>lists[i]</code> is sorted in <strong>ascending order</strong>.
+        </li>
+        <li>
+          The sum of <code>lists[i].length</code> will not exceed{" "}
+          <code>
+            10<sup>4</sup>
+          </code>
+          .
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
