6ece539c382bbc31d0e72a5169950686a39e7930
saiashish9 <saiashish3760@gmail.com>
2024-06-13 10:00:47 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -3786,8 +3786,169 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
-  // 1227: (
-  // )
+  1227: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        <code>n</code> passengers board an airplane with exactly <code>n</code>{" "}
+        seats. The first passenger has lost the ticket and picks a seat
+        randomly. But after that, the rest of the passengers will:
+      </p>
+
+      <ul>
+        <li>Take their own seat if it is still available, and</li>
+        <li>Pick other seats randomly when they find their seat occupied</li>
+      </ul>
+
+      <p>
+        Return <em>the probability that the </em>
+        <code>
+          n<sup>th</sup>
+        </code>
+        <em> person gets his own seat</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 1<br />
+        <strong>Output:</strong> 1.00000 <br />
+        <strong>Explanation: </strong>The first person can only get the first
+        seat.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2<br />
+        <strong>Output:</strong> 0.50000
+        <br />
+        <strong>Explanation: </strong>The second person has a probability of 0.5
+        to get the second seat (when first person gets the first seat).
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
+  1467: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given <code>2n</code> balls of <code>k</code> distinct colors. You will
+        be given an integer array <code>balls</code> of size <code>k</code>{" "}
+        where <code>balls[i]</code> is the number of balls of color{" "}
+        <code>i</code>.
+      </p>
+
+      <p>
+        All the balls will be <strong>shuffled uniformly at random</strong>,
+        then we will distribute the first <code>n</code> balls to the first box
+        and the remaining <code>n</code> balls to the other box (Please read the
+        explanation of the second example carefully).
+      </p>
+
+      <p>
+        Please note that the two boxes are considered different. For example, if
+        we have two balls of colors <code>a</code> and <code>b</code>, and two
+        boxes <code>[]</code> and <code>()</code>, then the distribution{" "}
+        <code>[a] (b)</code> is considered different than the distribution{" "}
+        <code>[b] (a) </code>(Please read the explanation of the first example
+        carefully).
+      </p>
+
+      <p>
+        Return<em> the probability</em> that the two boxes have the same number
+        of distinct balls. Answers within{" "}
+        <code>
+          10<sup>-5</sup>
+        </code>{" "}
+        of the actual value will be accepted as correct.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> balls = [1,1] <br />
+        <strong>Output:</strong> 1.00000 <br />
+        <strong>Explanation:</strong> Only 2 ways to divide the balls equally:{" "}
+        <br />
+        - A ball of color 1 to box 1 and a ball of color 2 to box 2 <br />
+        - A ball of color 2 to box 1 and a ball of color 1 to box 2 <br />
+        In both ways, the number of distinct colors in each box is equal. The
+        probability is 2/2 = 1
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> balls = [2,1,1] <br />
+        <strong>Output:</strong> 0.66667 <br />
+        <strong>Explanation:</strong> We have the set of balls [1, 1, 2, 3]{" "}
+        <br />
+        This set of balls will be shuffled randomly and we may have one of the
+        12 distinct shuffles with equal probability (i.e. 1/12): <br />
+        [1,1 / 2,3], [1,1 / 3,2], [1,2 / 1,3], [1,2 / 3,1], [1,3 / 1,2], [1,3 /{" "}
+        <br />
+        2,1], [2,1 / 1,3], [2,1 / 3,1], [2,3 / 1,1], [3,1 / 1,2], [3,1 / 2,1],{" "}
+        <br />
+        [3,2 / 1,1] <br />
+        After that, we add the first two balls to the first box and the second
+        two balls to the second box. <br />
+        We can see that 8 of these 12 possible random distributions have the
+        same number of distinct colors of balls in each box. <br />
+        Probability is 8/12 = 0.66667
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> balls = [1,2,1,2] <br />
+        <strong>Output:</strong> 0.60000 <br />
+        <strong>Explanation:</strong> The set of balls is [1, 2, 2, 3, 4, 4]. It
+        is hard to display all the 180 possible random shuffles of this set but
+        it is easy to check that 108 of them will have the same number of
+        distinct colors in each box. Probability = 108 / 180 = 0.6
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= balls.length &lt;= 8</code>
+        </li>
+        <li>
+          <code>1 &lt;= balls[i] &lt;= 6</code>
+        </li>
+        <li>
+          <code>sum(balls)</code> is even.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
