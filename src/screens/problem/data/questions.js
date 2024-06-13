007e857d7259b27c05b19fb76f20e92c4bc0fb67
saiashish9 <saiashish3760@gmail.com>
2024-06-13 17:00:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -5862,6 +5862,79 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  148: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>head</code> of a linked list, return{" "}
+        <em>
+          the list after sorting it in <strong>ascending order</strong>
+        </em>
+        .
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <br />
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/09/14/sort_list_1.jpg"
+        style={{ width: 450, height: 194 }}
+      />
+      <br />
+      <pre>
+        <strong>Input:</strong> head = [4,2,1,3] <br />
+        <strong>Output:</strong> [1,2,3,4]
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/09/14/sort_list_2.jpg"
+        style={{ width: 550, height: 184 }}
+      />{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> head = [-1,5,3,4,0]
+        <br />
+        <strong>Output:</strong> [-1,0,3,4,5]
+      </pre>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <br />
+      <pre>
+        <strong>Input:</strong> head = []
+        <br />
+        <strong>Output:</strong> []
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          The number of nodes in the list is in the range{" "}
+          <code>
+            [0, 5 * 10<sup>4</sup>]
+          </code>
+          .
+        </li>
+        <li>
+          <code>
+            -10<sup>5</sup> &lt;= Node.val &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong> Can you sort the linked list in{" "}
+        <code>O(n logn)</code> time and <code>O(1)</code> memory (i.e. constant
+        space)?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
