d2ad3f88b6eb4a2662c852951401575a842e8bb0
saiashish9 <saiashish3760@gmail.com>
2024-06-17 17:59:27 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -7214,6 +7214,59 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  716: (
+    <div class="markdown-body div-width">
+      <p>
+        Design a max stack that supports push, pop, top, peekMax and popMax.
+      </p>
+
+      <p></p>
+      <ol>
+        <li>push(x) -- Push element x onto stack.</li>
+        <li>pop() -- Remove the element on top of the stack and return it.</li>
+        <li>top() -- Get the element on the top.</li>
+        <li>peekMax() -- Retrieve the maximum element in the stack.</li>
+        <li>
+          popMax() -- Retrieve the maximum element in the stack, and remove it.
+          If you find more than one maximum elements, only remove the top-most
+          one.
+        </li>
+      </ol>
+      <p></p>
+
+      <p>
+        <b>Example 1:</b>
+        <br />
+      </p>
+      <pre>
+        MaxStack stack = new MaxStack(); <br />
+        stack.push(5);
+        <br />
+        stack.push(1);
+        <br />
+        stack.push(5);
+        <br />
+        stack.top(); -&gt; 5<br />
+        stack.popMax(); -&gt; 5<br />
+        stack.top(); -&gt; 1<br />
+        stack.peekMax(); -&gt; 5<br />
+        stack.pop(); -&gt; 1<br />
+        stack.top(); -&gt; 5
+      </pre>
+      <p></p>
+
+      <p>
+        <b>Note:</b>
+        <br />
+      </p>
+      <ol>
+        <li>-1e7 &lt;= x &lt;= 1e7</li>
+        <li>Number of operations won't exceed 10000.</li>
+        <li>The last four operations won't be called when stack is empty.</li>
+      </ol>
+      <p></p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
