71aa1c58386fbc75d33f3d3f37456ee196fc36c6
saiashish9 <saiashish3760@gmail.com>
2024-06-18 14:47:39 +0530

    k

--- src/screens/problem/data/script.js
+++ src/screens/problem/data/script.js
@@ -3,7 +3,7 @@ const tags = require("./tags.json");
 let index = 0;
 
 tags.forEach((x, i) => {
-  if (x.tag.toLowerCase().includes("concurre")) {
+  if (x.tag.toLowerCase().includes("iterator")) {
     index = i;
   }
 });
