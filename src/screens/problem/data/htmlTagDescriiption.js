cc7030c2f3182429e69c8dea02e0dad1f78fb0e4
saiashish9 <saiashish3760@gmail.com>
2024-06-19 11:48:26 +0530

    k

--- src/screens/problem/data/htmlTagDescriiption.js
+++ src/screens/problem/data/htmlTagDescriiption.js
@@ -7,6 +7,18 @@ const TAG_DESCRIPTION = {
         The spanning tree includes all the vertices of the graph, and the total
         weight of all the edges in the tree is minimized.
       </p>
+      <br />
+      <p>
+        Kruskal's algorithm and Prim's algorithm are both popular algorithms
+        used to find the minimum spanning tree (MST) of a weighted undirected
+        graph, but they use different approaches to achieve this.
+      </p>
+      <br />
+      <p>
+        In summary, both algorithms are effective for finding minimum spanning
+        trees, but the choice between them often depends on factors such as the
+        graph representation, graph density, and available data structures.
+      </p>
     </>
   ),
 };
