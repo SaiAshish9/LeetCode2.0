daa24649bbb43d61292a6f2154e61257af9a2b3d
saiashish9 <saiashish3760@gmail.com>
2024-06-19 15:09:08 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -8588,6 +8588,84 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  384: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code>, design an algorithm to
+        randomly shuffle the array. All permutations of the array should be{" "}
+        <strong>equally likely</strong> as a result of the shuffling.
+      </p>
+
+      <p>
+        Implement the <code>Solution</code> class:
+      </p>
+
+      <ul>
+        <li>
+          <code>Solution(int[] nums)</code> Initializes the object with the
+          integer array <code>nums</code>.
+        </li>
+        <li>
+          <code>int[] reset()</code> Resets the array to its original
+          configuration and returns it.
+        </li>
+        <li>
+          <code>int[] shuffle()</code> Returns a random shuffling of the array.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input</strong> <br />
+        ["Solution", "shuffle", "reset", "shuffle"] <br />
+        [[[1, 2, 3]], [], [], []] <br />
+        <strong>Output</strong> <br />
+        [null, [3, 1, 2], [1, 2, 3], [1, 3, 2]] <br/>
+        <strong>Explanation</strong> <br />
+        Solution solution = new Solution([1, 2, 3]); <br />
+        solution.shuffle(); // Shuffle the array [1,2,3] and return its result.{" "}
+        <br />
+        // Any permutation of [1,2,3] must be equally likely to be returned.{" "}
+        <br />
+        // Example: return [3, 1, 2] <br />
+        solution.reset(); // Resets the array back to its original configuration
+        [1,2,3]. Return [1, 2, 3] <br />
+        solution.shuffle(); // Returns the random shuffling of array [1,2,3].
+        Example: return [1, 3, 2]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 50</code>
+        </li>
+        <li>
+          <code>
+            -10<sup>6</sup> &lt;= nums[i] &lt;= 10<sup>6</sup>
+          </code>
+        </li>
+        <li>
+          All the elements of <code>nums</code> are <strong>unique</strong>.
+        </li>
+        <li>
+          At most{" "}
+          <code>
+            10<sup>4</sup>
+          </code>{" "}
+          calls <strong>in total</strong> will be made to <code>reset</code> and{" "}
+          <code>shuffle</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
