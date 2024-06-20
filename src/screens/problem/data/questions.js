f74a7f3b3f0335ac8c36838e39b02171b67667b7
saiashish9 <saiashish3760@gmail.com>
2024-06-21 00:30:44 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -9673,7 +9673,8 @@ Window position                Max
           <strong>5</strong>
         </u>
         ,4],
-        <br />workers = [
+        <br />
+        workers = [
         <u>
           <strong>0</strong>
         </u>
@@ -9693,7 +9694,8 @@ Window position                Max
 
       <pre>
         <strong>Input:</strong>
-        <br />tasks = [
+        <br />
+        tasks = [
         <u>
           <strong>10</strong>
         </u>
@@ -9702,7 +9704,8 @@ Window position                Max
           <strong>15</strong>
         </u>
         ,30],
-        <br />workers = [
+        <br />
+        workers = [
         <u>
           <strong>0</strong>
         </u>
@@ -9749,6 +9752,103 @@ Window position                Max
         <li>
           <code>
             0 &lt;= tasks[i], workers[j], strength &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
+  2407: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>nums</code> and an integer{" "}
+        <code>k</code>.
+      </p>
+
+      <p>
+        Find the longest subsequence of <code>nums</code> that meets the
+        following requirements:
+      </p>
+
+      <ul>
+        <li>
+          The subsequence is <strong>strictly increasing</strong> and
+        </li>
+        <li>
+          The difference between adjacent elements in the subsequence is{" "}
+          <strong>at most</strong> <code>k</code>.
+        </li>
+      </ul>
+
+      <p>
+        Return
+        <em>
+          {" "}
+          the length of the <strong>longest</strong>{" "}
+          <strong>subsequence</strong> that meets the requirements.
+        </em>
+      </p>
+
+      <p>
+        A <strong>subsequence</strong> is an array that can be derived from
+        another array by deleting some or no elements without changing the order
+        of the remaining elements.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [4,2,1,4,3,4,5,8,15], k = 3 <br />
+        <strong>Output:</strong> 5 <br />
+        <strong>Explanation:</strong> <br />
+        The longest subsequence that meets the requirements is [1,3,4,5,8].{" "}
+        <br />
+        The subsequence has a length of 5, so we return 5. <br />
+        Note that the subsequence [1,3,4,5,8,15] does not meet the requirements
+        because 15 - 8 = 7 is larger than 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [7,4,5,1,8,12,4,7], k = 5 <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> <br />
+        The longest subsequence that meets the requirements is [4,5,8,12].{" "}
+        <br />
+        The subsequence has a length of 4, so we return 4.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,5], k = 1 <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> <br />
+        The longest subsequence that meets the requirements is [1]. <br />
+        The subsequence has a length of 1, so we return 1.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= nums[i], k &lt;= 10<sup>5</sup>
           </code>
         </li>
       </ul>
