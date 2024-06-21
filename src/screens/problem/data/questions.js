593e25d8d21ff29e64ee4dedc437bc7c5129a7a0
saiashish9 <saiashish3760@gmail.com>
2024-06-22 01:28:30 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -11699,6 +11699,78 @@ for (int i = 0; i &lt; actualLength; i++) {
       </p>
     </div>
   ),
+  125: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        A phrase is a <strong>palindrome</strong> if, after converting all
+        uppercase letters into lowercase letters and removing all
+        non-alphanumeric characters, it reads the same forward and backward.
+        Alphanumeric characters include letters and numbers.
+      </p>
+
+      <p>
+        Given a string <code>s</code>, return <code>true</code>
+        <em>
+          {" "}
+          if it is a <strong>palindrome</strong>, or{" "}
+        </em>
+        <code>false</code>
+        <em> otherwise</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "A man, a plan, a canal: Panama" <br />
+        <strong>Output:</strong> true
+        <br />
+        <strong>Explanation:</strong> "amanaplanacanalpanama" is a palindrome.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "race a car"
+        <br />
+        <strong>Output:</strong> false
+        <br />
+        <strong>Explanation:</strong> "raceacar" is not a palindrome.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = " "<br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> s is an empty string "" after removing
+        non-alphanumeric characters. Since an empty string reads the same
+        forward and backward, it is a palindrome.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 2 * 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>s</code> consists only of printable ASCII characters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
