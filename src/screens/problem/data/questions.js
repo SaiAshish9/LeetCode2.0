247ff8a89c94e06f93f02ccc140be4f6d1db1b12
saiashish9 <saiashish3760@gmail.com>
2024-06-22 00:49:25 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -11543,6 +11543,61 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  86: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>head</code> of a linked list and a value <code>x</code>,
+        partition it such that all nodes <strong>less than</strong>{" "}
+        <code>x</code> come before nodes <strong>greater than or equal</strong>{" "}
+        to <code>x</code>.
+      </p>
+
+      <p>
+        You should <strong>preserve</strong> the original relative order of the
+        nodes in each of the two partitions.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/01/04/partition.jpg"
+        style={{ width: 662, height: 222 }}
+      />
+      <pre>
+        <strong>Input:</strong> head = [1,4,3,2,5,2], x = 3 <br />
+        <strong>Output:</strong> [1,2,2,4,3,5]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> head = [2,1], x = 2 <br />
+        <strong>Output:</strong> [1,2]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the list is in the range <code>[0, 200]</code>.
+        </li>
+        <li>
+          <code>-100 &lt;= Node.val &lt;= 100</code>
+        </li>
+        <li>
+          <code>-200 &lt;= x &lt;= 200</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
