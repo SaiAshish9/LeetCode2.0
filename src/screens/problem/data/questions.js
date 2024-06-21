a6340d9702ad1ce3a4308eb4bf51f01ec1a41021
saiashish9 <saiashish3760@gmail.com>
2024-06-21 15:59:04 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -10612,6 +10612,63 @@ Window position                Max
       </ul>
     </div>
   ),
+  16: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code> of length <code>n</code> and an
+        integer <code>target</code>, find three integers in <code>nums</code>{" "}
+        such that the sum is closest to <code>target</code>.
+      </p>
+
+      <p>
+        Return <em>the sum of the three integers</em>.
+      </p>
+
+      <p>You may assume that each input would have exactly one solution.</p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [-1,2,1,-4], target = 1 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> The sum that is closest to the target is
+        2. (-1 + 2 + 1 = 2).
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [0,0,0], target = 1 <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> The sum that is closest to the target is
+        0. (0 + 0 + 0 = 0).
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>3 &lt;= nums.length &lt;= 500</code>
+        </li>
+        <li>
+          <code>-1000 &lt;= nums[i] &lt;= 1000</code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= target &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
