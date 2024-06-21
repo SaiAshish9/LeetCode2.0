7db4e7075b9570483aea86c43fda4eb0bb193e31
saiashish9 <saiashish3760@gmail.com>
2024-06-22 01:44:49 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -11876,6 +11876,106 @@ for (int i = 0; i &lt; actualLength; i++) {
       </p>
     </div>
   ),
+  142: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>head</code> of a linked list, return{" "}
+        <em>the node where the cycle begins. If there is no cycle, return </em>
+        <code>null</code>.
+      </p>
+
+      <p>
+        There is a cycle in a linked list if there is some node in the list that
+        can be reached again by continuously following the <code>next</code>{" "}
+        pointer. Internally, <code>pos</code> is used to denote the index of the
+        node that tail's <code>next</code> pointer is connected to (
+        <strong>0-indexed</strong>). It is <code>-1</code> if there is no cycle.{" "}
+        <strong>Note that</strong> <code>pos</code>{" "}
+        <strong>is not passed as a parameter</strong>.
+      </p>
+
+      <p>
+        <strong>Do not modify</strong> the linked list.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2018/12/07/circularlinkedlist.png"
+        style={{ height: 145, width: 450 }}
+      />
+      <pre>
+        <strong>Input:</strong> head = [3,2,0,-4], pos = 1 <br />
+        <strong>Output:</strong> tail connects to node index 1<br />
+        <strong>Explanation:</strong> There is a cycle in the linked list, where
+        tail connects to the second node.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2018/12/07/circularlinkedlist_test2.png"
+        style={{ height: 105, width: 201 }}
+      />
+      <pre>
+        <strong>Input:</strong> head = [1,2], pos = 0<br />
+        <strong>Output:</strong> tail connects to node index 0<br />
+        <strong>Explanation:</strong> There is a cycle in the linked list, where
+        tail connects to the first node.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2018/12/07/circularlinkedlist_test3.png"
+        style={{ height: 65, width: 65 }}
+      />
+      <pre>
+        <strong>Input:</strong> head = [1], pos = -1
+        <br />
+        <strong>Output:</strong> no cycle
+        <br />
+        <strong>Explanation:</strong> There is no cycle in the linked list.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of the nodes in the list is in the range{" "}
+          <code>
+            [0, 10<sup>4</sup>]
+          </code>
+          .
+        </li>
+        <li>
+          <code>
+            -10<sup>5</sup> &lt;= Node.val &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>pos</code> is <code>-1</code> or a <strong>valid index</strong>{" "}
+          in the linked-list.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong> Can you solve it using <code>O(1)</code>{" "}
+        (i.e. constant) memory?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
