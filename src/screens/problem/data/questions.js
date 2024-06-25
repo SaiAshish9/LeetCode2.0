e665ddc231aa81f0bb5ffc7e245b187894db2d1d
saiashish9 <saiashish3760@gmail.com>
2024-06-25 14:04:52 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -13227,6 +13227,88 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  683: (
+    <div class="markdown-body div-width">
+      <p>
+        You have <code>N</code> bulbs in a row numbered from <code>1</code> to{" "}
+        <code>N</code>. Initially, all the bulbs everyday until all bulbs are on
+        after <code>N</code> days.
+      </p>
+
+      <p>
+        You are given an array <code>bulbs</code>&nbsp;of length <code>N</code>
+        &nbsp;where <code>bulbs[i] = x</code> means that on the{" "}
+        <code>(i+1)th</code> day, we will turn on the bulb at position
+        <code>x</code>&nbsp;where&nbsp;<code>i</code>&nbsp;is&nbsp;
+        <code>0-indexed</code>&nbsp;and&nbsp;<code>x</code>&nbsp;is&nbsp;
+        <code>1-indexed.</code>
+      </p>
+
+      <p>
+        Given an integer <code>K</code>, find out the{" "}
+        <strong>minimum day number</strong> such that there exists two{" "}
+        <strong>turned on</strong> bulbs that have
+        <strong>exactly</strong>&nbsp;<code>K</code> bulbs between them that are{" "}
+        <strong>all turned off</strong>.
+      </p>
+
+      <p>
+        If there isn't such day, return <code>-1</code>.
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <b>Example 1:</b>
+      </p>
+
+      <pre>
+        <strong>Input:</strong>
+        bulbs: [1,3,2] K: 1
+        <br />
+        <strong>Output:</strong> 2
+        <strong>
+          <br />
+          Explanation:
+        </strong>
+        On the first day: bulbs[0] = 1, first bulb is turned on: [1,0,0] On the
+        second day: bulbs[1] = 3, third bulb is turned on: [1,0,1] On the third
+        day: bulbs[2] = 2, second bulb is turned on: [1,1,1] We return 2 because
+        on the second day, there were two on bulbs with one off bulb between
+        them.
+      </pre>
+
+      <p>
+        <b>Example 2:</b>
+      </p>
+
+      <pre>
+        <strong>Input:</strong>
+        bulbs: [1,2,3] K: 1<br />
+        <strong>Output:</strong> -1
+      </pre>
+
+      <p>
+        <b>Note:</b>
+      </p>
+
+      <ol>
+        <li>
+          <code>1 &lt;= N &lt;= 20000</code>
+        </li>
+        <li>
+          <code>1 &lt;= bulbs[i] &lt;= N</code>
+        </li>
+        <li>
+          <code>bulbs</code>&nbsp;is a permutation of numbers from&nbsp;
+          <code>1</code>&nbsp;to&nbsp;<code>N</code>.
+        </li>
+        <li>
+          <code>0 &lt;= K &lt;= 20000</code>
+        </li>
+      </ol>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
