aa51a3012ed51541c8fee544cc29c2a91c15302c
saiashish9 <saiashish3760@gmail.com>
2024-06-26 00:26:10 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -16056,6 +16056,70 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1876: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        A string is <strong>good</strong> if there are no repeated characters.
+      </p>
+
+      <p>
+        Given a string <code>s</code>​​​​​, return{" "}
+        <em>
+          the number of <strong>good substrings</strong> of length{" "}
+          <strong>three </strong>in{" "}
+        </em>
+        <code>s</code>​​​​​​.
+      </p>
+
+      <p>
+        Note that if there are multiple occurrences of the same substring, every
+        occurrence should be counted.
+      </p>
+
+      <p>
+        A <strong>substring</strong> is a contiguous sequence of characters in a
+        string.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "xyzzaz" <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> There are 4 substrings of size 3: "xyz",
+        "yzz", "zza", and "zaz". The only good substring of length 3 is "xyz".
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "aababcabc" <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> There are 7 substrings of size 3: "aab",
+        "aba", "bab", "abc", "bca", "cab", and "abc". The good substrings are
+        "abc", "bca", "cab", and "abc".
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= s.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>s</code>​​​​​​ consists of lowercase English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
