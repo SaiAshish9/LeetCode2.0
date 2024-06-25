f9c6c5748af60ca101c0e57967b729d98fac6bc3
saiashish9 <saiashish3760@gmail.com>
2024-06-25 12:15:55 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -12636,6 +12636,71 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  424: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a string <code>s</code> and an integer <code>k</code>. You
+        can choose any character of the string and change it to any other
+        uppercase English character. You can perform this operation at most{" "}
+        <code>k</code> times.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the length of the longest substring containing the same letter you can
+          get after performing the above operations
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "ABAB", k = 2 <br />
+        <strong>Output:</strong> 4<br />
+        <strong>Explanation:</strong> Replace the two 'A's with two 'B's or vice
+        versa.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "AABABBA", k = 1<br />
+        <strong>Output:</strong> 4<br />
+        <strong>Explanation:</strong> Replace the one 'A' in the middle with 'B'
+        and form "AABBBBA".
+        <br />
+        The substring "BBBB" has the longest repeating letters, which is 4.
+        <br />
+        There may exists other ways to achieve this answer too.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>s</code> consists of only uppercase English letters.
+        </li>
+        <li>
+          <code>0 &lt;= k &lt;= s.length</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
