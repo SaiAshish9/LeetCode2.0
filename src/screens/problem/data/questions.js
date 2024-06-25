0f0839ce9fcd7b0769bf0a405f676d15b0148f4d
saiashish9 <saiashish3760@gmail.com>
2024-06-25 12:59:49 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -12901,6 +12901,63 @@ for (int i = 0; i &lt; actualLength; i++) {
       </p>
     </div>
   ),
+  567: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given two strings <code>s1</code> and <code>s2</code>, return{" "}
+        <code>true</code>
+        <em> if </em>
+        <code>s2</code>
+        <em> contains a permutation of </em>
+        <code>s1</code>
+        <em>, or </em>
+        <code>false</code>
+        <em> otherwise</em>.
+      </p>
+
+      <p>
+        In other words, return <code>true</code> if one of <code>s1</code>'s
+        permutations is the substring of <code>s2</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s1 = "ab", s2 = "eidbaooo" <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> s2 contains one permutation of s1 ("ba").
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s1 = "ab", s2 = "eidboaoo" <br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s1.length, s2.length &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>s1</code> and <code>s2</code> consist of lowercase English
+          letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
