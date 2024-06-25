14b7707fb96a52836133b0ccaa5ffdee073d3dba
saiashish9 <saiashish3760@gmail.com>
2024-06-25 18:24:12 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -14526,6 +14526,90 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1234: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a string s of length <code>n</code> containing only four
+        kinds of characters: <code>'Q'</code>, <code>'W'</code>,{" "}
+        <code>'E'</code>, and <code>'R'</code>.
+      </p>
+
+      <p>
+        A string is said to be <strong>balanced</strong>
+        <em> </em>if each of its characters appears <code>n / 4</code> times
+        where <code>n</code> is the length of the string.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the minimum length of the substring that can be replaced with{" "}
+          <strong>any</strong> other string of the same length to make{" "}
+        </em>
+        <code>s</code>
+        <em>
+          {" "}
+          <strong>balanced</strong>
+        </em>
+        . If s is already <strong>balanced</strong>, return <code>0</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "QWER" <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> s is already balanced.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "QQWE" <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> We need to replace a 'Q' to 'R', so that
+        "RQWE" (or "QRWE") is balanced.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "QQQW" <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> We can replace the first "QQ" to "ER".
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == s.length</code>
+        </li>
+        <li>
+          <code>
+            4 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>n</code> is a multiple of <code>4</code>.
+        </li>
+        <li>
+          <code>s</code> contains only <code>'Q'</code>, <code>'W'</code>,{" "}
+          <code>'E'</code>, and <code>'R'</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
