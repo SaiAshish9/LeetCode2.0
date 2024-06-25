e54705d7397d5b02959c98ff28a03900060f6eee
saiashish9 <saiashish3760@gmail.com>
2024-06-25 16:17:17 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -13893,6 +13893,68 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1004: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a binary array <code>nums</code> and an integer <code>k</code>,
+        return <em>the maximum number of consecutive </em>
+        <code>1</code>
+        <em>'s in the array if you can flip at most</em> <code>k</code>{" "}
+        <code>0</code>'s.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2 <br />
+        <strong>Output:</strong> 6 <br />
+        <strong>Explanation:</strong> [1,1,1,0,0,
+        <u>
+          <strong>1</strong>,1,1,1,1,<strong>1</strong>
+        </u>
+        ] Bolded numbers were flipped from 0 to 1. The longest subarray is
+        underlined.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [0,0,1,1,0,0,1,1,1,0,1,1,0,0,0,1,1,1,1],
+        k = 3 <br />
+        <strong>Output:</strong> 10 <br />
+        <strong>Explanation:</strong> [0,0,
+        <u>
+          1,1,<strong>1</strong>,<strong>1</strong>,1,1,1,<strong>1</strong>,1,1
+        </u>
+        ,0,0,0,1,1,1,1] Bolded numbers were flipped from 0 to 1. The longest
+        subarray is underlined.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>nums[i]</code> is either <code>0</code> or <code>1</code>.
+        </li>
+        <li>
+          <code>0 &lt;= k &lt;= nums.length</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
