73a0cfcab8d6e924843d53eaa1b32ffd76e9183f
saiashish9 <saiashish3760@gmail.com>
2024-06-25 11:30:42 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -12471,6 +12471,75 @@ for (int i = 0; i &lt; actualLength; i++) {
       <code>O(n log(n))</code>.
     </div>
   ),
+  219: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code> and an integer <code>k</code>,
+        return <code>true</code>{" "}
+        <em>
+          if there are two <strong>distinct indices</strong>{" "}
+        </em>
+        <code>i</code>
+        <em> and </em>
+        <code>j</code>
+        <em> in the array such that </em>
+        <code>nums[i] == nums[j]</code>
+        <em> and </em>
+        <code>abs(i - j) &lt;= k</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,1], k = 3 <br />
+        <strong>Output:</strong> true
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,0,1,1], k = 1<br />
+        <strong>Output:</strong> true
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,1,2,3], k = 2<br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>9</sup> &lt;= nums[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= k &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
