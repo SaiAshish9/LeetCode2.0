7b6db7ef953505dc96305f18fde79b6234e54fb0
saiashish9 <saiashish3760@gmail.com>
2024-06-25 16:59:11 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -14130,6 +14130,71 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1156: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a string <code>text</code>. You can swap two of the
+        characters in the <code>text</code>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>the length of the longest substring with repeated characters</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> text = "ababa" <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> We can swap the first 'b' with the last
+        'a', or the last 'b' with the first 'a'. Then, the longest repeated
+        character substring is "aaa" with length 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> text = "aaabaaa" <br />
+        <strong>Output:</strong> 6 <br />
+        <strong>Explanation:</strong> Swap 'b' with the last 'a' (or the first
+        'a'), and we get longest repeated character substring "aaaaaa" with
+        length 6.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> text = "aaaaa" <br />
+        <strong>Output:</strong> 5 <br />
+        <strong>Explanation:</strong> No need to swap, longest repeated
+        character substring is "aaaaa" with length is 5.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= text.length &lt;= 2 * 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>text</code> consist of lowercase English characters only.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
