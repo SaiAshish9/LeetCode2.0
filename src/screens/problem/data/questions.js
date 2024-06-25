f535a350306a15610e9cc66bb74fdd767c96811c
saiashish9 <saiashish3760@gmail.com>
2024-06-25 17:29:16 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -14250,6 +14250,78 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1151: (
+    <div class="markdown-body div-width">
+      <p>
+        Given a&nbsp;binary array <code>data</code>, return&nbsp;the minimum
+        number of swaps required to group all <code>1</code>’s present in the
+        array together in <strong>any place</strong> in the array.
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <span id="example-input-1-1">[1,0,1,0,1]</span> <br />
+        <strong>Output: </strong>
+        <span id="example-output-1">1</span> <br />
+        <strong>Explanation: </strong> <br />
+        There are 3 ways to group all 1's together: <br />
+        [1,1,1,0,0] using 1 swap. <br />
+        [0,1,1,1,0] using 2 swaps. <br />
+        [0,0,1,1,1] using 1 swap. <br />
+        The minimum is 1.
+      </pre>
+
+      <p>
+        <strong>Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <span id="example-input-2-1">[0,0,0,1,0]</span> <br />
+        <strong>Output: </strong>
+        <span id="example-output-2">0</span> <br />
+        <strong>Explanation: </strong> <br />
+        Since there is only one 1 in the array, no swaps needed.
+      </pre>
+
+      <p>
+        <strong>Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <span id="example-input-3-1">[1,0,1,0,1,0,0,1,1,0,1]</span> <br />
+        <strong>Output: </strong>
+        <span id="example-output-3">3</span> <br />
+        <strong>Explanation: </strong> <br />
+        One possible solution that uses 3 swaps is [0,0,0,0,0,1,1,1,1,1,1].
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <span>
+          <strong>Note</strong>
+          <strong>:</strong>
+        </span>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= data.length &lt;= 10^5</code>
+        </li>
+        <li>
+          <code>0 &lt;= data[i] &lt;= 1</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
