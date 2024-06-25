c01ed566866c11e3921b98ccf08ffd0b0a322516
saiashish9 <saiashish3760@gmail.com>
2024-06-25 21:37:11 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -14675,6 +14675,74 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1297: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string <code>s</code>, return the maximum number of occurrences
+        of <strong>any</strong> substring under the following rules:
+      </p>
+
+      <ul>
+        <li>
+          The number of unique characters in the substring must be less than or
+          equal to <code>maxLetters</code>.
+        </li>
+        <li>
+          The substring size must be between <code>minSize</code> and{" "}
+          <code>maxSize</code> inclusive.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "aababcaab", maxLetters = 2, minSize = 3,
+        maxSize = 4 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> Substring "aab" has 2 occurrences in the
+        original string. <br />
+        It satisfies the conditions, 2 unique letters and size 3 (between
+        minSize and maxSize).
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "aaaa", maxLetters = 1, minSize = 3, maxSize
+        = 3 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> Substring "aaa" occur 2 times in the
+        string. It can overlap.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>1 &lt;= maxLetters &lt;= 26</code>
+        </li>
+        <li>
+          <code>1 &lt;= minSize &lt;= maxSize &lt;= min(26, s.length)</code>
+        </li>
+        <li>
+          <code>s</code> consists of only lowercase English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
