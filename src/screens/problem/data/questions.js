ee47866a7e00e8658c147d229991b7b85a6dbb61
saiashish9 <saiashish3760@gmail.com>
2024-06-25 23:28:26 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -15422,6 +15422,88 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1658: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>nums</code> and an integer{" "}
+        <code>x</code>. In one operation, you can either remove the leftmost or
+        the rightmost element from the array <code>nums</code> and subtract its
+        value from <code>x</code>. Note that this <strong>modifies</strong> the
+        array for future operations.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>minimum number</strong> of operations to reduce{" "}
+        </em>
+        <code>x</code>{" "}
+        <em>
+          to <strong>exactly</strong>
+        </em>{" "}
+        <code>0</code> <em>if it is possible</em>
+        <em>, otherwise, return </em>
+        <code>-1</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,4,2,3], x = 5 <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong> The optimal solution is to remove the last
+        two elements to reduce x to zero.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [5,6,7,8,9], x = 4<br />
+        <strong>Output:</strong> -1
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [3,2,20,1,1,3], x = 10
+        <br />
+        <strong>Output:</strong> 5<br />
+        <strong>Explanation:</strong> The optimal solution is to remove the last
+        three elements and the first two elements (5 operations in total) to
+        reduce x to zero.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= nums[i] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= x &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
