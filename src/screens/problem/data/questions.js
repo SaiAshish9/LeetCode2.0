ef2e86c590a769c178ff509b9ea0b3a67902819d
saiashish9 <saiashish3760@gmail.com>
2024-06-25 14:34:10 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -13572,6 +13572,82 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  930: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a binary array <code>nums</code> and an integer <code>goal</code>,
+        return{" "}
+        <em>
+          the number of non-empty <strong>subarrays</strong> with a sum
+        </em>{" "}
+        <code>goal</code>.
+      </p>
+
+      <p>
+        A <strong>subarray</strong> is a contiguous part of the array.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,0,1,0,1], goal = 2
+        <strong>Output:</strong> 4<br />
+        <strong>Explanation:</strong> The 4 subarrays are bolded and underlined
+        below:
+        <br />[
+        <u>
+          <strong>1,0,1</strong>
+        </u>
+        ,0,1] <br />[
+        <u>
+          <strong>1,0,1,0</strong>
+        </u>
+        ,1]
+        <br />
+        [1,
+        <u>
+          <strong>0,1,0,1</strong>
+        </u>
+        ]<br />
+        [1,0,
+        <u>
+          <strong>1,0,1</strong>
+        </u>
+        ]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [0,0,0,0,0], goal = 0<br />
+        <strong>Output:</strong> 15
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 3 * 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>nums[i]</code> is either <code>0</code> or <code>1</code>.
+        </li>
+        <li>
+          <code>0 &lt;= goal &lt;= nums.length</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
