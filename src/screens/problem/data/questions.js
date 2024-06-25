5e22b628c1fa66661dc317af1683fb3c10dc5276
saiashish9 <saiashish3760@gmail.com>
2024-06-25 14:18:15 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -13369,6 +13369,57 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  718: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given two integer arrays <code>nums1</code> and <code>nums2</code>,
+        return{" "}
+        <em>
+          the maximum length of a subarray that appears in <strong>both</strong>{" "}
+          arrays
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums1 = [1,2,3,2,1], nums2 = [3,2,1,4,7] <br />
+        <strong>Output:</strong> 3<br />
+        <strong>Explanation:</strong> The repeated subarray with maximum length
+        is [3,2,1].
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums1 = [0,0,0,0,0], nums2 = [0,0,0,0,0]
+        <br />
+        <strong>Output:</strong> 5<br />
+        <strong>Explanation:</strong> The repeated subarray with maximum length
+        is [0,0,0,0,0].
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums1.length, nums2.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>0 &lt;= nums1[i], nums2[i] &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
