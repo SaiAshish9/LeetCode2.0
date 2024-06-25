30d926bb988f4f8fbe810a8938457ec9ff0ad914
saiashish9 <saiashish3760@gmail.com>
2024-06-26 00:14:06 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -15904,6 +15904,79 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1852: (
+    <div class="markdown-body div-width">
+      <div class="content__u3I1 question-content__JfgR">
+        <div>
+          <p>
+            Given an integer array <code>nums</code> and an integer{" "}
+            <code>k</code>, you are asked to construct the array{" "}
+            <code>ans</code> of size <code>n-k+1</code> where{" "}
+            <code>ans[i]</code> is the number of <strong>distinct</strong>{" "}
+            numbers in the subarray{" "}
+            <code>nums[i:i+k-1] = [nums[i], nums[i+1], ..., nums[i+k-1]]</code>.
+          </p>
+
+          <p>
+            Return <em>the array </em>
+            <code>ans</code>.
+          </p>
+
+          <p>&nbsp;</p>
+          <p>
+            <strong>Example 1:</strong>
+          </p>
+
+          <pre>
+            <strong>Input:</strong> nums = [1,2,3,2,2,1,3], k = 3<br />
+            <strong>Output:</strong> [3,2,2,2,3]
+            <br />
+            <strong>Explanation: </strong>The number of distinct elements in
+            each subarray goes as follows: - nums[0:2] = [1,2,3] so ans[0] = 3{" "}
+            <br />
+            - nums[1:3] = [2,3,2] so ans[1] = 2<br />
+            - nums[2:4] = [3,2,2] so ans[2] = 2<br />
+            - nums[3:5] = [2,2,1] so ans[3] = 2<br />- nums[4:6] = [2,1,3] so
+            ans[4] = 3
+          </pre>
+
+          <p>
+            <strong>Example 2:</strong>
+          </p>
+
+          <pre>
+            <strong>Input:</strong> nums = [1,1,1,1,2,3,4], k = 4<br />
+            <strong>Output:</strong> [1,2,3,4]
+            <br />
+            <strong>Explanation: </strong>The number of distinct elements in
+            each subarray goes as follows:
+            <br />
+            - nums[0:3] = [1,1,1,1] so ans[0] = 1<br />
+            - nums[1:4] = [1,1,1,2] so ans[1] = 2<br />
+            - nums[2:5] = [1,1,2,3] so ans[2] = 3<br />- nums[3:6] = [1,2,3,4]
+            so ans[3] = 4
+          </pre>
+
+          <p>
+            <strong>Constraints:</strong>
+          </p>
+
+          <ul>
+            <li>
+              <code>
+                1 &lt;= k &lt;= nums.length &lt;= 10<sup>5</sup>
+              </code>
+            </li>
+            <li>
+              <code>
+                1 &lt;= nums[i] &lt;= 10<sup>5</sup>
+              </code>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
