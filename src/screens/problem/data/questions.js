0ba71518794fea2b3f38a2f2ef4367aece7f111b
saiashish9 <saiashish3760@gmail.com>
2024-06-26 16:41:52 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -17657,6 +17657,85 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2401: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an array <code>nums</code> consisting of{" "}
+        <strong>positive</strong> integers.
+      </p>
+
+      <p>
+        We call a subarray of <code>nums</code> <strong>nice</strong> if the
+        bitwise <strong>AND</strong> of every pair of elements that are in{" "}
+        <strong>different</strong> positions in the subarray is equal to{" "}
+        <code>0</code>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the length of the <strong>longest</strong> nice subarray
+        </em>
+        .
+      </p>
+
+      <p>
+        A <strong>subarray</strong> is a <strong>contiguous</strong> part of an
+        array.
+      </p>
+
+      <p>
+        <strong>Note</strong> that subarrays of length <code>1</code> are always
+        considered nice.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,3,8,48,10] <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> The longest nice subarray is [3,8,48].
+        This subarray satisfies the conditions: <br />
+        - 3 AND 8 = 0. <br />
+        - 3 AND 48 = 0. <br />
+        - 8 AND 48 = 0. <br />
+        It can be proven that no longer nice subarray can be obtained, so we
+        return 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [3,1,5,11,13] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> The length of the longest nice subarray is
+        1. Any subarray of length 1 can be chosen.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= nums[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
