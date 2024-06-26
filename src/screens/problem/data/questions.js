4aa0e437313f8e8dd2d95fe3027459ea9a58e80f
saiashish9 <saiashish3760@gmail.com>
2024-06-26 15:45:22 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -17267,6 +17267,115 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2269: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        The <strong>k-beauty</strong> of an integer <code>num</code> is defined
+        as the number of <strong>substrings</strong> of <code>num</code> when it
+        is read as a string that meet the following conditions:
+      </p>
+
+      <ul>
+        <li>
+          It has a length of <code>k</code>.
+        </li>
+        <li>
+          It is a divisor of <code>num</code>.
+        </li>
+      </ul>
+
+      <p>
+        Given integers <code>num</code> and <code>k</code>, return{" "}
+        <em>the k-beauty of </em>
+        <code>num</code>.
+      </p>
+
+      <p>Note:</p>
+
+      <ul>
+        <li>
+          <strong>Leading zeros</strong> are allowed.
+        </li>
+        <li>
+          <code>0</code> is not a divisor of any value.
+        </li>
+      </ul>
+
+      <p>
+        A <strong>substring</strong> is a contiguous sequence of characters in a
+        string.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> num = 240, k = 2 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> The following are the substrings of num of
+        length k: <br />- "24" from "
+        <strong>
+          <u>24</u>
+        </strong>
+        0": 24 is a divisor of 240. <br />- "40" from "2
+        <u>
+          <strong>40</strong>
+        </u>
+        ": 40 is a divisor of 240. <br />
+        Therefore, the k-beauty is 2.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> num = 430043, k = 2 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> The following are the substrings of num of
+        length k: <br />- "43" from "
+        <u>
+          <strong>43</strong>
+        </u>
+        0043": 43 is a divisor of 430043. <br />- "30" from "4
+        <u>
+          <strong>30</strong>
+        </u>
+        043": 30 is not a divisor of 430043. <br />- "00" from "43
+        <u>
+          <strong>00</strong>
+        </u>
+        43": 0 is not a divisor of 430043. <br />- "04" from "430
+        <u>
+          <strong>04</strong>
+        </u>
+        3": 4 is not a divisor of 430043. <br />- "43" from "4300
+        <u>
+          <strong>43</strong>
+        </u>
+        ": 43 is a divisor of 430043. Therefore, the k-beauty is 2.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= num &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+        <li>
+          <code>1 &lt;= k &lt;= num.length</code> (taking <code>num</code> as a
+          string)
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
