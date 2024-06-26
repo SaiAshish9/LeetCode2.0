3f8f6ff0bed18ad983632998583c67c2980c287b
saiashish9 <saiashish3760@gmail.com>
2024-06-27 00:11:07 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -18534,6 +18534,131 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2730: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a digit string <code>s</code> that consists of digits from
+        0 to 9.
+      </p>
+
+      <p>
+        A string is called <strong>semi-repetitive</strong> if there is{" "}
+        <strong>at most</strong> one adjacent pair of the same digit. For
+        example, <code>"0010"</code>, <code>"002020"</code>, <code>"0123"</code>
+        , <code>"2002"</code>, and <code>"54944"</code> are semi-repetitive
+        while the following are not: <code>"00101022"</code> (adjacent same
+        digit pairs are 00 and 22), and <code>"1101234883"</code> (adjacent same
+        digit pairs are 11 and 88).
+      </p>
+
+      <p>
+        Return the length of the{" "}
+        <strong>
+          longest semi-repetitive{" "}
+          <span
+            data-keyword="substring-nonempty"
+            class=" cursor-pointer relative text-dark-blue-s text-sm"
+          >
+            <div class="popover-wrapper inline-block" data-headlessui-state="">
+              <div>
+                <div
+                  aria-expanded="false"
+                  data-headlessui-state=""
+                  id="headlessui-popover-button-:rt:"
+                >
+                  <div>substring</div>
+                </div>
+              </div>
+            </div>
+          </span>
+        </strong>{" "}
+        of <code>s</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">s = "52233"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">4</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          The longest semi-repetitive substring is "5223". Picking the whole
+          string "52233" has two adjacent same digit pairs 22 and 33, but at
+          most one is allowed.
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">s = "5494"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">4</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          <code>s</code> is a semi-repetitive string.
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">s = "1111111"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">2</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>
+        <p>
+          The longest semi-repetitive substring is "11". Picking the substring
+          "111" has two adjacent same digit pairs, but at most one is allowed.
+        </p>
+      </div>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= s.length &lt;= 50</code>
+        </li>
+        <li>
+          <code>'0' &lt;= s[i] &lt;= '9'</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
