c1d3d4635c8ea694b0d3ed2df94046817ed35fd9
saiashish9 <saiashish3760@gmail.com>
2024-06-28 13:00:55 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -19219,6 +19219,91 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2831: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <strong>0-indexed</strong> integer array{" "}
+        <code>nums</code> and an integer <code>k</code>.
+      </p>
+
+      <p>
+        A subarray is called <strong>equal</strong> if all of its elements are
+        equal. Note that the empty subarray is an <strong>equal</strong>{" "}
+        subarray.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the length of the <strong>longest</strong> possible equal subarray
+          after deleting <strong>at most</strong>{" "}
+        </em>
+        <code>k</code>
+        <em> elements from </em>
+        <code>nums</code>.
+      </p>
+
+      <p>
+        A <b>subarray</b> is a contiguous, possibly empty sequence of elements
+        within an array.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,3,2,3,1,3], k = 3<br />
+        <strong>Output:</strong> 3<br />
+        <strong>Explanation:</strong> It's optimal to delete the elements at
+        index 2 and index 4.
+        <br />
+        After deleting them, nums becomes equal to [1, 3, 3, 3].
+        <br />
+        The longest equal subarray starts at i = 1 and ends at j = 3 with length
+        equal to 3.
+        <br />
+        It can be proven that no longer equal subarrays can be created.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,2,2,1,1], k = 2<br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> It's optimal to delete the elements at
+        index 2 and index 3.
+        <br />
+        After deleting them, nums becomes equal to [1, 1, 1, 1].
+        <br />
+        The array itself is an equal subarray, so the answer is 4.
+        <br />
+        It can be proven that no longer equal subarrays can be created.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>1 &lt;= nums[i] &lt;= nums.length</code>
+        </li>
+        <li>
+          <code>0 &lt;= k &lt;= nums.length</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
