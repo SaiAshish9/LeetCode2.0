f39e0d659a78c8f25e6d59ff83d19f379142dcac
saiashish9 <saiashish3760@gmail.com>
2024-06-28 15:00:15 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -21096,6 +21096,260 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  3086: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a binary array <code>nums</code> of length <code>n</code>,
+        a <strong>positive</strong> integer <code>k</code> and a{" "}
+        <strong>non-negative</strong> integer <code>maxChanges</code>.
+      </p>
+
+      <p>
+        Alice plays a game, where the goal is for Alice to pick up{" "}
+        <code>k</code> ones from <code>nums</code> using the{" "}
+        <strong>minimum</strong> number of <strong>moves</strong>. When the game
+        starts, Alice picks up any index <code>aliceIndex</code> in the range{" "}
+        <code>[0, n - 1]</code> and stands there. If{" "}
+        <code>nums[aliceIndex] == 1</code> , Alice picks up the one and{" "}
+        <code>nums[aliceIndex]</code> becomes <code>0</code>(this{" "}
+        <strong>does not</strong> count as a move). After this, Alice can make{" "}
+        <strong>any</strong> number of <strong>moves</strong> (
+        <strong>including</strong> <strong>zero</strong>) where in each move
+        Alice must perform <strong>exactly</strong> one of the following
+        actions:
+      </p>
+
+      <ul>
+        <li>
+          Select any index <code>j != aliceIndex</code> such that{" "}
+          <code>nums[j] == 0</code> and set <code>nums[j] = 1</code>. This
+          action can be performed <strong>at</strong> <strong>most</strong>{" "}
+          <code>maxChanges</code> times.
+        </li>
+        <li>
+          Select any two adjacent indices <code>x</code> and <code>y</code> (
+          <code>|x - y| == 1</code>) such that <code>nums[x] == 1</code>,{" "}
+          <code>nums[y] == 0</code>, then swap their values (set{" "}
+          <code>nums[y] = 1</code> and <code>nums[x] = 0</code>). If{" "}
+          <code>y == aliceIndex</code>, Alice picks up the one after this move
+          and <code>nums[y]</code> becomes <code>0</code>.
+        </li>
+      </ul>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>minimum</strong> number of moves required by Alice to pick{" "}
+          <strong>exactly </strong>
+        </em>
+        <code>k</code> <em>ones</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre class="example-block">
+        <strong>Input: </strong>
+        <span class="example-io">
+          nums = [1,1,0,0,0,1,1,0,0,1], k = 3, maxChanges = 1
+        </span>
+        <br />
+        <strong>Output: </strong>
+        <span class="example-io">3</span>
+        <br />
+        <strong>Explanation:</strong> Alice can pick up <code>3</code> ones in{" "}
+        <code>3</code> moves, if Alice performs the following actions in each
+        move when standing at <code>aliceIndex == 1</code>:
+        <ul>
+          <li>
+            &nbsp;At the start of the game Alice picks up the one and{" "}
+            <code>nums[1]</code> becomes <code>0</code>. <code>nums</code>{" "}
+            becomes{" "}
+            <code>
+              [1,
+              <strong>
+                <u>1</u>
+              </strong>
+              ,1,0,0,1,1,0,0,1]
+            </code>
+            .
+          </li>
+          <li>
+            Select <code>j == 2</code> and perform an action of the first type.{" "}
+            <code>nums</code> becomes{" "}
+            <code>
+              [1,
+              <strong>
+                <u>0</u>
+              </strong>
+              ,1,0,0,1,1,0,0,1]
+            </code>
+          </li>
+          <li>
+            Select <code>x == 2</code> and <code>y == 1</code>, and perform an
+            action of the second type. <code>nums</code> becomes{" "}
+            <code>
+              [1,
+              <strong>
+                <u>1</u>
+              </strong>
+              ,0,0,0,1,1,0,0,1]
+            </code>
+            . As <code>y == aliceIndex</code>, Alice picks up the one and{" "}
+            <code>nums</code> becomes&nbsp;
+            <code>
+              [1,
+              <strong>
+                <u>0</u>
+              </strong>
+              ,0,0,0,1,1,0,0,1]
+            </code>
+            .
+          </li>
+          <li>
+            Select <code>x == 0</code> and <code>y == 1</code>, and perform an
+            action of the second type. <code>nums</code> becomes{" "}
+            <code>
+              [0,
+              <strong>
+                <u>1</u>
+              </strong>
+              ,0,0,0,1,1,0,0,1]
+            </code>
+            . As <code>y == aliceIndex</code>, Alice picks up the one and{" "}
+            <code>nums</code> becomes&nbsp;
+            <code>
+              [0,
+              <strong>
+                <u>0</u>
+              </strong>
+              ,0,0,0,1,1,0,0,1]
+            </code>
+            .
+          </li>
+        </ul>
+        <p>
+          Note that it may be possible for Alice to pick up <code>3</code> ones
+          using some other sequence of <code>3</code> moves.
+        </p>
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre class="example-block">
+        <strong>Input: </strong>
+        <span class="example-io">nums = [0,0,0,0], k = 2, maxChanges = 3</span>
+        <br />
+        <strong>Output: </strong>
+        <span class="example-io">4</span>
+        <br />
+        <strong>Explanation:</strong> Alice can pick up <code>2</code> ones in{" "}
+        <code>4</code> moves, if Alice performs the following actions in each
+        move when standing at <code>aliceIndex == 0</code>:
+        <ul>
+          <li>
+            Select <code>j == 1</code> and perform an action of the first type.{" "}
+            <code>nums</code> becomes{" "}
+            <code>
+              [
+              <strong>
+                <u>0</u>
+              </strong>
+              ,1,0,0]
+            </code>
+            .
+          </li>
+          <li>
+            Select <code>x == 1</code> and <code>y == 0</code>, and perform an
+            action of the second type. <code>nums</code> becomes{" "}
+            <code>
+              [
+              <strong>
+                <u>1</u>
+              </strong>
+              ,0,0,0]
+            </code>
+            . As <code>y == aliceIndex</code>, Alice picks up the one and{" "}
+            <code>nums</code> becomes&nbsp;
+            <code>
+              [
+              <strong>
+                <u>0</u>
+              </strong>
+              ,0,0,0]
+            </code>
+            .
+          </li>
+          <li>
+            Select <code>j == 1</code> again and perform an action of the first
+            type. <code>nums</code> becomes{" "}
+            <code>
+              [
+              <strong>
+                <u>0</u>
+              </strong>
+              ,1,0,0]
+            </code>
+            .
+          </li>
+          <li>
+            Select <code>x == 1</code> and <code>y == 0</code> again, and
+            perform an action of the second type. <code>nums</code> becomes{" "}
+            <code>
+              [
+              <strong>
+                <u>1</u>
+              </strong>
+              ,0,0,0]
+            </code>
+            . As <code>y == aliceIndex</code>, Alice picks up the one and{" "}
+            <code>nums</code> becomes&nbsp;
+            <code>
+              [
+              <strong>
+                <u>0</u>
+              </strong>
+              ,0,0,0]
+            </code>
+            .
+          </li>
+        </ul>
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            2 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>0 &lt;= nums[i] &lt;= 1</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= k &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= maxChanges &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>maxChanges + sum(nums) &gt;= k</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
