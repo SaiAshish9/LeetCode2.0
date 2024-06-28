65a42762d2c3dc064f72aa6ecc77348c07be0a9c
saiashish9 <saiashish3760@gmail.com>
2024-06-28 14:46:11 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -20744,6 +20744,112 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2981: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a string <code>s</code> that consists of lowercase English
+        letters.
+      </p>
+
+      <p>
+        A string is called <strong>special</strong> if it is made up of only a
+        single character. For example, the string <code>"abc"</code> is not
+        special, whereas the strings <code>"ddd"</code>, <code>"zz"</code>, and{" "}
+        <code>"f"</code> are special.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the length of the <strong>longest special substring</strong> of{" "}
+        </em>
+        <code>s</code>{" "}
+        <em>
+          which occurs <strong>at least thrice</strong>
+        </em>
+        , <em>or </em>
+        <code>-1</code>
+        <em> if no special substring occurs at least thrice</em>.
+      </p>
+
+      <p>
+        A <strong>substring</strong> is a contiguous <strong>non-empty</strong>{" "}
+        sequence of characters within a string.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "aaaa" <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> The longest special substring which occurs
+        thrice is "aa": substrings "
+        <u>
+          <strong>aa</strong>
+        </u>
+        aa", "a
+        <u>
+          <strong>aa</strong>
+        </u>
+        a", and "aa
+        <u>
+          <strong>aa</strong>
+        </u>
+        ". It can be shown that the maximum length achievable is 2.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "abcdef" <br />
+        <strong>Output:</strong> -1 <br />
+        <strong>Explanation:</strong> There exists no special substring which
+        occurs at least thrice. Hence return -1.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "abcaba" <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> The longest special substring which occurs
+        thrice is "a": substrings "
+        <u>
+          <strong>a</strong>
+        </u>
+        bcaba", "abc
+        <u>
+          <strong>a</strong>
+        </u>
+        ba", and "abcab
+        <u>
+          <strong>a</strong>
+        </u>
+        ". It can be shown that the maximum length achievable is 1.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>3 &lt;= s.length &lt;= 50</code>
+        </li>
+        <li>
+          <code>s</code> consists of only lowercase English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
