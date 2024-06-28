00228fb1d383f5e88b510fa8b4d79f5623118a8b
saiashish9 <saiashish3760@gmail.com>
2024-06-28 13:45:23 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -19719,6 +19719,124 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </article>
   ),
+  3037: (
+    <article role="main" class="blog-post">
+      <p>
+        You are given a binary array <code>pattern</code> and an object{" "}
+        <code>stream</code> of class <code>InfiniteStream</code> representing a{" "}
+        <strong>0-indexed</strong> infinite stream of bits.
+      </p>
+
+      <p>
+        The class <code>InfiniteStream</code> contains the following function:
+      </p>
+
+      <ul>
+        <li>
+          <code>int next()</code>: Reads a <strong>single</strong> bit (which is
+          either <code>0</code> or <code>1</code>) from the stream and returns
+          it.
+        </li>
+      </ul>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>first starting</strong> index where the pattern matches
+          the bits read from the stream
+        </em>
+        . For example, if the pattern is <code>[1, 0]</code>, the first match is
+        the highlighted part in the stream{" "}
+        <code>
+          [0,{" "}
+          <strong>
+            <u>1, 0</u>
+          </strong>
+          , 1, ...]
+        </code>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> stream = [1,1,1,0,1,1,1,...], pattern = [0,1]{" "}
+        <br />
+        <strong>Output:</strong> 3<br />
+        <strong>Explanation:</strong> The first occurrence of the pattern [0,1]
+        is highlighted in the stream [1,1,1,
+        <strong>
+          <u>0,1</u>
+        </strong>
+        ,...], which starts at index 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> stream = [0,0,0,0,...], pattern = [0]
+        <br />
+        <strong>Output:</strong> 0<br />
+        <strong>Explanation:</strong> The first occurrence of the pattern [0] is
+        highlighted in the stream [
+        <strong>
+          <u>0</u>
+        </strong>
+        ,...], which starts at index 0.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> stream = [1,0,1,1,0,1,1,0,1,...], pattern =
+        [1,1,0,1]
+        <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong> The first occurrence of the pattern
+        [1,1,0,1] is highlighted in the stream [1,0,
+        <strong>
+          <u>1,1,0,1</u>
+        </strong>
+        ,...], which starts at index 2.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= pattern.length &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>pattern</code> consists only of <code>0</code> and{" "}
+          <code>1</code>.
+        </li>
+        <li>
+          <code>stream</code> consists only of <code>0</code> and <code>1</code>
+          .
+        </li>
+        <li>
+          The input is generated such that the pattern's start index exists in
+          the first{" "}
+          <code>
+            10<sup>5</sup>
+          </code>{" "}
+          bits of the stream.
+        </li>
+      </ul>
+    </article>
+  ),
 };
 
 export default QUESTIONS;
