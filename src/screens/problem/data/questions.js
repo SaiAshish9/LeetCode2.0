27b838b4960c5de1b023c163a3167ca48825ab6c
saiashish9 <saiashish3760@gmail.com>
2024-06-28 15:06:52 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -21350,6 +21350,129 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  3134: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>nums</code>. The{" "}
+        <strong>uniqueness array</strong> of <code>nums</code> is the sorted
+        array that contains the number of distinct elements of all the{" "}
+        <span
+          data-keyword="subarray-nonempty"
+          class=" cursor-pointer relative text-dark-blue-s text-sm"
+        >
+          <div class="popover-wrapper inline-block" data-headlessui-state="">
+            <div>
+              <div
+                aria-expanded="false"
+                data-headlessui-state=""
+                id="headlessui-popover-button-:rt:"
+              >
+                <div>subarrays</div>
+              </div>
+            </div>
+          </div>
+        </span>{" "}
+        of <code>nums</code>. In other words, it is a sorted array consisting of{" "}
+        <code>distinct(nums[i..j])</code>, for all{" "}
+        <code>0 &lt;= i &lt;= j &lt; nums.length</code>.
+      </p>
+
+      <p>
+        Here, <code>distinct(nums[i..j])</code> denotes the number of distinct
+        elements in the subarray that starts at index <code>i</code> and ends at
+        index <code>j</code>.
+      </p>
+
+      <p>
+        Return the <strong>median</strong> of the{" "}
+        <strong>uniqueness array</strong> of <code>nums</code>.
+      </p>
+
+      <p>
+        <strong>Note</strong> that the <strong>median</strong> of an array is
+        defined as the middle element of the array when it is sorted in
+        non-decreasing order. If there are two choices for a median, the{" "}
+        <strong>smaller</strong> of the two values is taken.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">nums = [1,2,3]</span>
+        </p>
+
+        <p>
+          <strong>Output:</strong> <span class="example-io">1</span>
+        </p>
+
+        <p>
+          <strong>Explanation:</strong>
+        </p>
+
+        <p>
+          The uniqueness array of <code>nums</code> is{" "}
+          <code>
+            [distinct(nums[0..0]), distinct(nums[1..1]), distinct(nums[2..2]),
+            distinct(nums[0..1]), distinct(nums[1..2]), distinct(nums[0..2])]
+          </code>{" "}
+          which is equal to <code>[1, 1, 1, 2, 2, 3]</code>. The uniqueness
+          array has a median of 1. Therefore, the answer is 1.
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre class="example-block">
+        <strong>Input:</strong>{" "}
+        <span class="example-io">nums = [3,4,3,4,5]</span>
+        <br />
+        <strong>Output:</strong> <span class="example-io">2</span>
+        <br />
+        <strong>Explanation:</strong>
+        The uniqueness array of <code>nums</code> is{" "}
+        <code>[1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2, 3, 3, 3]</code>. The
+        uniqueness array has a median of 2. Therefore, the answer is 2.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre class="example-block">
+        <strong>Input:</strong> <span class="example-io">nums = [4,3,5,4]</span>
+        <br />
+        <strong>Output:</strong> <span class="example-io">2</span>
+        <br />
+        <strong>Explanation:</strong>
+        The uniqueness array of <code>nums</code> is{" "}
+        <code>[1, 1, 1, 1, 2, 2, 2, 3, 3, 3]</code>. The uniqueness array has a
+        median of 2. Therefore, the answer is 2.
+      </pre>
+
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= nums[i] &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
