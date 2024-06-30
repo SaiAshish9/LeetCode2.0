328347c4422a7941e020d94ff7aac7a498beee2a
saiashish9 <saiashish3760@gmail.com>
2024-07-01 00:00:26 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -21798,6 +21798,110 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  36: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Determine if a&nbsp;<code>9 x 9</code> Sudoku board&nbsp;is
+        valid.&nbsp;Only the filled cells need to be validated&nbsp;
+        <strong>according to the following rules</strong>:
+      </p>
+
+      <ol>
+        <li>
+          Each row&nbsp;must contain the&nbsp;digits&nbsp;<code>1-9</code>{" "}
+          without repetition.
+        </li>
+        <li>
+          Each column must contain the digits&nbsp;<code>1-9</code>&nbsp;without
+          repetition.
+        </li>
+        <li>
+          Each of the nine&nbsp;<code>3 x 3</code> sub-boxes of the grid must
+          contain the digits&nbsp;<code>1-9</code>&nbsp;without repetition.
+        </li>
+      </ol>
+
+      <p>
+        <strong>Note:</strong>
+      </p>
+
+      <ul>
+        <li>
+          A Sudoku board (partially filled) could be valid but is not
+          necessarily solvable.
+        </li>
+        <li>
+          Only the filled cells need to be validated according to the
+          mentioned&nbsp;rules.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Sudoku-by-L2G-20050714.svg/250px-Sudoku-by-L2G-20050714.svg.png"
+        style={{ height: 250, width: 250 }}
+      />
+      <pre>
+        <strong>Input:</strong> board = <br />
+   {`[["5","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]`}{" "}
+        <br />
+        <strong>Output:</strong> true
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> board = <br />
+{`[["8","3",".",".","7",".",".",".","."]
+,["6",".",".","1","9","5",".",".","."]
+,[".","9","8",".",".",".",".","6","."]
+,["8",".",".",".","6",".",".",".","3"]
+,["4",".",".","8",".","3",".",".","1"]
+,["7",".",".",".","2",".",".",".","6"]
+,[".","6",".",".",".",".","2","8","."]
+,[".",".",".","4","1","9",".",".","5"]
+,[".",".",".",".","8",".",".","7","9"]]`}{" "}
+        <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> Same as Example 1, except with the{" "}
+        <strong>5</strong> in the top left corner being modified to{" "}
+        <strong>8</strong>. Since there are two 8's in the top left 3x3 sub-box,
+        it is invalid.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>board.length == 9</code>
+        </li>
+        <li>
+          <code>board[i].length == 9</code>
+        </li>
+        <li>
+          <code>board[i][j]</code> is a digit <code>1-9</code> or{" "}
+          <code>'.'</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
