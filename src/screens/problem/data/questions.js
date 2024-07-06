8a477734af177cbab96bee96818b1e48ff369e80
saiashish9 <saiashish3760@gmail.com>
2024-07-07 00:10:24 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -23888,6 +23888,95 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ol>
     </div>
   ),
+  1712: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        A split of an integer array is <strong>good</strong> if:
+      </p>
+
+      <ul>
+        <li>
+          The array is split into three <strong>non-empty</strong> contiguous
+          subarrays - named <code>left</code>, <code>mid</code>,{" "}
+          <code>right</code> respectively from left to right.
+        </li>
+        <li>
+          The sum of the elements in <code>left</code> is less than or equal to
+          the sum of the elements in <code>mid</code>, and the sum of the
+          elements in <code>mid</code> is less than or equal to the sum of the
+          elements in <code>right</code>.
+        </li>
+      </ul>
+
+      <p>
+        Given <code>nums</code>, an array of <strong>non-negative</strong>{" "}
+        integers, return{" "}
+        <em>
+          the number of <strong>good</strong> ways to split
+        </em>{" "}
+        <code>nums</code>. As the number may be too large, return it{" "}
+        <strong>modulo</strong>{" "}
+        <code>
+          10<sup>9 </sup>+ 7
+        </code>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,1] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> The only good way to split nums is [1] [1]
+        [1].
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,2,2,5,0] <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> There are three good ways of splitting
+        nums: <br />
+        [1] [2] [2,2,5,0] <br />
+        [1] [2,2] [2,5,0] <br />
+        [1,2] [2,2] [5,0]
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [3,2,1] <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> There is no good way to split nums.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            3 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= nums[i] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
