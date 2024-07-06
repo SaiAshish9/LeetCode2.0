8b287dd399cd92fa60f236dd8632e724787e5a06
saiashish9 <saiashish3760@gmail.com>
2024-07-06 17:41:04 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -22491,6 +22491,75 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  202: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Write an algorithm to determine if a number <code>n</code> is happy.
+      </p>
+
+      <p>
+        A <strong>happy number</strong> is a number defined by the following
+        process:
+      </p>
+
+      <ul>
+        <li>
+          Starting with any positive integer, replace the number by the sum of
+          the squares of its digits.
+        </li>
+        <li>
+          Repeat the process until the number equals 1 (where it will stay), or
+          it <strong>loops endlessly in a cycle</strong> which does not include
+          1.
+        </li>
+        <li>
+          Those numbers for which this process <strong>ends in 1</strong> are
+          happy.
+        </li>
+      </ul>
+
+      <p>
+        Return <code>true</code> <em>if</em> <code>n</code>{" "}
+        <em>is a happy number, and</em> <code>false</code> <em>if not</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 19 <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> <br />1<sup>2</sup> + 9<sup>2</sup> = 82{" "}
+        <br />8<sup>2</sup> + 2<sup>2</sup> = 68 <br />6<sup>2</sup> + 8
+        <sup>2</sup> = 100 <br />1<sup>2</sup> + 0<sup>2</sup> + 0<sup>2</sup> =
+        1
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2 <br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 2<sup>31</sup> - 1
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
