b56416309d4d0c7ef7021bc91923bfdeea9b3ecb
saiashish9 <saiashish3760@gmail.com>
2024-07-07 00:03:37 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -23772,6 +23772,62 @@ for (int i = 0; i &lt; actualLength; i++) {
       a different approach?
     </div>
   ),
+  1089: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a fixed-length integer array <code>arr</code>, duplicate each
+        occurrence of zero, shifting the remaining elements to the right.
+      </p>
+
+      <p>
+        <strong>Note</strong> that elements beyond the length of the original
+        array are not written. Do the above modifications to the input array in
+        place and do not return anything.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [1,0,2,3,0,4,5,0] <br />
+        <strong>Output:</strong> [1,0,0,2,3,0,0,4]
+        <br />
+        <strong>Explanation:</strong> After calling your function, the input
+        array is modified to: [1,0,0,2,3,0,0,4]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [1,2,3]
+        <br />
+        <strong>Output:</strong> [1,2,3]
+        <br />
+        <strong>Explanation:</strong> After calling your function, the input
+        array is modified to: [1,2,3]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= arr.length &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>0 &lt;= arr[i] &lt;= 9</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
