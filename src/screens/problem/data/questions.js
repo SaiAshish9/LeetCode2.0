99ba55147edf943272929f21f8ee9a0fab50bcb3
saiashish9 <saiashish3760@gmail.com>
2024-07-06 23:12:08 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -23475,6 +23475,87 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  719: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        The <strong>distance of a pair</strong> of integers <code>a</code> and{" "}
+        <code>b</code> is defined as the absolute difference between{" "}
+        <code>a</code> and <code>b</code>.
+      </p>
+
+      <p>
+        Given an integer array <code>nums</code> and an integer <code>k</code>,
+        return <em>the</em>{" "}
+        <code>
+          k<sup>th</sup>
+        </code>{" "}
+        <em>
+          smallest <strong>distance among all the pairs</strong>
+        </em>{" "}
+        <code>nums[i]</code> <em>and</em> <code>nums[j]</code> <em>where</em>{" "}
+        <code>0 &lt;= i &lt; j &lt; nums.length</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,3,1], k = 1 <br />
+        <strong>Output:</strong> 0<br />
+        <strong>Explanation:</strong> Here are all the pairs:
+        <br />
+        (1,3) -&gt; 2<br />
+        (1,1) -&gt; 0<br />
+        (3,1) -&gt; 2<br />
+        Then the 1<sup>st</sup> smallest distance pair is (1,1), and its
+        distance is 0.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,1], k = 2<br />
+        <strong>Output:</strong> 0
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,6,1], k = 3<br />
+        <strong>Output:</strong> 5
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == nums.length</code>
+        </li>
+        <li>
+          <code>
+            2 &lt;= n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= nums[i] &lt;= 10<sup>6</sup>
+          </code>
+        </li>
+        <li>
+          <code>1 &lt;= k &lt;= n * (n - 1) / 2</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
