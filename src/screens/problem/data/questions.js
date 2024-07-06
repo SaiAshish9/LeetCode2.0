c673f712c80f7418819939e6aff9e3ffe5e398bf
saiashish9 <saiashish3760@gmail.com>
2024-07-06 22:53:16 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -23323,6 +23323,55 @@ for (int i = 0; i &lt; actualLength; i++) {
       complexity?
     </div>
   ),
+  611: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code>, return{" "}
+        <em>
+          the number of triplets chosen from the array that can make triangles
+          if we take them as side lengths of a triangle
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [2,2,3,4] <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> Valid combinations are: <br />
+        2,3,4 (using the first 2) <br />
+        2,3,4 (using the second 2) <br />
+        2,2,3
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [4,2,3,4] <br />
+        <strong>Output:</strong> 4
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>0 &lt;= nums[i] &lt;= 1000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
