5694db91bda32cec5194fb3918636dd73fe284fc
saiashish9 <saiashish3760@gmail.com>
2024-07-06 22:57:53 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -23372,6 +23372,52 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  633: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a non-negative integer <code>c</code>, decide whether there're two
+        integers <code>a</code> and <code>b</code> such that{" "}
+        <code>
+          a<sup>2</sup> + b<sup>2</sup> = c
+        </code>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> c = 5 <br />
+        <strong>Output:</strong> true
+        <br />
+        <strong>Explanation:</strong> 1 * 1 + 2 * 2 = 5
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> c = 3<br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            0 &lt;= c &lt;= 2<sup>31</sup> - 1
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
