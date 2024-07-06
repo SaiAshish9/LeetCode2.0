7bb37471956134b2c0d90d0fa9c3e77ee06cd089
saiashish9 <saiashish3760@gmail.com>
2024-07-06 22:47:30 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -23265,6 +23265,64 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  581: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code>, you need to find one{" "}
+        <b>continuous subarray</b> such that if you only sort this subarray in
+        non-decreasing order, then the whole array will be sorted in
+        non-decreasing order.
+      </p>
+      <p>
+        Return <em>the shortest such subarray and output its length</em>.
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> nums = [2,6,4,8,10,9,15] <br />
+        <strong>Output:</strong> 5<br />
+        <strong>Explanation:</strong> You need to sort [6, 4, 8, 10, 9] in
+        ascending order to make the whole array sorted in ascending order.
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,4]
+        <br />
+        <strong>Output:</strong> 0
+      </pre>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> nums = [1]
+        <br />
+        <strong>Output:</strong> 0
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>5</sup> &lt;= nums[i] &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <strong>Follow up:</strong> Can you solve it in <code>O(n)</code> time
+      complexity?
+    </div>
+  ),
 };
 
 export default QUESTIONS;
