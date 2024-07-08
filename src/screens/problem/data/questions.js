f9c0b090333fb9215b5dc79a5c8261533416b99a
saiashish9 <saiashish3760@gmail.com>
2024-07-09 00:59:59 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -27127,6 +27127,116 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1823: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        There are <code>n</code> friends that are playing a game. The friends
+        are sitting in a circle and are numbered from <code>1</code> to{" "}
+        <code>n</code> in <strong>clockwise order</strong>. More formally,
+        moving clockwise from the{" "}
+        <code>
+          i<sup>th</sup>
+        </code>{" "}
+        friend brings you to the{" "}
+        <code>
+          (i+1)<sup>th</sup>
+        </code>{" "}
+        friend for <code>1 &lt;= i &lt; n</code>, and moving clockwise from the{" "}
+        <code>
+          n<sup>th</sup>
+        </code>{" "}
+        friend brings you to the{" "}
+        <code>
+          1<sup>st</sup>
+        </code>{" "}
+        friend.
+      </p>
+
+      <p>The rules of the game are as follows:</p>
+
+      <ol>
+        <li>
+          <strong>Start</strong> at the{" "}
+          <code>
+            1<sup>st</sup>
+          </code>{" "}
+          friend.
+        </li>
+        <li>
+          Count the next <code>k</code> friends in the clockwise direction{" "}
+          <strong>including</strong> the friend you started at. The counting
+          wraps around the circle and may count some friends more than once.
+        </li>
+        <li>
+          The last friend you counted leaves the circle and loses the game.
+        </li>
+        <li>
+          If there is still more than one friend in the circle, go back to step{" "}
+          <code>2</code> <strong>starting</strong> from the friend{" "}
+          <strong>immediately clockwise</strong> of the friend who just lost and
+          repeat.
+        </li>
+        <li>Else, the last friend in the circle wins the game.</li>
+      </ol>
+
+      <p>
+        Given the number of friends, <code>n</code>, and an integer{" "}
+        <code>k</code>, return <em>the winner of the game</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/03/25/ic234-q2-ex11.png"
+        style={{ width: 500, height: 345 }}
+      />
+      <pre>
+        <strong>Input:</strong> n = 5, k = 2<strong>Output:</strong> 3
+        <strong>Explanation:</strong> Here are the steps of the game: <br />1
+        {")"} Start at friend 1. <br />2{")"} Count 2 friends clockwise, which
+        are friends 1 and 2. <br />3{")"} Friend 2 leaves the circle. Next start
+        is friend 3. <br />4{")"} Count 2 friends clockwise, which are friends 3
+        and 4. <br />5{")"} Friend 4 leaves the circle. Next start is friend 5.{" "}
+        <br />6{")"} Count 2 friends clockwise, which are friends 5 and 1.{" "}
+        <br />7{")"} Friend 1 leaves the circle. Next start is friend 3. <br />8
+        {")"} Count 2 friends clockwise, which are friends 3 and 5. <br />9{")"}{" "}
+        Friend 5 leaves the circle. Only friend 3 is left, so they are the
+        winner.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 6, k = 5 <br />
+        <strong>Output:</strong> 1<br />
+        <strong>Explanation:</strong> The friends leave in this order: 5, 4, 6,
+        2, 3. The winner is friend 1.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= k &lt;= n &lt;= 500</code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong>
+      </p>
+
+      <p>Could you solve this problem in linear time with constant space?</p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
