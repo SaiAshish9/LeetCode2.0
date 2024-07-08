6452b95ba884ccba888d8d8c35c93612dedf718c
saiashish9 <saiashish3760@gmail.com>
2024-07-08 15:02:47 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -25365,6 +25365,55 @@ for (int i = 0; i &lt; actualLength; i++) {
       </pre>
     </div>
   ),
+  272: (
+    <div class="markdown-body div-width">
+      <p>
+        Given a non-empty binary search tree and a target value, find <i>k</i>{" "}
+        values in the BST that are closest to the target.
+      </p>
+
+      <p>
+        <b>Note:</b>
+      </p>
+
+      <ul>
+        <li>Given target value is a floating point.</li>
+        <li>
+          You may assume <i>k</i> is always valid, that is: <i>k</i> ≤ total
+          nodes.
+        </li>
+        <li>
+          You are guaranteed to have only one unique set of <i>k</i> values in
+          the BST that are closest to the target.
+        </li>
+      </ul>
+
+      <p>
+        <strong>Example:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [4,2,5,1,3], target = 3.714286, and{" "}
+        <em>k</em> = 2 <br />
+        <br />
+        <img
+          alt=""
+          src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/0200-0299/0272.Closest%20Binary%20Search%20Tree%20Value%20II/images/closest1-1-tree.jpg"
+          style={{ width: 292, height: 302 }}
+        />
+        <br />
+        <br />
+        <strong>Output:</strong> [4,3]
+      </pre>
+
+      <p>
+        <b>Follow up:</b>
+        <br />
+        Assume that the BST is balanced, could you solve it in less than{" "}
+        <i>O</i>(<i>n</i>) runtime (where <i>n</i> = total nodes)?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
