3861e19e1c03deb479c6e28f41cc35e08cf3a8b4
saiashish9 <saiashish3760@gmail.com>
2024-07-08 17:16:57 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -26101,6 +26101,68 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  776: (
+    <article role="main" class="blog-post">
+      <p>
+        Given the <code>root</code> of a binary search tree (BST) and an integer{" "}
+        <code>target</code>, split the tree into two subtrees where one subtree
+        has nodes that are all smaller or equal to the target value, while the
+        other subtree has all nodes that are greater than the target value. It
+        Is not necessarily the case that the tree contains a node with the value{" "}
+        <code>target</code>.
+      </p>
+
+      <p>
+        Additionally, most of the structure of the original tree should remain.
+        Formally, for any child <code>c</code> with parent <code>p</code> in the
+        original tree, if they are both in the same subtree after the split,
+        then node <code>c</code> should still have the parent <code>p</code>.
+      </p>
+
+      <p>
+        Return <em>an array of the two roots of the two subtrees</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <p>
+        <img
+          alt=""
+          src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/0700-0799/0776.Split%20BST/images/split-tree.jpg"
+          style={{ width: 600, height: 193 }}
+        />
+      </p>
+      <pre>
+        <strong>Input:</strong> root = [4,2,6,1,3,5,7], target = 2 <br />
+        <strong>Output:</strong> [[2,1],[4,3,6,null,null,5,7]]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [1], target = 1<br />
+        <strong>Output:</strong> [[1],[]]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range <code>[1, 50]</code>.
+        </li>
+        <li>
+          <code>0 &lt;= Node.val, target &lt;= 1000</code>
+        </li>
+      </ul>
+    </article>
+  ),
 };
 
 export default QUESTIONS;
