d0dabb959e1c5b3feb755a1db34e93829d1d6893
saiashish9 <saiashish3760@gmail.com>
2024-07-08 13:38:46 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -24834,6 +24834,97 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  98: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary tree,{" "}
+        <em>determine if it is a valid binary search tree (BST)</em>.
+      </p>
+      <p>
+        A <strong>valid BST</strong> is defined as follows:
+      </p>
+      <ul>
+        <li>
+          The left{" "}
+          <span
+            data-keyword="subtree"
+            class=" cursor-pointer relative text-dark-blue-s text-sm"
+          >
+            <div class="popover-wrapper inline-block" data-headlessui-state="">
+              <div>
+                <div
+                  aria-expanded="false"
+                  data-headlessui-state=""
+                  id="headlessui-popover-button-:r5o:"
+                >
+                  <div>subtree</div>
+                </div>
+              </div>
+            </div>
+          </span>{" "}
+          of a node contains only nodes with keys <strong>less than</strong> the
+          node's key.
+        </li>
+        <li>
+          The right subtree of a node contains only nodes with keys{" "}
+          <strong>greater than</strong> the node's key.
+        </li>
+        <li>
+          Both the left and right subtrees must also be binary search trees.
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/12/01/tree1.jpg"
+        style={{ width: 302, height: 182 }}
+      />{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> root = [2,1,3]
+        <br />
+        <strong>Output:</strong> true
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/12/01/tree2.jpg"
+        style={{ width: 422, height: 292 }}
+      />{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> root = [5,1,4,null,null,3,6]
+        <br />
+        <strong>Output:</strong> false
+        <br />
+        <strong>Explanation:</strong> The root node's value is 5 but its right
+        child's value is 4.
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range{" "}
+          <code>
+            [1, 10<sup>4</sup>]
+          </code>
+          .
+        </li>
+        <li>
+          <code>
+            -2<sup>31</sup> &lt;= Node.val &lt;= 2<sup>31</sup> - 1
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
