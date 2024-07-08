13eb051fe8c6186ca477230d5894a77c4f02b2d6
saiashish9 <saiashish3760@gmail.com>
2024-07-08 14:06:10 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -25130,6 +25130,74 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  230: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary search tree, and an integer{" "}
+        <code>k</code>, return <em>the</em>{" "}
+        <code>
+          k<sup>th</sup>
+        </code>{" "}
+        <em>
+          smallest value (<strong>1-indexed</strong>) of all the values of the
+          nodes in the tree
+        </em>
+        .
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/01/28/kthtree1.jpg"
+        style={{ width: 212, height: 301 }}
+      />{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> root = [3,1,4,null,2], k = 1<br />
+        <strong>Output:</strong> 1
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/01/28/kthtree2.jpg"
+        style={{ width: 382, height: 302 }}
+      />
+      <br />
+      <pre>
+        <strong>Input:</strong> root = [5,3,6,2,4,null,null,1], k = 3<br />
+        <strong>Output:</strong> 3
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          The number of nodes in the tree is <code>n</code>.
+        </li>
+        <li>
+          <code>
+            1 &lt;= k &lt;= n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= Node.val &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong> If the BST is modified often (i.e., we can
+        do insert and delete operations) and you need to find the kth smallest
+        frequently, how would you optimize?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
