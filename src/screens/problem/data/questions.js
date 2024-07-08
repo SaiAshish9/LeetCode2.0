3be345b37dae7a54a9b2b48e914419657b5ff35f
saiashish9 <saiashish3760@gmail.com>
2024-07-08 17:20:43 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -26163,6 +26163,73 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </article>
   ),
+  783: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a Binary Search Tree (BST), return{" "}
+        <em>
+          the minimum difference between the values of any two different nodes
+          in the tree
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/02/05/bst1.jpg"
+        style={{ width: 292, height: 301 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [4,2,6,1,3] <br />
+        <strong>Output:</strong> 1
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/02/05/bst2.jpg"
+        style={{ width: 282, height: 301 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [1,0,48,null,null,12,49] <br />
+        <strong>Output:</strong> 1
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range <code>[2, 100]</code>.
+        </li>
+        <li>
+          <code>
+            0 &lt;= Node.val &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Note:</strong> This question is the same as 530:{" "}
+        <a
+          href="https://leetcode.com/problems/minimum-absolute-difference-in-bst/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://leetcode.com/problems/minimum-absolute-difference-in-bst/
+        </a>
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
