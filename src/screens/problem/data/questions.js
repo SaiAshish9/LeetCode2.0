4c585465c702dd5fb81e3487baed55e92eab4657
saiashish9 <saiashish3760@gmail.com>
2024-07-09 01:56:21 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -27359,6 +27359,74 @@ for (int i = 0; i &lt; actualLength; i++) {
       </div>
     </div>
   ),
+  2745: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given three integers <code>x</code>, <code>y</code>, and{" "}
+        <code>z</code>.
+      </p>
+
+      <p>
+        You have <code>x</code> strings equal to <code>"AA"</code>,{" "}
+        <code>y</code> strings equal to <code>"BB"</code>, and <code>z</code>{" "}
+        strings equal to <code>"AB"</code>. You want to choose some (possibly
+        all or none) of these strings and concatenate them in some order to form
+        a new string. This new string must not contain <code>"AAA"</code> or{" "}
+        <code>"BBB"</code> as a substring.
+      </p>
+
+      <p>
+        Return <em>the maximum possible length of the new string</em>.
+      </p>
+
+      <p>
+        A <b>substring</b> is a contiguous <strong>non-empty</strong> sequence
+        of characters within a string.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> x = 2, y = 5, z = 1 <br />
+        <strong>Output:</strong> 12
+        <br />
+        <strong>Explanation: </strong>We can concactenate the strings "BB",
+        "AA", "BB", "AA", "BB", and "AB" in that order. Then, our new string is
+        "BBAABBAABBAB". <br />
+        That string has length 12, and we can show that it is impossible to
+        construct a string of longer length.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> x = 3, y = 2, z = 2<br />
+        <strong>Output:</strong> 14
+        <br />
+        <strong>Explanation:</strong> We can concactenate the strings "AB",
+        "AB", "AA", "BB", "AA", "BB", and "AA" in that order. Then, our new
+        string is "ABABAABBAABBAA". <br />
+        That string has length 14, and we can show that it is impossible to
+        construct a string of longer length.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= x, y, z &lt;= 50</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
