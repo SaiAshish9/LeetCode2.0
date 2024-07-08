4e0613489b46c54e0ccb3a41f1f43094de837601
saiashish9 <saiashish3760@gmail.com>
2024-07-09 01:05:38 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -27238,6 +27238,127 @@ for (int i = 0; i &lt; actualLength; i++) {
       <p>Could you solve this problem in linear time with constant space?</p>
     </div>
   ),
+  1429: (
+    <div class="markdown-body div-width">
+      <div class="content__u3I1 question-content__JfgR">
+        <div>
+          <p>
+            You have a queue of integers, you need to retrieve the first unique
+            integer in the queue.
+          </p>
+
+          <p>
+            Implement the <code>FirstUnique</code>&nbsp;class:
+          </p>
+
+          <ul>
+            <li>
+              <code>FirstUnique(int[] nums)</code> Initializes the object with
+              the numbers in the queue.
+            </li>
+            <li>
+              <code>int showFirstUnique()</code>&nbsp;returns the value of{" "}
+              <strong>the&nbsp;first unique</strong> integer of the queue, and
+              returns <strong>-1</strong> if there is no such integer.
+            </li>
+            <li>
+              <code>void add(int value)</code>&nbsp;insert
+              value&nbsp;to&nbsp;the queue.
+            </li>
+          </ul>
+
+          <p>&nbsp;</p>
+          <p>
+            <strong>Example 1:</strong>
+          </p>
+
+          <pre>
+            <b>Input: </b> <br />
+            ["FirstUnique","showFirstUnique","add","showFirstUnique","add","showFirstUnique","add","showFirstUnique"]{" "}
+            <br />
+            [[[2,3,5]],[],[5],[],[2],[],[3],[]] <br />
+            <b>Output: </b> <br />
+            [null,2,null,2,null,3,null,-1] <br />
+            <b>Explanation: </b> <br />
+            FirstUnique firstUnique = new FirstUnique([2,3,5]); <br />
+            firstUnique.showFirstUnique(); // return 2 <br />
+            firstUnique.add(5); // the queue is now [2,3,5,5] <br />
+            firstUnique.showFirstUnique(); // return 2 <br />
+            firstUnique.add(2);&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; // the
+            queue is now [2,3,5,5,2] <br />
+            firstUnique.showFirstUnique(); // return 3 <br />
+            firstUnique.add(3);&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; // the
+            queue is now [2,3,5,5,2,3] <br />
+            firstUnique.showFirstUnique(); // return -1
+          </pre>
+
+          <p>
+            <strong>Example 2:</strong>
+          </p>
+
+          <pre>
+            <b>Input: </b>
+            ["FirstUnique","showFirstUnique","add","add","add","add","add","showFirstUnique"]{" "}
+            <br />
+            [[[7,7,7,7,7,7]],[],[7],[3],[3],[7],[17],[]] <br />
+            <b>Output: </b> <br />
+            [null,-1,null,null,null,null,null,17] <br />
+            <b>Explanation: </b> <br />
+            FirstUnique firstUnique = new FirstUnique([7,7,7,7,7,7]); <br />
+            firstUnique.showFirstUnique(); // return -1 <br />
+            firstUnique.add(7); // the queue is now [7,7,7,7,7,7,7] <br />
+            firstUnique.add(3);&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; // the
+            queue is now [7,7,7,7,7,7,7,3] <br />
+            firstUnique.add(3);&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; // the
+            queue is now [7,7,7,7,7,7,7,3,3] <br />
+            firstUnique.add(7);&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; // the
+            queue is now [7,7,7,7,7,7,7,3,3,7] <br />
+            firstUnique.add(17);&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;// the
+            queue is now [7,7,7,7,7,7,7,3,3,7,17] <br />
+            firstUnique.showFirstUnique(); // return 17
+          </pre>
+
+          <p>
+            <strong>Example 3:</strong>
+          </p>
+
+          <pre>
+            <b>Input: </b>
+            ["FirstUnique","showFirstUnique","add","showFirstUnique"] <br />
+            [[[809]],[],[809],[]] <br />
+            <b>Output: </b> <br />
+            [null,809,null,-1] <br />
+            <b>Explanation: </b> <br />
+            FirstUnique firstUnique = new FirstUnique([809]); <br />
+            firstUnique.showFirstUnique(); // return 809 <br />
+            firstUnique.add(809); // the queue is now [809,809] <br />
+            firstUnique.showFirstUnique(); // return -1
+          </pre>
+
+          <p>&nbsp;</p>
+          <p>
+            <strong>Constraints:</strong>
+          </p>
+
+          <ul>
+            <li>
+              <code>1 &lt;= nums.length &lt;= 10^5</code>
+            </li>
+            <li>
+              <code>1 &lt;= nums[i] &lt;= 10^8</code>
+            </li>
+            <li>
+              <code>1 &lt;= value &lt;= 10^8</code>
+            </li>
+            <li>
+              At most <code>50000</code>&nbsp;calls will be made to
+              <code>showFirstUnique</code>&nbsp;and <code>add</code>.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
