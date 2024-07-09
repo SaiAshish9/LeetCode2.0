aa7b3b500144a89b248e7e3e200597456f2626d5
saiashish9 <saiashish3760@gmail.com>
2024-07-09 15:59:23 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -28033,6 +28033,108 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1337: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an <code>m x n</code> binary matrix <code>mat</code> of{" "}
+        <code>1</code>'s (representing soldiers) and <code>0</code>'s
+        (representing civilians). The soldiers are positioned{" "}
+        <strong>in front</strong> of the civilians. That is, all the{" "}
+        <code>1</code>'s will appear to the <strong>left</strong> of all the{" "}
+        <code>0</code>'s in each row.
+      </p>
+
+      <p>
+        A row <code>i</code> is <strong>weaker</strong> than a row{" "}
+        <code>j</code> if one of the following is true:
+      </p>
+
+      <ul>
+        <li>
+          The number of soldiers in row <code>i</code> is less than the number
+          of soldiers in row <code>j</code>.
+        </li>
+        <li>
+          Both rows have the same number of soldiers and <code>i &lt; j</code>.
+        </li>
+      </ul>
+
+      <p>
+        Return <em>the indices of the </em>
+        <code>k</code>
+        <em>
+          {" "}
+          <strong>weakest</strong> rows in the matrix ordered from weakest to
+          strongest
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> mat = <br />
+        [[1,1,0,0,0], <br />
+        [1,1,1,1,0], <br />
+        [1,0,0,0,0], <br />
+        [1,1,0,0,0], <br />
+        [1,1,1,1,1]], k = 3<strong>Output:</strong> [2,0,3] <br />
+        <strong>Explanation:</strong> <br />
+        The number of soldiers in each row is: <br />
+        - Row 0: 2 <br />
+        - Row 1: 4 <br />
+        - Row 2: 1 <br />
+        - Row 3: 2 <br />
+        - Row 4: 5 <br />
+        The rows ordered from weakest to strongest are [2,0,3,1,4].
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> mat = <br />
+        [[1,0,0,0], <br />
+        [1,1,1,1], <br />
+        [1,0,0,0], <br />
+        [1,0,0,0]], k = 2<strong>Output:</strong> [0,2] <br />
+        <strong>Explanation:</strong> <br />
+        The number of soldiers in each row is: <br />
+        - Row 0: 1 <br />
+        - Row 1: 4 <br />
+        - Row 2: 1 <br />
+        - Row 3: 1 <br />
+        The rows ordered from weakest to strongest are [0,2,3,1].
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>m == mat.length</code>
+        </li>
+        <li>
+          <code>n == mat[i].length</code>
+        </li>
+        <li>
+          <code>2 &lt;= n, m &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;= k &lt;= m</code>
+        </li>
+        <li>
+          <code>matrix[i][j]</code> is either 0 or 1.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
