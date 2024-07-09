1f3447d813c942be1072a5b2a66d2a4217949d81
saiashish9 <saiashish3760@gmail.com>
2024-07-09 15:27:53 +0530

    k

--- src/screens/problem/data/solutioning.js
+++ src/screens/problem/data/solutioning.js
@@ -12590,6 +12590,31 @@ const SOLUTIONING = {
           </li>
         </ul>
       </li>
+      <li>
+        <strong>Alternate Solution:</strong>
+        <pre>
+          <code>
+            {`/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var longestOnes = function(nums, k) {
+    let ans = 0;
+    for (let l = 0, r = 0; r < nums.length; ++r) {
+        if (nums[r] == 0)
+            --k;
+        while (k < 0)
+            if (nums[l++] == 0)
+                ++k;
+        ans = Math.max(ans, r - l + 1);
+    }
+    return ans;
+};
+console.log(longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2))`}
+          </code>
+        </pre>
+      </li>
     </ul>
   ),
   1031: (
