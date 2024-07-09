a382f7bb8422d8cc0d8ff32ff3330363c17054a2
saiashish9 <saiashish3760@gmail.com>
2024-07-09 22:16:33 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -28788,6 +28788,81 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </article>
   ),
+  316: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string <code>s</code>, remove duplicate letters so that every
+        letter appears once and only once. You must make sure your result is{" "}
+        <span
+          data-keyword="lexicographically-smaller-string"
+          class=" cursor-pointer relative text-dark-blue-s text-sm"
+        >
+          <div class="popover-wrapper inline-block" data-headlessui-state="">
+            <div>
+              <div
+                aria-expanded="false"
+                data-headlessui-state=""
+                id="headlessui-popover-button-:rt:"
+              >
+                <div>
+                  <strong>the smallest in lexicographical order</strong>
+                </div>
+              </div>
+            </div>
+          </div>
+        </span>{" "}
+        among all possible results.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "bcabc" <br />
+        <strong>Output:</strong> "abc"
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "cbacdcbc"
+        <br />
+        <strong>Output:</strong> "acdb"
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>s</code> consists of lowercase English letters.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Note:</strong> This question is the same as 1081:{" "}
+        <a
+          href="https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+        </a>
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
