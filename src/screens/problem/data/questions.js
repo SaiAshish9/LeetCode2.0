c036217ed31323565fbd0d563a2fcf884adccf60
saiashish9 <saiashish3760@gmail.com>
2024-07-10 00:03:49 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -28998,6 +28998,88 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  456: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array of <code>n</code> integers <code>nums</code>, a{" "}
+        <strong>132 pattern</strong> is a subsequence of three integers{" "}
+        <code>nums[i]</code>, <code>nums[j]</code> and <code>nums[k]</code> such
+        that <code>i &lt; j &lt; k</code> and{" "}
+        <code>nums[i] &lt; nums[k] &lt; nums[j]</code>.
+      </p>
+
+      <p>
+        Return <code>true</code>
+        <em>
+          {" "}
+          if there is a <strong>132 pattern</strong> in{" "}
+        </em>
+        <code>nums</code>
+        <em>, otherwise, return </em>
+        <code>false</code>
+        <em>.</em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,4] <br />
+        <strong>Output:</strong> false
+        <br />
+        <strong>Explanation:</strong> There is no 132 pattern in the sequence.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [3,1,4,2]
+        <br />
+        <strong>Output:</strong> true
+        <br />
+        <strong>Explanation:</strong> There is a 132 pattern in the sequence:
+        [1, 4, 2].
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [-1,3,2,0]
+        <br />
+        <strong>Output:</strong> true
+        <br />
+        <strong>Explanation:</strong> There are three 132 patterns in the
+        sequence: [-1, 3, 2], [-1, 3, 0] and [-1, 2, 0].
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == nums.length</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 2 * 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>9</sup> &lt;= nums[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
