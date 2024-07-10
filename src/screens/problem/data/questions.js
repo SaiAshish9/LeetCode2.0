1253f1506c28eccb18fd934c8751950c5df082f2
saiashish9 <saiashish3760@gmail.com>
2024-07-10 11:50:46 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -29224,6 +29224,68 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  768: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>arr</code>.
+      </p>
+
+      <p>
+        We split <code>arr</code> into some number of <strong>chunks</strong>{" "}
+        (i.e., partitions), and individually sort each chunk. After
+        concatenating them, the result should equal the sorted array.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>the largest number of chunks we can make to sort the array</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [5,4,3,2,1] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> <br />
+        Splitting into two or more chunks will not return the required result.{" "}
+        <br />
+        For example, splitting into [5, 4], [3, 2, 1] will result in [4, 5, 1,
+        2, 3], which isn't sorted.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [2,1,3,4,4] <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> <br />
+        We can split into two chunks, such as [2, 1], [3, 4, 4]. <br />
+        However, splitting into [2, 1], [3], [4], [4] is the highest number of
+        chunks possible.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= arr.length &lt;= 2000</code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= arr[i] &lt;= 10<sup>8</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
