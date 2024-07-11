7dc82842e7c6366a7a8c198b3b41ffea466dcb23
saiashish9 <saiashish3760@gmail.com>
2024-07-11 23:42:35 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -30183,6 +30183,99 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2104: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>nums</code>. The{" "}
+        <strong>range</strong> of a subarray of <code>nums</code> is the
+        difference between the largest and smallest element in the subarray.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>sum of all</strong> subarray ranges of{" "}
+        </em>
+        <code>nums</code>
+        <em>.</em>
+      </p>
+
+      <p>
+        A subarray is a contiguous <strong>non-empty</strong> sequence of
+        elements within an array.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3] <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> The 6 subarrays of nums are the following:{" "}
+        <br />
+        [1], range = largest - smallest = 1 - 1 = 0 <br />
+        [2], range = 2 - 2 = 0 <br />
+        [3], range = 3 - 3 = 0 <br />
+        [1,2], range = 2 - 1 = 1 <br />
+        [2,3], range = 3 - 2 = 1 <br />
+        [1,2,3], range = 3 - 1 = 2 <br />
+        So the sum of all ranges is 0 + 0 + 0 + 1 + 1 + 2 = 4.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,3,3] <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> The 6 subarrays of nums are the following:{" "}
+        <br />
+        [1], range = largest - smallest = 1 - 1 = 0 <br />
+        [3], range = 3 - 3 = 0 <br />
+        [3], range = 3 - 3 = 0 <br />
+        [1,3], range = 3 - 1 = 2 <br />
+        [3,3], range = 3 - 3 = 0 <br />
+        [1,3,3], range = 3 - 1 = 2 <br />
+        So the sum of all ranges is 0 + 0 + 0 + 2 + 0 + 2 = 4.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [4,-2,-3,4,1] <br />
+        <strong>Output:</strong> 59 <br />
+        <strong>Explanation:</strong> The sum of all subarray ranges of nums is
+        59.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>
+            -10<sup>9</sup> &lt;= nums[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow-up:</strong> Could you find a solution with{" "}
+        <code>O(n)</code> time complexity?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
