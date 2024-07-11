55ba2fae6210ea7e1259b33c4973b022a89de0ce
saiashish9 <saiashish3760@gmail.com>
2024-07-11 22:06:18 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -29503,6 +29503,80 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1063: (
+    <div class="markdown-body div-width">
+      <p>
+        Given an array <code>A</code> of integers, return the number of{" "}
+        <strong>non-empty continuous subarrays</strong> that satisfy the
+        following condition:
+      </p>
+
+      <p>
+        The leftmost element of the subarray is not larger than other elements
+        in the subarray.
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <span id="example-input-1-1">[1,4,2,5,3]</span> <br />
+        <strong>Output: </strong>
+        <span id="example-output-1">11</span>
+        <br />
+        <strong>Explanation: </strong>There are 11 valid subarrays:
+        [1],[4],[2],[5],[3],[1,4],[2,5],[1,4,2],[2,5,3],[1,4,2,5],[1,4,2,5,3].
+      </pre>
+
+      <p>
+        <strong>Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <span id="example-input-2-1">[3,2,1]</span>
+        <br />
+        <strong>Output: </strong>
+        <span id="example-output-2">3</span>
+        <br />
+        <strong>Explanation: </strong>The 3 valid subarrays are: [3],[2],[1].
+      </pre>
+
+      <p>
+        <strong>Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <span id="example-input-3-1">[2,2,2]</span>
+        <br />
+        <strong>Output: </strong>
+        <span id="example-output-3">6</span>
+        <br />
+        <strong>Explanation: </strong>There are 6 valid subarrays:
+        [2],[2],[2],[2,2],[2,2],[2,2,2].
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Note:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= A.length &lt;= 50000</code>
+        </li>
+        <li>
+          <code>0 &lt;= A[i] &lt;= 100000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
