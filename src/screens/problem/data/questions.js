fe25320d5943a8d68b5d1cf62b518e22654954ce
saiashish9 <saiashish3760@gmail.com>
2024-07-11 19:44:17 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -26691,9 +26691,9 @@ for (int i = 0; i &lt; actualLength; i++) {
       </p>
 
       <p>
-        A binary tree’s <strong>depth is the number of nodes</strong> along
-        the <strong>longest path</strong> from the root node down to the
-        farthest leaf node.
+        A binary tree’s <strong>depth is the number of nodes</strong> along the{" "}
+        <strong>longest path</strong> from the root node down to the farthest
+        leaf node.
       </p>
 
       <p>
@@ -29282,6 +29282,74 @@ for (int i = 0; i &lt; actualLength; i++) {
           <code>
             0 &lt;= arr[i] &lt;= 10<sup>8</sup>
           </code>
+        </li>
+      </ul>
+    </div>
+  ),
+  769: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>arr</code> of length <code>n</code>{" "}
+        that represents a permutation of the integers in the range{" "}
+        <code>[0, n - 1]</code>.
+      </p>
+
+      <p>
+        We split <code>arr</code> into some number of <strong>chunks</strong>{" "}
+        (i.e., partitions), and individually sort each chunk. After
+        concatenating them, the result should equal the sorted array.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>the largest number of chunks we can make to sort the array</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [4,3,2,1,0] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> <br />
+        Splitting into two or more chunks will not return the required result.{" "}
+        <br />
+        For example, splitting into [4, 3], [2, 1, 0] will result in [3, 4, 0,
+        1, 2], which isn't sorted.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [1,0,2,3,4] <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> <br />
+        We can split into two chunks, such as [1, 0], [2, 3, 4]. <br />
+        However, splitting into [1, 0], [2], [3], [4] is the highest number of
+        chunks possible.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == arr.length</code>
+        </li>
+        <li>
+          <code>1 &lt;= n &lt;= 10</code>
+        </li>
+        <li>
+          <code>0 &lt;= arr[i] &lt; n</code>
+        </li>
+        <li>
+          All the elements of <code>arr</code> are <strong>unique</strong>.
         </li>
       </ul>
     </div>
