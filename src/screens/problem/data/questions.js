c82a002519d8f7e59f36908303c9aff005cebb4d
saiashish9 <saiashish3760@gmail.com>
2024-07-12 14:23:34 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -32239,6 +32239,90 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1209: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a string <code>s</code> and an integer <code>k</code>, a{" "}
+        <code>k</code> <strong>duplicate removal</strong> consists of choosing{" "}
+        <code>k</code> adjacent and equal letters from <code>s</code> and
+        removing them, causing the left and the right side of the deleted
+        substring to concatenate together.
+      </p>
+
+      <p>
+        We repeatedly make <code>k</code> <strong>duplicate removals</strong> on{" "}
+        <code>s</code> until we no longer can.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the final string after all such duplicate removals have been made
+        </em>
+        . It is guaranteed that the answer is <strong>unique</strong>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "abcd", k = 2 <br />
+        <strong>Output:</strong> "abcd"
+        <br />
+        <strong>Explanation: </strong>There's nothing to delete.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "deeedbbcccbdaa", k = 3<br />
+        <strong>Output:</strong> "aa"
+        <br />
+        <strong>
+          Explanation: <br />
+        </strong>
+        First delete "eee" and "ccc", get "ddbbbdaa"
+        <br />
+        Then delete "bbb", get "dddaa"
+        <br />
+        Finally delete "ddd", get "aa"
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "pbbcggttciiippooaais", k = 2<br />
+        <strong>Output:</strong> "ps"
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            2 &lt;= k &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>s</code> only contains lowercase English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
