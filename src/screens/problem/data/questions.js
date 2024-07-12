9138533c63dfbc558dc6a4dbaf50da6bf1ecea04
saiashish9 <saiashish3760@gmail.com>
2024-07-12 12:23:44 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -31128,6 +31128,67 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  84: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array of integers <code>heights</code> representing the
+        histogram's bar height where the width of each bar is <code>1</code>,
+        return <em>the area of the largest rectangle in the histogram</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/01/04/histogram.jpg"
+        style={{ width: 522, height: 242 }}
+      />
+      <pre>
+        <strong>Input:</strong> heights = [2,1,5,6,2,3] <br />
+        <strong>Output:</strong> 10
+        <br />
+        <strong>Explanation:</strong> The above is a histogram where width of
+        each bar is 1.
+        <br />
+        The largest rectangle is shown in the red area, which has an area = 10
+        units.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/01/04/histogram-1.jpg"
+        style={{ width: 202, height: 362 }}
+      />
+      <pre>
+        <strong>Input:</strong> heights = [2,4]
+        <br />
+        <strong>Output:</strong> 4
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= heights.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= heights[i] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
