81baac1551e74c50bf65986742b26fe0e54c5b7b
saiashish9 <saiashish3760@gmail.com>
2024-07-12 12:32:05 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -31189,6 +31189,89 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  155: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Design a stack that supports push, pop, top, and retrieving the minimum
+        element in constant time.
+      </p>
+
+      <p>
+        Implement the <code>MinStack</code> class:
+      </p>
+
+      <ul>
+        <li>
+          <code>MinStack()</code> initializes the stack object.
+        </li>
+        <li>
+          <code>void push(int val)</code> pushes the element <code>val</code>{" "}
+          onto the stack.
+        </li>
+        <li>
+          <code>void pop()</code> removes the element on the top of the stack.
+        </li>
+        <li>
+          <code>int top()</code> gets the top element of the stack.
+        </li>
+        <li>
+          <code>int getMin()</code> retrieves the minimum element in the stack.
+        </li>
+      </ul>
+
+      <p>
+        You must implement a solution with <code>O(1)</code> time complexity for
+        each function.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input</strong>
+        ["MinStack","push","push","push","getMin","pop","top","getMin"] <br />
+        [[],[-2],[0],[-3],[],[],[],[]] <br />
+        <strong>Output</strong> <br />
+        [null,null,null,null,-3,null,0,-2]
+        <strong>Explanation</strong> <br />
+        MinStack minStack = new MinStack(); <br />
+        minStack.push(-2); <br />
+        minStack.push(0); <br />
+        minStack.push(-3); <br />
+        minStack.getMin(); // return -3 <br />
+        minStack.pop(); <br />
+        minStack.top(); // return 0 <br />
+        minStack.getMin(); // return -2
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            -2<sup>31</sup> &lt;= val &lt;= 2<sup>31</sup> - 1
+          </code>
+        </li>
+        <li>
+          Methods <code>pop</code>, <code>top</code> and <code>getMin</code>{" "}
+          operations will always be called on <strong>non-empty</strong> stacks.
+        </li>
+        <li>
+          At most{" "}
+          <code>
+            3 * 10<sup>4</sup>
+          </code>{" "}
+          calls will be made to <code>push</code>, <code>pop</code>,{" "}
+          <code>top</code>, and <code>getMin</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
