f71511f30b9fb9f971af520d39d2bcfea08a9c91
saiashish9 <saiashish3760@gmail.com>
2024-07-12 13:34:15 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -31837,6 +31837,84 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  735: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        We are given an array <code>asteroids</code> of integers representing
+        asteroids in a row.
+      </p>
+
+      <p>
+        For each asteroid, the absolute value represents its size, and the sign
+        represents its direction (positive meaning right, negative meaning
+        left). Each asteroid moves at the same speed.
+      </p>
+
+      <p>
+        Find out the state of the asteroids after all collisions. If two
+        asteroids meet, the smaller one will explode. If both are the same size,
+        both will explode. Two asteroids moving in the same direction will never
+        meet.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> asteroids = [5,10,-5] <br />
+        <strong>Output:</strong> [5,10]
+        <br />
+        <strong>Explanation:</strong> The 10 and -5 collide resulting in 10. The
+        5 and 10 never collide.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> asteroids = [8,-8]
+        <br />
+        <strong>Output:</strong> []
+        <br />
+        <strong>Explanation:</strong> The 8 and -8 collide exploding each other.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> asteroids = [10,2,-5]
+        <br />
+        <strong>Output:</strong> [10]
+        <br />
+        <strong>Explanation:</strong> The 2 and -5 collide resulting in -5. The
+        10 and -5 collide resulting in 10.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            2 &lt;= asteroids.length &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>-1000 &lt;= asteroids[i] &lt;= 1000</code>
+        </li>
+        <li>
+          <code>asteroids[i] != 0</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
