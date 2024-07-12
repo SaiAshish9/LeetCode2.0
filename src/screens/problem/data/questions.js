1a2793e038edaefb446f9728485a49f62e235c19
saiashish9 <saiashish3760@gmail.com>
2024-07-12 13:56:40 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -32016,6 +32016,75 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1190: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a string <code>s</code> that consists of lower case
+        English letters and brackets.
+      </p>
+
+      <p>
+        Reverse the strings in each pair of matching parentheses, starting from
+        the innermost one.
+      </p>
+
+      <p>
+        Your result should <strong>not</strong> contain any brackets.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "(abcd)" <br />
+        <strong>Output:</strong> "dcba"
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "(u(love)i)"
+        <br />
+        <strong>Output:</strong> "iloveu"
+        <br />
+        <strong>Explanation:</strong> The substring "love" is reversed first,
+        then the whole string is reversed.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "(ed(et(oc))el)"
+        <br />
+        <strong>Output:</strong> "leetcode"
+        <br />
+        <strong>Explanation:</strong> First, we reverse the substring "oc", then
+        "etco", and finally, the whole string.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= s.length &lt;= 2000</code>
+        </li>
+        <li>
+          <code>s</code> only contains lower case English characters and
+          parentheses.
+        </li>
+        <li>It is guaranteed that all parentheses are balanced.</li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
