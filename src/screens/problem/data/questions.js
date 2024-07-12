720a8591cddb3e6c32e890a4b5f722b49fdc6c30
saiashish9 <saiashish3760@gmail.com>
2024-07-12 14:25:56 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -32323,6 +32323,89 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1249: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string <font face="monospace">s</font> of <code>'('</code> ,{" "}
+        <code>')'</code> and lowercase English characters.
+      </p>
+
+      <p>
+        Your task is to remove the minimum number of parentheses ({" "}
+        <code>'('</code> or <code>')'</code>, in any positions ) so that the
+        resulting <em>parentheses string</em> is valid and return{" "}
+        <strong>any</strong> valid string.
+      </p>
+
+      <p>
+        Formally, a <em>parentheses string</em> is valid if and only if:
+      </p>
+
+      <ul>
+        <li>It is the empty string, contains only lowercase characters, or</li>
+        <li>
+          It can be written as <code>AB</code> (<code>A</code> concatenated with{" "}
+          <code>B</code>), where <code>A</code> and <code>B</code> are valid
+          strings, or
+        </li>
+        <li>
+          It can be written as <code>(A)</code>, where <code>A</code> is a valid
+          string.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "lee(t(c)o)de{")"}" <br />
+        <strong>Output:</strong> "lee(t(c)o)de"
+        <br />
+        <strong>Explanation:</strong> "lee(t(co)de)" , "lee(t(c)ode)" would also
+        be accepted.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "a{")"}b(c)d"
+        <br />
+        <strong>Output:</strong> "ab(c)d"
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "{"))(("}"<br />
+        <strong>Output:</strong> ""
+        <br />
+        <strong>Explanation:</strong> An empty string is also valid.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>s[i]</code> is either&nbsp;<code>'('</code> , <code>')'</code>,
+          or lowercase English letter.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
