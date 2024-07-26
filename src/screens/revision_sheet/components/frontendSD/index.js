da8e29e6cab937f35b1a3cb1c65f8a00fa77994f
saiashish9 <saiashish3760@gmail.com>
2024-07-26 21:20:49 +0530

    k

--- src/screens/revision_sheet/components/frontendSD/index.js
+++ src/screens/revision_sheet/components/frontendSD/index.js
@@ -5,6 +5,28 @@ import { useNavigate } from "react-router-dom";
 
 const DATA_FSD = [
   {
+    title: "How do cookies work in browsers?",
+  },
+  {
+    title: "How Caching Works in Google Chrome Browser and React.js?",
+  },
+  {
+    title: "Reconcilation and diffing algorithm."
+  },
+  {
+    title:
+      "Internal working of Currying in JavaScript Using Memory and Lexical Environment.",
+  },
+  {
+    title: "Google Lighthouse Metrics",
+  },
+  {
+    title: "Event Loop Architecture And Examples",
+  },
+  {
+    title: "How Critical Rendering Path (CRP) works internally?",
+  },
+  {
     title:
       "How can we print a message when a promise is fulfilled within a Promise.all chain? Additionally, what are the different states and methods of promises that support parallel execution?How can we determine the status of a promise, and how do promises differ from async/await?",
   },
@@ -16,25 +38,24 @@ const DATA_FSD = [
       "Parallelism and Concurrency models of JavaScript and Go in terms of hardware kernel and threads",
   },
   {
-    title: "Event Loop Architecture And Examples",
-  },
-  {
-    title: "How Critical Rendering Path (CRP) works internally?",
-  },
-  {
     title: "GraphQL Caching",
   },
   {
-    title: "How do cookies work in browsers?",
+    title: "How Closures Work in JavaScript?",
   },
   {
-    title: "How Caching Works in Google Chrome Browser and React.js?"
+    title: "MonoRepo Creation Using Lerna",
   },
   {
-    title: "Internal working of Currying in JavaScript Using Memory and Lexical Environment."
+    title:
+      "Virtualization: Have you ever wondered how to efficiently display a list of 1 million data items in a user interface without needing backend pagination?",
   },
   {
-    title: "Google Lighthouse Metrics"
+    title:
+      "When you type www.google.com into a web browser, a detailed series of processes occur to bring up the Google homepage",
+  },
+  {
+    title: "Internationalization (i18n)",
   }
 ];
 
