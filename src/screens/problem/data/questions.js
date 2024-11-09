68285ac3d23b249f72fb27805c1448d333795b58
saiashish9 <saiashish3760@gmail.com>
2024-11-10 02:01:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -35272,6 +35272,104 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  782: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an <code>n x n</code> binary grid <code>board</code>. In
+        each move, you can swap any two rows with each other, or any two columns
+        with each other.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the minimum number of moves to transform the board into a{" "}
+          <strong>chessboard board</strong>
+        </em>
+        . If the task is impossible, return <code>-1</code>.
+      </p>
+
+      <p>
+        A <strong>chessboard board</strong> is a board where no <code>0</code>'s
+        and no <code>1</code>'s are 4-directionally adjacent.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/06/29/chessboard1-grid.jpg"
+        style={{ width: 164, height: 165 }}
+      />
+      <pre>
+        <strong>Input:</strong> board =
+        [[0,1,1,0],[0,1,1,0],[1,0,0,1],[1,0,0,1]]
+        <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong> One potential sequence of moves is shown.
+        <br />
+        The first move swaps the first and second column.
+        <br />
+        The second move swaps the second and third row.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/06/29/chessboard2-grid.jpg"
+        style={{ width: 164, height: 165 }}
+      />
+      <pre>
+        <strong>Input:</strong> board = [[0,1],[1,0]]
+        <br />
+        <strong>Output:</strong> 0<br />
+        <strong>Explanation:</strong> Also note that the board with 0 in the top
+        left corner, is also a valid chessboard.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/06/29/chessboard3-grid.jpg"
+        style={{ width: 164, height: 165 }}
+      />
+      <pre>
+        <strong>Input:</strong> board = [[1,0],[1,0]]
+        <br />
+        <strong>Output:</strong> -1
+        <br />
+        <strong>Explanation:</strong> No matter what sequence of moves you make,
+        you cannot end with a valid chessboard.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == board.length</code>
+        </li>
+        <li>
+          <code>n == board[i].length</code>
+        </li>
+        <li>
+          <code>2 &lt;= n &lt;= 30</code>
+        </li>
+        <li>
+          <code>board[i][j]</code> is either&nbsp;<code>0</code> or{" "}
+          <code>1</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
