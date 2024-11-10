d568c32b1636806362dca813fdc16251bb83bd19
saiashish9 <saiashish3760@gmail.com>
2024-11-11 00:29:00 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -36498,6 +36498,98 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1293: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an <code>m x n</code> integer matrix <code>grid</code>{" "}
+        where each cell is either <code>0</code> (empty) or <code>1</code>{" "}
+        (obstacle). You can move up, down, left, or right from and to an empty
+        cell in <strong>one step</strong>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the minimum number of <strong>steps</strong> to walk from the upper
+          left corner{" "}
+        </em>
+        <code>(0, 0)</code>
+        <em> to the lower right corner </em>
+        <code>(m - 1, n - 1)</code>
+        <em>
+          {" "}
+          given that you can eliminate <strong>at most</strong>{" "}
+        </em>
+        <code>k</code>
+        <em> obstacles</em>. If it is not possible to find such walk return{" "}
+        <code>-1</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/09/30/short1-grid.jpg"
+        style={{ width: 244, height: 405 }}
+      />
+      <pre>
+        <strong>Input:</strong> grid =
+        [[0,0,0],[1,1,0],[0,0,0],[0,1,1],[0,0,0]], k = 1 <br />
+        <strong>Output:</strong> 6<br />
+        <strong>Explanation:</strong> <br />
+        The shortest path without eliminating any obstacle is 10.
+        <br />
+        The shortest path with one obstacle elimination at position (3,2) is 6.
+        Such path is (0,0) -&gt; (0,1) -&gt; (0,2) -&gt; (1,2)
+        <br /> -&gt; (2,2) -&gt; <strong>(3,2)</strong> -&gt; (4,2).
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/09/30/short2-grid.jpg"
+        style={{ width: 244, height: 245 }}
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[0,1,1],[1,1,1],[1,0,0]], k = 1<br />
+        <strong>Output:</strong> -1
+        <br />
+        <strong>Explanation:</strong> We need to eliminate at least two
+        obstacles to find such a walk.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>m == grid.length</code>
+        </li>
+        <li>
+          <code>n == grid[i].length</code>
+        </li>
+        <li>
+          <code>1 &lt;= m, n &lt;= 40</code>
+        </li>
+        <li>
+          <code>1 &lt;= k &lt;= m * n</code>
+        </li>
+        <li>
+          <code>grid[i][j]</code> is either <code>0</code> <strong>or</strong>{" "}
+          <code>1</code>.
+        </li>
+        <li>
+          <code>grid[0][0] == grid[m - 1][n - 1] == 0</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
