da39fa9bea1a4150b1a79f64dc3c7d0f58cb66a5
saiashish9 <saiashish3760@gmail.com>
2024-11-10 18:35:06 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -35547,6 +35547,90 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  832: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an <code>n x n</code> binary matrix <code>image</code>, flip the
+        image <strong>horizontally</strong>, then invert it, and return{" "}
+        <em>the resulting image</em>.
+      </p>
+
+      <p>
+        To flip an image horizontally means that each row of the image is
+        reversed.
+      </p>
+
+      <ul>
+        <li>
+          For example, flipping <code>[1,1,0]</code> horizontally results in{" "}
+          <code>[0,1,1]</code>.
+        </li>
+      </ul>
+
+      <p>
+        To invert an image means that each <code>0</code> is replaced by{" "}
+        <code>1</code>, and each <code>1</code> is replaced by <code>0</code>.
+      </p>
+
+      <ul>
+        <li>
+          For example, inverting <code>[0,1,1]</code> results in{" "}
+          <code>[1,0,0]</code>.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> image = [[1,1,0],[1,0,1],[0,0,0]] <br />
+        <strong>Output:</strong> [[1,0,0],[0,1,0],[1,1,1]]
+        <br />
+        <strong>Explanation:</strong> First reverse each row:
+        [[0,1,1],[1,0,1],[0,0,0]].
+        <br />
+        Then, invert the image: [[1,0,0],[0,1,0],[1,1,1]]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> image =
+        [[1,1,0,0],[1,0,0,1],[0,1,1,1],[1,0,1,0]]
+        <br />
+        <strong>Output:</strong> [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+        <br />
+        <strong>Explanation:</strong> First reverse each row:
+        [[0,0,1,1],[1,0,0,1],[1,1,1,0],[0,1,0,1]].
+        <br />
+        Then invert the image: [[1,1,0,0],[0,1,1,0],[0,0,0,1],[1,0,1,0]]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == image.length</code>
+        </li>
+        <li>
+          <code>n == image[i].length</code>
+        </li>
+        <li>
+          <code>1 &lt;= n &lt;= 20</code>
+        </li>
+        <li>
+          <code>images[i][j]</code> is either <code>0</code> or <code>1</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
