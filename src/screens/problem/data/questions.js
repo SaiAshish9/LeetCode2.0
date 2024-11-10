f8b73b03548934010991c00d91757364126fb118
saiashish9 <saiashish3760@gmail.com>
2024-11-10 18:42:07 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -35631,6 +35631,88 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  840: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        A <code>3 x 3</code> <strong>magic square</strong> is a{" "}
+        <code>3 x 3</code> grid filled with distinct numbers{" "}
+        <strong>from </strong>1<strong> to </strong>9 such that each row,
+        column, and both diagonals all have the same sum.
+      </p>
+
+      <p>
+        Given a <code>row x col</code> <code>grid</code> of integers, how many{" "}
+        <code>3 x 3</code> magic square subgrids are there?
+      </p>
+
+      <p>
+        Note: while a magic square can only contain numbers from 1 to 9,{" "}
+        <code>grid</code> may contain numbers up to 15.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/09/11/magic_main.jpg"
+        style={{ width: 322, height: 242 }}
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[4,3,8,4],[9,5,1,9],[2,7,6,2]] <br />
+        <strong>Output:</strong> 1<br />
+        <strong>Explanation: </strong>
+        <br />
+        The following subgrid is a 3 x 3 magic square:
+        <br />
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2020/09/11/magic_valid.jpg"
+          style={{ width: 242, height: 242 }}
+        />
+        while this one is not:
+        <br />
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2020/09/11/magic_invalid.jpg"
+          style={{ width: 242, height: 242 }}
+        />
+        In total, there is only one magic square inside the given grid.
+        <br />
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid = [[8]]
+        <br />
+        <strong>Output:</strong> 0
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>row == grid.length</code>
+        </li>
+        <li>
+          <code>col == grid[i].length</code>
+        </li>
+        <li>
+          <code>1 &lt;= row, col &lt;= 10</code>
+        </li>
+        <li>
+          <code>0 &lt;= grid[i][j] &lt;= 15</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
