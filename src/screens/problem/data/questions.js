4af3cb031397fcc0a6e23bfadefeedbd4a82ca4f
saiashish9 <saiashish3760@gmail.com>
2024-11-11 00:21:10 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -36434,6 +36434,68 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1277: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a <code>m * n</code> matrix of ones and zeros, return how many{" "}
+        <strong>square</strong> submatrices have all ones.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> matrix = [ &nbsp; [0,1,1,1], &nbsp; [1,1,1,1],
+        &nbsp; [0,1,1,1] ]<strong>Output:</strong> 15 <br />
+        <strong>Explanation:</strong> <br />
+        There are <strong>10</strong> squares of side 1. <br />
+        There are <strong>4</strong> squares of side 2. <br />
+        There is <strong>1</strong> square of side 3. <br />
+        Total number of squares = 10 + 4 + 1 = <strong>15</strong>.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> matrix =
+        {`[
+      [1,0,1],
+      [1,1,0],
+      [1,1,0]
+    ]`}
+        <strong>Output:</strong> 7
+        <strong>
+          {" "}
+          <br />
+          Explanation:
+        </strong>
+        There are <b>6</b> squares of side 1. <br />
+        There is <strong>1</strong> square of side 2. <br />
+        Total number of squares = 6 + 1 = <b>7</b>.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= arr.length&nbsp;&lt;= 300</code>
+        </li>
+        <li>
+          <code>1 &lt;= arr[0].length&nbsp;&lt;= 300</code>
+        </li>
+        <li>
+          <code>0 &lt;= arr[i][j] &lt;= 1</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
