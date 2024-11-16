ba4c763679191b38c01cf9e2c75206136427bd12
saiashish9 <saiashish3760@gmail.com>
2024-11-16 08:59:05 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -38063,6 +38063,105 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2128: (
+    <article role="main" class="blog-post">
+      <p>
+        You are given an <code>m x n</code> binary matrix <code>grid</code>.
+      </p>
+
+      <p>
+        In one operation, you can choose <strong>any</strong> row or column and
+        flip each value in that row or column (i.e., changing all <code>0</code>
+        's to <code>1</code>'s, and all <code>1</code>'s to <code>0</code>'s).
+      </p>
+
+      <p>
+        Return <code>true</code>
+        <em> if it is possible to remove all </em>
+        <code>1</code>
+        <em>'s from </em>
+        <code>grid</code> using <strong>any</strong> number of operations or{" "}
+        <code>false</code> otherwise.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Example 1:</strong>
+      </p>
+      <p>
+        <img
+          src="https://assets.leetcode.com/uploads/2022/01/03/image-20220103191300-1.png"
+          style={{ width: 756, height: 225 }}
+          alt=""
+        />
+      </p>
+      <pre>
+        <strong>Input:</strong> grid = [[0,1,0],[1,0,1],[0,1,0]] <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> One possible way to remove all 1's from
+        grid is to: <br />
+        - Flip the middle row <br />- Flip the middle column
+      </pre>
+
+      <p>
+        <strong>Example 2:</strong>
+      </p>
+      <p>
+        <img
+          src="https://assets.leetcode.com/uploads/2022/01/03/image-20220103181204-7.png"
+          style={{ width: 237, height: 225 }}
+          alt=""
+        />
+      </p>
+      <pre>
+        <strong>Input:</strong> grid = [[1,1,0],[0,0,0],[0,0,0]] <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> It is impossible to remove all 1's from
+        grid. <br />
+      </pre>
+
+      <p>
+        <strong>Example 3:</strong>
+      </p>
+      <p>
+        <img
+          src="https://assets.leetcode.com/uploads/2022/01/03/image-20220103181224-8.png"
+          style={{ width: 114, height: 100 }}
+          alt=""
+        />
+      </p>
+      <pre>
+        <strong>Input:</strong> grid = [[0]] <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> There are no 1's in grid.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>m == grid.length</code>
+        </li>
+        <li>
+          <code>n == grid[i].length</code>
+        </li>
+        <li>
+          <code>1 &lt;= m, n &lt;= 300</code>
+        </li>
+        <li>
+          <code>grid[i][j]</code> is either <code>0</code> or <code>1</code>.
+        </li>
+      </ul>
+
+      <p>
+        <strong>Companies</strong>:<br />
+        <a href="https://leetcode.com/company/google">Google</a>
+      </p>
+    </article>
+  ),
 };
 
 export default QUESTIONS;
