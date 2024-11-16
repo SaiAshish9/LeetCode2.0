a46222848ff00eca0f7e0b7e7914713711c84802
saiashish9 <saiashish3760@gmail.com>
2024-11-16 09:12:38 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -38361,6 +38361,87 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </>
   ),
+  2352: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a <strong>0-indexed</strong> <code>n x n</code> integer matrix{" "}
+        <code>grid</code>, <em>return the number of pairs </em>
+        <code>
+          (r<sub>i</sub>, c<sub>j</sub>)
+        </code>
+        <em> such that row </em>
+        <code>
+          r<sub>i</sub>
+        </code>
+        <em> and column </em>
+        <code>
+          c<sub>j</sub>
+        </code>
+        <em> are equal</em>.
+      </p>
+
+      <p>
+        A row and column pair is considered equal if they contain the same
+        elements in the same order (i.e., an equal array).
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2022/06/01/ex1.jpg"
+        style={{ width: 150, height: 153 }}
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[3,2,1],[1,7,6],[2,7,7]]
+        <br />
+        <strong>Output:</strong> 1<br />
+        <strong>Explanation:</strong> There is 1 equal row and column pair:
+        <br />- (Row 2, Column 1): [2,7,7]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2022/06/01/ex2.jpg"
+        style={{ width: 200, height: 209 }}
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[3,1,2,2],[1,4,4,5],[2,4,2,2],[2,4,2,2]]{" "}
+        <br />
+        <strong>Output:</strong> 3<br />
+        <strong>Explanation:</strong> There are 3 equal row and column pairs:
+        <br />
+        - (Row 0, Column 0): [3,1,2,2]
+        <br />
+        - (Row 2, Column 2): [2,4,2,2]
+        <br />- (Row 3, Column 2): [2,4,2,2]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == grid.length == grid[i].length</code>
+        </li>
+        <li>
+          <code>1 &lt;= n &lt;= 200</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= grid[i][j] &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
