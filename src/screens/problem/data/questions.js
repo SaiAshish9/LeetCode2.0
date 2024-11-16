7c53c53f2d072bf11e37768fd17700790e060d11
saiashish9 <saiashish3760@gmail.com>
2024-11-16 08:11:13 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -37481,6 +37481,149 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1861: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an <code>m x n</code> matrix of characters{" "}
+        <code>box</code> representing a side-view of a box. Each cell of the box
+        is one of the following:
+      </p>
+
+      <ul>
+        <li>
+          A stone <code>'#'</code>
+        </li>
+        <li>
+          A stationary obstacle <code>'*'</code>
+        </li>
+        <li>
+          Empty <code>'.'</code>
+        </li>
+      </ul>
+
+      <p>
+        The box is rotated <strong>90 degrees clockwise</strong>, causing some
+        of the stones to fall due to gravity. Each stone falls down until it
+        lands on an obstacle, another stone, or the bottom of the box. Gravity{" "}
+        <strong>does not</strong> affect the obstacles' positions, and the
+        inertia from the box's rotation <strong>does not </strong>affect the
+        stones' horizontal positions.
+      </p>
+
+      <p>
+        It is <strong>guaranteed</strong> that each stone in <code>box</code>{" "}
+        rests on an obstacle, another stone, or the bottom of the box.
+      </p>
+
+      <p>
+        Return <em>an </em>
+        <code>n x m</code>
+        <em> matrix representing the box after the rotation described above</em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2021/04/08/rotatingtheboxleetcodewithstones.png"
+          style={{ width: 300, height: 150 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input:</strong> box = [["#",".","#"]],
+        <br />
+        <strong>Output:</strong> [["."],,
+        <br />
+        &nbsp; ["#"],,
+        <br />
+        &nbsp; ["#"]]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2021/04/08/rotatingtheboxleetcode2withstones.png"
+          style={{ width: 375, height: 195 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input:</strong> box = [["#",".","*","."],
+        <br />
+        &nbsp; ["#","#","*","."]]
+        <br />
+        <strong>Output:</strong> [["#","."],
+        <br />
+        &nbsp; ["#","#"],
+        <br />
+        &nbsp; ["*","*"],
+        <br />
+        &nbsp; [".","."]]
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2021/04/08/rotatingtheboxleetcode3withstone.png"
+          style={{ width: 400, height: 218 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input:</strong> box = [["#","#","*",".","*","."], <br />
+        &nbsp; ["#","#","#","*",".","."],
+        <br />
+        &nbsp; ["#","#","#",".","#","."]]
+        <br />
+        <strong>Output:</strong> [[".","#","#"],
+        <br />
+        &nbsp; [".","#","#"],
+        <br />
+        &nbsp; ["#","#","*"],
+        <br />
+        &nbsp; ["#","*","."],
+        <br />
+        &nbsp; ["#",".","*"],
+        <br />
+        &nbsp; ["#",".","."]]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>m == box.length</code>
+        </li>
+        <li>
+          <code>n == box[i].length</code>
+        </li>
+        <li>
+          <code>1 &lt;= m, n &lt;= 500</code>
+        </li>
+        <li>
+          <code>box[i][j]</code> is either <code>'#'</code>, <code>'*'</code>,
+          or <code>'.'</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
