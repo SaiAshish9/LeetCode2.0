b6ec5ff928c1bc7a4f9a5ae2aea28470214dd4e2
saiashish9 <saiashish3760@gmail.com>
2024-11-16 22:48:23 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -39122,6 +39122,116 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2435: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <strong>0-indexed</strong> <code>m x n</code> integer
+        matrix <code>grid</code> and an integer <code>k</code>. You are
+        currently at position <code>(0, 0)</code> and you want to reach position{" "}
+        <code>(m - 1, n - 1)</code> moving only <strong>down</strong> or{" "}
+        <strong>right</strong>.
+      </p>
+
+      <p>
+        Return
+        <em>
+          {" "}
+          the number of paths where the sum of the elements on the path is
+          divisible by{" "}
+        </em>
+        <code>k</code>. Since the answer may be very large, return it{" "}
+        <strong>modulo</strong>{" "}
+        <code>
+          10<sup>9</sup> + 7
+        </code>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2022/08/13/image-20220813183124-1.png"
+        style={{ width: 437, height: 200 }}
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[5,2,4],[3,0,5],[0,7,2]], k = 3 <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong> There are two paths where the sum of the
+        elements on the path is divisible by k.
+        <br />
+        The first path highlighted in red has a sum of 5 + 2 + 4 + 5 + 2 = 18
+        which is divisible by 3.
+        <br />
+        The second path highlighted in blue has a sum of 5 + 3 + 0 + 5 + 2 = 15
+        which is divisible by 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2022/08/17/image-20220817112930-3.png"
+        style={{ height: 85, width: 132 }}
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[0,0]], k = 5<br />
+        <strong>Output:</strong> 1<br />
+        <strong>Explanation:</strong> The path highlighted in red has a sum of 0
+        + 0 = 0 which is divisible by 5.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2022/08/12/image-20220812224605-3.png"
+        style={{ width: 257, height: 200 }}
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[7,3,4,9],[2,3,6,2],[2,3,7,0]], k = 1
+        <br />
+        <strong>Output:</strong> 10
+        <br />
+        <strong>Explanation:</strong> Every integer is divisible by 1 so the sum
+        of the elements on every possible path is divisible by k.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>m == grid.length</code>
+        </li>
+        <li>
+          <code>n == grid[i].length</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= m, n &lt;= 5 * 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= m * n &lt;= 5 * 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>0 &lt;= grid[i][j] &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;= k &lt;= 50</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
