152a44004eebd524f27885a688a9b279eaab6e2f
saiashish9 <saiashish3760@gmail.com>
2024-11-16 09:07:53 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -38235,6 +38235,132 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2174: (
+    <>
+      <p>
+        You are given a <strong>0-indexed</strong> <code>m x n</code>{" "}
+        <strong>binary</strong> matrix <code>grid</code>.
+      </p>
+
+      <p>
+        In one operation, you can choose any <code>i</code> and <code>j</code>{" "}
+        that meet the following conditions:
+      </p>
+
+      <ul>
+        <li>
+          <code>0 &lt;= i &lt; m</code>
+        </li>
+        <li>
+          <code>0 &lt;= j &lt; n</code>
+        </li>
+        <li>
+          <code>grid[i][j] == 1</code>
+        </li>
+      </ul>
+
+      <p>
+        and change the values of <strong>all</strong> cells in row{" "}
+        <code>i</code> and column <code>j</code> to zero.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>minimum</strong> number of operations needed to remove all{" "}
+        </em>
+        <code>1</code>
+        <em>&#39;s from </em>
+        <code>grid</code>
+        <em>.</em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/2100-2199/2174.Remove%20All%20Ones%20With%20Row%20and%20Column%20Flips%20II/images/image-20220213162716-1.png"
+        style={{ width: 709, height: 200 }}
+        alt=""
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[1,1,1],[1,1,1],[0,1,0]] <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong>
+        <br />
+        In the first operation, change all cell values of row 1 and column 1 to
+        zero.
+        <br />
+        In the second operation, change all cell values of row 0 and column 0 to
+        zero.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/2100-2199/2174.Remove%20All%20Ones%20With%20Row%20and%20Column%20Flips%20II/images/image-20220213162737-2.png"
+        style={{ width: 734, height: 200 }}
+        alt=""
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[0,1,0],[1,0,1],[0,1,0]]
+        <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong>
+        <br />
+        In the first operation, change all cell values of row 1 and column 0 to
+        zero.
+        <br />
+        In the second operation, change all cell values of row 2 and column 1 to
+        zero.
+        <br />
+        Note that we cannot perform an operation using row 1 and column 1
+        because grid[1][1] != 1.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <img
+        src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/2100-2199/2174.Remove%20All%20Ones%20With%20Row%20and%20Column%20Flips%20II/images/image-20220213162752-3.png"
+        style={{ width: 156, height: 150 }}
+        alt=""
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[0,0],[0,0]]
+        <br />
+        <strong>Output:</strong> 0<br />
+        <strong>Explanation:</strong>
+        <br />
+        There are no 1&#39;s to remove so return 0.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>m == grid.length</code>
+        </li>
+        <li>
+          <code>n == grid[i].length</code>
+        </li>
+        <li>
+          <code>1 &lt;= m, n &lt;= 15</code>
+        </li>
+        <li>
+          <code>1 &lt;= m * n &lt;= 15</code>
+        </li>
+        <li>
+          <code>grid[i][j]</code> is either <code>0</code> or <code>1</code>.
+        </li>
+      </ul>
+    </>
+  ),
 };
 
 export default QUESTIONS;
