d3080fa83cc12bbd143e5a057a97c7bd1821ec77
saiashish9 <saiashish3760@gmail.com>
2024-12-14 22:37:46 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -40829,6 +40829,85 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1588: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array of positive integers <code>arr</code>, return{" "}
+        <em>
+          the sum of all possible <strong>odd-length subarrays</strong> of{" "}
+        </em>
+        <code>arr</code>.
+      </p>
+
+      <p>
+        A <strong>subarray</strong> is a contiguous subsequence of the array.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [1,4,2,5,3] <br />
+        <strong>Output:</strong> 58 <br />
+        <strong>Explanation: </strong>The odd-length subarrays of arr and their
+        sums are: <br />
+        [1] = 1 <br />
+        [4] = 4 <br />
+        [2] = 2 <br />
+        [5] = 5 <br />
+        [3] = 3 <br />
+        [1,4,2] = 7 <br />
+        [4,2,5] = 11 <br />
+        [2,5,3] = 10 <br />
+        [1,4,2,5,3] = 15 <br />
+        If we add all these together we get 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15
+        = 58
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [1,2] <br />
+        <strong>Output:</strong> 3 <br />
+        <b>Explanation: </b>There are only 2 subarrays of odd length, [1] and
+        [2]. Their sum is 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [10,11,12] <br />
+        <strong>Output:</strong> 66
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= arr.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;= arr[i] &lt;= 1000</code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong>
+      </p>
+
+      <p>Could you solve this problem in O(n) time complexity?</p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
