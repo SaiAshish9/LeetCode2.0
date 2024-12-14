b093108059f7db6e4ffd747e38bc51a8e0295a8f
saiashish9 <saiashish3760@gmail.com>
2024-12-14 17:42:34 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -40163,6 +40163,57 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  525: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a binary array <code>nums</code>, return{" "}
+        <em>
+          the maximum length of a contiguous subarray with an equal number of{" "}
+        </em>
+        <code>0</code>
+        <em> and </em>
+        <code>1</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [0,1] <br />
+        <strong>Output:</strong> 2<strong>Explanation:</strong> [0, 1] is the
+        longest contiguous subarray with an equal number of 0 and 1.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [0,1,0] <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> [0, 1] (or [1, 0]) is a longest contiguous
+        subarray with equal number of 0 and 1.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>nums[i]</code> is either <code>0</code> or <code>1</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
