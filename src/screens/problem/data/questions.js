c08db84e3471139d4b0049168d5d685395dd0cc9
saiashish9 <saiashish3760@gmail.com>
2024-12-14 22:08:44 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -40626,6 +40626,78 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1422: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a&nbsp;string <code>s</code>&nbsp;of zeros and ones,{" "}
+        <em>
+          return the maximum score after splitting the string into two{" "}
+          <strong>non-empty</strong> substrings
+        </em>{" "}
+        (i.e. <strong>left</strong> substring and <strong>right</strong>{" "}
+        substring).
+      </p>
+
+      <p>
+        The score after splitting a string is the number of{" "}
+        <strong>zeros</strong> in the <strong>left</strong> substring plus the
+        number of <strong>ones</strong> in the <strong>right</strong> substring.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "011101" <br />
+        <strong>Output:</strong> 5 <br />
+        <strong>Explanation:</strong> <br />
+        All possible ways of splitting s into two non-empty substrings are:{" "}
+        <br />
+        left = "0" and right = "11101", score = 1 + 4 = 5 <br />
+        left = "01" and right = "1101", score = 1 + 3 = 4 <br />
+        left = "011" and right = "101", score = 1 + 2 = 3 <br />
+        left = "0111" and right = "01", score = 1 + 1 = 2 <br />
+        left = "01110" and right = "1", score = 2 + 1 = 3
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "00111" <br />
+        <strong>Output:</strong> 5 <br />
+        <strong>Explanation:</strong> When left = "00" and right = "111", we get
+        the maximum score = 2 + 3 = 5
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "1111" <br />
+        <strong>Output:</strong> 3
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>2 &lt;= s.length &lt;= 500</code>
+        </li>
+        <li>
+          The string <code>s</code> consists of characters <code>'0'</code> and{" "}
+          <code>'1'</code> only.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
