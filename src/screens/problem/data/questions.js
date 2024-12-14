44a650b4d85f880b277f87153735027a7a340c26
saiashish9 <saiashish3760@gmail.com>
2024-12-14 18:21:34 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -40424,6 +40424,70 @@ for (int i = 0; i &lt; actualLength; i++) {
       </p>
     </div>
   ),
+  974: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code> and an integer <code>k</code>,
+        return{" "}
+        <em>
+          the number of non-empty <strong>subarrays</strong> that have a sum
+          divisible by{" "}
+        </em>
+        <code>k</code>.
+      </p>
+
+      <p>
+        A <strong>subarray</strong> is a <strong>contiguous</strong> part of an
+        array.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [4,5,0,-2,-3,1], k = 5 <br />
+        <strong>Output:</strong> 7 <br />
+        <strong>Explanation:</strong> There are 7 subarrays with a sum divisible
+        by k = 5: <br />
+        [4, 5, 0, -2, -3, 1], [5], [5, 0], [5, 0, -2, -3], [0], [0, -2, -3],
+        [-2, -3]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [5], k = 9 <br />
+        <strong>Output:</strong> 0
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 3 * 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= nums[i] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            2 &lt;= k &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
