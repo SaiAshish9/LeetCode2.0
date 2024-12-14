ce3b92be4ed3e6272ec10a554b08eff7e008c558
saiashish9 <saiashish3760@gmail.com>
2024-12-14 18:34:08 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -40549,6 +40549,83 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  1413: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array of integers&nbsp;<code>nums</code>, you start with an
+        initial <strong>positive</strong> value <em>startValue</em>
+        <em>.</em>
+      </p>
+
+      <p>
+        In each iteration, you calculate the step by step sum of{" "}
+        <em>startValue</em>&nbsp;plus&nbsp;elements in <code>nums</code>
+        &nbsp;(from left to right).
+      </p>
+
+      <p>
+        Return the minimum <strong>positive</strong> value of&nbsp;
+        <em>startValue</em> such that the step by step sum is never less than 1.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [-3,2,-3,4,2]
+        <br />
+        <strong>Output:</strong> 5<br />
+        <strong>Explanation: </strong>If you choose startValue = 4, in the third
+        iteration your step by step sum is less than 1.
+        <br />
+        <strong>step by step sum</strong>
+        <br />
+        <strong>startValue = 4 | startValue = 5 | nums</strong>
+        <br />
+        (4 <strong>-3</strong> ) = 1 | (5 <strong>-3</strong> ) = 2 | -3 (1{" "}
+        <strong>+2</strong> ) = 3 | (2 <strong>+2</strong> ) = 4 | 2 (3{" "}
+        <strong>-3</strong> ) = 0 | (4 <strong>-3</strong> ) = 1 | -3 (0{" "}
+        <strong>+4</strong> ) = 4 | (1 <strong>+4</strong> ) = 5 | 4<br />
+        (4 <strong>+2</strong> ) = 6 | (5 <strong>+2</strong> ) = 7 | 2
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2]
+        <br />
+        <strong>Output:</strong> 1<br />
+        <strong>Explanation:</strong> Minimum start value should be positive.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,-2,-3] <br />
+        <strong>Output:</strong> 5
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>-100 &lt;= nums[i] &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
