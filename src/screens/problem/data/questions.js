988ae931acb5abba76ba03c3edd1b56617d8ff44
saiashish9 <saiashish3760@gmail.com>
2024-12-14 23:17:28 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -41291,6 +41291,74 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2485: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a positive integer <code>n</code>, find the{" "}
+        <strong>pivot integer</strong> <code>x</code> such that:
+      </p>
+
+      <ul>
+        <li>
+          The sum of all elements between <code>1</code> and <code>x</code>{" "}
+          inclusively equals the sum of all elements between <code>x</code> and{" "}
+          <code>n</code> inclusively.
+        </li>
+      </ul>
+
+      <p>
+        Return <em>the pivot integer </em>
+        <code>x</code>. If no such integer exists, return <code>-1</code>. It is
+        guaranteed that there will be at most one pivot index for the given
+        input.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 8 <br />
+        <strong>Output:</strong> 6<br />
+        <strong>Explanation:</strong> 6 is the pivot integer since: 1 + 2 + 3 +
+        4 + 5 + 6 = 6 + 7 + 8 = 21.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 1<br />
+        <strong>Output:</strong> 1<br />
+        <strong>Explanation:</strong> 1 is the pivot integer since: 1 = 1.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 4<br />
+        <strong>Output:</strong> -1
+        <br />
+        <strong>Explanation:</strong> It can be proved that no such integer
+        exist.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= n &lt;= 1000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
