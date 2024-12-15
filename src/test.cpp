f14d759507843e415c6d86d6723f12a29c067ba9
saiashish9 <saiashish3760@gmail.com>
2024-12-15 18:48:43 +0530

    k

--- src/test.cpp
+++ src/test.cpp
@@ -1,20 +1,25 @@
 #include <iostream>
 #include <limits.h>
 #include <cmath>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 typedef unsigned int UINT;
 
-class Node {
-    public:
-        int data;
-        Node *next;
+class Node
+{
+public:
+    int data;
+    Node *next;
 };
 
 int a = 10;
 
-int main(){
+string s = "asd";
+
+int main()
+{
     Node n;
     UINT u = 30;
     int a = 20;
@@ -27,5 +32,11 @@ int main(){
     cout << (pow(2, 63) - 1 == LLONG_MAX) << endl;
     cout << (pow(2, 31) - 1 == INT_MAX) << endl;
     cout << numeric_limits<double>::max() << endl;
-    cout << u << endl;
+    cout << u << endl
+         << s << endl;
+    map<int, char> m = {{1, 'A'}, {2, 'B'}, {3, 'C'}, {4, 'D'}, {5, 'E'}};
+    for (auto [k, v] : m)
+    {
+        cout << k << ": " << v << endl;
+    }
 }
