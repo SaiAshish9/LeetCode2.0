56d8abd3b406597831a701958fcdc34ed45a68d9
saiashish9 <saiashish3760@gmail.com>
2024-12-15 18:12:16 +0530

    k

--- src/test.cpp
+++ src/test.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <limits.h>
+#include <cmath>
 
 using namespace std;
+
+typedef unsigned int UINT;
 
 class Node {
     public:
@@ -8,9 +12,20 @@ class Node {
         Node *next;
 };
 
+int a = 10;
+
 int main(){
     Node n;
+    UINT u = 30;
+    int a = 20;
     n.data = 10;
     n.next = nullptr;
     cout << n.data << endl;
+    cout << ::a;
+    cout << LLONG_MAX << endl;
+    cout << pow(2, 63) - 1 << endl;
+    cout << (pow(2, 63) - 1 == LLONG_MAX) << endl;
+    cout << (pow(2, 31) - 1 == INT_MAX) << endl;
+    cout << numeric_limits<double>::max() << endl;
+    cout << u << endl;
 }
