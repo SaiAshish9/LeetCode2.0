2c6a7cb75be70a85ab1e51d30986d1b6d9ab0ee6
saiashish9 <saiashish3760@gmail.com>
2024-12-15 20:19:18 +0530

    k

--- src/test.cpp
+++ src/test.cpp
@@ -18,6 +18,55 @@ int a = 10;
 
 string s = "asd";
 
+// C++ Program to demonstrate inline functions and classes
+#include <iostream>
+
+using namespace std;
+
+class operation
+{
+    int a, b, add, sub, mul;
+    float div;
+
+public:
+    void get();
+    void sum();
+    void difference();
+    void product();
+    void division();
+};
+void operation ::get()
+{
+    cout << "Enter first value:";
+    cin >> a;
+    cout << "Enter second value:";
+    cin >> b;
+}
+
+inline void operation ::sum()
+{
+    add = a + b;
+    cout << "Addition of two numbers: " << a + b << "\n";
+}
+
+inline void operation ::difference()
+{
+    sub = a - b;
+    cout << "Difference of two numbers: " << a - b << "\n";
+}
+
+inline void operation ::product()
+{
+    mul = a * b;
+    cout << "Product of two numbers: " << a * b << "\n";
+}
+
+inline void operation ::division()
+{
+    div = a / b;
+    cout << "Division of two numbers: " << a / b << "\n";
+}
+
 int main()
 {
     Node n;
@@ -39,6 +88,13 @@ int main()
     {
         cout << c.first << ": " << c.second << endl;
     }
+    cout << "Program using inline function\n";
+    operation s;
+    s.get();
+    s.sum();
+    s.difference();
+    s.product();
+    s.division();
 }
 
 // clang++ -std=c++17 src/test.cpp -o test && ./test
