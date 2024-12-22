ce1457c35d2f05406eb5dcccb39711184874ee9c
saiashish9 <saiashish3760@gmail.com>
2024-12-23 00:08:15 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -42406,6 +42406,98 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2089: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <strong>0-indexed</strong> integer array{" "}
+        <code>nums</code> and a target element <code>target</code>.
+      </p>
+
+      <p>
+        A <strong>target index</strong> is an index <code>i</code> such that{" "}
+        <code>nums[i] == target</code>.
+      </p>
+
+      <p>
+        Return <em>a list of the target indices of</em> <code>nums</code> after
+        <em> sorting </em>
+        <code>nums</code>
+        <em>
+          {" "}
+          in <strong>non-decreasing</strong> order
+        </em>
+        . If there are no target indices, return{" "}
+        <em>
+          an <strong>empty</strong> list
+        </em>
+        . The returned list must be sorted in <strong>increasing</strong> order.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,5,2,3], target = 2 <br />
+        <strong>Output:</strong> [1,2]
+        <br />
+        <strong>Explanation:</strong> After sorting, nums is [1,
+        <u>
+          <strong>2</strong>
+        </u>
+        ,
+        <u>
+          <strong>2</strong>
+        </u>
+        ,3,5]. The indices where nums[i] == 2 are 1 and 2.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,5,2,3], target = 3<br />
+        <strong>Output:</strong> [3]
+        <br />
+        <strong>Explanation:</strong> After sorting, nums is [1,2,2,
+        <u>
+          <strong>3</strong>
+        </u>
+        ,5]. The index where nums[i] == 3 is 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,5,2,3], target = 5<br />
+        <strong>Output:</strong> [4]
+        <br />
+        <strong>Explanation:</strong> After sorting, nums is [1,2,2,3,
+        <u>
+          <strong>5</strong>
+        </u>
+        ]. The index where nums[i] == 5 is 4.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;= nums[i], target &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
