9847e28d79f4aa39d98ee8ce424ea5b3e221b2e3
saiashish9 <saiashish3760@gmail.com>
2024-12-23 23:13:04 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -43083,9 +43083,14 @@ for (int i = 0; i &lt; actualLength; i++) {
 
       <pre>
         <strong>Input:</strong> n = 3, k = 2, budget = 15, composition =
-        [[1,1,1],[1,1,10]], stock = [0,0,100], cost = [1,2,3]
-        <strong>Output:</strong> 5<strong>Explanation:</strong> It is optimal to
-        use the 2<sup>nd</sup> machine to create alloys. <br />
+        [[1,1,1],[1,1,10]], stock = [0,0,100], cost = [1,2,3] <br />
+        <strong>Output:</strong> 5
+        <strong>
+          {" "}
+          <br />
+          Explanation:
+        </strong>{" "}
+        It is optimal to use the 2<sup>nd</sup> machine to create alloys. <br />
         To create 5 alloys we need to buy: <br />- 5 units of metal of the 1
         <sup>st</sup> type. <br />- 5 units of metal of the 2<sup>nd</sup> type.{" "}
         <br />- 0 units of metal of the 3<sup>rd</sup> type. <br />
