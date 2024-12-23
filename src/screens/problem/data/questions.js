79a8ab3ddbb4a0dae8362c82beb609e18db9f4e4
saiashish9 <saiashish3760@gmail.com>
2024-12-23 23:27:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -43246,6 +43246,108 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  715: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        A Range Module is a module that tracks ranges of numbers. Design a data
+        structure to track the ranges represented as{" "}
+        <strong>half-open intervals</strong> and query about them.
+      </p>
+
+      <p>
+        A <strong>half-open interval</strong>{" "}
+        <code>
+          {"["}left, right{")"}
+        </code>{" "}
+        denotes all the real numbers <code>x</code> where{" "}
+        <code>left &lt;= x &lt; right</code>.
+      </p>
+
+      <p>
+        Implement the <code>RangeModule</code> class:
+      </p>
+
+      <ul>
+        <li>
+          <code>RangeModule()</code> Initializes the object of the data
+          structure.
+        </li>
+        <li>
+          <code>void addRange(int left, int right)</code> Adds the{" "}
+          <strong>half-open interval</strong>{" "}
+          <code>
+            {"["}left, right{")"}
+          </code>
+          , tracking every real number in that interval. Adding an interval that
+          partially overlaps with currently tracked numbers should add any
+          numbers in the interval <code>{"["}left, right{")"}</code> that are not
+          already tracked.
+        </li>
+        <li>
+          <code>boolean queryRange(int left, int right)</code> Returns{" "}
+          <code>true</code> if every real number in the interval{" "}
+          <code>
+            {"["}left, right{")"}
+          </code>{" "}
+          is currently being tracked, and <code>false</code> otherwise.
+        </li>
+        <li>
+          <code>void removeRange(int left, int right)</code> Stops tracking
+          every real number currently being tracked in the{" "}
+          <strong>half-open interval</strong>{" "}
+          <code>
+            {"["}left, right{")"}
+          </code>
+          .
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input</strong>
+        ["RangeModule", "addRange", "removeRange", "queryRange", "queryRange",
+        "queryRange"] <br />
+        [[], [10, 20], [14, 16], [10, 14], [13, 15], [16, 17]] <br />
+        <strong>Output</strong> <br />
+        [null, null, null, true, false, true] <br />
+        <strong>Explanation</strong> <br />
+        RangeModule rangeModule = new RangeModule(); <br />
+        rangeModule.addRange(10, 20); <br />
+        rangeModule.removeRange(14, 16); <br />
+        rangeModule.queryRange(10, 14); // return True,(Every number in {"["}10,
+        14{")"} is being tracked) <br />
+        rangeModule.queryRange(13, 15); // return False,(Numbers like 14, 14.03,
+        14.17 in {"["}13, 15{")"} are not being tracked) <br />
+        rangeModule.queryRange(16, 17); // return True, (The number 16 in {"["}
+        16, 17{")"} is still being tracked, despite the remove operation)
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= left &lt; right &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+        <li>
+          At most{" "}
+          <code>
+            10<sup>4</sup>
+          </code>{" "}
+          calls will be made to <code>addRange</code>, <code>queryRange</code>,
+          and <code>removeRange</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
