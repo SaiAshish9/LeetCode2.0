bfe7fa28388369166e1214594aa1c7e803b9c87c
saiashish9 <saiashish3760@gmail.com>
2024-12-25 20:48:29 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -45065,6 +45065,69 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  214: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a string <code>s</code>. You can convert <code>s</code> to
+        a{" "}
+        <span
+          data-keyword="palindrome-string"
+          class=" cursor-pointer relative text-dark-blue-s text-sm"
+        >
+          <div class="popover-wrapper inline-block" data-headlessui-state="">
+            <div>
+              <div
+                aria-expanded="false"
+                data-headlessui-state=""
+                id="headlessui-popover-button-:rt:"
+              >
+                <div>palindrome</div>
+              </div>
+            </div>
+          </div>
+        </span>{" "}
+        by adding characters in front of it.
+      </p>
+      <p>
+        Return{" "}
+        <em>
+          the shortest palindrome you can find by performing this transformation
+        </em>
+        .
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> s = "aacecaaa" <br />
+        <strong>Output:</strong> "aaacecaaa"
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> s = "abcd" <br />
+        <strong>Output:</strong> "dcbabcd"
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>
+            0 &lt;= s.length &lt;= 5 * 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>s</code> consists of lowercase English letters only.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
