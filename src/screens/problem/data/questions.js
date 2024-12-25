79c0299459e7d2d09d3b160c65180648435a6654
saiashish9 <saiashish3760@gmail.com>
2024-12-25 16:10:58 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -43512,10 +43512,7 @@ for (int i = 0; i &lt; actualLength; i++) {
         </code>{" "}
         means that a meeting will be held during the{" "}
         <strong>half-closed</strong> time interval{" "}
-        <code>
-          {"[start<sub>i</sub>, end<sub>i</sub>)"}
-        </code>
-        . All the values of{" "}
+        <code>{"[start<sub>i</sub>, end<sub>i</sub>)"}</code>. All the values of{" "}
         <code>
           start<sub>i</sub>
         </code>{" "}
@@ -43577,12 +43574,11 @@ for (int i = 0; i &lt; actualLength; i++) {
         room 1.
         <br />
         - At time 2, both rooms are being used. The third meeting is delayed.
-        <br />
-        - At time 3, both rooms are being used. The fourth meeting is delayed.{" "}
-        <br />- At time 5, the meeting in room 1 finishes. The third meeting
-        starts in room 1 for the time period {"["}5,10{")"}. <br />- At time 10,
-        the meetings in both rooms finish. The fourth meeting starts in room 0
-        for the time period {"[10,11)"}. <br />
+        <br />- At time 3, both rooms are being used. The fourth meeting is
+        delayed. <br />- At time 5, the meeting in room 1 finishes. The third
+        meeting starts in room 1 for the time period {"["}5,10{")"}. <br />- At
+        time 10, the meetings in both rooms finish. The fourth meeting starts in
+        room 0 for the time period {"[10,11)"}. <br />
         Both rooms 0 and 1 held 2 meetings, so we return 0.
       </pre>
 
@@ -43646,6 +43642,187 @@ for (int i = 0; i &lt; actualLength; i++) {
             start<sub>i</sub>
           </code>{" "}
           are <strong>unique</strong>.
+        </li>
+      </ul>
+    </div>
+  ),
+  2659: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>nums</code> containing{" "}
+        <strong>distinct</strong> numbers, and you can perform the following
+        operations <strong>until the array is empty</strong>:
+      </p>
+
+      <ul>
+        <li>
+          If the first element has the <strong>smallest</strong> value, remove
+          it
+        </li>
+        <li>
+          Otherwise, put the first element at the <strong>end</strong> of the
+          array.
+        </li>
+      </ul>
+
+      <p>
+        Return{" "}
+        <em>an integer denoting the number of operations it takes to make </em>
+        <code>nums</code>
+        <em> empty.</em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [3,4,-1] <br />
+        <strong>Output:</strong> 5
+      </pre>
+
+      <table style={{ border: "2px solid black", borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th style={{ border: "2px solid black", padding: "5px" }}>
+              Operation
+            </th>
+            <th style={{ border: "2px solid black", padding: "5px" }}>Array</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>1</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>
+              [4, -1, 3]
+            </td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>2</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>
+              [-1, 3, 4]
+            </td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>3</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>
+              [3, 4]
+            </td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>4</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>[4]</td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>5</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>[]</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,4,3] <br />
+        <strong>Output:</strong> 5
+      </pre>
+
+      <table style={{ border: "2px solid black", borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th style={{ border: "2px solid black", padding: "5px" }}>
+              Operation
+            </th>
+            <th style={{ border: "2px solid black", padding: "5px" }}>Array</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>1</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>
+              [2, 4, 3]
+            </td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>2</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>
+              [4, 3]
+            </td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>3</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>
+              [3, 4]
+            </td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>4</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>[4]</td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>5</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>[]</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3] <br />
+        <strong>Output:</strong> 3
+      </pre>
+
+      <table style={{ border: "2px solid black", borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th style={{ border: "2px solid black", padding: "5px" }}>
+              Operation
+            </th>
+            <th style={{ border: "2px solid black", padding: "5px" }}>Array</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>1</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>
+              [2, 3]
+            </td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>2</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>[3]</td>
+          </tr>
+          <tr>
+            <td style={{ border: "2px solid black", padding: "5px" }}>3</td>
+            <td style={{ border: "2px solid black", padding: "5px" }}>[]</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>9&nbsp;</sup>&lt;= nums[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+        <li>
+          All values in <code>nums</code> are <strong>distinct</strong>.
         </li>
       </ul>
     </div>
