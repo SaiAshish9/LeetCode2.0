3e0bd227cb79df8327a0b75de889ed2418d272c3
saiashish9 <saiashish3760@gmail.com>
2024-12-25 16:35:11 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -44242,6 +44242,84 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2526: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        For a stream of integers, implement a data structure that checks if the
+        last <code>k</code> integers parsed in the stream are{" "}
+        <strong>equal</strong> to <code>value</code>.
+      </p>
+
+      <p>
+        Implement the <strong>DataStream</strong> class:
+      </p>
+
+      <ul>
+        <li>
+          <code>DataStream(int value, int k)</code> Initializes the object with
+          an empty integer stream and the two integers <code>value</code> and{" "}
+          <code>k</code>.
+        </li>
+        <li>
+          <code>boolean consec(int num)</code> Adds <code>num</code> to the
+          stream of integers. Returns <code>true</code> if the last{" "}
+          <code>k</code> integers are equal to <code>value</code>, and{" "}
+          <code>false</code> otherwise. If there are less than <code>k</code>{" "}
+          integers, the condition does not hold true, so returns{" "}
+          <code>false</code>.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input</strong>
+        ["DataStream", "consec", "consec", "consec", "consec"] <br />
+        [[4, 3], [4], [4], [4], [3]] <br />
+        <strong>Output</strong> <br />
+        [null, false, false, true, false] <br />
+        <strong>Explanation</strong> <br />
+        DataStream dataStream = new DataStream(4, 3); //value = 4, k = 3 <br />
+        dataStream.consec(4); // Only 1 integer is parsed, so returns False.{" "}
+        <br />
+        dataStream.consec(4); // Only 2 integers are parsed. <br />
+        // Since 2 is less than k, returns False. <br />
+        dataStream.consec(4); // The 3 integers parsed are all equal to value,
+        so returns True. <br />
+        dataStream.consec(3); // The last k integers parsed in the stream are
+        [4,4,3]. <br />
+        // Since 3 is not equal to value, it returns False.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= value, num &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= k &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          At most{" "}
+          <code>
+            10<sup>5</sup>
+          </code>{" "}
+          calls will be made to <code>consec</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
