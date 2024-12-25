8258dc8f0cf749ed4618160838ac29950cb6c63b
saiashish9 <saiashish3760@gmail.com>
2024-12-25 16:21:55 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -43953,6 +43953,99 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  2817: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <strong>0-indexed</strong> integer array{" "}
+        <code>nums</code> and an integer <code>x</code>.
+      </p>
+
+      <p>
+        Find the <strong>minimum absolute difference</strong> between two
+        elements in the array that are at least <code>x</code> indices apart.
+      </p>
+
+      <p>
+        In other words, find two indices <code>i</code> and <code>j</code> such
+        that <code>abs(i - j) &gt;= x</code> and{" "}
+        <code>abs(nums[i] - nums[j])</code> is minimized.
+      </p>
+
+      <p>
+        Return
+        <em>
+          {" "}
+          an integer denoting the <strong>minimum</strong> absolute difference
+          between two elements that are at least
+        </em>{" "}
+        <code>x</code> <em>indices apart</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [4,3,2,4], x = 2 <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> We can select nums[0] = 4 and nums[3] = 4.{" "}
+        <br />
+        They are at least 2 indices apart, and their absolute difference is the
+        minimum, 0. <br />
+        It can be shown that 0 is the optimal answer.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [5,3,2,10,15], x = 1 <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> We can select nums[1] = 3 and nums[2] = 2.{" "}
+        <br />
+        They are at least 1 index apart, and their absolute difference is the
+        minimum, 1. <br />
+        It can be shown that 1 is the optimal answer.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,4], x = 3 <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> We can select nums[0] = 1 and nums[3] = 4.{" "}
+        <br />
+        They are at least 3 indices apart, and their absolute difference is the
+        minimum, 3. <br />
+        It can be shown that 3 is the optimal answer.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= nums[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+        <li>
+          <code>0 &lt;= x &lt; nums.length</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
