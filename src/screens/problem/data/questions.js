ab7e2896c67b4f0b28753c143b1a3f418d21cb8e
saiashish9 <saiashish3760@gmail.com>
2024-12-25 16:43:13 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -44320,6 +44320,140 @@ for (int i = 0; i &lt; actualLength; i++) {
       </ul>
     </div>
   ),
+  3200: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given two integers <code>red</code> and <code>blue</code>{" "}
+        representing the count of red and blue colored balls. You have to
+        arrange these balls to form a triangle such that the 1<sup>st</sup> row
+        will have 1 ball, the 2<sup>nd</sup> row will have 2 balls, the 3
+        <sup>rd</sup> row will have 3 balls, and so on.
+      </p>
+
+      <p>
+        All the balls in a particular row should be the <strong>same</strong>{" "}
+        color, and adjacent rows should have <strong>different</strong> colors.
+      </p>
+
+      <p>
+        Return the <strong>maximum</strong>
+        <em> height of the triangle</em> that can be achieved.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">red = 2, blue = 4</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> 3
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          <img
+            alt=""
+            src="https://assets.leetcode.com/uploads/2024/06/16/brb.png"
+            style={{ width: 300, height: 240, padding: 10 }}
+          />
+        </p>{" "}
+        <br />
+        <p>The only possible arrangement is shown above.</p>
+      </div>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">red = 2, blue = 1</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">2</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          <img
+            alt=""
+            src="https://assets.leetcode.com/uploads/2024/06/16/br.png"
+            style={{ width: 150, height: 135, padding: 10 }}
+          />
+          <br />
+          The only possible arrangement is shown above.
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">red = 1, blue = 1</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">1</span>
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 4:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">red = 10, blue = 1</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">2</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>
+        <p>
+          <img
+            alt=""
+            src="https://assets.leetcode.com/uploads/2024/06/16/br.png"
+            style={{ width: 150, height: 135, padding: 10 }}
+          />{" "}
+          <br />
+          The only possible arrangement is shown above.
+        </p>
+      </div>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= red, blue &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
