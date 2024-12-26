6d6abc3cd19dbc46b221b090230e887e466a2a23
saiashish9 <saiashish3760@gmail.com>
2024-12-27 00:35:23 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -45823,6 +45823,115 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  2992: (
+    <article role="main" class="blog-post">
+      <p>
+        Given an integer <code>n</code>, return{" "}
+        <em>
+          the number of <strong>permutations</strong> of the{" "}
+          <strong>1-indexed</strong> array
+        </em>{" "}
+        <code>nums = [1, 2, ..., n]</code>
+        <em>
+          , such that it's <strong>self-divisible</strong>
+        </em>
+        .
+      </p>
+
+      <p>
+        Array <code>nums</code> is <strong>self-divisible</strong> if for every{" "}
+        <code>1 &lt;= i &lt;= n</code>, <strong>at least</strong> one of the
+        following conditions holds:
+      </p>
+
+      <ul>
+        <li>
+          <code>nums[i] % i == 0</code>
+        </li>
+        <li>
+          <code>i % nums[i] == 0</code>
+        </li>
+      </ul>
+
+      <p>
+        A <strong>permutation</strong> of an array is a rearrangement of the
+        elements of that array, for example here are all of the permutations of
+        the array <code>[1, 2, 3]</code>:
+      </p>
+
+      <ul>
+        <li>
+          <code>[1, 2, 3]</code>
+        </li>
+        <li>
+          <code>[1, 3, 2]</code>
+        </li>
+        <li>
+          <code>[2, 1, 3]</code>
+        </li>
+        <li>
+          <code>[2, 3, 1]</code>
+        </li>
+        <li>
+          <code>[3, 1, 2]</code>
+        </li>
+        <li>
+          <code>[3, 2, 1]</code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 1 <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> The array [1] has only 1 permutation which
+        is self-divisible.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> The array [1,2] has 2 permutations both of
+        which are self-divisible: <br />
+        nums = [1,2]: This is self-divisible since nums[1] % 1 == 0 and nums[2]
+        % 2 == 0. <br />
+        nums = [2,1]: This is self-divisible since nums[1] % 1 == 0 and 2 %
+        nums[2] == 0.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 3 <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> The array [1,2,3] has 3 self-divisble
+        permutations: [1,2,3], [2,1,3], [3,2,1]. <br />
+        It can be shown that the other 3 permutations are not self-divisible.
+        Hence the answer is 3.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= n &lt;= 15</code>
+        </li>
+      </ul>
+    </article>
+  ),
 };
 
 export default QUESTIONS;
