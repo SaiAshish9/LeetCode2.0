bdf620bbc3dcbb3187373035a0ed243fa591bfa2
saiashish9 <saiashish3760@gmail.com>
2024-12-27 01:30:12 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -45932,6 +45932,64 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </article>
   ),
+  1392: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        A string is called a <strong>happy prefix</strong> if is a{" "}
+        <strong>non-empty</strong> prefix which is also a suffix (excluding
+        itself).
+      </p>
+
+      <p>
+        Given a string <code>s</code>, return{" "}
+        <em>
+          the <strong>longest happy prefix</strong> of
+        </em>{" "}
+        <code>s</code>. Return an empty string <code>""</code> if no such prefix
+        exists.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "level" <br />
+        <strong>Output:</strong> "l" <br />
+        <strong>Explanation:</strong> s contains 4 prefix excluding itself ("l",
+        "le", "lev", "leve"), and suffix ("l", "el", "vel", "evel"). The largest
+        prefix which is also suffix is given by "l".
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "ababab" <br />
+        <strong>Output:</strong> "abab" <br />
+        <strong>Explanation:</strong> "abab" is the largest prefix which is also
+        suffix. They can overlap in the original string.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>s</code> contains only lowercase English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
