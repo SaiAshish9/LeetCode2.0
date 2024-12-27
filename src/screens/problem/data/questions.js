18263c243536c6bdfe6c227ea5712a5a7ca2fab2
saiashish9 <saiashish3760@gmail.com>
2024-12-28 00:54:03 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -46875,6 +46875,99 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  2318: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer <code>n</code>. You roll a fair 6-sided dice{" "}
+        <code>n</code> times. Determine the total number of{" "}
+        <strong>distinct</strong> sequences of rolls possible such that the
+        following conditions are satisfied:
+      </p>
+
+      <ol>
+        <li>
+          The <strong>greatest common divisor</strong> of any{" "}
+          <strong>adjacent</strong> values in the sequence is equal to{" "}
+          <code>1</code>.
+        </li>
+        <li>
+          There is <strong>at least</strong> a gap of <code>2</code> rolls
+          between <strong>equal</strong> valued rolls. More formally, if the
+          value of the{" "}
+          <code>
+            i<sup>th</sup>
+          </code>{" "}
+          roll is <strong>equal</strong> to the value of the{" "}
+          <code>
+            j<sup>th</sup>
+          </code>{" "}
+          roll, then <code>abs(i - j) &gt; 2</code>.
+        </li>
+      </ol>
+
+      <p>
+        Return{" "}
+        <em>
+          the<strong> total number</strong> of distinct sequences possible
+        </em>
+        . Since the answer may be very large, return it <strong>modulo</strong>{" "}
+        <code>
+          10<sup>9</sup> + 7
+        </code>
+        .
+      </p>
+
+      <p>
+        Two sequences are considered distinct if at least one element is
+        different.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 4 <br />
+        <strong>Output:</strong> 184 <br />
+        <strong>Explanation:</strong> Some of the possible sequences are (1, 2,
+        3, 4), (6, 1, 2, 3), (1, 2, 3, 1), etc. <br />
+        Some invalid sequences are (1, 2, 1, 3), (1, 2, 3, 6). <br />
+        (1, 2, 1, 3) is invalid since the first and third roll have an equal
+        value and abs(1 - 3) = 2 (i and j are 1-indexed). <br />
+        (1, 2, 3, 6) is invalid since the greatest common divisor of 3 and 6 =
+        3. <br />
+        There are a total of 184 distinct sequences possible, so we return 184.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2 <br />
+        <strong>Output:</strong> 22 <br />
+        <strong>Explanation:</strong> Some of the possible sequences are (1, 2),
+        (2, 1), (3, 2). <br />
+        Some invalid sequences are (3, 6), (2, 4) since the greatest common
+        divisor is not equal to 1. <br />
+        There are a total of 22 distinct sequences possible, so we return 22.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
