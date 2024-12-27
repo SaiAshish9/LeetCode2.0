c52e1405ca2dd5723d6cf0d0ca1d4eac307412b8
saiashish9 <saiashish3760@gmail.com>
2024-12-28 00:57:31 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -46968,6 +46968,106 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  2876: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        There is a <strong>directed</strong> graph consisting of <code>n</code>{" "}
+        nodes numbered from <code>0</code> to <code>n - 1</code> and{" "}
+        <code>n</code> directed edges.
+      </p>
+
+      <p>
+        You are given a <strong>0-indexed</strong> array <code>edges</code>{" "}
+        where <code>edges[i]</code> indicates that there is an edge from node{" "}
+        <code>i</code> to node <code>edges[i]</code>.
+      </p>
+
+      <p>Consider the following process on the graph:</p>
+
+      <ul>
+        <li>
+          You start from a node <code>x</code> and keep visiting other nodes
+          through edges until you reach a node that you have already visited
+          before on this <strong>same</strong> process.
+        </li>
+      </ul>
+
+      <p>
+        Return <em>an array </em>
+        <code>answer</code>
+        <em> where </em>
+        <code>answer[i]</code>
+        <em>
+          {" "}
+          is the number of <strong>different</strong> nodes that you will visit
+          if you perform the process starting from node{" "}
+        </em>
+        <code>i</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2023/08/31/graaphdrawio-1.png"
+      />
+      <pre>
+        <strong>Input:</strong> edges = [1,2,0,0] <br />
+        <strong>Output:</strong> [3,3,3,4] <br />
+        <strong>Explanation:</strong> We perform the process starting from each
+        node in the following way: <br />
+        - Starting from node 0, we visit the nodes 0 -&gt; 1 -&gt; 2 -&gt; 0.
+        The number of different nodes we visit is 3.
+        <br />
+        - Starting from node 1, we visit the nodes 1 -&gt; 2 -&gt; 0 -&gt; 1.
+        The number of different nodes we visit is 3.
+        <br />
+        - Starting from node 2, we visit the nodes 2 -&gt; 0 -&gt; 1 -&gt; 2.
+        The number of different nodes we visit is 3.
+        <br />- Starting from node 3, we visit the nodes 3 -&gt; 0 -&gt; 1 -&gt;
+        2 -&gt; 0. The number of different nodes we visit is 4.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2023/08/31/graaph2drawio.png"
+        style={{ width: 191, height: 251 }}
+      />
+      <pre>
+        <strong>Input:</strong> edges = [1,2,3,4,0] <br />
+        <strong>Output:</strong> [5,5,5,5,5] <br />
+        <strong>Explanation:</strong> Starting from any node we can visit every
+        node in the graph in the process.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == edges.length</code>
+        </li>
+        <li>
+          <code>
+            2 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>0 &lt;= edges[i] &lt;= n - 1</code>
+        </li>
+        <li>
+          <code>edges[i] != i</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
