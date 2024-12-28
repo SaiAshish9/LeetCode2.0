c36b3aebcbed7c955ce4db0277e377fecf1a7266
saiashish9 <saiashish3760@gmail.com>
2024-12-29 00:14:38 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -49644,6 +49644,84 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  1023: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array of strings <code>queries</code> and a string{" "}
+        <code>pattern</code>, return a boolean array <code>answer</code> where{" "}
+        <code>answer[i]</code> is <code>true</code> if <code>queries[i]</code>{" "}
+        matches <code>pattern</code>, and <code>false</code> otherwise.
+      </p>
+
+      <p>
+        A query word <code>queries[i]</code> matches <code>pattern</code> if you
+        can insert lowercase English letters pattern so that it equals the
+        query. You may insert each character at any position and you may not
+        insert any characters.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> queries =
+        ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"],
+        pattern = "FB" <br />
+        <strong>Output:</strong> [true,false,true,true,false] <br />
+        <strong>Explanation:</strong> "FooBar" can be generated like this "F" +
+        "oo" + "B" + "ar". <br />
+        "FootBall" can be generated like this "F" + "oot" + "B" + "all". <br />
+        "FrameBuffer" can be generated like this "F" + "rame" + "B" + "uffer".
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> queries =
+        ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"],
+        pattern = "FoBa" <br />
+        <strong>Output:</strong> [true,false,true,false,false] <br />
+        <strong>Explanation:</strong> "FooBar" can be generated like this "Fo" +
+        "o" + "Ba" + "r". <br />
+        "FootBall" can be generated like this "Fo" + "ot" + "Ba" + "ll".
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> queries =
+        ["FooBar","FooBarTest","FootBall","FrameBuffer","ForceFeedBack"],
+        pattern = "FoBaT" <br />
+        <strong>Output:</strong> [false,true,false,false,false] <br />
+        <strong>Explanation:</strong> "FooBarTest" can be generated like this
+        "Fo" + "o" + "Ba" + "r" + "T" + "est".
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= pattern.length, queries.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;= queries[i].length &lt;= 100</code>
+        </li>
+        <li>
+          <code>queries[i]</code> and <code>pattern</code> consist of English
+          letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
