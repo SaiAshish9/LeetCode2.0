e1823e01ecc3bb6ca07e5326b1ec7a33b1073ed5
saiashish9 <saiashish3760@gmail.com>
2024-12-28 13:55:25 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -49477,6 +49477,62 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </div>
     </>
   ),
+  459: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string <code>s</code>, check if it can be constructed by taking
+        a substring of it and appending multiple copies of the substring
+        together.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "abab"
+        <strong>Output:</strong> true
+        <strong>Explanation:</strong> It is the substring "ab" twice. <br />
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "aba" <br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "abcabcabcabc" <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> It is the substring "abc" four times or
+        the substring "abcabc" twice.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>s</code> consists of lowercase English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
