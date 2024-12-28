a99dd4d6dfc78ef176bba1572dd7e095dc5d5de0
saiashish9 <saiashish3760@gmail.com>
2024-12-29 01:57:23 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -50019,6 +50019,79 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  559: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>Given a n-ary tree, find its maximum depth.</p>
+
+      <p>
+        The maximum depth is the number of nodes along the longest path from the
+        root node down to the farthest leaf node.
+      </p>
+
+      <p>
+        <em>
+          Nary-Tree input serialization is represented in their level order
+          traversal, each group of children is separated by the null value (See
+          examples).
+        </em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2018/10/12/narytreeexample.png"
+          style={{ width: "100%", maxWidth: 300 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [1,null,3,2,4,null,5,6] <br />
+        <strong>Output:</strong> 3
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2019/11/08/sample_4_964.png"
+          style={{ width: 296, height: 241 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root =
+        [1,null,2,3,4,5,null,null,6,7,null,8,null,9,10,null,null,11,null,12,null,13,null,null,14]
+        <br /> <strong>Output:</strong> 5
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The total number of nodes is in the range{" "}
+          <code>
+            [0, 10<sup>4</sup>]
+          </code>
+          .
+        </li>
+        <li>
+          The depth of the n-ary tree is less than or equal to <code>1000</code>
+          .
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
