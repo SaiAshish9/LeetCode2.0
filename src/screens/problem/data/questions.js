53ae89e33446213d9c4ad5e3fd804a615aeffba6
saiashish9 <saiashish3760@gmail.com>
2024-12-28 10:51:52 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -47620,6 +47620,147 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  3042: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <strong>0-indexed</strong> string array{" "}
+        <code>words</code>.
+      </p>
+
+      <p>
+        Let's define a <strong>boolean</strong> function{" "}
+        <code>isPrefixAndSuffix</code> that takes two strings, <code>str1</code>{" "}
+        and <code>str2</code>:
+      </p>
+
+      <ul>
+        <li>
+          <code>isPrefixAndSuffix(str1, str2)</code> returns <code>true</code>{" "}
+          if <code>str1</code> is <strong>both</strong> a{" "}
+          <span
+            data-keyword="string-prefix"
+            class=" cursor-pointer relative text-dark-blue-s text-sm"
+          >
+            <div class="popover-wrapper inline-block" data-headlessui-state="">
+              <div>
+                <div
+                  aria-expanded="false"
+                  data-headlessui-state=""
+                  id="headlessui-popover-button-:r5u:"
+                >
+                  <div>prefix</div>
+                </div>
+              </div>
+            </div>
+          </span>{" "}
+          and a{" "}
+          <span
+            data-keyword="string-suffix"
+            class=" cursor-pointer relative text-dark-blue-s text-sm"
+          >
+            <div class="popover-wrapper inline-block" data-headlessui-state="">
+              <div>
+                <div
+                  aria-expanded="false"
+                  data-headlessui-state=""
+                  id="headlessui-popover-button-:r60:"
+                >
+                  <div>suffix</div>
+                </div>
+              </div>
+            </div>
+          </span>{" "}
+          of <code>str2</code>, and <code>false</code> otherwise.
+        </li>
+      </ul>
+
+      <p>
+        For example, <code>isPrefixAndSuffix("aba", "ababa")</code> is{" "}
+        <code>true</code> because <code>"aba"</code> is a prefix of{" "}
+        <code>"ababa"</code> and also a suffix, but{" "}
+        <code>isPrefixAndSuffix("abc", "abcd")</code> is <code>false</code>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          an integer denoting the <strong>number</strong> of index pairs{" "}
+        </em>
+        <code>(i, j)</code>
+        <em> such that </em>
+        <code>i &lt; j</code>
+        <em>, and </em>
+        <code>isPrefixAndSuffix(words[i], words[j])</code>
+        <em> is </em>
+        <code>true</code>
+        <em>.</em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> words = ["a","aba","ababa","aa"] <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> In this example, the counted index pairs
+        are: <br />
+        i = 0 and j = 1 because isPrefixAndSuffix("a", "aba") is true. <br />
+        i = 0 and j = 2 because isPrefixAndSuffix("a", "ababa") is true. <br />
+        i = 0 and j = 3 because isPrefixAndSuffix("a", "aa") is true. <br />i =
+        1 and j = 2 because isPrefixAndSuffix("aba", "ababa") is true. <br />
+        Therefore, the answer is 4.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> words = ["pa","papa","ma","mama"]
+        <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong> In this example, the counted index pairs
+        are:
+        <br />
+        i = 0 and j = 1 because isPrefixAndSuffix("pa", "papa") is true.
+        <br />
+        i = 2 and j = 3 because isPrefixAndSuffix("ma", "mama") is true.
+        <br />
+        Therefore, the answer is 2.{" "}
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> words = ["abab","ab"] <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation: </strong>In this example, the only valid index pair
+        is i = 0 and j = 1, and isPrefixAndSuffix("abab", "ab") is false.
+        <br /> Therefore, the answer is 0.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= words.length &lt;= 50</code>
+        </li>
+        <li>
+          <code>1 &lt;= words[i].length &lt;= 10</code>
+        </li>
+        <li>
+          <code>words[i]</code> consists only of lowercase English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
