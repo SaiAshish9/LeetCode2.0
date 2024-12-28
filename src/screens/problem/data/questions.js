fe8594441b0d6652e5e7f767107fa815f3b4d737
saiashish9 <saiashish3760@gmail.com>
2024-12-29 02:01:50 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -50092,6 +50092,79 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  1245: (
+    <div class="markdown-body div-width">
+      <p>
+        Given an undirected tree, return&nbsp;its diameter: the number of{" "}
+        <strong>edges</strong> in a longest path in that tree.
+      </p>
+
+      <p>
+        The tree is given as an array&nbsp;of&nbsp;<code>edges</code>&nbsp;where{" "}
+        <code>edges[i] = [u, v]</code> is a bidirectional edge between
+        nodes&nbsp;<code>u</code> and <code>v</code>.&nbsp; Each node
+        has&nbsp;labels in the set <code>{`{0, 1, ..., edges.length}`}</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Example 1:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2019/06/14/1397_example_1.PNG"
+          style={{ width: 226, height: 233 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input:</strong> edges = [[0,1],[0,2]] <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation: </strong> <br />A longest path of the tree is the
+        path 1 - 0 - 2.
+      </pre>
+
+      <p>
+        <strong>Example 2:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2019/06/14/1397_example_2.PNG"
+          style={{ width: 350, height: 316 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input:</strong> edges = [[0,1],[1,2],[2,3],[1,4],[4,5]]
+        <br />
+        <strong>Output:</strong> 4<br />
+        <strong>Explanation: </strong>
+        <br />A longest path of the tree is the path 3 - 2 - 1 - 4 - 5.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>0 &lt;= edges.length &lt;&nbsp;10^4</code>
+        </li>
+        <li>
+          <code>edges[i][0] != edges[i][1]</code>
+        </li>
+        <li>
+          <code>0 &lt;= edges[i][j] &lt;= edges.length</code>
+        </li>
+        <li>The given edges form an undirected tree.</li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
