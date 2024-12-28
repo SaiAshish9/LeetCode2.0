fdcbc085edff40ef0443966c1dfa9c2ee2639150
saiashish9 <saiashish3760@gmail.com>
2024-12-28 11:40:50 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -48512,6 +48512,86 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  587: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an array <code>trees</code> where{" "}
+        <code>
+          trees[i] = [x<sub>i</sub>, y<sub>i</sub>]
+        </code>{" "}
+        represents the location of a tree in the garden.
+      </p>
+
+      <p>
+        Fence the entire garden using the minimum length of rope, as it is
+        expensive. The garden is well-fenced only if{" "}
+        <strong>all the trees are enclosed</strong>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the coordinates of trees that are exactly located on the fence
+          perimeter
+        </em>
+        . You may return the answer in <strong>any order</strong>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/04/24/erect2-plane.jpg"
+        style={{ width: 400, height: 393 }}
+      />
+      <pre>
+        <strong>Input:</strong> trees = [[1,1],[2,2],[2,0],[2,4],[3,3],[4,2]]{" "}
+        <br />
+        <strong>Output:</strong> [[1,1],[2,0],[4,2],[3,3],[2,4]] <br />
+        <strong>Explanation:</strong> All the trees will be on the perimeter of
+        the fence except the tree at [2, 2], which will be inside the fence.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/04/24/erect1-plane.jpg"
+        style={{ width: 400, height: 393 }}
+      />
+      <pre>
+        <strong>Input:</strong> trees = [[1,2],[2,2],[4,2]] <br />
+        <strong>Output:</strong> [[4,2],[2,2],[1,2]] <br />
+        <strong>Explanation:</strong> The fence forms a line that passes through
+        all the trees.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= trees.length &lt;= 3000</code>
+        </li>
+        <li>
+          <code>trees[i].length == 2</code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= x<sub>i</sub>, y<sub>i</sub> &lt;= 100
+          </code>
+        </li>
+        <li>
+          All the given positions are <strong>unique</strong>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
