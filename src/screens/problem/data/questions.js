53f6edfea0acf12206c7c5bc4aeebea60beba0c5
saiashish9 <saiashish3760@gmail.com>
2024-12-28 10:48:35 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -47514,6 +47514,112 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </p>
     </div>
   ),
+  2416: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an array <code>words</code> of size <code>n</code>{" "}
+        consisting of <strong>non-empty</strong> strings.
+      </p>
+
+      <p>
+        We define the <strong>score</strong> of a string <code>term</code> as
+        the <strong>number</strong> of strings <code>words[i]</code> such that{" "}
+        <code>term</code> is a <strong>prefix</strong> of <code>words[i]</code>.
+      </p>
+
+      <ul>
+        <li>
+          For example, if <code>words = ["a", "ab", "abc", "cab"]</code>, then
+          the score of <code>"ab"</code> is <code>2</code>, since{" "}
+          <code>"ab"</code> is a prefix of both <code>"ab"</code> and{" "}
+          <code>"abc"</code>.
+        </li>
+      </ul>
+
+      <p>
+        Return <em>an array </em>
+        <code>answer</code>
+        <em> of size </em>
+        <code>n</code>
+        <em> where </em>
+        <code>answer[i]</code>
+        <em>
+          {" "}
+          is the <strong>sum</strong> of scores of every{" "}
+          <strong>non-empty</strong> prefix of{" "}
+        </em>
+        <code>words[i]</code>.
+      </p>
+
+      <p>
+        <strong>Note</strong> that a string is considered as a prefix of itself.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> words = ["abc","ab","bc","b"] <br />
+        <strong>Output:</strong> [5,4,3,2] <br />
+        <strong>Explanation:</strong> The answer for each string is the
+        following: <br />
+        - "abc" has 3 prefixes: "a", "ab", and "abc". <br />
+        - There are 2 strings with the prefix "a", 2 strings with the prefix
+        "ab", and 1 string with the prefix "abc". <br />
+        The total is answer[0] = 2 + 2 + 1 = 5. <br />
+        - "ab" has 2 prefixes: "a" and "ab". <br />
+        - There are 2 strings with the prefix "a", and 2 strings with the prefix
+        "ab". <br />
+        The total is answer[1] = 2 + 2 = 4. <br />
+        - "bc" has 2 prefixes: "b" and "bc". <br />
+        - There are 2 strings with the prefix "b", and 1 string with the prefix
+        "bc".
+        <br />
+        The total is answer[2] = 2 + 1 = 3. <br />
+        - "b" has 1 prefix: "b".
+        <br />
+        - There are 2 strings with the prefix "b".
+        <br />
+        The total is answer[3] = 2.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> words = ["abcd"]
+        <br />
+        <strong>Output:</strong> [4]
+        <br />
+        <strong>Explanation:</strong>
+        <br />
+        "abcd" has 4 prefixes: "a", "ab", "abc", and "abcd".
+        <br />
+        Each prefix has a score of one, so the total is answer[0] = 1 + 1 + 1 +
+        1 = 4.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= words.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>1 &lt;= words[i].length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>words[i]</code> consists of lowercase English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
