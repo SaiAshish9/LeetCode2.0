28cab9c5008264bbf075333214512b091def6bab
saiashish9 <saiashish3760@gmail.com>
2024-12-29 00:19:57 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -49810,6 +49810,87 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  1688: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        For a string <code>sequence</code>, a string <code>word</code> is{" "}
+        <strong>
+          <code>k</code>-repeating
+        </strong>{" "}
+        if <code>word</code> concatenated <code>k</code> times is a substring of{" "}
+        <code>sequence</code>. The <code>word</code>'s{" "}
+        <strong>
+          maximum <code>k</code>-repeating value
+        </strong>{" "}
+        is the highest value <code>k</code> where <code>word</code> is{" "}
+        <code>k</code>-repeating in <code>sequence</code>. If <code>word</code>{" "}
+        is not a substring of <code>sequence</code>, <code>word</code>'s maximum{" "}
+        <code>k</code>-repeating value is <code>0</code>.
+      </p>
+
+      <p>
+        Given strings <code>sequence</code> and <code>word</code>, return{" "}
+        <em>
+          the{" "}
+          <strong>
+            maximum <code>k</code>-repeating value
+          </strong>{" "}
+          of <code>word</code> in <code>sequence</code>
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> sequence = "ababc", word = "ab" <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation: </strong>"abab" is a substring in "<u>abab</u>c".
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> sequence = "ababc", word = "ba" <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation: </strong>"ba" is a substring in "a<u>ba</u>bc".
+        "baba" is not a substring in "ababc".
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> sequence = "ababc", word = "ac" <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation: </strong>"ac" is not a substring in "ababc".
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= sequence.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;= word.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>sequence</code> and <code>word</code>&nbsp;contains only
+          lowercase English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
