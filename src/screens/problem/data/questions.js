8ba2e62d6ccde0c8664b52e58d4cef239e210a1a
saiashish9 <saiashish3760@gmail.com>
2024-12-30 01:08:36 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -51478,6 +51478,98 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  1136: (
+    <div class="markdown-body div-width">
+      <p>
+        There are <code>N</code> courses, labelled from 1 to <code>N</code>.
+      </p>
+
+      <p>
+        We are given&nbsp;<code>relations[i] = [X, Y]</code>, representing a
+        prerequisite relationship between course <code>X</code> and course{" "}
+        <code>Y</code>:&nbsp;course
+        <code>X</code>&nbsp;has to be studied before course <code>Y</code>.
+      </p>
+
+      <p>
+        In one semester you can study any number of courses as long as you have
+        studied all the prerequisites for the course you are studying.
+      </p>
+
+      <p>
+        Return the minimum number of semesters needed to study all
+        courses.&nbsp; If there is no way to study all the courses, return{" "}
+        <code>-1</code>.
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Example 1:</strong>
+      </p>
+
+      <p>
+        <strong>
+          <img src="https://leetcode.ca/all/img/1136_1.png" alt="" />
+        </strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>N = <span id="example-input-1-1">3</span>,
+        relations = <span id="example-input-1-2">[[1,3],[2,3]]</span>
+        <br />
+        <strong>Output: </strong>
+        <span id="example-output-1">2</span> <br />
+        <strong>Explanation: </strong>
+        <br />
+        In the first semester, courses 1 and 2 are studied. In the second
+        semester, course 3 is studied.
+      </pre>
+
+      <p>
+        <strong>Example 2:</strong>
+      </p>
+
+      <p>
+        <strong>
+          <img src="https://leetcode.ca/all/img/1136_2.png" alt="" />
+        </strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>N = <span id="example-input-2-1">3</span>,
+        relations = <span id="example-input-2-2">[[1,2],[2,3],[3,1]]</span>
+        <br />
+        <strong>Output: </strong>
+        <span id="example-output-2">-1</span>
+        <br />
+        <strong>Explanation: </strong>
+        <br />
+        No course can be studied because they depend on each other.
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>
+          <span>Note:</span>
+        </strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= N &lt;= 5000</code>
+        </li>
+        <li>
+          <code>1 &lt;= relations.length &lt;= 5000</code>
+        </li>
+        <li>
+          <code>relations[i][0] != relations[i][1]</code>
+        </li>
+        <li>There are no repeated relations in the input.</li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
