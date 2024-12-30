3a832e2acbe7baf4fa8bc438774c586ca41b3977
saiashish9 <saiashish3760@gmail.com>
2024-12-30 18:14:06 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -54067,6 +54067,56 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  258: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer <code>num</code>, repeatedly add all its digits until
+        the result has only one digit, and return it.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> num = 38 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> The process is <br />
+        38 --&gt; 3 + 8 --&gt; 11 <br />
+        11 --&gt; 1 + 1 --&gt; 2 <br />
+        Since 2 has only one digit, return it.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> num = 0 <br />
+        <strong>Output:</strong> 0
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            0 &lt;= num &lt;= 2<sup>31</sup> - 1
+          </code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong> Could you do it without any loop/recursion
+        in <code>O(1)</code> runtime?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
