756214d146e1056d0313558ce250230249918ea0
saiashish9 <saiashish3760@gmail.com>
2024-12-30 21:24:41 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -54805,6 +54805,83 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  3115: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>nums</code>.
+      </p>
+
+      <p>
+        Return an integer that is the <strong>maximum</strong> distance between
+        the <strong>indices</strong> of two (not necessarily different) prime
+        numbers in <code>nums</code>
+        <em>.</em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">nums = [4,2,9,5,3]</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">3</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong> <code>nums[1]</code>,{" "}
+          <code>nums[3]</code>, and <code>nums[4]</code> are prime. So the
+          answer is <code>|4 - 1| = 3</code>.
+        </p>
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">nums = [4,8,2,8]</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">0</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong> <code>nums[2]</code> is prime. Because
+          there is just one prime number, the answer is <code>|2 - 2| = 0</code>
+          .
+        </p>
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 3 * 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>1 &lt;= nums[i] &lt;= 100</code>
+        </li>
+        <li>
+          The input is generated such that the number of prime numbers in the{" "}
+          <code>nums</code> is at least one.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
