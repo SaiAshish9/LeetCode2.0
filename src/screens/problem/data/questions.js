da7288c4e0bcbb85cd25bc48f2ff4a6b24a9ae40
saiashish9 <saiashish3760@gmail.com>
2024-12-31 00:21:01 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -55257,6 +55257,90 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  721: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a list of <code>accounts</code> where each element{" "}
+        <code>accounts[i]</code> is a list of strings, where the first element{" "}
+        <code>accounts[i][0]</code> is a name, and the rest of the elements are{" "}
+        <strong>emails</strong> representing emails of the account.
+      </p>
+
+      <p>
+        Now, we would like to merge these accounts. Two accounts definitely
+        belong to the same person if there is some common email to both
+        accounts. Note that even if two accounts have the same name, they may
+        belong to different people as people could have the same name. A person
+        can have any number of accounts initially, but all of their accounts
+        definitely have the same name.
+      </p>
+
+      <p>
+        After merging the accounts, return the accounts in the following format:
+        the first element of each account is the name, and the rest of the
+        elements are emails <strong>in sorted order</strong>. The accounts
+        themselves can be returned in <strong>any order</strong>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> accounts =
+        [["John","johnsmith@mail.com","john_newyork@mail.com"],["John","johnsmith@mail.com","john00@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+        <br /> <strong>Output:</strong>{" "}
+        [["John","john00@mail.com","john_newyork@mail.com","johnsmith@mail.com"],["Mary","mary@mail.com"],["John","johnnybravo@mail.com"]]
+        <br /> <strong>Explanation:</strong> <br />
+        The first and second John's are the same person as they have the common
+        email "johnsmith@mail.com". <br />
+        The third John and Mary are different people as none of their email
+        addresses are used by other accounts. <br />
+        We could return these lists in any order, for example the answer
+        [['Mary', 'mary@mail.com'], <br /> ['John', 'johnnybravo@mail.com'],
+        <br /> ['John', 'john00@mail.com', 'john_newyork@mail.com',
+        'johnsmith@mail.com']] would still be accepted. <br />
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> accounts =
+        [["Gabe","Gabe0@m.co","Gabe3@m.co","Gabe1@m.co"],["Kevin","Kevin3@m.co","Kevin5@m.co","Kevin0@m.co"],{" "}
+        <br />
+        ["Ethan","Ethan5@m.co","Ethan4@m.co","Ethan0@m.co"],["Hanzo","Hanzo3@m.co","Hanzo1@m.co","Hanzo0@m.co"],["Fern","Fern5@m.co","Fern1@m.co","Fern0@m.co"]]
+        <br /> <strong>Output:</strong>{" "}
+        [["Ethan","Ethan0@m.co","Ethan4@m.co","Ethan5@m.co"], <br />
+        ["Gabe","Gabe0@m.co","Gabe1@m.co","Gabe3@m.co"],["Hanzo","Hanzo0@m.co","Hanzo1@m.co","Hanzo3@m.co"],["Kevin","Kevin0@m.co","Kevin3@m.co","Kevin5@m.co"],["Fern","Fern0@m.co","Fern1@m.co","Fern5@m.co"]]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= accounts.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>2 &lt;= accounts[i].length &lt;= 10</code>
+        </li>
+        <li>
+          <code>1 &lt;= accounts[i][j].length &lt;= 30</code>
+        </li>
+        <li>
+          <code>accounts[i][0]</code> consists of English letters.
+        </li>
+        <li>
+          <code>accounts[i][j] (for j &gt; 0)</code> is a valid email.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
