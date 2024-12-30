1a2170f18da93c04056d12b702cb2ad2c8d6c95f
saiashish9 <saiashish3760@gmail.com>
2024-12-30 17:08:04 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -53493,6 +53493,25 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  247: (
+    <div class="markdown-body div-width">
+      <p>
+        A strobogrammatic number is a number that looks the same when rotated
+        180 degrees (looked at upside down).
+      </p>
+
+      <p>Find all strobogrammatic numbers that are of length = n.</p>
+
+      <p>
+        <b>Example:</b>
+      </p>
+
+      <pre>
+        <b>Input:</b> n = 2 <br />
+        <b>Output:</b> <code>["11","69","88","96"]</code>
+      </pre>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
