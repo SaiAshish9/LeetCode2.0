87b7c107c25ae44b4bbd4e693344e1a77be2e62b
saiashish9 <saiashish3760@gmail.com>
2024-12-30 18:32:03 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -54254,6 +54254,61 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       <p>Could you solve this problem in less than O(n) complexity?</p>
     </div>
   ),
+  1952: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer <code>n</code>, return <code>true</code>
+        <em> if </em>
+        <code>n</code>
+        <em>
+          {" "}
+          has <strong>exactly three positive divisors</strong>. Otherwise,
+          return{" "}
+        </em>
+        <code>false</code>.
+      </p>
+
+      <p>
+        An integer <code>m</code> is a <strong>divisor</strong> of{" "}
+        <code>n</code> if there exists an integer <code>k</code> such that{" "}
+        <code>n = k * m</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2 <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explantion:</strong> 2 has only two divisors: 1 and 2.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 4 <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explantion:</strong> 4 has three divisors: 1, 2, and 4.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
