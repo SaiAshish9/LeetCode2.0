005722e9c7cbdd533f9fb32cc3d6f104d4b7e307
saiashish9 <saiashish3760@gmail.com>
2024-12-30 12:27:45 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -52474,6 +52474,81 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </p>
     </div>
   ),
+  234: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>head</code> of a singly linked list, return{" "}
+        <code>true</code>
+        <em> if it is a </em>
+        <span
+          data-keyword="palindrome-sequence"
+          class=" cursor-pointer relative text-dark-blue-s text-sm"
+        >
+          <div class="popover-wrapper inline-block" data-headlessui-state="">
+            <div>
+              <div
+                aria-expanded="false"
+                data-headlessui-state=""
+                id="headlessui-popover-button-:rh6:"
+              >
+                <div>
+                  <em>palindrome</em>
+                </div>
+              </div>
+              <div style="position: fixed; z-index: 40; inset: 0px auto auto 0px; transform: translate(434px, 183px);"></div>
+            </div>
+          </div>
+        </span>
+        <em> or </em>
+        <code>false</code>
+        <em> otherwise</em>.
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/03/03/pal1linked-list.jpg"
+        style={{ width: 422, height: 62 }}
+      />
+      <pre>
+        <strong>Input:</strong> head = [1,2,2,1] <br />
+        <strong>Output:</strong> true
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/03/03/pal2linked-list.jpg"
+        style={{ width: 182, height: 62 }}
+      />
+      <pre>
+        <strong>Input:</strong> head = [1,2] <br />
+        <strong>Output:</strong> false
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          The number of nodes in the list is in the range{" "}
+          <code>
+            [1, 10<sup>5</sup>]
+          </code>
+          .
+        </li>
+        <li>
+          <code>0 &lt;= Node.val &lt;= 9</code>
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <strong>Follow up:</strong> Could you do it in <code>O(n)</code> time and{" "}
+      <code>O(1)</code> space?
+    </div>
+  ),
 };
 
 export default QUESTIONS;
