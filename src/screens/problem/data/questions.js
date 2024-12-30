9ece3d40d1da253330567068f8a8820c689b3b1e
saiashish9 <saiashish3760@gmail.com>
2024-12-30 18:42:00 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -54390,6 +54390,45 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  2413: (
+    <div class="elfjS" data-track-load="description_content">
+      Given a <strong>positive</strong> integer <code>n</code>, return{" "}
+      <em>
+        the smallest positive integer that is a multiple of{" "}
+        <strong>both</strong>{" "}
+      </em>
+      <code>2</code>
+      <em> and </em>
+      <code>n</code>.<p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> n = 5 <br />
+        <strong>Output:</strong> 10 <br />
+        <strong>Explanation:</strong> The smallest multiple of both 5 and 2 is
+        10.
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> n = 6 <br />
+        <strong>Output:</strong> 6 <br />
+        <strong>Explanation:</strong> The smallest multiple of both 6 and 2 is
+        6. Note that a number is a multiple of itself.
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>1 &lt;= n &lt;= 150</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
