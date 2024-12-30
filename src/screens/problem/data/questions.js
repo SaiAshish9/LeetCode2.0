9e275eefd8316badae93a44bcc85ac6e4261a5c1
saiashish9 <saiashish3760@gmail.com>
2024-12-30 20:59:59 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -54429,6 +54429,58 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  2427: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given two positive integers <code>a</code> and <code>b</code>, return{" "}
+        <em>
+          the number of <strong>common</strong> factors of{" "}
+        </em>
+        <code>a</code>
+        <em> and </em>
+        <code>b</code>.
+      </p>
+
+      <p>
+        An integer <code>x</code> is a <strong>common factor</strong> of{" "}
+        <code>a</code> and <code>b</code> if <code>x</code> divides both{" "}
+        <code>a</code> and <code>b</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> a = 12, b = 6 <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> The common factors of 12 and 6 are 1, 2,
+        3, 6.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> a = 25, b = 30 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> The common factors of 25 and 30 are 1, 5.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= a, b &lt;= 1000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
