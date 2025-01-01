0025c1b168984fa99a1f917e92cd24f1e2801d4a
saiashish9 <saiashish3760@gmail.com>
2025-01-02 00:10:32 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -57287,6 +57287,132 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  3157: (
+    <>
+      <p>
+        Given the root of a binary tree <code>root</code> where each node has a
+        value, return the level of the tree that has the{" "}
+        <strong>minimum</strong> sum of values among all the levels (in case of
+        a tie, return the <strong>lowest</strong> level).
+      </p>
+
+      <p>
+        <strong>Note</strong> that the root of the tree is at level 1 and the
+        level of any other node is its distance from the root + 1.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">root = [50,6,2,30,80,7]</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">2</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>
+        <p>
+          <img
+            alt=""
+            src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/3100-3199/3157.Find%20the%20Level%20of%20Tree%20with%20Minimum%20Sum/images/image_2024-05-17_16-15-46.png"
+            style={{
+              padding: 10,
+              background: "rgb(255, 255, 255)",
+              borderRadius: "0.5rem",
+              width: 265,
+              height: 129,
+            }}
+          />
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">root = [36,17,10,null,null,24]</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">3</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>
+        <p>
+          <img
+            alt=""
+            src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/3100-3199/3157.Find%20the%20Level%20of%20Tree%20with%20Minimum%20Sum/images/image_2024-05-17_16-14-18.png"
+            style={{
+              padding: 10,
+              background: "rgb(255, 255, 255)",
+              borderRadius: "0.5rem",
+              width: 170,
+              height: 135,
+            }}
+          />
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">root = [5,null,5,null,5]</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">1</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>
+        <p>
+          <img
+            alt=""
+            src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/3100-3199/3157.Find%20the%20Level%20of%20Tree%20with%20Minimum%20Sum/images/image_2024-05-19_19-07-20.png"
+            style="padding: 10px; background: rgb(255, 255, 255); border-radius: 0.5rem; width: 170px; height: 135px;"
+          />
+        </p>
+      </div>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range{" "}
+          <code>
+            [1, 10<sup>5</sup>]
+          </code>
+          .
+        </li>
+        <li>
+          <code>
+            1 &lt;= Node.val &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </>
+  ),
 };
 
 export default QUESTIONS;
