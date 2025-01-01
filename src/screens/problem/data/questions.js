016cee68c39661df201ad87718e74129d8fd2e2f
saiashish9 <saiashish3760@gmail.com>
2025-01-01 23:54:55 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -57046,6 +57046,114 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  1631: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are a hiker preparing for an upcoming hike. You are given{" "}
+        <code>heights</code>, a 2D array of size <code>rows x columns</code>,
+        where <code>heights[row][col]</code> represents the height of cell{" "}
+        <code>(row, col)</code>. You are situated in the top-left cell,{" "}
+        <code>(0, 0)</code>, and you hope to travel to the bottom-right cell,{" "}
+        <code>(rows-1, columns-1)</code> (i.e.,&nbsp;<strong>0-indexed</strong>
+        ). You can move <strong>up</strong>, <strong>down</strong>,{" "}
+        <strong>left</strong>, or <strong>right</strong>, and you wish to find a
+        route that requires the minimum <strong>effort</strong>.
+      </p>
+
+      <p>
+        A route's <strong>effort</strong> is the{" "}
+        <strong>maximum absolute difference</strong>
+        <strong> </strong>in heights between two consecutive cells of the route.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the minimum <strong>effort</strong> required to travel from the
+          top-left cell to the bottom-right cell.
+        </em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2020/10/04/ex1.png"
+          style={{ width: 300, height: 300 }}
+        />{" "}
+      </p>
+
+      <pre>
+        <strong>Input:</strong> heights = [[1,2,2],[3,8,2],[5,3,5]] <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> The route of [1,3,5,3,5] has a maximum
+        absolute difference of 2 in consecutive cells. <br />
+        This is better than the route of [1,2,2,2,5], where the maximum absolute
+        difference is 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2020/10/04/ex2.png"
+          style={{ width: 300, height: 300 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input:</strong> heights = [[1,2,3],[3,8,4],[5,3,5]] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> The route of [1,2,3,4,5] has a maximum
+        absolute difference of 1 in consecutive cells, which is better than
+        route [1,3,5,3,5].
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/10/04/ex3.png"
+        style={{ width: 300, height: 300 }}
+      />
+      <pre>
+        <strong>Input:</strong> heights =
+        [[1,2,1,1,1],[1,2,1,2,1],[1,2,1,2,1],[1,2,1,2,1],[1,1,1,2,1]] <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> This route does not require any effort.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>rows == heights.length</code>
+        </li>
+        <li>
+          <code>columns == heights[i].length</code>
+        </li>
+        <li>
+          <code>1 &lt;= rows, columns &lt;= 100</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= heights[i][j] &lt;= 10<sup>6</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
