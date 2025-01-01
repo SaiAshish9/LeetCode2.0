dc0b93892b22a95dae47861b5e58e6787942d0e0
saiashish9 <saiashish3760@gmail.com>
2025-01-01 22:13:52 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -56497,6 +56497,64 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  226: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary tree, invert the tree, and
+        return <em>its root</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/03/14/invert1-tree.jpg"
+        style={{ width: 500, height: 165 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [4,2,7,1,3,6,9] <br />
+        <strong>Output:</strong> [4,7,2,9,6,3,1]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/03/14/invert2-tree.jpg"
+        style={{ width: 500, height: 120 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [2,1,3] <br />
+        <strong>Output:</strong> [2,3,1]
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [] <br />
+        <strong>Output:</strong> []
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range <code>[0, 100]</code>.
+        </li>
+        <li>
+          <code>-100 &lt;= Node.val &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
