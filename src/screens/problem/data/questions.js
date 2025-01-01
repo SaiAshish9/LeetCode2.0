7a9d5b8a4592f2d008f5251da400479f188c8f14
saiashish9 <saiashish3760@gmail.com>
2025-01-01 22:21:51 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -56555,6 +56555,59 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  286: (
+    <div class="markdown-body div-width">
+      <p>
+        You are given a <i>m x n</i> 2D grid initialized with these three
+        possible values.
+      </p>
+
+      <ul>
+        <li>
+          <code>-1</code> - A wall or an obstacle.
+        </li>
+        <li>
+          <code>0</code> - A gate.
+        </li>
+        <li>
+          <code>INF</code> - Infinity means an empty room. We use the value{" "}
+          <code>
+            2<sup>31</sup> - 1 = 2147483647
+          </code>{" "}
+          to represent <code>INF</code> as you may assume that the distance to a
+          gate is less than <code>2147483647</code>.
+        </li>
+      </ul>
+
+      <p>
+        Fill each empty room with the distance to its <i>nearest</i> gate. If it
+        is impossible to reach a gate, it should be filled with <code>INF</code>
+        .
+      </p>
+
+      <p>
+        <strong>Example:&nbsp;</strong>
+      </p>
+
+      <p>Given the 2D grid:</p>
+
+      <pre>
+        {`INF  -1  0  INF
+INF INF INF  -1
+INF  -1 INF  -1
+  0  -1 INF INF`}
+      </pre>
+
+      <p>After running your function, the 2D grid should be:</p>
+
+      <pre>
+        {`  3  -1   0   1
+  2   2   1  -1
+  1  -1   2  -1
+  0  -1   3   4`}
+      </pre>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
