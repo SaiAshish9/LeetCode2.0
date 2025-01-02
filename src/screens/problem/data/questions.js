7721302ece530af213ec29bf6d29724bafa9808f
saiashish9 <saiashish3760@gmail.com>
2025-01-02 12:18:48 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -57823,6 +57823,97 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  1140: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Alice and Bob continue their games with piles of stones. There are a
+        number of piles <strong>arranged in a row</strong>, and each pile has a
+        positive integer number of stones <code>piles[i]</code>. The objective
+        of the game is to end with the most stones.
+      </p>
+
+      <p>Alice and Bob take turns, with Alice starting first.</p>
+
+      <p>
+        On each player's turn, that player can take{" "}
+        <strong>all the stones</strong> in the <strong>first</strong>{" "}
+        <code>X</code> remaining piles, where <code>1 &lt;= X &lt;= 2M</code>.
+        Then, we set <code>M = max(M, X)</code>. Initially, M = 1.
+      </p>
+
+      <p>The game continues until all the stones have been taken.</p>
+
+      <p>
+        Assuming Alice and Bob play optimally, return the maximum number of
+        stones Alice can get.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">piles = [2,7,9,4,4]</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">10</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>
+        <ul>
+          <li>
+            If Alice takes one pile at the beginning, Bob takes two piles, then
+            Alice takes 2 piles again. Alice can get <code>2 + 4 + 4 = 10</code>{" "}
+            stones in total.
+          </li>
+          <li>
+            If Alice takes two piles at the beginning, then Bob can take all
+            three piles left. In this case, Alice get <code>2 + 7 = 9</code>{" "}
+            stones in total.
+          </li>
+        </ul>
+        <br />
+        <p>So we return 10 since it's larger.</p>
+      </div>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">piles = [1,2,3,4,5,100]</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">104</span>
+        </p>
+      </div>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= piles.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= piles[i]&nbsp;&lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
