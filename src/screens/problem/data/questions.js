7912a320bb72e7c314b1271d563b730b7083df98
saiashish9 <saiashish3760@gmail.com>
2025-01-02 14:02:54 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -58502,6 +58502,142 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  2038: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        There are <code>n</code> pieces arranged in a line, and each piece is
+        colored either by <code>'A'</code> or by <code>'B'</code>. You are given
+        a string <code>colors</code> of length <code>n</code> where{" "}
+        <code>colors[i]</code> is the color of the{" "}
+        <code>
+          i<sup>th</sup>
+        </code>{" "}
+        piece.
+      </p>
+
+      <p>
+        Alice and Bob are playing a game where they take{" "}
+        <strong>alternating turns</strong> removing pieces from the line. In
+        this game, Alice moves<strong> first</strong>.
+      </p>
+
+      <ul>
+        <li>
+          Alice is only allowed to remove a piece colored <code>'A'</code> if{" "}
+          <strong>both its neighbors</strong> are also colored <code>'A'</code>.
+          She is <strong>not allowed</strong> to remove pieces that are colored{" "}
+          <code>'B'</code>.
+        </li>
+        <li>
+          Bob is only allowed to remove a piece colored <code>'B'</code> if{" "}
+          <strong>both its neighbors</strong> are also colored <code>'B'</code>.
+          He is <strong>not allowed</strong> to remove pieces that are colored{" "}
+          <code>'A'</code>.
+        </li>
+        <li>
+          Alice and Bob <strong>cannot</strong> remove pieces from the edge of
+          the line.
+        </li>
+        <li>
+          If a player cannot make a move on their turn, that player{" "}
+          <strong>loses</strong> and the other player <strong>wins</strong>.
+        </li>
+      </ul>
+
+      <p>
+        Assuming Alice and Bob play optimally, return <code>true</code>
+        <em> if Alice wins, or return </em>
+        <code>false</code>
+        <em> if Bob wins</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> colors = "AAABABB" <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> <br />A<u>A</u>ABABB -&gt; AABABB
+        <br />
+        Alice moves first.
+        <br />
+        She removes the second 'A' from the left since that is the only 'A'
+        whose neighbors are both 'A'.
+        <br />
+        Now it's Bob's turn.
+        <br />
+        Bob cannot make a move on his turn since there are no 'B's whose
+        neighbors are both 'B'.
+        <br />
+        Thus, Alice wins, so return true.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> colors = "AA"
+        <br />
+        <strong>Output:</strong> false
+        <br />
+        <strong>Explanation:</strong>
+        <br />
+        Alice has her turn first.
+        <br />
+        There are only two 'A's and both are on the edge of the line, so she
+        cannot move on her turn.
+        <br />
+        Thus, Bob wins, so return false.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> colors = "ABBBBBBBAAA"
+        <br />
+        <strong>Output:</strong> false
+        <br />
+        <strong>Explanation:</strong>
+        <br />
+        ABBBBBBBA<u>A</u>A -&gt; ABBBBBBBAA
+        <br />
+        Alice moves first.
+        <br />
+        Her only option is to remove the second to last 'A' from the right.
+        <br />
+        ABBBB<u>B</u>BBAA -&gt; ABBBBBBAA Next is Bob's turn.
+        <br />
+        He has many options for which 'B' piece to remove. He can pick any.
+        <br />
+        <br />
+        On Alice's second turn, she has no more pieces that she can remove.
+        <br />
+        Thus, Bob wins, so return false.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;=&nbsp;colors.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>colors</code>&nbsp;consists of only the letters&nbsp;
+          <code>'A'</code>&nbsp;and&nbsp;<code>'B'</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
