9bc56f6f75ec99f4a179b1cb0834bb1b245b6db1
saiashish9 <saiashish3760@gmail.com>
2025-01-02 12:24:38 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -58015,6 +58015,73 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  1510: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>Alice and Bob take turns playing a game, with Alice starting first.</p>
+
+      <p>
+        Initially, there are <code>n</code> stones in a pile. On each player's
+        turn, that player makes a <em>move</em> consisting of removing{" "}
+        <strong>any</strong> non-zero <strong>square number</strong> of stones
+        in the pile.
+      </p>
+
+      <p>Also, if a player cannot make a move, he/she loses the game.</p>
+
+      <p>
+        Given a positive integer <code>n</code>, return <code>true</code> if and
+        only if Alice wins the game otherwise return <code>false</code>,
+        assuming both players play optimally.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 1 <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation: </strong>Alice can remove 1 stone winning the game
+        because Bob doesn't have any moves.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2 <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation: </strong>Alice can only remove 1 stone, after that
+        Bob removes the last one winning the game (2 -&gt; 1 -&gt; 0).
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 4 <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> n is already a perfect square, Alice can
+        win with one move, removing 4 stones (4 -&gt; 0).
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
