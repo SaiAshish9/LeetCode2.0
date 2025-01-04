12ff222c1638749cf2a469d32600e3a87073fb85
saiashish9 <saiashish3760@gmail.com>
2025-01-04 14:36:38 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -58947,6 +58947,76 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  1726: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array <code>nums</code> of <strong>distinct</strong> positive
+        integers, return <em>the number of tuples </em>
+        <code>(a, b, c, d)</code>
+        <em> such that </em>
+        <code>a * b = c * d</code>
+        <em> where </em>
+        <code>a</code>
+        <em>, </em>
+        <code>b</code>
+        <em>, </em>
+        <code>c</code>
+        <em>, and </em>
+        <code>d</code>
+        <em> are elements of </em>
+        <code>nums</code>
+        <em>, and </em>
+        <code>a != b != c != d</code>
+        <em>.</em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [2,3,4,6] <br />
+        <strong>Output:</strong> 8 <br />
+        <strong>Explanation:</strong> There are 8 valid tuples: <br />
+        (2,6,3,4) , (2,6,4,3) , (6,2,3,4) , (6,2,4,3) <br />
+        (3,4,2,6) , (4,3,2,6) , (3,4,6,2) , (4,3,6,2)
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,4,5,10] <br />
+        <strong>Output:</strong> 16 <br />
+        <strong>Explanation:</strong> There are 16 valid tuples: <br />
+        (1,10,2,5) , (1,10,5,2) , (10,1,2,5) , (10,1,5,2) <br />
+        (2,5,1,10) , (2,5,10,1) , (5,2,1,10) , (5,2,10,1) <br />
+        (2,10,4,5) , (2,10,5,4) , (10,2,4,5) , (10,2,5,4) <br />
+        (4,5,2,10) , (4,5,10,2) , (5,4,2,10) , (5,4,10,2)
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= nums[i] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          All elements in <code>nums</code> are <strong>distinct</strong>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
