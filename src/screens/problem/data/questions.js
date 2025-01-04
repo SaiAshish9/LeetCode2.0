75cae02c8bc19310ffef3c1cb0d24639836d22fa
saiashish9 <saiashish3760@gmail.com>
2025-01-04 14:33:25 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -58886,6 +58886,67 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  1512: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array of integers <code>nums</code>, return{" "}
+        <em>
+          the number of <strong>good pairs</strong>
+        </em>
+        .
+      </p>
+
+      <p>
+        A pair <code>(i, j)</code> is called <em>good</em> if{" "}
+        <code>nums[i] == nums[j]</code> and <code>i</code> &lt; <code>j</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,1,1,3] <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> There are 4 good pairs (0,3), (0,4),
+        (3,4), (2,5) 0-indexed.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,1,1] <br />
+        <strong>Output:</strong> 6 <br />
+        <strong>Explanation:</strong> Each pair in the array are <em>good</em>.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3] <br />
+        <strong>Output:</strong> 0
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;= nums[i] &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
