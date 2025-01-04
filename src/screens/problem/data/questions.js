6cebf55769df98d8ffa20ddbaaf0f9bb086f33bc
saiashish9 <saiashish3760@gmail.com>
2025-01-04 15:31:29 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -59341,6 +59341,103 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  2068: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Two strings <code>word1</code> and <code>word2</code> are considered{" "}
+        <strong>almost equivalent</strong> if the differences between the
+        frequencies of each letter from <code>'a'</code> to <code>'z'</code>{" "}
+        between <code>word1</code> and <code>word2</code> is{" "}
+        <strong>at most</strong> <code>3</code>.
+      </p>
+
+      <p>
+        Given two strings <code>word1</code> and <code>word2</code>, each of
+        length <code>n</code>, return <code>true</code> <em>if </em>
+        <code>word1</code> <em>and</em> <code>word2</code>{" "}
+        <em>
+          are <strong>almost equivalent</strong>, or
+        </em>{" "}
+        <code>false</code> <em>otherwise</em>.
+      </p>
+
+      <p>
+        The <strong>frequency</strong> of a letter <code>x</code> is the number
+        of times it occurs in the string.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> word1 = "aaaa", word2 = "bccb" <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> There are 4 'a's in "aaaa" but 0 'a's in
+        "bccb". <br />
+        The difference is 4, which is more than the allowed 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> word1 = "abcdeef", word2 = "abaaacc" <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> The differences between the frequencies of
+        each letter in word1 and word2 are at most 3: <br />
+        - 'a' appears 1 time in word1 and 4 times in word2. The difference is 3.{" "}
+        <br />
+        - 'b' appears 1 time in word1 and 1 time in word2. The difference is 0.{" "}
+        <br />
+        - 'c' appears 1 time in word1 and 2 times in word2. The difference is 1.{" "}
+        <br />
+        - 'd' appears 1 time in word1 and 0 times in word2. The difference is 1.{" "}
+        <br />
+        - 'e' appears 2 times in word1 and 0 times in word2. The difference is
+        2. <br />- 'f' appears 1 time in word1 and 0 times in word2. The
+        difference is 1.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> word1 = "cccddabba", word2 = "babababab" <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> The differences between the frequencies of
+        each letter in word1 and word2 are at most 3: <br />
+        - 'a' appears 2 times in word1 and 4 times in word2. The difference is
+        2. <br />
+        - 'b' appears 2 times in word1 and 5 times in word2. The difference is
+        3. <br />
+        - 'c' appears 3 times in word1 and 0 times in word2. The difference is
+        3. <br />- 'd' appears 2 times in word1 and 0 times in word2. The
+        difference is 2.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == word1.length == word2.length</code>
+        </li>
+        <li>
+          <code>1 &lt;= n &lt;= 100</code>
+        </li>
+        <li>
+          <code>word1</code> and <code>word2</code> consist only of lowercase
+          English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
