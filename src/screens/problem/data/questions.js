d42adb116bc54cbb74bdf029d14cd1c3a618aa99
saiashish9 <saiashish3760@gmail.com>
2025-01-04 14:44:45 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -59017,6 +59017,71 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  1748: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>nums</code>. The unique elements of
+        an array are the elements that appear <strong>exactly once</strong> in
+        the array.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>sum</strong> of all the unique elements of{" "}
+        </em>
+        <code>nums</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,2] <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> The unique elements are [1,3], and the sum
+        is 4.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,1,1,1] <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> There are no unique elements, and the sum
+        is 0.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,4,5] <br />
+        <strong>Output:</strong> 15 <br />
+        <strong>Explanation:</strong> The unique elements are [1,2,3,4,5], and
+        the sum is 15.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;= nums[i] &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
