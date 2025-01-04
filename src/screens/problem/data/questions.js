20d799ba14ca3f798a5e12dc2240b11ffc254336
saiashish9 <saiashish3760@gmail.com>
2025-01-05 00:40:47 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -61167,6 +61167,126 @@ INF  -1 INF  -1
       </ul>
     </article>
   ),
+  2577: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <code>m x n</code> matrix <code>grid</code> consisting
+        of <b>non-negative</b> integers where <code>grid[row][col]</code>{" "}
+        represents the <strong>minimum</strong> time required to be able to
+        visit the cell <code>(row, col)</code>, which means you can visit the
+        cell <code>(row, col)</code> only when the time you visit it is greater
+        than or equal to <code>grid[row][col]</code>.
+      </p>
+
+      <p>
+        You are standing in the <strong>top-left</strong> cell of the matrix in
+        the{" "}
+        <code>
+          0<sup>th</sup>
+        </code>{" "}
+        second, and you must move to <strong>any</strong> adjacent cell in the
+        four directions: up, down, left, and right. Each move you make takes 1
+        second.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>minimum</strong> time required in which you can visit the
+          bottom-right cell of the matrix
+        </em>
+        . If you cannot visit the bottom-right cell, then return <code>-1</code>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2023/02/14/yetgriddrawio-8.png"
+        />
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid = [[0,1,3,2],[5,1,2,5],[4,3,8,6]] <br />
+        <strong>Output:</strong> 7 <br />
+        <strong>Explanation:</strong> One of the paths that we can take is the
+        following: <br />
+        - at t = 0, we are on the cell (0,0). <br />
+        - at t = 1, we move to the cell (0,1). It is possible because grid[0][1]
+        &lt;= 1. <br />
+        - at t = 2, we move to the cell (1,1). It is possible because grid[1][1]
+        &lt;= 2. <br />
+        - at t = 3, we move to the cell (1,2). It is possible because grid[1][2]
+        &lt;= 3. <br />
+        - at t = 4, we move to the cell (1,1). It is possible because grid[1][1]
+        &lt;= 4. <br />
+        - at t = 5, we move to the cell (1,2). It is possible because grid[1][2]
+        &lt;= 5. <br />
+        - at t = 6, we move to the cell (1,3). It is possible because grid[1][3]
+        &lt;= 6. <br />
+        - at t = 7, we move to the cell (2,3). It is possible because grid[2][3]
+        &lt;= 7. <br />
+        The final time is 7. It can be shown that it is the minimum time
+        possible.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2023/02/14/yetgriddrawio-9.png"
+          style={{ width: 151, height: 151 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid = [[0,2,4],[3,2,1],[1,0,4]] <br />
+        <strong>Output:</strong> -1 <br />
+        <strong>Explanation:</strong> There is no path from the top left to the
+        bottom-right cell.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>m == grid.length</code>
+        </li>
+        <li>
+          <code>n == grid[i].length</code>
+        </li>
+        <li>
+          <code>2 &lt;= m, n &lt;= 1000</code>
+        </li>
+        <li>
+          <code>
+            4 &lt;= m * n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= grid[i][j] &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>grid[0][0] == 0</code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
