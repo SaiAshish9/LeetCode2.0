297a2164551e9b7731363a340197552848db8d1d
saiashish9 <saiashish3760@gmail.com>
2025-01-05 12:06:59 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -62077,7 +62077,7 @@ INF  -1 INF  -1
       </p>
       <pre>
         <strong>Input:</strong> nums = [-1,-2,-3] <br />
-        <strong>Output:</strong> -6 
+        <strong>Output:</strong> -6
       </pre>
       <p>&nbsp;</p>
       <p>
@@ -62092,6 +62092,67 @@ INF  -1 INF  -1
         </li>
         <li>
           <code>-1000 &lt;= nums[i] &lt;= 1000</code>
+        </li>
+      </ul>
+    </div>
+  ),
+  747: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>nums</code> where the largest
+        integer is <strong>unique</strong>.
+      </p>
+
+      <p>
+        Determine whether the largest element in the array is{" "}
+        <strong>at least twice</strong> as much as every other number in the
+        array. If it is, return{" "}
+        <em>
+          the <strong>index</strong> of the largest element, or return{" "}
+        </em>
+        <code>-1</code>
+        <em> otherwise</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [3,6,1,0] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> 6 is the largest integer. <br />
+        For every other number in the array x, 6 is at least twice as big as x.{" "}
+        <br />
+        The index of value 6 is 1, so we return 1.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,4] <br />
+        <strong>Output:</strong> -1 <br />
+        <strong>Explanation:</strong> 4 is less than twice the value of 3, so we
+        return -1.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>2 &lt;= nums.length &lt;= 50</code>
+        </li>
+        <li>
+          <code>0 &lt;= nums[i] &lt;= 100</code>
+        </li>
+        <li>
+          The largest element in <code>nums</code> is unique.
         </li>
       </ul>
     </div>
