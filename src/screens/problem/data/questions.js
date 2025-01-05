33a56e24820fa00a39ef0312d2f9c21a6bc0aab7
saiashish9 <saiashish3760@gmail.com>
2025-01-05 12:46:23 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -62247,6 +62247,119 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  791: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given two strings <code>order</code> and <code>s</code>. All the
+        characters of <code>order</code> are <strong>unique</strong> and were
+        sorted in some custom order previously.
+      </p>
+
+      <p>
+        Permute the characters of <code>s</code> so that they match the order
+        that <code>order</code> was sorted. More specifically, if a character{" "}
+        <code>x</code> occurs before a character <code>y</code> in{" "}
+        <code>order</code>, then <code>x</code> should occur before{" "}
+        <code>y</code> in the permuted string.
+      </p>
+
+      <p>
+        Return <em>any permutation of </em>
+        <code>s</code>
+        <em> that satisfies this property</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input: </strong>{" "}
+          <span class="example-io"> order = "cba", s = "abcd" </span>
+        </p>
+        <br />
+        <p>
+          <strong>Output: </strong> <span class="example-io"> "cbad" </span>
+        </p>
+        <br />
+
+        <p>
+          <strong>Explanation: </strong> <code>"a"</code>, <code>"b"</code>,{" "}
+          <code>"c"</code> appear in order, so the order of <code>"a"</code>,{" "}
+          <code>"b"</code>, <code>"c"</code> should be <code>"c"</code>,{" "}
+          <code>"b"</code>, and <code>"a"</code>.
+        </p>
+        <br />
+
+        <p>
+          Since <code>"d"</code> does not appear in <code>order</code>, it can
+          be at any position in the returned string. <code>"dcba"</code>,{" "}
+          <code>"cdba"</code>, <code>"cbda"</code> are also valid outputs.
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input: </strong>{" "}
+          <span class="example-io"> order = "bcafg", s = "abcd" </span>
+        </p>
+        <br />
+
+        <p>
+          <strong>Output: </strong> <span class="example-io"> "bcad" </span>
+        </p>
+        <br />
+
+        <p>
+          <strong>Explanation: </strong> The characters <code>"b"</code>,{" "}
+          <code>"c"</code>, and <code>"a"</code> from <code>order</code> dictate
+          the order for the characters in <code>s</code>. The character{" "}
+          <code>"d"</code> in <code>s</code> does not appear in{" "}
+          <code>order</code>, so its position is flexible.
+        </p>
+        <br />
+
+        <p>
+          Following the order of appearance in <code>order</code>,{" "}
+          <code>"b"</code>, <code>"c"</code>, and <code>"a"</code> from{" "}
+          <code>s</code> should be arranged as <code>"b"</code>,{" "}
+          <code>"c"</code>, <code>"a"</code>. <code>"d"</code> can be placed at
+          any position since it's not in order. The output <code>"bcad"</code>{" "}
+          correctly follows this rule. Other arrangements like{" "}
+          <code>"dbca"</code> or <code>"bcda"</code> would also be valid, as
+          long as <code>"b"</code>, <code>"c"</code>, <code>"a"</code> maintain
+          their order.
+        </p>
+      </div>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= order.length &lt;= 26</code>
+        </li>
+        <li>
+          <code>1 &lt;= s.length &lt;= 200</code>
+        </li>
+        <li>
+          <code>order</code> and <code>s</code> consist of lowercase English
+          letters.
+        </li>
+        <li>
+          All the characters of <code>order</code> are <strong>unique</strong>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
