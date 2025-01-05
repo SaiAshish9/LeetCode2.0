9e478fb498beefe39001baff57b37ce23fdc03dd
saiashish9 <saiashish3760@gmail.com>
2025-01-06 01:05:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -63656,6 +63656,169 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  2164: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <strong>0-indexed</strong> integer array{" "}
+        <code>nums</code>. Rearrange the values of <code>nums</code> according
+        to the following rules:
+      </p>
+
+      <ul>
+        <li>
+          Sort the values at <strong>odd indices</strong> of <code>nums</code>{" "}
+          in <strong>non-increasing</strong> order.
+          <ul>
+            <li>
+              For example, if{" "}
+              <code>
+                nums = [4,
+                <strong>
+                  <u>1</u>
+                </strong>
+                ,2,
+                <u>
+                  <strong>3</strong>
+                </u>
+                ]
+              </code>{" "}
+              before this step, it becomes{" "}
+              <code>
+                [4,
+                <u>
+                  <strong>3</strong>
+                </u>
+                ,2,
+                <strong>
+                  <u>1</u>
+                </strong>
+                ]
+              </code>{" "}
+              after. The values at odd indices <code>1</code> and <code>3</code>{" "}
+              are sorted in non-increasing order.
+            </li>
+          </ul>
+        </li>
+        <li>
+          Sort the values at <strong>even indices</strong> of <code>nums</code>{" "}
+          in <strong>non-decreasing</strong> order.
+          <ul>
+            <li>
+              For example, if{" "}
+              <code>
+                nums = [
+                <u>
+                  <strong>4</strong>
+                </u>
+                ,1,
+                <u>
+                  <strong>2</strong>
+                </u>
+                ,3]
+              </code>{" "}
+              before this step, it becomes{" "}
+              <code>
+                [
+                <u>
+                  <strong>2</strong>
+                </u>
+                ,1,
+                <u>
+                  <strong>4</strong>
+                </u>
+                ,3]
+              </code>{" "}
+              after. The values at even indices <code>0</code> and{" "}
+              <code>2</code> are sorted in non-decreasing order.
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+      <p>
+        Return <em>the array formed after rearranging the values of</em>{" "}
+        <code>nums</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [4,1,2,3]
+        <br /> <strong>Output:</strong> [2,3,4,1]
+        <br /> <strong>Explanation:</strong>
+        <br /> First, we sort the values present at odd indices (1 and 3) in
+        non-increasing order.
+        <br /> So, nums changes from [4,
+        <strong>
+          <u>1</u>
+        </strong>
+        ,2,
+        <strong>
+          <u>3</u>
+        </strong>
+        ] to [4,
+        <u>
+          <strong>3</strong>
+        </u>
+        ,2,
+        <strong>
+          <u>1</u>
+        </strong>
+        ].
+        <br /> Next, we sort the values present at even indices (0 and 2) in
+        non-decreasing order.
+        <br /> So, nums changes from [
+        <u>
+          <strong>4</strong>
+        </u>
+        ,1,
+        <strong>
+          <u>2</u>
+        </strong>
+        ,3] to [
+        <u>
+          <strong>2</strong>
+        </u>
+        ,3,
+        <u>
+          <strong>4</strong>
+        </u>
+        ,1].
+        <br /> Thus, the array formed after rearranging the values is [2,3,4,1].
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [2,1]
+        <br /> <strong>Output:</strong> [2,1] <br />
+        <strong>Explanation:</strong> <br />
+        <br /> Since there is exactly one odd index and one even index, no
+        rearrangement of values takes place. <br />
+        The resultant array formed is [2,1], which is the same as the initial
+        array.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;= nums[i] &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
