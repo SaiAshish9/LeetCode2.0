a89a2d04cfd0e33fcb1eee3ead084b883d80364e
saiashish9 <saiashish3760@gmail.com>
2025-01-05 12:04:11 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -62046,6 +62046,56 @@ INF  -1 INF  -1
       <strong>Follow up:</strong> Can you find an <code>O(n)</code> solution?
     </div>
   ),
+  628: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code>,{" "}
+        <em>
+          find three numbers whose product is maximum and return the maximum
+          product
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3] <br />
+        <strong>Output:</strong> 6
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,4] <br />
+        <strong>Output:</strong> 24
+      </pre>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> nums = [-1,-2,-3] <br />
+        <strong>Output:</strong> -6 
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            3 &lt;= nums.length &lt;=&nbsp;10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>-1000 &lt;= nums[i] &lt;= 1000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
