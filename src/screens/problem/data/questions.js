24443d66b7dac9230e2f71a7ab38833bdb49e8a9
saiashish9 <saiashish3760@gmail.com>
2025-01-05 12:11:21 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -62157,6 +62157,96 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  757: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a 2D integer array <code>intervals</code> where{" "}
+        <code>
+          intervals[i] = [start<sub>i</sub>, end<sub>i</sub>]
+        </code>{" "}
+        represents all the integers from{" "}
+        <code>
+          start<sub>i</sub>
+        </code>{" "}
+        to{" "}
+        <code>
+          end<sub>i</sub>
+        </code>{" "}
+        inclusively.
+      </p>
+
+      <p>
+        A <strong>containing set</strong> is an array <code>nums</code> where
+        each interval from <code>intervals</code> has{" "}
+        <strong>at least two</strong> integers in <code>nums</code>.
+      </p>
+
+      <ul>
+        <li>
+          For example, if <code>intervals = [[1,3], [3,7], [8,9]]</code>, then{" "}
+          <code>[1,2,4,7,8,9]</code> and <code>[2,3,4,8,9]</code> are{" "}
+          <strong>containing sets</strong>.
+        </li>
+      </ul>
+
+      <p>
+        Return <em>the minimum possible size of a containing set</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> intervals = [[1,3],[3,7],[8,9]] <br />
+        <strong>Output:</strong> 5 <br />
+        <strong>Explanation:</strong> let nums = [2, 3, 4, 8, 9]. <br />
+        It can be shown that there cannot be any containing array of size 4.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> intervals = [[1,3],[1,4],[2,5],[3,5]] <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> let nums = [2, 3, 4]. <br />
+        It can be shown that there cannot be any containing array of size 2.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> intervals = [[1,2],[2,3],[2,4],[4,5]] <br />
+        <strong>Output:</strong> 5 <br />
+        <strong>Explanation:</strong> let nums = [1, 2, 3, 4, 5]. <br />
+        It can be shown that there cannot be any containing array of size 4.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= intervals.length &lt;= 3000</code>
+        </li>
+        <li>
+          <code>intervals[i].length == 2</code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= start<sub>i</sub> &lt; end<sub>i</sub> &lt;= 10<sup>8</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
