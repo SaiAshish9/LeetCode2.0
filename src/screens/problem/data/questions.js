138c427fd822112ba38e05286a3104b1b6d1dbf7
saiashish9 <saiashish3760@gmail.com>
2025-01-05 19:41:11 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -63409,6 +63409,82 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  1619: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>arr</code>, return{" "}
+        <em>
+          the mean of the remaining integers after removing the smallest{" "}
+          <code>5%</code> and the largest <code>5%</code> of the elements.
+        </em>
+      </p>
+
+      <p>
+        Answers within{" "}
+        <code>
+          10<sup>-5</sup>
+        </code>{" "}
+        of the <strong>actual answer</strong> will be considered accepted.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3]{" "}
+        <br />
+        <strong>Output:</strong> 2.00000 <br />
+        <strong>Explanation:</strong> After erasing the minimum and the maximum
+        values of this array, all elements are equal to 2, so the mean is 2.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [6,2,7,5,1,2,0,3,10,2,5,0,5,5,0,8,7,6,8,0]{" "}
+        <br />
+        <strong>Output:</strong> 4.00000
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr =
+        [6,0,7,0,7,5,7,8,3,4,0,7,8,1,6,8,1,1,2,4,8,1,9,5,4,3,8,5,10,8,6,6,1,0,6,10,8,2,3,4]{" "}
+        <br />
+        <strong>Output:</strong> 4.77778
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>20 &lt;= arr.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>arr.length</code>
+          <b> </b>
+          <strong>is a multiple</strong> of <code>20</code>.
+        </li>
+        <li>
+          <code>
+            <font face="monospace">
+              0 &lt;= arr[i] &lt;= 10<sup>5</sup>
+            </font>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
