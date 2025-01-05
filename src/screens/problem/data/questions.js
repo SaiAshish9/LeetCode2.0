edf143794cf41ffe9b1248527d65c339b847f02f
saiashish9 <saiashish3760@gmail.com>
2025-01-05 11:57:58 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -61945,6 +61945,40 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  252: (
+    <div class="markdown-body div-width">
+      <p>
+        Given an array of meeting time intervals consisting of start and end
+        times <code>[[s1,e1],[s2,e2],...]</code>
+        (s<sub>i</sub> &lt; e<sub>i</sub>), determine if a person could attend
+        all meetings.
+      </p>
+
+      <p>
+        <b>Example 1:</b>
+      </p>
+
+      <pre>
+        <b>Input:</b> <code>[[0,30],[5,10],[15,20]]</code> <br />
+        <b>Output:</b> false
+      </pre>
+
+      <p>
+        <b>Example 2:</b>
+      </p>
+
+      <pre>
+        <b>Input:</b> [[7,10],[2,4]] <br />
+        <b>Output:</b> true
+      </pre>
+
+      <p>
+        <strong>NOTE:</strong>&nbsp;input types have been changed on April 15,
+        2019. Please reset to default code definition to get new method
+        signature.
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
