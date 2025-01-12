950383f89b9d7a7a877cdddb9311148e816f693a
saiashish9 <saiashish3760@gmail.com>
2025-01-12 16:45:40 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -63919,6 +63919,81 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  66: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <strong>large integer</strong> represented as an integer
+        array <code>digits</code>, where each <code>digits[i]</code> is the{" "}
+        <code>
+          i<sup>th</sup>
+        </code>{" "}
+        digit of the integer. The digits are ordered from most significant to
+        least significant in left-to-right order. The large integer does not
+        contain any leading <code>0</code>'s.
+      </p>
+
+      <p>
+        Increment the large integer by one and return{" "}
+        <em>the resulting array of digits</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> digits = [1,2,3] <br />
+        <strong>Output:</strong> [1,2,4] <br />
+        <strong>Explanation:</strong> The array represents the integer 123.{" "}
+        <br />
+        Incrementing by one gives 123 + 1 = 124. <br />
+        Thus, the result should be [1,2,4].
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> digits = [4,3,2,1] <br />
+        <strong>Output:</strong> [4,3,2,2] <br />
+        <strong>Explanation:</strong> The array represents the integer 4321.{" "}
+        <br />
+        Incrementing by one gives 4321 + 1 = 4322. <br />
+        Thus, the result should be [4,3,2,2].
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> digits = [9] <br />
+        <strong>Output:</strong> [1,0] <br />
+        <strong>Explanation:</strong> The array represents the integer 9. <br />
+        Incrementing by one gives 9 + 1 = 10. <br />
+        Thus, the result should be [1,0].
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= digits.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>0 &lt;= digits[i] &lt;= 9</code>
+        </li>
+        <li>
+          <code>digits</code> does not contain any leading <code>0</code>'s.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
