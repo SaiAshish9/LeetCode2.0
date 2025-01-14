426dfa6209fdc6e7999804d3b3d39c47678ed294
saiashish9 <saiashish3760@gmail.com>
2025-01-14 23:02:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -64569,6 +64569,77 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  1450: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given two integer arrays <code>startTime</code> and <code>endTime</code>{" "}
+        and given an integer <code>queryTime</code>.
+      </p>
+
+      <p>
+        The <code>ith</code> student started doing their homework at the time{" "}
+        <code>startTime[i]</code> and finished it at time{" "}
+        <code>endTime[i]</code>.
+      </p>
+
+      <p>
+        Return <em>the number of students</em> doing their homework at time{" "}
+        <code>queryTime</code>. More formally, return the number of students
+        where <code>queryTime</code> lays in the interval{" "}
+        <code>[startTime[i], endTime[i]]</code> inclusive.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> startTime = [1,2,3], endTime = [3,2,7],
+        queryTime = 4 <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> We have 3 students where: <br />
+        The first student started doing homework at time 1 and finished at time
+        3 and wasn't doing anything at time 4. <br />
+        The second student started doing homework at time 2 and finished at time
+        2 and also wasn't doing anything at time 4. <br />
+        The third student started doing homework at time 3 and finished at time
+        7 and was the only student doing homework at time 4.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> startTime = [4], endTime = [4], queryTime = 4{" "}
+        <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> The only student was doing their homework
+        at the queryTime.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>startTime.length == endTime.length</code>
+        </li>
+        <li>
+          <code>1 &lt;= startTime.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;= startTime[i] &lt;= endTime[i] &lt;= 1000</code>
+        </li>
+        <li>
+          <code>1 &lt;= queryTime &lt;= 1000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
