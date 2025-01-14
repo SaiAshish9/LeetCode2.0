4d7bda2e59072a1810c429cb4e81a5a33ee26fea
saiashish9 <saiashish3760@gmail.com>
2025-01-14 23:07:07 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -64710,6 +64710,87 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  1662: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given two string arrays <code>word1</code> and <code>word2</code>,
+        return<em> </em>
+        <code>true</code>
+        <em>
+          {" "}
+          if the two arrays <strong>represent</strong> the same string, and{" "}
+        </em>
+        <code>false</code>
+        <em> otherwise.</em>
+      </p>
+
+      <p>
+        A string is <strong>represented</strong> by an array if the array
+        elements concatenated <strong>in order</strong> forms the string.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> word1 = ["ab", "c"], word2 = ["a", "bc"] <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> <br />
+        word1 represents string "ab" + "c" -&gt; "abc" <br />
+        word2 represents string "a" + "bc" -&gt; "abc" <br />
+        The strings are the same, so return true.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> word1 = ["a", "cb"], word2 = ["ab", "c"] <br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> word1 = ["abc", "d", "defg"], word2 =
+        ["abcddefg"] <br />
+        <strong>Output:</strong> true
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= word1.length, word2.length &lt;= 10<sup>3</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= word1[i].length, word2[i].length &lt;= 10<sup>3</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= sum(word1[i].length), sum(word2[i].length) &lt;= 10
+            <sup>3</sup>
+          </code>
+        </li>
+        <li>
+          <code>word1[i]</code> and <code>word2[i]</code> consist of lowercase
+          letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
