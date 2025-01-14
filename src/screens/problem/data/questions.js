3f36cbfbd58b48f6682b618ce51ef7329a6e22c2
saiashish9 <saiashish3760@gmail.com>
2025-01-14 23:27:17 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -65334,6 +65334,93 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  3065: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <strong>0-indexed</strong> integer array{" "}
+        <code>nums</code>, and an integer <code>k</code>.
+      </p>
+
+      <p>
+        In one operation, you can remove one occurrence of the smallest element
+        of <code>nums</code>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>minimum</strong> number of operations needed so that all
+          elements of the array are greater than or equal to
+        </em>{" "}
+        <code>k</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [2,11,10,1,3], k = 10 <br />
+        <strong>Output:</strong> 3<br />
+        <strong>Explanation:</strong> After one operation, nums becomes equal to
+        [2, 11, 10, 3]. <br />
+        After two operations, nums becomes equal to [11, 10, 3]. <br />
+        After three operations, nums becomes equal to [11, 10]. <br />
+        At this stage, all the elements of nums are greater than or equal to 10
+        so we can stop. <br />
+        It can be shown that 3 is the minimum number of operations needed so
+        that all elements of the array are greater than or equal to 10.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,2,4,9], k = 1 <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> All elements of the array are greater than
+        or equal to 1 so we do not need to apply any operations on nums.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,2,4,9], k = 9 <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> only a single element of nums is greater
+        than or equal to 9 so we need to apply the operations 4 times on nums.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 50</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= nums[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= k &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+        <li>
+          The input is generated such that there is at least one index{" "}
+          <code>i</code> such that <code>nums[i] &gt;= k</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
