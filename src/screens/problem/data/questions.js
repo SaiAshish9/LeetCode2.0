a41d304be2f5af5c48e7ac52e24182075190c1aa
saiashish9 <saiashish3760@gmail.com>
2025-01-14 22:49:36 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -64320,6 +64320,75 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  915: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code>, partition it into two
+        (contiguous) subarrays <code>left</code> and <code>right</code> so that:
+      </p>
+
+      <ul>
+        <li>
+          Every element in <code>left</code> is less than or equal to every
+          element in <code>right</code>.
+        </li>
+        <li>
+          <code>left</code> and <code>right</code> are non-empty.
+        </li>
+        <li>
+          <code>left</code> has the smallest possible size.
+        </li>
+      </ul>
+
+      <p>
+        Return <em>the length of </em>
+        <code>left</code>
+        <em> after such a partitioning</em>.
+      </p>
+
+      <p>Test cases are generated such that partitioning exists.</p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [5,0,3,8,6] <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> left = [5,0,3], right = [8,6]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,1,0,6,12] <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> left = [1,1,1,0], right = [6,12]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            2 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= nums[i] &lt;= 10<sup>6</sup>
+          </code>
+        </li>
+        <li>There is at least one valid answer for the given input.</li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
