412e40eb383dadd6a879f6a052c822b26260fc03
saiashish9 <saiashish3760@gmail.com>
2025-01-14 22:42:15 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -64200,6 +64200,66 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  665: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array <code>nums</code> with <code>n</code> integers, your task
+        is to check if it could become non-decreasing by modifying{" "}
+        <strong>at most one element</strong>.
+      </p>
+
+      <p>
+        We define an array is non-decreasing if{" "}
+        <code>nums[i] &lt;= nums[i + 1]</code> holds for every <code>i</code> (
+        <strong>0-based</strong>) such that (<code>0 &lt;= i &lt;= n - 2</code>
+        ).
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [4,2,3] <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> You could modify the first 4 to 1 to get a
+        non-decreasing array.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [4,2,1] <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> You cannot get a non-decreasing array by
+        modifying at most one element.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == nums.length</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>5</sup> &lt;= nums[i] &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
