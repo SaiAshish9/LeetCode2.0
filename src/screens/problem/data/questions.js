8380de0098e5fa5ae9c360979aff3a15b9f317f8
saiashish9 <saiashish3760@gmail.com>
2025-01-14 22:59:58 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -64505,6 +64505,70 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  1437: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an binary array <code>nums</code> and an integer <code>k</code>,
+        return <code>true</code>
+        <em> if all </em>
+        <code>1</code>
+        <em>'s are at least </em>
+        <code>k</code>
+        <em> places away from each other, otherwise return </em>
+        <code>false</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/04/15/sample_1_1791.png"
+        style={{ width: 428, height: 181 }}
+      />
+      <pre>
+        <strong>Input:</strong> nums = [1,0,0,0,1,0,0,1], k = 2 <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> Each of the 1s are at least 2 places away
+        from each other.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/04/15/sample_2_1791.png"
+        style={{ width: 320, height: 173 }}
+      />
+      <pre>
+        <strong>Input:</strong> nums = [1,0,0,1,0,1], k = 2 <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> The second 1 and third 1 are only one
+        apart from each other.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>0 &lt;= k &lt;= nums.length</code>
+        </li>
+        <li>
+          <code>nums[i]</code> is <code>0</code> or <code>1</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
