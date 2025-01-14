7f037bb86715fbbf1740d025f06b359a350ac3d9
saiashish9 <saiashish3760@gmail.com>
2025-01-14 22:46:35 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -64260,6 +64260,66 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  717: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>We have two special characters:</p>
+
+      <ul>
+        <li>
+          The first character can be represented by one bit <code>0</code>.
+        </li>
+        <li>
+          The second character can be represented by two bits (<code>10</code>{" "}
+          or <code>11</code>).
+        </li>
+      </ul>
+
+      <p>
+        Given a binary array <code>bits</code> that ends with <code>0</code>,
+        return <code>true</code> if the last character must be a one-bit
+        character.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> bits = [1,0,0] <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> The only way to decode it is two-bit
+        character and one-bit character. <br />
+        So the last character is one-bit character.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> bits = [1,1,1,0] <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> The only way to decode it is two-bit
+        character and two-bit character. <br />
+        So the last character is not one-bit character.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= bits.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>bits[i]</code> is either <code>0</code> or <code>1</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
