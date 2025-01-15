ddd7e9a8bcf5207652772a28cce21878b7c8e0fa
saiashish9 <saiashish3760@gmail.com>
2025-01-15 17:06:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -67459,6 +67459,53 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  784: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string <code>s</code>, you&nbsp;can transform every letter
+        individually to be lowercase or uppercase to create another string.
+      </p>
+
+      <p>
+        Return <em>a list of all possible strings we could create</em>. Return
+        the output in <strong>any order</strong>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "a1b2" <br />
+        <strong>Output:</strong> ["a1b2","a1B2","A1b2","A1B2"]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "3z4" <br />
+        <strong>Output:</strong> ["3z4","3Z4"]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= s.length &lt;= 12</code>
+        </li>
+        <li>
+          <code>s</code> consists of lowercase English letters, uppercase
+          English letters, and digits.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
