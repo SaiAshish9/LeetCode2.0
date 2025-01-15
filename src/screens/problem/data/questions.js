951744b54fd5f6113ede669fc3d86ed7a2d8f34b
saiashish9 <saiashish3760@gmail.com>
2025-01-15 16:38:54 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -66958,6 +66958,86 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  216: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Find all valid combinations of <code>k</code> numbers that sum up to{" "}
+        <code>n</code> such that the following conditions are true:
+      </p>
+
+      <ul>
+        <li>
+          Only numbers <code>1</code> through <code>9</code> are used.
+        </li>
+        <li>
+          Each number is used <strong>at most once</strong>.
+        </li>
+      </ul>
+
+      <p>
+        Return <em>a list of all possible valid combinations</em>. The list must
+        not contain the same combination twice, and the combinations may be
+        returned in any order.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> k = 3, n = 7<br />
+        <strong>Output:</strong> [[1,2,4]]
+        <br />
+        <strong>Explanation:</strong>
+        <br />
+        1 + 2 + 4 = 7<br />
+        There are no other valid combinations.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> k = 3, n = 9<br />
+        <strong>Output:</strong> [[1,2,6],[1,3,5],[2,3,4]] <br />
+        <strong>Explanation:</strong>
+        <br />
+        1 + 2 + 6 = 9<br />
+        1 + 3 + 5 = 9<br />
+        2 + 3 + 4 = 9<br />
+        There are no other valid combinations.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> k = 4, n = 1 <br />
+        <strong>Output:</strong> [] <br />
+        <strong>Explanation:</strong> There are no valid combinations. <br />
+        Using 4 different numbers in the range [1,9], the smallest sum we can{" "}
+        <br />
+        get is 1+2+3+4 = 10 and since 10 &gt; 1, there are no valid combination.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>2 &lt;= k &lt;= 9</code>
+        </li>
+        <li>
+          <code>1 &lt;= n &lt;= 60</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
