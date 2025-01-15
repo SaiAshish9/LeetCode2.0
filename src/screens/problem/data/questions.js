e1c7718912bc57a796504c77ab0eb07de365d4bb
saiashish9 <saiashish3760@gmail.com>
2025-01-15 16:44:23 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -67112,6 +67112,58 @@ INF  -1 INF  -1
       </pre>
     </div>
   ),
+  257: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary tree, return{" "}
+        <em>
+          all root-to-leaf paths in <strong>any order</strong>
+        </em>
+        .
+      </p>
+
+      <p>
+        A <strong>leaf</strong> is a node with no children.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/03/12/paths-tree.jpg"
+        style={{ width: 207, height: 293 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [1,2,3,null,5] <br />
+        <strong>Output:</strong> ["1-&gt;2-&gt;5","1-&gt;3"]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [1] <br />
+        <strong>Output:</strong> ["1"]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range <code>[1, 100]</code>.
+        </li>
+        <li>
+          <code>-100 &lt;= Node.val &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
