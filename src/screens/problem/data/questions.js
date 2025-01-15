b25c4772c2ad43ef8406c07b9bd5fdc0dd5bfd9c
saiashish9 <saiashish3760@gmail.com>
2025-01-15 16:56:34 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -67255,6 +67255,69 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  473: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>matchsticks</code> where{" "}
+        <code>matchsticks[i]</code> is the length of the{" "}
+        <code>
+          i<sup>th</sup>
+        </code>{" "}
+        matchstick. You want to use <strong>all the matchsticks</strong> to make
+        one square. You <strong>should not break</strong> any stick, but you can
+        link them up, and each matchstick must be used{" "}
+        <strong>exactly one time</strong>.
+      </p>
+
+      <p>
+        Return <code>true</code> if you can make this square and{" "}
+        <code>false</code> otherwise.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/04/09/matchsticks1-grid.jpg"
+        style={{ width: 253, height: 253 }}
+      />
+      <pre>
+        <strong>Input:</strong> matchsticks = [1,1,2,2,2] <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> You can form a square with length 2, one
+        side of the square came two sticks with length 1.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> matchsticks = [3,3,3,3,4] <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> You cannot find a way to form a square
+        with all the matchsticks.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= matchsticks.length &lt;= 15</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= matchsticks[i] &lt;= 10<sup>8</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
