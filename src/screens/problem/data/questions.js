b9a7ecbe1d552d93c960c697fae5bc82c72d7e2c
saiashish9 <saiashish3760@gmail.com>
2025-01-15 16:59:12 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -67318,6 +67318,73 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  526: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Suppose you have <code>n</code> integers labeled <code>1</code> through{" "}
+        <code>n</code>. A permutation of those <code>n</code> integers{" "}
+        <code>perm</code> (<strong>1-indexed</strong>) is considered a{" "}
+        <strong>beautiful arrangement</strong> if for every <code>i</code> (
+        <code>1 &lt;= i &lt;= n</code>), <strong>either</strong> of the
+        following is true:
+      </p>
+
+      <ul>
+        <li>
+          <code>perm[i]</code> is divisible by <code>i</code>.
+        </li>
+        <li>
+          <code>i</code> is divisible by <code>perm[i]</code>.
+        </li>
+      </ul>
+
+      <p>
+        Given an integer <code>n</code>, return{" "}
+        <em>
+          the <strong>number</strong> of the{" "}
+          <strong>beautiful arrangements</strong> that you can construct
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2 <br />
+        <strong>Output:</strong> 2 <br />
+        <b>Explanation:</b> <br />
+        The first beautiful arrangement is [1,2]: <br />
+        - perm[1] = 1 is divisible by i = 1 <br />
+        - perm[2] = 2 is divisible by i = 2 <br />
+        The second beautiful arrangement is [2,1]: <br />
+        - perm[1] = 2 is divisible by i = 1 <br />- i = 2 is divisible by
+        perm[2] = 1
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 1 <br />
+        <strong>Output:</strong> 1
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= n &lt;= 15</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
