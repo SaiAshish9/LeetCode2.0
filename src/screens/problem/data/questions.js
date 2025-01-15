02eab48febe619ee59e4bc2e5fde791b004c8ac8
saiashish9 <saiashish3760@gmail.com>
2025-01-15 18:36:38 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -68620,6 +68620,134 @@ P     I`}
       </ul>
     </div>
   ),
+  65: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string <code>s</code>, return whether <code>s</code> is a{" "}
+        <strong>valid number</strong>.<br />
+        For example, all the following are valid numbers:{" "}
+        <code>
+          "2", "0089", "-0.1", "+3.14", "4.", "-.9", "2e10", "-90E3", "3e+7",
+          "+6e-1", "53.5e93", "-123.456e789"
+        </code>
+        , while the following are not valid numbers:{" "}
+        <code>"abc", "1a", "1e", "e3", "99e2.5", "--6", "-+3", "95a54e53"</code>
+        .
+      </p>
+
+      <p>
+        Formally, a&nbsp;<strong>valid number</strong> is defined using one of
+        the following definitions:
+      </p>
+
+      <ul>
+        <li>
+          An <strong>integer number</strong> followed by an{" "}
+          <strong>optional exponent</strong>.
+        </li>
+        <li>
+          A <strong>decimal number</strong> followed by an{" "}
+          <strong>optional exponent</strong>.
+        </li>
+      </ul>
+
+      <p>
+        An <strong>integer number</strong> is defined with an{" "}
+        <strong>optional sign</strong> <code>'-'</code> or <code>'+'</code>{" "}
+        followed by <strong>digits</strong>.
+      </p>
+
+      <p>
+        A <strong>decimal number</strong> is defined with an{" "}
+        <strong>optional sign</strong> <code>'-'</code> or <code>'+'</code>{" "}
+        followed by one of the following definitions:
+      </p>
+
+      <ul>
+        <li>
+          <strong>Digits</strong> followed by a <strong>dot</strong>{" "}
+          <code>'.'</code>.
+        </li>
+        <li>
+          <strong>Digits</strong> followed by a <strong>dot</strong>{" "}
+          <code>'.'</code> followed by <strong>digits</strong>.
+        </li>
+        <li>
+          A <strong>dot</strong> <code>'.'</code> followed by{" "}
+          <strong>digits</strong>.
+        </li>
+      </ul>
+
+      <p>
+        An <strong>exponent</strong> is defined with an{" "}
+        <strong>exponent notation</strong> <code>'e'</code> or <code>'E'</code>{" "}
+        followed by an <strong>integer number</strong>.
+      </p>
+
+      <p>
+        The <strong>digits</strong> are defined as one or more digits.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">s = "0"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">true</span>
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">s = "e"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">false</span>
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">s = "."</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">false</span>
+        </p>
+      </div>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= s.length &lt;= 20</code>
+        </li>
+        <li>
+          <code>s</code> consists of only English letters (both uppercase and
+          lowercase), digits (<code>0-9</code>), plus <code>'+'</code>, minus{" "}
+          <code>'-'</code>, or dot <code>'.'</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
