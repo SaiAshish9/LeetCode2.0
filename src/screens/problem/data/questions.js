a9d68c6b12efe0aafcb4327aba1f8829d6e0e63f
saiashish9 <saiashish3760@gmail.com>
2025-01-15 16:41:50 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -67038,6 +67038,80 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  254: (
+    <div class="markdown-body div-width">
+      <p>Numbers can be regarded as product of its factors. For example,</p>
+
+      <pre>8 = 2 x 2 x 2; = 2 x 4.</pre>
+
+      <p>
+        Write a function that takes an integer <i>n</i> and return all possible
+        combinations of its factors.
+      </p>
+
+      <p>
+        <b>Note:</b>
+      </p>
+
+      <ol>
+        <li>
+          You may assume that <i>n</i> is always positive.
+        </li>
+        <li>
+          Factors should be greater than 1 and less than <i>n</i>.
+        </li>
+      </ol>
+
+      <p>
+        <b>Example&nbsp;1: </b>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> <code>1</code> <br />
+        <strong>Output:</strong> []
+      </pre>
+
+      <p>
+        <b>Example&nbsp;2: </b>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> <code>37</code> <br />
+        <strong>Output:</strong>[]
+      </pre>
+
+      <p>
+        <b>Example&nbsp;3: </b>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> <code>12</code> <br />
+        <strong>Output:</strong>
+        {`[
+  [2, 6],
+  [2, 2, 3],
+  [3, 4]
+]`}
+      </pre>
+
+      <p>
+        <b>Example&nbsp;4: </b>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> <code>32</code> <br />
+        <strong>Output:</strong>
+        {`[
+  [2, 16],
+  [2, 2, 8],
+  [2, 2, 2, 4],
+  [2, 2, 2, 2, 2],
+  [2, 4, 4],
+  [4, 8]
+]`}
+      </pre>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
