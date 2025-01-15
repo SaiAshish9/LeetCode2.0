f82557a3a599f4bc545c1b1498a4db3354dab85d
saiashish9 <saiashish3760@gmail.com>
2025-01-15 22:14:04 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -68966,6 +68966,37 @@ P     I`}
       </pre>
     </div>
   ),
+  248: (
+    <div class="markdown-body div-width">
+      <p>
+        A strobogrammatic number is a number that looks the same when rotated
+        180 degrees (looked at upside down).
+      </p>
+
+      <p>
+        Write a function to count the total strobogrammatic numbers that exist
+        in the range of low &lt;= num &lt;= high.
+      </p>
+
+      <p>
+        <b>Example:</b>
+      </p>
+
+      <pre>
+        <b>Input:</b> low = "50", high = "100" <br />
+        <b>Output:</b> 3 <br />
+        <strong>Explanation: </strong>69, 88, and 96 are three strobogrammatic
+        numbers.
+      </pre>
+
+      <p>
+        <strong>Note:</strong>
+        <br />
+        Because the range might be a large number, the <em>low</em> and{" "}
+        <em>high</em> numbers are represented as string.
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
