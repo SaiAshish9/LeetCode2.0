082f0ce248c3c79a886e5e06eaf1e4561f222b75
saiashish9 <saiashish3760@gmail.com>
2025-01-15 18:50:44 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -68816,6 +68816,40 @@ P     I`}
       </ul>
     </div>
   ),
+  243: (
+    <div class="markdown-body div-width">
+      <p>
+        Given a list of words and two words <em>word1</em> and <em>word2</em>,
+        return the shortest distance between these two words in the list.
+      </p>
+      <p>
+        <strong>Example:</strong>
+        <br />
+        Assume that words ={" "}
+        <code>["practice", "makes", "perfect", "coding", "makes"]</code>.
+      </p>{" "}
+      <br />
+      <pre>
+        <b>Input:</b> <em>word1</em> = <code>“coding”</code>, <em>word2</em> ={" "}
+        <code>“practice”</code>
+        <br />
+        <b>Output:</b> 3
+      </pre>
+      <pre>
+        <b>Input:</b> <em>word1</em> = <code>"makes"</code>, <em>word2</em> ={" "}
+        <code>"coding"</code>
+        <br />
+        <b>Output:</b> 1
+      </pre>
+      <p>
+        <strong>Note:</strong>
+        <br />
+        You may assume that <em>word1</em> <strong>does not equal to</strong>{" "}
+        <em>word2</em>, and
+        <em>word1</em> and <em>word2</em> are both in the list.
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
