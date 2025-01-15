ca1df92d938129c764d2a2e31a87f385728f3402
saiashish9 <saiashish3760@gmail.com>
2025-01-15 17:10:27 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -67578,6 +67578,93 @@ INF  -1 INF  -1
       </ul>
     </div>
   ),
+  988: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given the <code>root</code> of a binary tree where each node has
+        a value in the range <code>[0, 25]</code> representing the letters{" "}
+        <code>'a'</code> to <code>'z'</code>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>lexicographically smallest</strong> string that starts at
+          a leaf of this tree and ends at the root
+        </em>
+        .
+      </p>
+
+      <p>
+        As a reminder, any shorter prefix of a string is{" "}
+        <strong>lexicographically smaller</strong>.
+      </p>
+
+      <ul>
+        <li>
+          For example, <code>"ab"</code> is lexicographically smaller than{" "}
+          <code>"aba"</code>.
+        </li>
+      </ul>
+
+      <p>A leaf of a node is a node that has no children.</p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2019/01/30/tree1.png"
+        style={{ width: 534, height: 358 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [0,1,2,3,4,3,4] <br />
+        <strong>Output:</strong> "dba"
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2019/01/30/tree2.png"
+        style={{ width: 534, height: 358 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [25,1,3,1,3,0,2] <br />
+        <strong>Output:</strong> "adz"
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2019/02/01/tree3.png"
+        style={{ height: 490, width: 468 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [2,2,1,null,1,0,null,0] <br />
+        <strong>Output:</strong> "abc"
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range <code>[1, 8500]</code>
+          .
+        </li>
+        <li>
+          <code>0 &lt;= Node.val &lt;= 25</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
