d7f9fa87b21807f5e100d5f09eabc3918f8e9e4c
saiashish9 <saiashish3760@gmail.com>
2025-01-16 22:00:30 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -69904,6 +69904,71 @@ P     I`}
       </ul>
     </div>
   ),
+  2414: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        An <strong>alphabetical continuous string</strong> is a string
+        consisting of consecutive letters in the alphabet. In other words, it is
+        any substring of the string <code>"abcdefghijklmnopqrstuvwxyz"</code>.
+      </p>
+
+      <ul>
+        <li>
+          For example, <code>"abc"</code> is an alphabetical continuous string,
+          while <code>"acb"</code> and <code>"za"</code> are not.
+        </li>
+      </ul>
+
+      <p>
+        Given a string <code>s</code> consisting of lowercase letters only,
+        return the{" "}
+        <em>
+          length of the <strong>longest</strong> alphabetical continuous
+          substring.
+        </em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "abacaba" <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> There are 4 distinct continuous
+        substrings: "a", "b", "c" and "ab". <br />
+        "ab" is the longest continuous substring.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "abcde" <br />
+        <strong>Output:</strong> 5 <br />
+        <strong>Explanation:</strong> "abcde" is the longest continuous
+        substring.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>s</code> consists of only English lowercase letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
