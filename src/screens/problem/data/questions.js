3d20c1c261a074f752d516991356cdcf7eda1962
saiashish9 <saiashish3760@gmail.com>
2025-01-16 21:54:00 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -69719,6 +69719,93 @@ P     I`}
       </ul>
     </div>
   ),
+  1614: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a <strong>valid parentheses string</strong> <code>s</code>, return
+        the <strong>nesting depth</strong> of<em> </em>
+        <code>s</code>. The nesting depth is the <strong>maximum</strong> number
+        of nested parentheses.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">s = "(1+(2*3)+((8)/4))+1"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">3</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>Digit 8 is inside of 3 nested parentheses in the string.</p>
+      </div>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">s = "(1)+((2))+(((3)))"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">3</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>Digit 3 is inside of 3 nested parentheses in the string.</p>
+      </div>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">s = "()(())((()()))"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">3</span>
+        </p>
+      </div>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= s.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>s</code> consists of digits <code>0-9</code> and characters{" "}
+          <code>'+'</code>, <code>'-'</code>, <code>'*'</code>, <code>'/'</code>
+          , <code>'('</code>, and <code>')'</code>.
+        </li>
+        <li>
+          It is guaranteed that parentheses expression <code>s</code> is a VPS.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
