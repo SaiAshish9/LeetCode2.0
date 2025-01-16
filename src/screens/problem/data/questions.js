86e20895c9681f4f9b8fa353bdda26cc925da419
saiashish9 <saiashish3760@gmail.com>
2025-01-16 21:43:27 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -69538,6 +69538,60 @@ P     I`}
       </ul>
     </div>
   ),
+  709: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string <code>s</code>, return{" "}
+        <em>
+          the string after replacing every uppercase letter with the same
+          lowercase letter
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "Hello" <br />
+        <strong>Output:</strong> "hello"
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "here" <br />
+        <strong>Output:</strong> "here"
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "LOVELY" <br />
+        <strong>Output:</strong> "lovely"
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= s.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>s</code> consists of printable ASCII characters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
