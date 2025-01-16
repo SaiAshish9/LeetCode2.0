748ae9761013233e4947f2274d019e8740dda06a
saiashish9 <saiashish3760@gmail.com>
2025-01-16 22:20:54 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -70260,6 +70260,116 @@ P     I`}
       </ul>
     </div>
   ),
+  3170: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a string <code>s</code>. It may contain any number of{" "}
+        <code>'*'</code> characters. Your task is to remove all <code>'*'</code>{" "}
+        characters.
+      </p>
+
+      <p>
+        While there is a <code>'*'</code>, do the following operation:
+      </p>
+
+      <ul>
+        <li>
+          Delete the leftmost <code>'*'</code> and the <strong>smallest</strong>{" "}
+          non-<code>'*'</code> character to its <em>left</em>. If there are
+          several smallest characters, you can delete any of them.
+        </li>
+      </ul>
+
+      <p>
+        Return the{" "}
+        <span
+          data-keyword="lexicographically-smaller-string"
+          class=" cursor-pointer relative text-dark-blue-s text-sm"
+        >
+          <div class="popover-wrapper inline-block" data-headlessui-state="">
+            <div>
+              <div
+                aria-expanded="false"
+                data-headlessui-state=""
+                id="headlessui-popover-button-:r15i:"
+              >
+                <div>lexicographically smallest</div>
+              </div>
+            </div>
+          </div>
+        </span>{" "}
+        resulting string after removing all <code>'*'</code> characters.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">s = "aaba*"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">"aab"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          We should delete one of the <code>'a'</code> characters with{" "}
+          <code>'*'</code>. If we choose <code>s[3]</code>, <code>s</code>{" "}
+          becomes the lexicographically smallest.
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">s = "abc"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">"abc"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          There is no <code>'*'</code> in the string.
+        </p>
+      </div>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>s</code> consists only of lowercase English letters and{" "}
+          <code>'*'</code>.
+        </li>
+        <li>
+          The input is generated such that it is possible to delete all{" "}
+          <code>'*'</code> characters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
