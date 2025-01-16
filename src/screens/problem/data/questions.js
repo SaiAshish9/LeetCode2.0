7be9ede897a746078c08395913d01ebd053054dc
saiashish9 <saiashish3760@gmail.com>
2025-01-16 21:47:50 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -69592,6 +69592,66 @@ P     I`}
       </ul>
     </div>
   ),
+  1078: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given two strings <code>first</code> and <code>second</code>, consider
+        occurrences in some text of the form <code>"first second third"</code>,
+        where <code>second</code> comes immediately after <code>first</code>,
+        and <code>third</code> comes immediately after <code>second</code>.
+      </p>
+
+      <p>
+        Return <em>an array of all the words</em> <code>third</code>{" "}
+        <em>for each occurrence of</em> <code>"first second third"</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> text = "alice is a good girl she is a good
+        student", first = "a", second = "good" <br />
+        <strong>Output:</strong> ["girl","student"]
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> text = "we will we will rock you", first = "we",
+        second = "will" <br />
+        <strong>Output:</strong> ["we","rock"]
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= text.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>text</code> consists of lowercase English letters and spaces.
+        </li>
+        <li>
+          All the words in <code>text</code> are separated by{" "}
+          <strong>a single space</strong>.
+        </li>
+        <li>
+          <code>1 &lt;= first.length, second.length &lt;= 10</code>
+        </li>
+        <li>
+          <code>first</code> and <code>second</code> consist of lowercase
+          English letters.
+        </li>
+        <li>
+          <code>text</code> will not have any leading or trailing spaces.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
