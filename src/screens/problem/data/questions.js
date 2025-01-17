f2440ee709ac5460734ec3ac70367124abb8e91a
saiashish9 <saiashish3760@gmail.com>
2025-01-17 17:35:17 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -71554,6 +71554,118 @@ P     I`}
       </ul>
     </div>
   ),
+  1377: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an undirected tree consisting of <code>n</code> vertices numbered
+        from <code>1</code> to <code>n</code>. A frog starts jumping from{" "}
+        <strong>vertex 1</strong>. In one second, the frog jumps from its
+        current vertex to another <strong>unvisited</strong> vertex if they are
+        directly connected. The frog can not jump back to a visited vertex. In
+        case the frog can jump to several vertices, it jumps randomly to one of
+        them with the same probability. Otherwise, when the frog can not jump to
+        any unvisited vertex, it jumps forever on the same vertex.
+      </p>
+
+      <p>
+        The edges of the undirected tree are given in the array{" "}
+        <code>edges</code>, where{" "}
+        <code>
+          edges[i] = [a<sub>i</sub>, b<sub>i</sub>]
+        </code>{" "}
+        means that exists an edge connecting the vertices{" "}
+        <code>
+          a<sub>i</sub>
+        </code>{" "}
+        and{" "}
+        <code>
+          b<sub>i</sub>
+        </code>
+        .
+      </p>
+
+      <p>
+        <em>
+          Return the probability that after <code>t</code> seconds the frog is
+          on the vertex <code>target</code>.{" "}
+        </em>
+        Answers within{" "}
+        <code>
+          10<sup>-5</sup>
+        </code>{" "}
+        of the actual answer will be accepted.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/12/21/frog1.jpg"
+        style={{ width: 338, height: 304 }}
+      />
+      <pre>
+        <strong>Input:</strong> n = 7, edges =
+        [[1,2],[1,3],[1,7],[2,4],[2,6],[3,5]], t = 2, target = 4 <br />
+        <strong>Output:</strong> 0.16666666666666666 <br />
+        <strong>Explanation:</strong> The figure above shows the given graph.
+        The frog starts at vertex 1, jumping with 1/3 probability to the vertex
+        2 after <strong>second 1</strong> and then jumping with 1/2 probability
+        to vertex 4 after <strong>second 2</strong>. Thus the probability for
+        the frog is on the vertex 4 after 2 seconds is 1/3 * 1/2 = 1/6 =
+        0.16666666666666666.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <strong>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2021/12/21/frog2.jpg"
+          style={{ width: 304, height: 304 }}
+        />
+      </strong>
+
+      <pre>
+        <strong>Input:</strong> n = 7, edges =
+        [[1,2],[1,3],[1,7],[2,4],[2,6],[3,5]], t = 1, target = 7 <br />
+        <strong>Output:</strong> 0.3333333333333333 <br />
+        <strong>Explanation: </strong>The figure above shows the given graph.
+        The frog starts at vertex 1, jumping with 1/3 = 0.3333333333333333
+        probability to the vertex 7 after <strong>second 1</strong>.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= n &lt;= 100</code>
+        </li>
+        <li>
+          <code>edges.length == n - 1</code>
+        </li>
+        <li>
+          <code>edges[i].length == 2</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= a<sub>i</sub>, b<sub>i</sub> &lt;= n
+          </code>
+        </li>
+        <li>
+          <code>1 &lt;= t &lt;= 50</code>
+        </li>
+        <li>
+          <code>1 &lt;= target &lt;= n</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
