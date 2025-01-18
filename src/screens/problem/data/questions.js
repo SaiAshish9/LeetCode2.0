07499e5cde9355959f6be9a0dae3c4762c99ba61
saiashish9 <saiashish3760@gmail.com>
2025-01-18 11:29:48 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -73562,23 +73562,71 @@ P     I`}
         On 2013-10-01: <br />
         - There were 4 requests in total, 2 of which were canceled. <br />
         - However, the request with Id=2 was made by a banned client
-        (User_Id=2), so it is ignored in the calculation. <br />
-        - Hence there are 3 unbanned requests in total, 1 of which was canceled.{" "}
-        <br />
+        (User_Id=2), so it is ignored in the calculation. <br />- Hence there
+        are 3 unbanned requests in total, 1 of which was canceled. <br />
         - The Cancellation Rate is (1 / 3) = 0.33 <br />
         On 2013-10-02: <br />
-        - There were 3 requests in total, 0 of which were canceled. <br />
-        - The request with Id=6 was made by a banned client, so it is ignored.{" "}
-        <br />
+        - There were 3 requests in total, 0 of which were canceled. <br />- The
+        request with Id=6 was made by a banned client, so it is ignored. <br />
         - Hence there are 2 unbanned requests in total, 0 of which were
         canceled. <br />
         - The Cancellation Rate is (0 / 2) = 0.00 <br />
         On 2013-10-03: <br />
-        - There were 3 requests in total, 1 of which was canceled. <br />
-        - The request with Id=8 was made by a banned client, so it is ignored.{" "}
-        <br />
-        - Hence there are 2 unbanned request in total, 1 of which were canceled.{" "}
+        - There were 3 requests in total, 1 of which was canceled. <br />- The
+        request with Id=8 was made by a banned client, so it is ignored. <br />-
+        Hence there are 2 unbanned request in total, 1 of which were canceled.{" "}
         <br />- The Cancellation Rate is (1 / 2) = 0.50
+      </pre>
+    </div>
+  ),
+  569: (
+    <div class="markdown-body div-width">
+      <p>
+        The <code>Employee</code> table holds all employees. The employee table
+        has three columns: <br />
+        Employee Id, Company Name, and Salary.
+      </p>
+
+      <pre>
+        {`+-----+------------+--------+
+|Id   | Company    | Salary |
++-----+------------+--------+
+|1    | A          | 2341   |
+|2    | A          | 341    |
+|3    | A          | 15     |
+|4    | A          | 15314  |
+|5    | A          | 451    |
+|6    | A          | 513    |
+|7    | B          | 15     |
+|8    | B          | 13     |
+|9    | B          | 1154   |
+|10   | B          | 1345   |
+|11   | B          | 1221   |
+|12   | B          | 234    |
+|13   | C          | 2345   |
+|14   | C          | 2645   |
+|15   | C          | 2645   |
+|16   | C          | 2652   |
+|17   | C          | 65     |
++-----+------------+--------+`}
+      </pre>
+
+      <p>
+        Write a <span>SQL query</span> to find the median salary of each
+        company. Bonus points if you can solve it without using any built-in{" "}
+        <span>SQL</span> functions.
+      </p>
+
+      <pre>
+        {`+-----+------------+--------+
+|Id   | Company    | Salary |
++-----+------------+--------+
+|5    | A          | 451    |
+|6    | A          | 513    |
+|12   | B          | 234    |
+|9    | B          | 1154   |
+|14   | C          | 2645   |
++-----+------------+--------+`}
       </pre>
     </div>
   ),
