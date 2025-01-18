55a971b1eb4689dd858cf77205cfcbe4cccd2959
saiashish9 <saiashish3760@gmail.com>
2025-01-18 15:16:59 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -74258,6 +74258,128 @@ P     I`}
       </pre>
     </div>
   ),
+  1412: (
+    <div class="markdown-body div-width">
+      <div class="content__u3I1 question-content__JfgR">
+        <div class="sql-schema-wrapper__3VBi">
+          <a class="sql-schema-link__3cEg">
+            SQL Schema
+            <svg
+              viewBox="0 0 24 24"
+              width="1em"
+              height="1em"
+              class="icon__3Su4"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"
+              ></path>
+            </svg>
+          </a>
+        </div>
+        <div>
+          <p>
+            Table: <code>Student</code>
+          </p>
+          <br />
+          <pre>
+            {`+---------------------+---------+
+| Column Name         | Type    |
++---------------------+---------+
+| student_id          | int     |
+| student_name        | varchar |
++---------------------+---------+`}
+            <br />
+            student_id is the primary key for this table. student_name is the
+            name of the student.
+          </pre>
+
+          <p>&nbsp;</p>
+
+          <p>
+            Table: <code>Exam</code>
+          </p>
+          <br />
+          <pre>
+            {`+---------------+---------+
+| Column Name   | Type    |
++---------------+---------+
+| exam_id       | int     |
+| student_id    | int     |
+| score         | int     |
++---------------+---------+`}
+            <br />
+            (exam_id, student_id) is the primary key for this table. Student
+            with student_id got score points in exam with id exam_id.
+          </pre>
+
+          <p>&nbsp;</p>
+
+          <p>
+            A "quite" student is the one who took at least one exam and didn't
+            score neither the high score nor the low score.
+          </p>
+
+          <p>
+            Write an SQL query to report the students (student_id, student_name)
+            being "quiet" in ALL exams.
+          </p>
+
+          <p>
+            Don't return the student who has never taken any exam. Return the
+            result table
+            <strong>ordered</strong> by student_id.
+          </p>
+
+          <p>The query result format is in the following example.</p>
+
+          <p>&nbsp;</p>
+
+          <pre>
+            Student table: <br />
+            {`+-------------+---------------+
+| student_id  | student_name  |
++-------------+---------------+
+| 1           | Daniel        |
+| 2           | Jade          |
+| 3           | Stella        |
+| 4           | Jonathan      |
+| 5           | Will          |
++-------------+---------------+`}
+            <br />
+            Exam table: <br />
+            {`+------------+--------------+-----------+
+| exam_id    | student_id   | score     |
++------------+--------------+-----------+
+| 10         |     1        |    70     |
+| 10         |     2        |    80     |
+| 10         |     3        |    90     |
+| 20         |     1        |    80     |
+| 30         |     1        |    70     |
+| 30         |     3        |    80     |
+| 30         |     4        |    90     |
+| 40         |     1        |    60     |
+| 40         |     2        |    70     |
+| 40         |     4        |    80     |
++------------+--------------+-----------+`}
+            Result table:
+            {`+-------------+---------------+
+| student_id  | student_name  |
++-------------+---------------+
+| 2           | Jade          |
++-------------+---------------+`}
+            For exam 1: Student 1 and 3 hold the lowest and high score
+            respectively. For exam 2: Student 1 hold both highest and lowest
+            score. For exam 3 and 4: Studnet 1 and 4 hold the lowest and high
+            score respectively. Student 2 and 5 have never got the highest or
+            lowest in any of the exam. Since student 5 is not taking any exam,
+            he is excluded from the result. So, we only return the information
+            of Student 2.
+          </pre>
+        </div>
+      </div>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
