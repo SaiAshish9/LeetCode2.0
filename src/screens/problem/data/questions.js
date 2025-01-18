861f03af784fddf336e7e019676fd0b27081a21b
saiashish9 <saiashish3760@gmail.com>
2025-01-19 00:10:31 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -77930,7 +77930,7 @@ retrieve("2016:01:01:01:01:01","2017:01:01:23:00:00","Hour"); // return [1,2], b
         &quot;contains&quot;, &quot;remove&quot;, &quot;contains&quot;]
         <br /> [[], [1], [2], [1], [3], [2], [2], [2], [2]] <br />
         <strong>Output</strong> <br />
-        [null, null, null, true, false, null, true, null, false] <br/>
+        [null, null, null, true, false, null, true, null, false] <br />
         <strong>Explanation</strong> <br />
         {`MyHashSet myHashSet = new MyHashSet();
 myHashSet.add(1);      // set = [1]
@@ -77962,6 +77962,81 @@ myHashSet.contains(2); // return False, (already removed)`}
           calls will be made to <code>add</code>, <code>remove</code>, and{" "}
           <code>contains</code>.
         </li>
+      </ul>
+    </div>
+  ),
+  707: (
+    <div class="markdown-body div-width">
+      <p>
+        Design your&nbsp;implementation of the linked list. You can choose to
+        use the singly linked list or the doubly linked list. A node in a
+        singly&nbsp;linked list should have two attributes: <code>val</code>
+        &nbsp;and <code>next</code>. <code>val</code> is the value of the
+        current node, and <code>next</code>
+        &nbsp;is&nbsp;a&nbsp;pointer/reference to the next node. If you want to
+        use the doubly linked list,&nbsp;you will need&nbsp;one more attribute
+        <code>prev</code> to indicate the previous node in the linked list.
+        Assume all nodes in the linked list are 0-indexed.
+      </p>
+
+      <p>Implement these functions in your linked list class:</p>
+
+      <ul>
+        <li>
+          get(index) : Get the value of&nbsp;the <code>index</code>-th&nbsp;node
+          in the linked list. If the index is invalid, return <code>-1</code>.
+        </li>
+        <li>
+          addAtHead(val) : Add a node of value <code>val</code>&nbsp;before the
+          first element of the linked list. After the insertion, the new node
+          will be the first node of the linked list.
+        </li>
+        <li>
+          addAtTail(val) : Append a node of value <code>val</code>&nbsp;to the
+          last element of the linked list.
+        </li>
+        <li>
+          addAtIndex(index, val) : Add a node of value <code>val</code>
+          &nbsp;before the <code>index</code>-th&nbsp;node in the linked
+          list.&nbsp;If <code>index</code>&nbsp;equals&nbsp;to the length
+          of&nbsp;linked list, the node will be appended to the end of linked
+          list. If index is greater than the length, the node will not be
+          inserted. If index is negative, the node will be inserted at the
+          head&nbsp;of the list.
+        </li>
+        <li>
+          deleteAtIndex(index) : Delete&nbsp;the <code>index</code>-th&nbsp;node
+          in the linked list, if the index is valid.
+        </li>
+      </ul>
+
+      <p>
+        <strong>Example:</strong>
+      </p>
+
+      <pre>
+        {`MyLinkedList linkedList = new MyLinkedList();
+linkedList.addAtHead(1);
+linkedList.addAtTail(3);
+linkedList.addAtIndex(1, 2);  // linked list becomes 1-&gt;2-&gt;3
+linkedList.get(1);            // returns 2
+linkedList.deleteAtIndex(1);  // now the linked list is 1-&gt;3
+linkedList.get(1);&nbsp;&nbsp;&nbsp;         // returns 3`}
+      </pre>
+
+      <p>
+        <strong>Note:</strong>
+      </p>
+
+      <ul>
+        <li>
+          All values will be in the range of <code>[1, 1000]</code>.
+        </li>
+        <li>
+          The number of operations will be in the range of&nbsp;
+          <code>[1, 1000]</code>.
+        </li>
+        <li>Please do not use the built-in LinkedList library.</li>
       </ul>
     </div>
   ),
