5032950e47be30e26e43535927f35d31e19eb780
saiashish9 <saiashish3760@gmail.com>
2025-01-18 14:47:16 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -73981,6 +73981,41 @@ P     I`}
       <p>&nbsp;</p>
     </div>
   ),
+  618: (
+    <div class="markdown-body div-width">
+      A U.S graduate school has students from Asia, Europe and America. The
+      students' location information are stored in table <code>student</code> as
+      below.
+      <p>&nbsp;</p> <br />
+      <pre>
+        {`| name   | continent |
+|--------|-----------|
+| Jack   | America   |
+| Pascal | Europe    |
+| Xi     | Asia      |
+| Jane   | America   |`}
+      </pre> <br />
+      <p>&nbsp;</p>
+      <a href="https://en.wikipedia.org/wiki/Pivot_table"> Pivot</a> the
+      continent column in this table so that each name is sorted alphabetically
+      and displayed underneath its corresponding continent. The output headers
+      should be America, Asia and Europe respectively. It is guaranteed that the
+      student number from America is no less than either Asia or Europe.
+      <p>&nbsp;</p>
+      For the sample input, the output is:
+      <p>&nbsp;</p> <br />
+      <pre> <br />
+        {`| America | Asia | Europe |
+|---------|------|--------|
+| Jack    | Xi   | Pascal |
+| Jane    |      |        |`}
+      </pre> <br />
+      <p>&nbsp;</p>
+      <b>Follow-up:</b> If it is unknown which continent has the most students,
+      can you write a query to generate the student report?
+      <p>&nbsp;</p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
