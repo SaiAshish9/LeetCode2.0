bed34a1d59a8e76799330ab8b889437d1a8cb612
saiashish9 <saiashish3760@gmail.com>
2025-01-18 22:12:34 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -77347,6 +77347,55 @@ counter.getHits(301);`}
       </p>
     </div>
   ),
+  379: (
+    <div class="markdown-body div-width">
+      <p>Design a Phone Directory which supports the following operations:</p>
+
+      <p></p>
+      <ul>
+        <li>
+          <code>get</code>: Provide a number which is not assigned to anyone.
+        </li>
+        <li>
+          <code>check</code>: Check if a number is available or not.
+        </li>
+        <li>
+          <code>release</code>: Recycle or release a number.
+        </li>
+      </ul>
+      <p></p>
+
+      <p>
+        <b>Example:</b>
+      </p>
+      <pre>
+        {`// Init a phone directory containing a total of 3 numbers: 0, 1, and 2.
+PhoneDirectory directory = new PhoneDirectory(3);
+
+// It can return any available phone number. Here we assume it returns 0.
+directory.get();
+
+// Assume it returns 1.
+directory.get();
+
+// The number 2 is available, so return true.
+directory.check(2);
+
+// It returns 2, the only number that is left.
+directory.get();
+
+// The number 2 is no longer available, so return false.
+directory.check(2);
+
+// Release number 2 back to the pool.
+directory.release(2);
+
+// Number 2 is available again, return true.
+directory.check(2);`}
+      </pre>
+      <p></p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
