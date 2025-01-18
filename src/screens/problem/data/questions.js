38f1af730b88bcb89f50210b3fcbf0e3f13af539
saiashish9 <saiashish3760@gmail.com>
2025-01-18 11:33:10 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -73630,6 +73630,44 @@ P     I`}
       </pre>
     </div>
   ),
+  571: (
+    <div class="markdown-body div-width">
+      <p>
+        The <code>Numbers</code> table keeps the value of number and its
+        frequency.
+      </p>
+
+      <pre>
+        {`+----------+-------------+
+|  Number  |  Frequency  |
++----------+-------------|
+|  0       |  7          |
+|  1       |  1          |
+|  2       |  3          |
+|  3       |  1          |
++----------+-------------+`}
+      </pre>
+      <br />
+      <p>
+        In this table, the numbers are{" "}
+        <code>0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 2, 3</code>, so the median is{" "}
+        <code>(0 + 0) / 2 = 0</code>.
+      </p>
+
+      <pre>
+        {`+--------+
+| median |
++--------|
+| 0.0000 |
++--------+`}
+      </pre>
+      <br />
+      <p>
+        Write a query to find the median of all numbers and name the result as{" "}
+        <code>median</code>.
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
