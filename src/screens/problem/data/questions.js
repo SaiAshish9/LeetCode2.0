48acfdbe3c83aed7805112cfa2ca6e63cd035f37
saiashish9 <saiashish3760@gmail.com>
2025-01-18 16:45:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -75054,6 +75054,118 @@ P     I`}
       </p>
     </div>
   ),
+  191: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a positive integer <code>n</code>, write a function that returns
+        the number of{" "}
+        <span
+          data-keyword="set-bit"
+          class=" cursor-pointer relative text-dark-blue-s text-sm"
+        >
+          <div class="popover-wrapper inline-block" data-headlessui-state="">
+            <div>
+              <div
+                aria-expanded="false"
+                data-headlessui-state=""
+                id="headlessui-popover-button-:rb6:"
+              >
+                <div>set bits</div>
+              </div>
+            </div>
+          </div>
+        </span>{" "}
+        in its binary representation (also known as the{" "}
+        <a
+          href="http://en.wikipedia.org/wiki/Hamming_weight"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Hamming weight
+        </a>
+        ).
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">n = 11</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">3</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          The input binary string <strong>1011</strong> has a total of three set
+          bits.
+        </p>
+      </div>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">n = 128</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">1</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          The input binary string <strong>10000000</strong> has a total of one
+          set bit.
+        </p>
+      </div>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">n = 2147483645</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">30</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          The input binary string{" "}
+          <strong>1111111111111111111111111111101</strong> has a total of thirty
+          set bits.
+        </p>
+      </div>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 2<sup>31</sup> - 1
+          </code>
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <strong>Follow up:</strong> If this function is called many times, how
+      would you optimize it?
+    </div>
+  ),
 };
 
 export default QUESTIONS;
