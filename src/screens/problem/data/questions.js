b62ca480c53d8c07c41beab6e9790c7b86a875dc
saiashish9 <saiashish3760@gmail.com>
2025-01-18 16:53:56 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -75282,6 +75282,118 @@ P     I`}
       </ul>
     </div>
   ),
+  268: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array <code>nums</code> containing <code>n</code> distinct
+        numbers in the range <code>[0, n]</code>, return{" "}
+        <em>the only number in the range that is missing from the array.</em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">nums = [3,0,1]</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">2</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          <code>n = 3</code> since there are 3 numbers, so all numbers are in
+          the range <code>[0,3]</code>. 2 is the missing number in the range
+          since it does not appear in <code>nums</code>.
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">nums = [0,1]</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">2</span>
+        </p>
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>
+        <br />
+        <p>
+          <code>n = 2</code> since there are 2 numbers, so all numbers are in
+          the range <code>[0,2]</code>. 2 is the missing number in the range
+          since it does not appear in <code>nums</code>.
+        </p>
+      </div>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <div class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">nums = [9,6,4,2,3,5,7,0,1]</span>
+        </p>
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">8</span>
+        </p>
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>
+        <br />
+        <p>
+          <code>n = 9</code> since there are 9 numbers, so all numbers are in
+          the range <code>[0,9]</code>. 8 is the missing number in the range
+          since it does not appear in <code>nums</code>.
+        </p>
+      </div>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == nums.length</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>0 &lt;= nums[i] &lt;= n</code>
+        </li>
+        <li>
+          All the numbers of <code>nums</code> are <strong>unique</strong>.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong> Could you implement a solution using only{" "}
+        <code>O(1)</code> extra space complexity and <code>O(n)</code> runtime
+        complexity?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
