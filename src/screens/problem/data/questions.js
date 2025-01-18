1d4fb6a3b2e7a73f0b8e5d4517f0ca9a934db9cf
saiashish9 <saiashish3760@gmail.com>
2025-01-18 14:42:01 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -73883,6 +73883,45 @@ P     I`}
       </pre>
     </div>
   ),
+  603: (
+    <div class="markdown-body div-width">
+      Several friends at a cinema ticket office would like to reserve
+      consecutive available seats.
+      <br />
+      Can you help to query all the consecutive available seats order by the
+      seat_id using the following <code>cinema</code> table?
+      <pre>
+        {`| seat_id | free |
+|---------|------|
+| 1       | 1    |
+| 2       | 0    |
+| 3       | 1    |
+| 4       | 1    |
+| 5       | 1    |`}
+      </pre>
+      <p>&nbsp;</p>
+      Your query should return the following result for the sample case above.
+      <p>&nbsp;</p>
+      <pre>
+        {`| seat_id |
+|---------|
+| 3       |
+| 4       |
+| 5       |`}
+      </pre>
+      <b>Note</b>:
+      <ul>
+        <li>
+          The seat_id is an auto increment int, and free is bool ('1' means
+          free, and '0' means occupied.).
+        </li>
+        <li>
+          Consecutive available seats are more than 2(inclusive) seats
+          consecutively available.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
