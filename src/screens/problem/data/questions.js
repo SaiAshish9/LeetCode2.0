ce4746a689e458c3c1a722638b98c9c99a5ce059
saiashish9 <saiashish3760@gmail.com>
2025-01-18 17:22:21 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -75673,6 +75673,80 @@ P     I`}
       </ul>
     </div>
   ),
+  898: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>arr</code>, return{" "}
+        <em>
+          the number of distinct bitwise ORs of all the non-empty subarrays of
+        </em>{" "}
+        <code>arr</code>.
+      </p>
+
+      <p>
+        The bitwise OR of a subarray is the bitwise OR of each integer in the
+        subarray. The bitwise OR of a subarray of one integer is that integer.
+      </p>
+
+      <p>
+        A <strong>subarray</strong> is a contiguous non-empty sequence of
+        elements within an array.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [0] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> There is only one possible result: 0.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [1,1,2] <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> The possible subarrays are [1], [1], [2],
+        [1, 1], [1, 2], [1, 1, 2]. <br />
+        These yield the results 1, 1, 2, 1, 3, 3. <br />
+        There are 3 unique values, so the answer is 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [1,2,4] <br />
+        <strong>Output:</strong> 6 <br />
+        <strong>Explanation:</strong> The possible results are 1, 2, 3, 4, 6,
+        and 7.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= arr.length &lt;= 5 * 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= arr[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
