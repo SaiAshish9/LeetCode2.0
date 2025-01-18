da776a6c1a6aa5139c7d889ac086b92a7e880fae
saiashish9 <saiashish3760@gmail.com>
2025-01-18 15:04:44 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -74173,6 +74173,91 @@ P     I`}
       </pre>
     </div>
   ),
+  1378: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Table: <code>Employees</code>
+      </p>
+      <pre>
+        {`+---------------+---------+
+    | Column Name   | Type    |
+    +---------------+---------+
+    | id            | int     |
+    | name          | varchar |
+    +---------------+---------+`}{" "}
+        <br />
+        id is the primary key (column with unique values) for this table. <br />
+        Each row of this table contains the id and the name of an employee in a
+        company.
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        Table: <code>EmployeeUNI</code>
+      </p>{" "}
+      <br />
+      <pre>
+        {`+---------------+---------+
+    | Column Name   | Type    |
+    +---------------+---------+
+    | id            | int     |
+    | unique_id     | int     |
+    +---------------+---------+`}
+        (id, unique_id) is the primary key (combination of columns with unique
+        values) for this table. <br />
+        Each row of this table contains the id and the corresponding unique id
+        of an employee in the company.
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        Write a solution to show the <strong>unique ID </strong>of each user, If
+        a user does not have a unique ID replace just show <code>null</code>.
+      </p>
+      <p>
+        Return the result table in <strong>any</strong> order.
+      </p>
+      <p>The result format is in the following example.</p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong>
+        Employees table: <br />
+        {`+----+----------+
+    | id | name     |
+    +----+----------+
+    | 1  | Alice    |
+    | 7  | Bob      |
+    | 11 | Meir     |
+    | 90 | Winston  |
+    | 3  | Jonathan |
+    +----+----------+`}
+        EmployeeUNI table: <br />
+        {`+----+-----------+
+    | id | unique_id |
+    +----+-----------+
+    | 3  | 1         |
+    | 11 | 2         |
+    | 90 | 3         |
+    +----+-----------+`}
+        <strong>Output:</strong> <br />
+        {`+-----------+----------+
+    | unique_id | name     |
+    +-----------+----------+
+    | null      | Alice    |
+    | null      | Bob      |
+    | 2         | Meir     |
+    | 3         | Winston  |
+    | 1         | Jonathan |
+    +-----------+----------+`}
+        <strong>Explanation:</strong> <br />
+        Alice and Bob do not have a unique ID, We will show null instead. <br />
+        The unique ID of Meir is 2. <br />
+        The unique ID of Winston is 3. <br />
+        The unique ID of Jonathan is 1.
+      </pre>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
