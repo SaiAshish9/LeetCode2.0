c5879392eb791ae12269addafaac90d5ad4d9582
saiashish9 <saiashish3760@gmail.com>
2025-01-18 17:19:51 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -75590,6 +75590,89 @@ P     I`}
       </ul>
     </div>
   ),
+  868: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a positive integer <code>n</code>, find and return{" "}
+        <em>
+          the <strong>longest distance</strong> between any two{" "}
+          <strong>adjacent</strong>{" "}
+        </em>
+        <code>1</code>
+        <em>'s in the binary representation of </em>
+        <code>n</code>
+        <em>. If there are no two adjacent </em>
+        <code>1</code>
+        <em>'s, return </em>
+        <code>0</code>
+        <em>.</em>
+      </p>
+
+      <p>
+        Two <code>1</code>'s are <strong>adjacent</strong> if there are only{" "}
+        <code>0</code>'s separating them (possibly no <code>0</code>'s). The{" "}
+        <b>distance</b> between two <code>1</code>'s is the absolute difference
+        between their bit positions. For example, the two <code>1</code>'s in{" "}
+        <code>"1001"</code> have a distance of 3.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 22 <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong> 22 in binary is "10110".
+        <br />
+        The first adjacent pair of 1's is "<u>1</u>0<u>1</u>10" with a distance
+        of 2.
+        <br />
+        The second adjacent pair of 1's is "10<u>11</u>0" with a distance of 1.
+        <br />
+        The answer is the largest of these two distances, which is 2.
+        <br />
+        Note that "<u>1</u>01<u>1</u>0" is not a valid pair since there is a 1
+        separating the two 1's underlined.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 8 <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> 8 in binary is "1000". <br />
+        There are not any adjacent pairs of 1's in the binary representation of
+        8, so we return 0.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 5<br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong> 5 in binary is "101".
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
