df9b0dfee5a4fcb6d16c4421b4b7ae41d24ae04e
saiashish9 <saiashish3760@gmail.com>
2025-01-18 14:54:31 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -73994,7 +73994,8 @@ P     I`}
 | Pascal | Europe    |
 | Xi     | Asia      |
 | Jane   | America   |`}
-      </pre> <br />
+      </pre>{" "}
+      <br />
       <p>&nbsp;</p>
       <a href="https://en.wikipedia.org/wiki/Pivot_table"> Pivot</a> the
       continent column in this table so that each name is sorted alphabetically
@@ -74004,16 +74005,91 @@ P     I`}
       <p>&nbsp;</p>
       For the sample input, the output is:
       <p>&nbsp;</p> <br />
-      <pre> <br />
+      <pre>
+        {" "}
+        <br />
         {`| America | Asia | Europe |
 |---------|------|--------|
 | Jack    | Xi   | Pascal |
 | Jane    |      |        |`}
-      </pre> <br />
+      </pre>{" "}
+      <br />
       <p>&nbsp;</p>
       <b>Follow-up:</b> If it is unknown which continent has the most students,
       can you write a query to generate the student report?
       <p>&nbsp;</p>
+    </div>
+  ),
+  1097: (
+    <div class="markdown-body div-width">
+      <p>
+        Table:&nbsp;<code>Activity</code>
+      </p>
+
+      <pre>
+        {`+--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+`}
+        (player_id, event_date) is the primary key of this table. This table
+        shows the activity of players of some game. Each row is a record of a
+        player who logged in and played a number of games (possibly 0) before
+        logging out on some day using some device.
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        We define the <em>install date</em> of a player to be the first login
+        day of that player.
+      </p>
+
+      <p>
+        We also define <em>day 1 retention</em>&nbsp;of some date <code>X</code>
+        &nbsp;to be the number&nbsp;of players whose install date is&nbsp;
+        <code>X</code>&nbsp;and they logged back in on the day right after{" "}
+        <code>X</code>, divided by the number of players whose install date
+        is&nbsp;<code>X</code>, <strong>rounded to 2 decimal places</strong>.
+      </p>
+
+      <p>
+        Write an SQL query that reports for each <strong>install date</strong>,
+        the <strong>number&nbsp;of players</strong> that installed the game on
+        that day and the <strong>day 1 retention</strong>. Best video game
+        consoles{" "}
+      </p>
+
+      <p>The query result format is in the following example:</p>
+
+      <pre>
+        Activity table:
+        <br />
+        {`+-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-03-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-01 | 0            |
+| 3         | 4         | 2016-07-03 | 5            |
++-----------+-----------+------------+--------------+`}
+        <br />
+        Result table: <br />
+        {`+------------+----------+----------------+
+| install_dt | installs | Day1_retention |
++------------+----------+----------------+
+| 2016-03-01 | 2        | 0.50           |
+| 2017-06-25 | 1        | 0.00           |
++------------+----------+----------------+`}
+        Player 1 and 3 installed the game on 2016-03-01 but only player 1 logged
+        back in on 2016-03-02 so the day 1 retention of 2016-03-01 is 1 / 2 =
+        0.50 Player 2 installed the game on 2017-06-25 but didn't log back in on
+        2017-06-26 so the day 1 retention of 2017-06-25 is 0 / 1 = 0.00
+      </pre>
     </div>
   ),
 };
