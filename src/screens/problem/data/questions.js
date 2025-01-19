7a61ba2e300d7e2dd966478d636506a020e513c1
saiashish9 <saiashish3760@gmail.com>
2025-01-19 22:09:18 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -81192,6 +81192,63 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1224: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array <code>nums</code> of positive integers, return the
+        longest possible length of an array prefix of <code>nums</code>, such
+        that it is possible to remove <strong>exactly one</strong> element from
+        this prefix so that every number that has appeared in it will have the
+        same number of occurrences.
+      </p>
+
+      <p>
+        If after removing one element there are no remaining elements, it's
+        still considered that every appeared number has the same number of
+        ocurrences (0).
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [2,2,1,1,5,3,3,5] <br />
+        <strong>Output:</strong> 7 <br />
+        <strong>Explanation:</strong> For the subarray [2,2,1,1,5,3,3] of length
+        7, if we remove nums[4] = 5, we will get [2,2,1,1,3,3], so that each
+        number will appear exactly twice.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,1,2,2,2,3,3,3,4,4,4,5] <br />
+        <strong>Output:</strong> 13
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            2 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= nums[i] &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
