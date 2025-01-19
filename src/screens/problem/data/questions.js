3c01b22759ea31f780025038bfc03ced7d0a9cff
saiashish9 <saiashish3760@gmail.com>
2025-01-19 23:20:10 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -81697,6 +81697,78 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  2475: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <strong>0-indexed</strong> array of positive integers{" "}
+        <code>nums</code>. Find the number of triplets <code>(i, j, k)</code>{" "}
+        that meet the following conditions:
+      </p>
+
+      <ul>
+        <li>
+          <code>0 &lt;= i &lt; j &lt; k &lt; nums.length</code>
+        </li>
+        <li>
+          <code>nums[i]</code>, <code>nums[j]</code>, and <code>nums[k]</code>{" "}
+          are <strong>pairwise distinct</strong>.
+          <ul>
+            <li>
+              In other words, <code>nums[i] != nums[j]</code>,{" "}
+              <code>nums[i] != nums[k]</code>, and{" "}
+              <code>nums[j] != nums[k]</code>.
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+      <p>
+        Return <em>the number of triplets that meet the conditions.</em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [4,4,2,4,3] <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> The following triplets meet the
+        conditions: <br />
+        - (0, 2, 4) because 4 != 2 != 3 <br />
+        - (1, 2, 4) because 4 != 2 != 3 <br />
+        - (2, 3, 4) because 2 != 4 != 3 <br />
+        Since there are 3 triplets, we return 3. <br />
+        Note that (2, 0, 4) is not a valid triplet because 2 &gt; 0.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,1,1,1,1] <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> No triplets meet the conditions so we
+        return 0.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>3 &lt;= nums.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;= nums[i] &lt;= 1000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
