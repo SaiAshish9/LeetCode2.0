1ca52037b7ad02bf32026c4bba8f09022561b3d6
saiashish9 <saiashish3760@gmail.com>
2025-01-19 16:00:31 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -78859,6 +78859,72 @@ fileSharing.join([]);        // A user who doesn't have any chunks joined the sy
       </ul>
     </div>
   ),
+  1756: (
+    <div class="markdown-body div-width">
+      <div class="content__u3I1 question-content__JfgR">
+        <div>
+          <p>
+            Design a queue-like data structure that moves the most recently used
+            element to the end of the queue.
+          </p>
+          <p>
+            Implement the <code>MRUQueue</code> class:
+          </p>
+          <ul>
+            <li>
+              <code>MRUQueue(int n)</code> constructs the <code>MRUQueue</code>{" "}
+              with <code>n</code> elements: <code>[1,2,3,...,n]</code>.
+            </li>
+            <li>
+              <code>fetch(int k)</code> moves the{" "}
+              <code>
+                k<sup>th</sup>
+              </code>{" "}
+              element <strong>(1-indexed)</strong> to the end of the queue and
+              returns it.
+            </li>
+          </ul>
+          <p>&nbsp;</p>
+          <p>
+            <strong>Example 1:</strong>
+          </p>
+          <pre>
+            <strong>Input:</strong> <br />
+            ["MRUQueue", "fetch", "fetch", "fetch", "fetch"] <br />
+            [[8], [3], [5], [2], [8]] <br />
+            <strong>Output:</strong> <br />
+            [null, 3, 6, 2, 2]
+            <strong>Explanation:</strong> <br />
+            {`MRUQueue mRUQueue = new MRUQueue(8); // Initializes the queue to [1,2,3,4,5,6,7,8].
+    mRUQueue.fetch(3); // Moves the 3<sup>rd</sup> element (3) to the end of the queue to become [1,2,4,5,6,7,8,3] and returns it.
+    mRUQueue.fetch(5); // Moves the 5<sup>th</sup> element (6) to the end of the queue to become [1,2,4,5,7,8,3,6] and returns it.
+    mRUQueue.fetch(2); // Moves the 2<sup>nd</sup> element (2) to the end of the queue to become [1,4,5,7,8,3,6,2] and returns it.
+    mRUQueue.fetch(8); // The 8<sup>th</sup> element (2) is already at the end of the queue so just return it.`}
+          </pre>
+          <p>&nbsp;</p>
+          <p>
+            <strong>Constraints:</strong>
+          </p>
+          <ul>
+            <li>
+              <code>1 &lt;= n &lt;= 2000</code>
+            </li>
+            <li>
+              <code>1 &lt;= k &lt;= n</code>
+            </li>
+            <li>
+              At most <code>2000</code> calls will be made to <code>fetch</code>
+              .
+            </li>
+          </ul>
+          <p>&nbsp;</p>
+          <strong>Follow up:</strong> Finding an <code>O(n)</code> algorithm per{" "}
+          <code>fetch</code> is a bit easy. Can you find an algorithm with a
+          better complexity for each <code>fetch</code> call?
+        </div>
+      </div>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
