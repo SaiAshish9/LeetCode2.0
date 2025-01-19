5145096e3f0bad54ba1aac1f0b5f58d91ec904d6
saiashish9 <saiashish3760@gmail.com>
2025-01-19 23:41:03 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -82298,6 +82298,106 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  3120: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a string <code>word</code>. A letter is called{" "}
+        <strong>special</strong> if it appears <strong>both</strong> in
+        lowercase and uppercase in <code>word</code>.
+      </p>
+
+      <p>
+        Return the number of<em> </em>
+        <strong>special</strong> letters in<em> </em>
+        <code>word</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">word = "aaAbcBC"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">3</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          The special characters in <code>word</code> are <code>'a'</code>,{" "}
+          <code>'b'</code>, and <code>'c'</code>.
+        </p>
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre class="example-block">
+        <p>
+          <strong>Input:</strong> <span class="example-io">word = "abc"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> <span class="example-io">0</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          No character in <code>word</code> appears in uppercase.
+        </p>
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre class="example-block">
+        <p>
+          <strong>Input:</strong>{" "}
+          <span class="example-io">word = "abBCab"</span>
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Output:</strong> 1
+        </p>{" "}
+        <br />
+        <p>
+          <strong>Explanation:</strong>
+        </p>{" "}
+        <br />
+        <p>
+          The only special character in <code>word</code> is <code>'b'</code>.
+        </p>
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= word.length &lt;= 50</code>
+        </li>
+        <li>
+          <code>word</code> consists of only lowercase and uppercase English
+          letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
