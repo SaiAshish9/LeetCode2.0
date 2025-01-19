08194f28cfee8cd719fc6c2e5ddf7d6998b791e5
saiashish9 <saiashish3760@gmail.com>
2025-01-19 22:31:38 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -81394,6 +81394,59 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1399: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer <code>n</code>.
+      </p>
+
+      <p>
+        Each number from <code>1</code> to <code>n</code> is grouped according
+        to the sum of its digits.
+      </p>
+
+      <p>
+        Return <em>the number of groups that have the largest size</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 13 <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> There are 9 groups in total, they are
+        grouped according sum of its digits of numbers from 1 to 13:
+        <br /> [1,10], [2,11], [3,12], [4,13], [5], [6], [7], [8], [9]. <br />
+        There are 4 groups with largest size.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> There are 2 groups [1], [2] of size 1.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
