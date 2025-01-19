8f77820ecb475bf5de644827dbe18cf19b149716
saiashish9 <saiashish3760@gmail.com>
2025-01-19 23:44:16 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -82398,6 +82398,67 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  3032: (
+    <article role="main" class="blog-post">
+      <p>
+        Given two <strong>positive</strong> integers <code>a</code> and{" "}
+        <code>b</code>, return{" "}
+        <em>
+          the count of numbers having&nbsp;<strong>unique</strong> digits in the
+          range
+        </em>{" "}
+        <code>[a, b]</code>{" "}
+        <em>
+          (<strong>inclusive</strong>).
+        </em>
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> a = 1, b = 20 <br />
+        <strong>Output:</strong> 19 <br />
+        <strong>Explanation:</strong> All the numbers in the range [1, 20] have
+        unique digits except 11. Hence, the answer is 19.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> a = 9, b = 19 <br />
+        <strong>Output:</strong> 10 <br />
+        <strong>Explanation:</strong> All the numbers in the range [9, 19] have
+        unique digits except 11. Hence, the answer is 10.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> a = 80, b = 120 <br />
+        <strong>Output:</strong> 27 <br />
+        <strong>Explanation:</strong> There are 41 numbers in the range [80,
+        120], 27 of which have unique digits.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= a &lt;= b &lt;= 1000</code>
+        </li>
+      </ul>
+    </article>
+  ),
 };
 
 export default QUESTIONS;
