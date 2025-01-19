59afa8fbad7d7bf0734db9e17feb954e1ea8c55f
saiashish9 <saiashish3760@gmail.com>
2025-01-19 22:02:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -81135,6 +81135,63 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1207: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array of integers <code>arr</code>, return <code>true</code>{" "}
+        <em>
+          if the number of occurrences of each value in the array is{" "}
+          <strong>unique</strong> or{" "}
+        </em>
+        <code>false</code>
+        <em> otherwise</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [1,2,2,1,1,3] <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong>&nbsp;The value 1 has 3 occurrences, 2 has
+        2 and 3 has 1. No two values have the same number of occurrences.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [1,2] <br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [-3,0,1,-3,1,1,1,-3,10,0] <br />
+        <strong>Output:</strong> true
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= arr.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>-1000 &lt;= arr[i] &lt;= 1000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
