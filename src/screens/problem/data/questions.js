0d9065aa3cd34e6309e9f8c2c23208219b0dadf3
saiashish9 <saiashish3760@gmail.com>
2025-01-19 21:28:46 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -80714,6 +80714,74 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  522: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array of strings <code>strs</code>, return{" "}
+        <em>
+          the length of the <strong>longest uncommon subsequence</strong>{" "}
+          between them
+        </em>
+        . If the longest uncommon subsequence does not exist, return{" "}
+        <code>-1</code>.
+      </p>
+      <p>
+        An <strong>uncommon subsequence</strong> between an array of strings is
+        a string that is a{" "}
+        <strong>subsequence of one string but not the others</strong>.
+      </p>
+      <p>
+        A <strong>subsequence</strong> of a string <code>s</code> is a string
+        that can be obtained after deleting any number of characters from{" "}
+        <code>s</code>.
+      </p>
+      <ul>
+        <li>
+          For example, <code>"abc"</code> is a subsequence of{" "}
+          <code>"aebdc"</code> because you can delete the underlined characters
+          in{" "}
+          <code>
+            "a<u>e</u>b<u>d</u>c"
+          </code>{" "}
+          to get <code>"abc"</code>. Other subsequences of <code>"aebdc"</code>{" "}
+          include <code>"aebdc"</code>, <code>"aeb"</code>, and <code>""</code>{" "}
+          (empty string).
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> strs = ["aba","cdc","eae"] <br />
+        <strong>Output:</strong> 3 <br />
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> strs = ["aaa","aaa","aa"] <br />
+        <strong>Output:</strong> -1
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>2 &lt;= strs.length &lt;= 50</code>
+        </li>
+        <li>
+          <code>1 &lt;= strs[i].length &lt;= 10</code>
+        </li>
+        <li>
+          <code>strs[i]</code> consists of lowercase English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
