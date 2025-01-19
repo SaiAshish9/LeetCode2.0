fc225747665510c29fe01f63c451f6ba033b6e05
saiashish9 <saiashish3760@gmail.com>
2025-01-19 21:39:31 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -80900,6 +80900,82 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  822: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given two <strong>0-indexed</strong> integer arrays{" "}
+        <code>fronts</code> and <code>backs</code> of length <code>n</code>,
+        where the{" "}
+        <code>
+          i<sup>th</sup>
+        </code>{" "}
+        card has the positive integer <code>fronts[i]</code> printed on the
+        front and <code>backs[i]</code> printed on the back. Initially, each
+        card is placed on a table such that the front number is facing up and
+        the other is facing down. You may flip over any number of cards
+        (possibly zero).
+      </p>
+
+      <p>
+        After flipping the cards, an integer is considered <strong>good</strong>{" "}
+        if it is facing down on some card and <strong>not</strong> facing up on
+        any card.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>the minimum possible good integer after flipping the cards</em>. If
+        there are no good integers, return <code>0</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> fronts = [1,2,4,4,7], backs = [1,3,4,1,3] <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> <br />
+        If we flip the second card, the face up numbers are [1,3,4,4,7] and the
+        face down are [1,2,4,1,3]. <br />
+        2 is the minimum good integer as it appears facing down but not facing
+        up. <br />
+        It can be shown that 2 is the minimum possible good integer obtainable
+        after flipping some cards.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> fronts = [1], backs = [1] <br />
+        <strong>Output:</strong> 0<br />
+        <strong>Explanation:</strong>
+        <br />
+        There are no good integers no matter how we flip the cards, so we return
+        0.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == fronts.length == backs.length</code>
+        </li>
+        <li>
+          <code>1 &lt;= n &lt;= 1000</code>
+        </li>
+        <li>
+          <code>1 &lt;= fronts[i], backs[i] &lt;= 2000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
