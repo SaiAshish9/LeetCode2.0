69039797fb0d7a4c947a4c33a3a8c8da3a276988
saiashish9 <saiashish3760@gmail.com>
2025-01-19 21:36:10 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -80842,6 +80842,64 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       <p></p>
     </div>
   ),
+  697: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a non-empty array of non-negative integers <code>nums</code>, the{" "}
+        <b>degree</b> of this array is defined as the maximum frequency of any
+        one of its elements.
+      </p>
+
+      <p>
+        Your task is to find the smallest possible length of a (contiguous)
+        subarray of <code>nums</code>, that has the same degree as{" "}
+        <code>nums</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,2,3,1] <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> <br />
+        The input array has a degree of 2 because both elements 1 and 2 appear
+        twice. <br />
+        Of the subarrays that have the same degree: <br />
+        [1, 2, 2, 3, 1], [1, 2, 2, 3], [2, 2, 3, 1], [1, 2, 2], [2, 2, 3], [2,
+        2] <br />
+        The shortest length is 2. So return 2.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,2,3,1,4,2] <br />
+        <strong>Output:</strong> 6 <br />
+        <strong>Explanation:</strong> <br />
+        The degree is 3 because the element 2 is repeated 3 times. <br />
+        So [2,2,3,1,4,2] is the shortest subarray, therefore returning 6.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>nums.length</code> will be between 1 and 50,000.
+        </li>
+        <li>
+          <code>nums[i]</code> will be an integer between 0 and 49,999.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
