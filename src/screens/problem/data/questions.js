74484a10d92cdd54b266e550396ce50f03fa7650
saiashish9 <saiashish3760@gmail.com>
2025-01-19 21:22:26 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -80548,6 +80548,56 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </div>
     </div>
   ),
+  448: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array <code>nums</code> of <code>n</code> integers where{" "}
+        <code>nums[i]</code> is in the range <code>[1, n]</code>, return{" "}
+        <em>an array of all the integers in the range</em> <code>[1, n]</code>{" "}
+        <em>that do not appear in</em> <code>nums</code>.
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> nums = [4,3,2,7,8,2,3,1] <br />
+        <strong>Output:</strong> [5,6] <br />
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> nums = [1,1] <br />
+        <strong>Output:</strong> [2]
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>n == nums.length</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>1 &lt;= nums[i] &lt;= n</code>
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong> Could you do it without extra space and in{" "}
+        <code>O(n)</code> runtime? You may assume the returned list does not
+        count as extra space.
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
