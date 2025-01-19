eb4fe0b2b7a2f52d529ef10dd325a5ca940f2c7e
saiashish9 <saiashish3760@gmail.com>
2025-01-19 16:32:39 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -79197,6 +79197,75 @@ videoSharingPlatform.getViews(0);            // There is no video associated wit
       </ul>
     </div>
   ),
+  2336: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You have a set which contains all positive integers{" "}
+        <code>[1, 2, 3, 4, 5, ...]</code>.
+      </p>
+
+      <p>
+        Implement the <code>SmallestInfiniteSet</code> class:
+      </p>
+
+      <ul>
+        <li>
+          <code>SmallestInfiniteSet()</code> Initializes the{" "}
+          <strong>SmallestInfiniteSet</strong> object to contain{" "}
+          <strong>all</strong> positive integers.
+        </li>
+        <li>
+          <code>int popSmallest()</code> <strong>Removes</strong> and returns
+          the smallest integer contained in the infinite set.
+        </li>
+        <li>
+          <code>void addBack(int num)</code> <strong>Adds</strong> a positive
+          integer <code>num</code> back into the infinite set, if it is{" "}
+          <strong>not</strong> already in the infinite set.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input</strong>
+        <br /> ["SmallestInfiniteSet", "addBack", "popSmallest", "popSmallest",
+        "popSmallest", "addBack", "popSmallest", "popSmallest", "popSmallest"]
+        <br /> [[], [2], [], [], [], [1], [], [], []] <br />
+        <strong>Output</strong> <br />
+        [null, null, 1, 2, 3, null, 1, 4, 5]
+        <strong>Explanation</strong> <br />
+        {`SmallestInfiniteSet smallestInfiniteSet = new SmallestInfiniteSet();
+    smallestInfiniteSet.addBack(2);    // 2 is already in the set, so no change is made.
+    smallestInfiniteSet.popSmallest(); // return 1, since 1 is the smallest number, and remove it from the set.
+    smallestInfiniteSet.popSmallest(); // return 2, and remove it from the set.
+    smallestInfiniteSet.popSmallest(); // return 3, and remove it from the set.
+    smallestInfiniteSet.addBack(1);    // 1 is added back to the set.
+    smallestInfiniteSet.popSmallest(); // return 1, since 1 was added back to the set and
+                                       // is the smallest number, and remove it from the set.
+    smallestInfiniteSet.popSmallest(); // return 4, and remove it from the set.
+    smallestInfiniteSet.popSmallest(); // return 5, and remove it from the set.`}
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= num &lt;= 1000</code>
+        </li>
+        <li>
+          At most <code>1000</code> calls will be made <strong>in total</strong>{" "}
+          to <code>popSmallest</code> and <code>addBack</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
