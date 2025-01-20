0906ba9698d482721afb466e1b2df66d4ae54983
saiashish9 <saiashish3760@gmail.com>
2025-01-21 00:21:21 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -83573,6 +83573,45 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  504: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer <code>num</code>, return{" "}
+        <em>
+          a string of its <strong>base 7</strong> representation
+        </em>
+        .
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> num = 100 <br />
+        <strong>Output:</strong> "202" <br />
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> num = -7 <br />
+        <strong>Output:</strong> "-10"
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>
+            -10<sup>7</sup> &lt;= num &lt;= 10<sup>7</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
