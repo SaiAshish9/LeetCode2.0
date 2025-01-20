d16ec5d6d34bb2e4b449990b2317d4412daf7698
saiashish9 <saiashish3760@gmail.com>
2025-01-20 22:54:57 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -83045,6 +83045,68 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  172: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer <code>n</code>, return{" "}
+        <em>the number of trailing zeroes in </em>
+        <code>n!</code>.
+      </p>
+
+      <p>
+        Note that <code>n! = n * (n - 1) * (n - 2) * ... * 3 * 2 * 1</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 3 <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> 3! = 6, no trailing zero.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 5 <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> 5! = 120, one trailing zero.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 0 <br />
+        <strong>Output:</strong> 0
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            0 &lt;= n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong> Could you write a solution that works in
+        logarithmic time complexity?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
