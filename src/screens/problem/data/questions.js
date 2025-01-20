6c65e737fbef4f7b97f02b2c3e695044e8675777
saiashish9 <saiashish3760@gmail.com>
2025-01-20 23:11:15 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -83107,6 +83107,75 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </p>
     </div>
   ),
+  223: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the coordinates of two <strong>rectilinear</strong> rectangles in
+        a 2D plane, return <em>the total area covered by the two rectangles</em>
+        .
+      </p>
+      <p>
+        The first rectangle is defined by its <strong>bottom-left</strong>{" "}
+        corner <code>(ax1, ay1)</code> and its <strong>top-right</strong> corner{" "}
+        <code>(ax2, ay2)</code>.
+      </p>
+      <p>
+        The second rectangle is defined by its <strong>bottom-left</strong>{" "}
+        corner <code>(bx1, by1)</code> and its <strong>top-right</strong> corner{" "}
+        <code>(bx2, by2)</code>.
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <img
+        alt="Rectangle Area"
+        src="https://assets.leetcode.com/uploads/2021/05/08/rectangle-plane.png"
+        style={{ width: 700, height: 365 }}
+      />
+      <pre>
+        <strong>Input:</strong> ax1 = -3, ay1 = 0, ax2 = 3, ay2 = 4, bx1 = 0,
+        by1 = -1, bx2 = 9, by2 = 2 <br />
+        <strong>Output:</strong> 45
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> ax1 = -2, ay1 = -2, ax2 = 2, ay2 = 2, bx1 = -2,
+        by1 = -2, bx2 = 2, by2 = 2 <br />
+        <strong>Output:</strong> 16
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= ax1 &lt;= ax2 &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= ay1 &lt;= ay2 &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= bx1 &lt;= bx2 &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= by1 &lt;= by2 &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
