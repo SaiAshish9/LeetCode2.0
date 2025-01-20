102c25d20eb2c1bcac1e1936752a69959672e37f
saiashish9 <saiashish3760@gmail.com>
2025-01-20 19:18:38 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -82866,6 +82866,81 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  60: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        The set <code>[1, 2, 3, ...,&nbsp;n]</code> contains a total of{" "}
+        <code>n!</code> unique permutations.
+      </p>
+      <p>
+        By listing and labeling all of the permutations in order, we get the
+        following sequence for <code>n = 3</code>:
+      </p>
+      <ul>
+        <li>
+          <code>"123"</code>
+        </li>
+        <li>
+          <code>"132"</code>
+        </li>
+        <li>
+          <code>"213"</code>
+        </li>
+        <li>
+          <code>"231"</code>
+        </li>
+        <li>
+          <code>"312"</code>
+        </li>
+        <li>
+          <code>"321"</code>
+        </li>
+      </ul>
+      <p>
+        Given <code>n</code> and <code>k</code>, return the{" "}
+        <code>
+          k<sup>th</sup>
+        </code>{" "}
+        permutation sequence.
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> n = 3, k = 3 <br />
+        <strong>Output:</strong> "213" <br />
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> n = 4, k = 9 <br />
+        <strong>Output:</strong> "2314" <br />
+      </pre>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> n = 3, k = 1 <br />
+        <strong>Output:</strong> "123"
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>1 &lt;= n &lt;= 9</code>
+        </li>
+        <li>
+          <code>1 &lt;= k &lt;= n!</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
