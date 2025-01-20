a58ad7c0e65e855b22f5e497d7321345abf07934
saiashish9 <saiashish3760@gmail.com>
2025-01-21 00:12:57 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -83529,6 +83529,50 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  479: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer n, return{" "}
+        <em>
+          the <strong>largest palindromic integer</strong> that can be
+          represented as the product of two <code>n</code>-digits integers
+        </em>
+        . Since the answer can be very large, return it <strong>modulo</strong>{" "}
+        <code>1337</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2 <br />
+        <strong>Output:</strong> 987 <br />
+        Explanation: 99 x 91 = 9009, 9009 % 1337 = 987
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 1 <br />
+        <strong>Output:</strong> 9
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= n &lt;= 8</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
