64aecb3f4463c4d4189bbcbfd2c24a5fb7921d9b
saiashish9 <saiashish3760@gmail.com>
2025-01-20 23:53:08 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -83295,6 +83295,64 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  326: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer <code>n</code>, return{" "}
+        <em>
+          <code>true</code> if it is a power of three. Otherwise, return{" "}
+          <code>false</code>
+        </em>
+        .
+      </p>
+      <p>
+        An integer <code>n</code> is a power of three, if there exists an
+        integer <code>x</code> such that{" "}
+        <code>
+          n == 3<sup>x</sup>
+        </code>
+        .
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> n = 27 <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> 27 = 3<sup>3</sup>
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> n = 0 <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> There is no x where 3<sup>x</sup> = 0.
+      </pre>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> n = -1 <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> There is no x where 3<sup>x</sup> = (-1).
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>
+            -2<sup>31</sup> &lt;= n &lt;= 2<sup>31</sup> - 1
+          </code>
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <strong>Follow up:</strong> Could you solve it without loops/recursion?
+    </div>
+  ),
 };
 
 export default QUESTIONS;
