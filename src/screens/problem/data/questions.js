a1dae3d0de8961c7bdd935cd7f8bb931d81ec013
saiashish9 <saiashish3760@gmail.com>
2025-01-21 10:12:19 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -83865,6 +83865,61 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </p>
     </div>
   ),
+  728: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        A <strong>self-dividing number</strong> is a number that is divisible by
+        every digit it contains.
+      </p>
+      <ul>
+        <li>
+          For example, <code>128</code> is{" "}
+          <strong>a self-dividing number</strong> because{" "}
+          <code>128 % 1 == 0</code>, <code>128 % 2 == 0</code>, and{" "}
+          <code>128 % 8 == 0</code>.
+        </li>
+      </ul>
+      <p>
+        A <strong>self-dividing number</strong> is not allowed to contain the
+        digit zero.
+      </p>
+      <p>
+        Given two integers <code>left</code> and <code>right</code>, return{" "}
+        <em>
+          a list of all the <strong>self-dividing numbers</strong> in the range
+        </em>{" "}
+        <code>[left, right]</code> (both <strong>inclusive</strong>).
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> left = 1, right = 22 <br />
+        <strong>Output:</strong> [1,2,3,4,5,6,7,8,9,11,12,15,22] <br />
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> left = 47, right = 85 <br />
+        <strong>Output:</strong> [48,55,66,77]
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>
+            1 &lt;= left &lt;= right &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
