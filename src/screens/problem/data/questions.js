7f9afd17be049b95106641fd7ecb1ef8c00c76f7
saiashish9 <saiashish3760@gmail.com>
2025-01-21 22:08:19 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -84156,6 +84156,65 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1017: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer <code>n</code>, return{" "}
+        <em>a binary string representing its representation in base</em>{" "}
+        <code>-2</code>.
+      </p>
+
+      <p>
+        <strong>Note</strong> that the returned string should not have leading
+        zeros unless the string is <code>"0"</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2 <br />
+        <strong>Output:</strong> "110" <br />
+        <strong>Explantion:</strong> (-2)<sup>2</sup> + (-2)<sup>1</sup> = 2
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 3 <br />
+        <strong>Output:</strong> "111" <br />
+        <strong>Explantion:</strong> (-2)<sup>2</sup> + (-2)<sup>1</sup> + (-2)
+        <sup>0</sup> = 3
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 4 <br />
+        <strong>Output:</strong> "100" <br />
+        <strong>Explantion:</strong> (-2)<sup>2</sup> = 4
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            0 &lt;= n &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
