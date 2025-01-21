68838e30133f49703bdfd4ade7bb623b9e9d3934
saiashish9 <saiashish3760@gmail.com>
2025-01-21 23:47:36 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -84647,6 +84647,84 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1276: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given two integers <code>tomatoSlices</code> and{" "}
+        <code>cheeseSlices</code>. The ingredients of different burgers are as
+        follows:
+      </p>
+
+      <ul>
+        <li>
+          <strong>Jumbo Burger:</strong> <code>4</code> tomato slices and{" "}
+          <code>1</code> cheese slice.
+        </li>
+        <li>
+          <strong>Small Burger:</strong> <code>2</code> Tomato slices and{" "}
+          <code>1</code> cheese slice.
+        </li>
+      </ul>
+
+      <p>
+        Return <code>[total_jumbo, total_small]</code> so that the number of
+        remaining <code>tomatoSlices</code> equal to <code>0</code> and the
+        number of remaining <code>cheeseSlices</code> equal to <code>0</code>.
+        If it is not possible to make the remaining <code>tomatoSlices</code>{" "}
+        and <code>cheeseSlices</code> equal to <code>0</code> return{" "}
+        <code>[]</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> tomatoSlices = 16, cheeseSlices = 7 <br />
+        <strong>Output:</strong> [1,6] <br />
+        <strong>Explantion:</strong> To make one jumbo burger and 6 small
+        burgers we need 4*1 + 2*6 = 16 tomato and 1 + 6 = 7 cheese.
+        <br /> There will be no remaining ingredients.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> tomatoSlices = 17, cheeseSlices = 4 <br />
+        <strong>Output:</strong> [] <br />
+        <strong>Explantion:</strong> There will be no way to use all ingredients
+        to make small and jumbo burgers.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> tomatoSlices = 4, cheeseSlices = 17 <br />
+        <strong>Output:</strong> [] <br />
+        <strong>Explantion:</strong> Making 1 jumbo burger there will be 16
+        cheese remaining and making 2 small burgers there will be 15 cheese
+        remaining.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            0 &lt;= tomatoSlices, cheeseSlices &lt;= 10<sup>7</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
