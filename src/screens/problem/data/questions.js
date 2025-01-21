e8f7bc8361074a5d55d0cbae705efb67ca8a34f6
saiashish9 <saiashish3760@gmail.com>
2025-01-21 23:41:18 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -84593,6 +84593,60 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1228: (
+    <div class="markdown-body div-width">
+      <p>
+        In some array <code>arr</code>, the values were in arithmetic
+        progression: the values&nbsp;<code>arr[i+1] - arr[i]</code>&nbsp;are
+        all&nbsp;equal for every&nbsp;<code>0 &lt;= i &lt; arr.length - 1</code>
+        .
+      </p>
+
+      <p>
+        Then, a value from <code>arr</code>&nbsp;was removed that{" "}
+        <strong>was&nbsp;not the first or last value in the array</strong>.
+      </p>
+
+      <p>Return the removed value.</p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [5,7,11,13] <br />
+        <strong>Output:</strong> 9 <br />
+        <strong>Explanation: </strong>The previous array was [5,7,
+        <strong>9</strong>,11,13].
+      </pre>
+
+      <p>
+        <strong>Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> arr = [15,13,12] <br />
+        <strong>Output:</strong> 14 <br />
+        <strong>Explanation: </strong>The previous array was [15,
+        <strong>14</strong>,13,12].
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>3 &lt;= arr.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>0 &lt;= arr[i] &lt;= 10^5</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
