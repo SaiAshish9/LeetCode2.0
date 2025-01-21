fe016b3ce6161877945f9b0a259f8bf0336ea804
saiashish9 <saiashish3760@gmail.com>
2025-01-21 23:21:03 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -84411,6 +84411,64 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1118: (
+    <div class="markdown-body div-width" style="margin-top: 20px">
+      <p>
+        Given a year <code>Y</code> and a month <code>M</code>, return how many
+        days there are in that month.
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>Y = <span id="example-input-1-1">1992</span>, M
+        = <span id="example-input-1-2">7</span>
+        <br /> <strong>Output: </strong>
+        <span id="example-output-1">31</span>
+      </pre>
+
+      <p>
+        <strong>Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>Y = <span id="example-input-2-1">2000</span>, M
+        = <span id="example-input-2-2">2</span>
+        <br /> <strong>Output: </strong>
+        <span id="example-output-2">29</span>
+      </pre>
+
+      <p>
+        <strong>Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>Y = <span id="example-input-3-1">1900</span>, M
+        = <span id="example-input-3-2">2</span>
+        <br /> <strong>Output: </strong>
+        <span id="example-output-3">28</span>
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Note:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1583 &lt;= Y &lt;= 2100</code>
+        </li>
+        <li>
+          <code>1 &lt;= M &lt;= 12</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
