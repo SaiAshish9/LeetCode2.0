0079816ec87533dfeadc9244d2ce6d4e12387c93
saiashish9 <saiashish3760@gmail.com>
2025-01-21 23:35:54 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -84525,6 +84525,74 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </article>
   ),
+  1185: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a date, return the corresponding day of the week for that date.
+      </p>
+
+      <p>
+        The input is given as three integers representing the <code>day</code>,{" "}
+        <code>month</code> and <code>year</code> respectively.
+      </p>
+
+      <p>
+        Return the answer as one of the following values&nbsp;
+        <code>
+          {`("Sunday",
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday")
+          `}
+        </code>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> day = 31, month = 8, year = 2019 <br />
+        <strong>Output:</strong> "Saturday"
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> day = 18, month = 7, year = 1999 <br />
+        <strong>Output:</strong> "Sunday"
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> day = 15, month = 8, year = 1993
+        <br />
+        <strong>Output:</strong> "Sunday"
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The given dates are valid dates between the years <code>1971</code>{" "}
+          and <code>2100</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
