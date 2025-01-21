1b0f34872dea5a4c05767e1b5e9ce4b27c1915d8
saiashish9 <saiashish3760@gmail.com>
2025-01-22 01:17:33 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -85520,7 +85520,7 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
 
       <p>&nbsp;</p>
       <p>
-        <strong class="example">Example 1:</strong> <br/>
+        <strong class="example">Example 1:</strong> <br />
       </p>
 
       <pre>
@@ -85531,7 +85531,7 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </pre>
 
       <p>
-        <strong class="example">Example 2:</strong> <br/>
+        <strong class="example">Example 2:</strong> <br />
       </p>
 
       <pre>
@@ -85549,6 +85549,77 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       <ul>
         <li>
           <code>1 &lt;= n &lt;= 250</code>
+        </li>
+      </ul>
+    </div>
+  ),
+  2119: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        <strong>Reversing</strong> an integer means to reverse all its digits.
+      </p>
+
+      <ul>
+        <li>
+          For example, reversing <code>2021</code> gives <code>1202</code>.
+          Reversing <code>12300</code> gives <code>321</code> as the{" "}
+          <strong>leading zeros are not retained</strong>.
+        </li>
+      </ul>
+
+      <p>
+        Given an integer <code>num</code>, <strong>reverse</strong>{" "}
+        <code>num</code> to get <code>reversed1</code>,{" "}
+        <strong>then reverse</strong> <code>reversed1</code> to get{" "}
+        <code>reversed2</code>. Return <code>true</code> <em>if</em>{" "}
+        <code>reversed2</code> <em>equals</em> <code>num</code>. Otherwise
+        return <code>false</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> num = 526 <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> Reverse num to get 625, then reverse 625
+        to get 526, which equals num.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> num = 1800 <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> Reverse num to get 81, then reverse 81 to
+        get 18, which does not equal num.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> num = 0 <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> Reverse num to get 0, then reverse 0 to
+        get 0, which equals num.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            0 &lt;= num &lt;= 10<sup>6</sup>
+          </code>
         </li>
       </ul>
     </div>
