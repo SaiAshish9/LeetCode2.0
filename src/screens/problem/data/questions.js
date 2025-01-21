9a5021cb5c90031aaee808a54581ea9c13ffd359
saiashish9 <saiashish3760@gmail.com>
2025-01-22 01:34:48 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -85694,6 +85694,41 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  2235: (
+    <div class="elfjS" data-track-load="description_content">
+      Given two integers <code>num1</code> and <code>num2</code>, return{" "}
+      <em>
+        the <strong>sum</strong> of the two integers
+      </em>
+      .<p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong> <br />
+      </p>
+      <pre>
+        <strong>Input:</strong> num1 = 12, num2 = 5 <br />
+        <strong>Output:</strong> 17 <br />
+        <strong>Explanation:</strong> num1 is 12, num2 is 5, and their sum is 12
+        + 5 = 17, so 17 is returned.
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong> <br />
+      </p>
+      <pre>
+        <strong>Input:</strong> num1 = -10, num2 = 4 <br />
+        <strong>Output:</strong> -6 <br />
+        <strong>Explanation:</strong> num1 + num2 = -6, so -6 is returned.
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>-100 &lt;= num1, num2 &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
