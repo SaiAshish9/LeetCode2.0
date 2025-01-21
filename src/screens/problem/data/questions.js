147f9a049d5d16651606949b7f886a3928647052
saiashish9 <saiashish3760@gmail.com>
2025-01-22 00:15:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -84885,6 +84885,84 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1323: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a positive integer <code>num</code> consisting only of
+        digits <code>6</code> and <code>9</code>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the maximum number you can get by changing <strong>at most</strong>{" "}
+          one digit (
+        </em>
+        <code>6</code>
+        <em> becomes </em>
+        <code>9</code>
+        <em>, and </em>
+        <code>9</code>
+        <em> becomes </em>
+        <code>6</code>
+        <em>)</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> num = 9669 <br />
+        <strong>Output:</strong> 9969 <br />
+        <strong>Explanation:</strong> <br />
+        Changing the first digit results in 6669. <br />
+        Changing the second digit results in 9969. <br />
+        Changing the third digit results in 9699. <br />
+        Changing the fourth digit results in 9666. <br />
+        The maximum number is 9969.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> num = 9996 <br />
+        <strong>Output:</strong> 9999 <br />
+        <strong>Explanation:</strong> Changing the last digit 6 to 9 results in
+        the maximum number.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> num = 9999 <br />
+        <strong>Output:</strong> 9999 <br />
+        <strong>Explanation:</strong> It is better not to apply any change.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= num &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>num</code>&nbsp;consists of only <code>6</code> and{" "}
+          <code>9</code> digits.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
