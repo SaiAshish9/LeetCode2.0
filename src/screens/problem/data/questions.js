96f69532f74bff01bf32becf88bae3afacfce370
saiashish9 <saiashish3760@gmail.com>
2025-01-21 10:15:35 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -83920,6 +83920,74 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  780: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given four integers <code>sx</code>, <code>sy</code>, <code>tx</code>,
+        and <code>ty</code>, return <code>true</code>
+        <em> if it is possible to convert the point </em>
+        <code>(sx, sy)</code>
+        <em> to the point </em>
+        <code>(tx, ty)</code> <em>through some operations</em>
+        <em>, or </em>
+        <code>false</code>
+        <em> otherwise</em>.
+      </p>
+
+      <p>
+        The allowed operation on some point <code>(x, y)</code> is to convert it
+        to either <code>(x, x + y)</code> or <code>(x + y, y)</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> sx = 1, sy = 1, tx = 3, ty = 5 <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong>
+        <br />
+        One series of moves that transforms the starting point to the target is:
+        <br />
+        {`(1, 1) -&gt; (1, 2)
+  (1, 2) -&gt; (3, 2)
+  (3, 2) -&gt; (3, 5)`}
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> sx = 1, sy = 1, tx = 2, ty = 2 <br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> sx = 1, sy = 1, tx = 1, ty = 1 <br />
+        <strong>Output:</strong> true
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= sx, sy, tx, ty &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
