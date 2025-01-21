730b2a4215e8229d262a37321aca620c62285afa
saiashish9 <saiashish3760@gmail.com>
2025-01-22 01:14:27 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -85498,6 +85498,61 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1925: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        A <strong>square triple</strong> <code>(a,b,c)</code> is a triple where{" "}
+        <code>a</code>, <code>b</code>, and <code>c</code> are{" "}
+        <strong>integers</strong> and{" "}
+        <code>
+          a<sup>2</sup> + b<sup>2</sup> = c<sup>2</sup>
+        </code>
+        .
+      </p>
+
+      <p>
+        Given an integer <code>n</code>, return{" "}
+        <em>
+          the number of <strong>square triples</strong> such that{" "}
+        </em>
+        <code>1 &lt;= a, b, c &lt;= n</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong> <br/>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 5 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation</strong>: The square triples are (3,4,5) and
+        (4,3,5).
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong> <br/>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 10 <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation</strong>: The square triples are (3,4,5), (4,3,5),
+        (6,8,10), and (8,6,10).
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= n &lt;= 250</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
