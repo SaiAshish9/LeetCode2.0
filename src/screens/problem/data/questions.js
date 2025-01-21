6984f872414a4209f312c18b65707401be4319d9
saiashish9 <saiashish3760@gmail.com>
2025-01-22 01:42:14 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -85762,7 +85762,7 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
 
       <p>&nbsp;</p>
       <p>
-        <strong class="example">Example 1:</strong> <br/>
+        <strong class="example">Example 1:</strong> <br />
       </p>
 
       <pre>
@@ -85776,7 +85776,7 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </pre>
 
       <p>
-        <strong class="example">Example 2:</strong> <br/>
+        <strong class="example">Example 2:</strong> <br />
       </p>
 
       <pre>
@@ -85802,6 +85802,80 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
         </li>
         <li>
           The given dates are valid dates of a <strong>non-leap</strong> year.
+        </li>
+      </ul>
+    </div>
+  ),
+  2469: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a non-negative floating point number rounded to two
+        decimal places <code>celsius</code>, that denotes the{" "}
+        <strong>temperature in Celsius</strong>.
+      </p>
+
+      <p>
+        You should convert Celsius into <strong>Kelvin</strong> and{" "}
+        <strong>Fahrenheit</strong> and return it as an array{" "}
+        <code>ans = [kelvin, fahrenheit]</code>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the array <code>ans</code>.{" "}
+        </em>
+        Answers within{" "}
+        <code>
+          10<sup>-5</sup>
+        </code>{" "}
+        of the actual answer will be accepted.
+      </p>
+
+      <p>
+        <strong>Note that:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>Kelvin = Celsius + 273.15</code>
+        </li>
+        <li>
+          <code>Fahrenheit = Celsius * 1.80 + 32.00</code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> celsius = 36.50 <br />
+        <strong>Output:</strong> [309.65000,97.70000] <br />
+        <strong>Explanation:</strong> Temperature at 36.50 Celsius converted in
+        Kelvin is 309.65 and converted in Fahrenheit is 97.70.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> celsius = 122.11 <br />
+        <strong>Output:</strong> [395.26000,251.79800] <br />
+        <strong>Explanation:</strong> Temperature at 122.11 Celsius converted in
+        Kelvin is 395.26 and converted in Fahrenheit is 251.798.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>0 &lt;= celsius &lt;= 1000</code>
         </li>
       </ul>
     </div>
