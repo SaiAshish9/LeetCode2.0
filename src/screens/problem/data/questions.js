6b26db865ced8b38520b7c6207b83ac10b8e4d19
saiashish9 <saiashish3760@gmail.com>
2025-01-21 22:16:57 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -84215,6 +84215,131 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1056: (
+    <div class="markdown-body div-width">
+      <p>
+        Given a number <code>N</code>, return <code>true</code> if and only if
+        it is a <em>confusing number</em>, which satisfies the following
+        condition:
+      </p>
+
+      <p>
+        We can rotate digits by 180 degrees to form new digits. When 0, 1, 6, 8,
+        9 are rotated 180 degrees, they become 0, 1, 9, 8, 6 respectively. When
+        2, 3, 4, 5 and 7 are rotated 180 degrees, they become invalid. A{" "}
+        <em>confusing number</em> is a number that when rotated 180 degrees
+        becomes a <strong>different</strong> number with each digit valid.
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Example 1:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2019/03/23/1268_1.png"
+          style={{ width: 180, height: 90 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <span id="example-input-1-1">6</span> <br />
+        <strong>Output: </strong>
+        <span id="example-output-1">true</span> <br />
+        <strong>Explanation: </strong> <br />
+        We get <code>9</code> after rotating <code>6</code>, <code>9</code> is a
+        valid number and <code>9!=6</code>.
+      </pre>
+
+      <p>
+        <strong>Example 2:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2019/03/23/1268_2.png"
+          style={{ width: 180, height: 90 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <span id="example-input-2-1">89</span> <br />
+        <strong>Output: </strong>
+        <span id="example-output-2">true</span> <br />
+        <strong>Explanation: </strong> <br />
+        We get <code>68</code> after rotating <code>89</code>, <code>86</code>{" "}
+        is a valid number and <code>86!=89</code>.
+      </pre>
+
+      <p>
+        <strong>Example 3:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2019/03/26/1268_3.png"
+          style={{ width: 301, height: 121 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <span id="example-input-3-1">11</span>
+        <strong>Output: </strong>
+        <span id="example-output-3">false</span> <br />
+        <strong>Explanation: </strong> <br />
+        We get <code>11</code> after rotating <code>11</code>, <code>11</code>{" "}
+        is a valid number but the value remains the same, thus <code>11</code>{" "}
+        is not a confusing number.
+      </pre>
+
+      <p>
+        <strong>Example 4:</strong>
+      </p>
+
+      <p>
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2019/03/23/1268_4.png"
+          style={{ width: 180, height: 90 }}
+        />
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <span id="example-input-4-1">25</span>
+        <strong>Output: </strong>
+        <span id="example-output-4">false</span> <br />
+        <strong>Explanation: </strong> <br />
+        We get an invalid number after rotating <code>25</code>.
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Note:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>0 &lt;= N &lt;= 10^9</code>
+        </li>
+        <li>
+          After the rotation we can ignore leading zeros, for example if after
+          rotation we have
+          <code>0008</code>&nbsp;then this number is considered as just{" "}
+          <code>8</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
