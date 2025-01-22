69961f009f4f29ab1ce97bb34048245661c78754
saiashish9 <saiashish3760@gmail.com>
2025-01-22 23:32:38 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -88138,6 +88138,90 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  517: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You have <code>n</code> super washing machines on a line. Initially,
+        each washing machine has some dresses or is empty.
+      </p>
+
+      <p>
+        For each move, you could choose any <code>m</code> (
+        <code>1 &lt;= m &lt;= n</code>) washing machines, and pass one dress of
+        each washing machine to one of its adjacent washing machines at the same
+        time.
+      </p>
+
+      <p>
+        Given an integer array <code>machines</code> representing the number of
+        dresses in each washing machine from left to right on the line, return{" "}
+        <em>
+          the minimum number of moves to make all the washing machines have the
+          same number of dresses
+        </em>
+        . If it is not possible to do it, return <code>-1</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> machines = [1,0,5] <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> <br />
+        {`1st move:    1     0 &lt;-- 5    =&gt;    1     1     4 
+    2nd move:    1 &lt;-- 1 &lt;-- 4    =&gt;    2     1     3
+    3rd move:    2     1 &lt;-- 3    =&gt;    2     2     2`}
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> machines = [0,3,0] <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong>
+        {`1st move:    0 &lt;-- 3     0    =&gt;    1     2     0
+    2nd move:    1     2 --&gt; 0    =&gt;    1     1     1`}
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> machines = [0,2,0] <br />
+        <strong>Output:</strong> -1 <br />
+        <strong>Explanation:</strong> <br />
+        It's impossible to make all three washing machines have the same number
+        of dresses.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == machines.length</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= machines[i] &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
