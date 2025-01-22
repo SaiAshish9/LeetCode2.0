9035869a2f2a757f768367332b6e8f7c44a578c7
saiashish9 <saiashish3760@gmail.com>
2025-01-23 00:16:38 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -88654,6 +88654,64 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  984: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given two integers <code>a</code> and <code>b</code>, return{" "}
+        <strong>any</strong> string <code>s</code> such that:
+      </p>
+
+      <ul>
+        <li>
+          <code>s</code> has length <code>a + b</code> and contains exactly{" "}
+          <code>a</code> <code>'a'</code> letters, and exactly <code>b</code>{" "}
+          <code>'b'</code> letters,
+        </li>
+        <li>
+          The substring <code>'aaa'</code> does not occur in <code>s</code>, and
+        </li>
+        <li>
+          The substring <code>'bbb'</code> does not occur in <code>s</code>.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> a = 1, b = 2 <br />
+        <strong>Output:</strong> "abb" <br />
+        <strong>Explanation:</strong> "abb", "bab" and "bba" are all correct
+        answers.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> a = 4, b = 1 <br />
+        <strong>Output:</strong> "aabaa"
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>0 &lt;= a, b &lt;= 100</code>
+        </li>
+        <li>
+          It is guaranteed such an <code>s</code> exists for the given{" "}
+          <code>a</code> and <code>b</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
