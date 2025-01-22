db76620b80ec481aebe2b901aee6b92ea174421e
saiashish9 <saiashish3760@gmail.com>
2025-01-22 22:59:19 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -87645,6 +87645,81 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  330: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a sorted integer array <code>nums</code> and an integer{" "}
+        <code>n</code>, add/patch elements to the array such that any number in
+        the range <code>[1, n]</code> inclusive can be formed by the sum of some
+        elements in the array.
+      </p>
+
+      <p>
+        Return <em>the minimum number of patches required</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,3], n = 6 <br />
+        <strong>Output:</strong> 1 <br />
+        Explanation: <br />
+        Combinations of nums are [1], [3], [1,3], which form possible sums of:
+        1, 3, 4. <br />
+        Now if we add/patch 2 to nums, the combinations are: [1], [2], [3],
+        [1,3], [2,3], [1,2,3]. <br />
+        Possible sums are 1, 2, 3, 4, 5, 6, which now covers the range [1, 6].{" "}
+        <br />
+        So we only need 1 patch.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,5,10], n = 20 <br />
+        <strong>Output:</strong> 2 <br />
+        Explanation: The two patches can be [2, 4].
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,2], n = 5 <br />
+        <strong>Output:</strong> 0
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= nums[i] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>nums</code> is sorted in <strong>ascending order</strong>.
+        </li>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 2<sup>31</sup> - 1
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
