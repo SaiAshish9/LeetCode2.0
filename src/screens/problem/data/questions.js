3fa3192893e0f53ccb75bf164519a34867edd671
saiashish9 <saiashish3760@gmail.com>
2025-01-22 23:28:51 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -88033,6 +88033,111 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       <p></p>
     </div>
   ),
+  502: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Suppose LeetCode will start its <strong>IPO</strong> soon. In order to
+        sell a good price of its shares to Venture Capital, LeetCode would like
+        to work on some projects to increase its capital before the{" "}
+        <strong>IPO</strong>. Since it has limited resources, it can only finish
+        at most <code>k</code> distinct projects before the <strong>IPO</strong>
+        . Help LeetCode design the best way to maximize its total capital after
+        finishing at most <code>k</code> distinct projects.
+      </p>
+
+      <p>
+        You are given <code>n</code> projects where the{" "}
+        <code>
+          i<sup>th</sup>
+        </code>{" "}
+        project has a pure profit <code>profits[i]</code> and a minimum capital
+        of <code>capital[i]</code> is needed to start it.
+      </p>
+
+      <p>
+        Initially, you have <code>w</code> capital. When you finish a project,
+        you will obtain its pure profit and the profit will be added to your
+        total capital.
+      </p>
+
+      <p>
+        Pick a list of <strong>at most</strong> <code>k</code> distinct projects
+        from given projects to <strong>maximize your final capital</strong>, and
+        return <em>the final maximized capital</em>.
+      </p>
+
+      <p>The answer is guaranteed to fit in a 32-bit signed integer.</p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> k = 2, w = 0, profits = [1,2,3], capital =
+        [0,1,1] <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> Since your initial capital is 0, you can
+        only start the project indexed 0. <br />
+        After finishing it you will obtain profit 1 and your capital becomes 1.
+        <br />
+        With capital 1, you can either start the project indexed 1 or the
+        project indexed 2. <br />
+        Since you can choose at most 2 projects, you need to finish the project
+        indexed 2 to get the maximum capital. <br />
+        Therefore, output the final maximized capital, which is 0 + 1 + 3 = 4.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> k = 3, w = 0, profits = [1,2,3], capital =
+        [0,1,2] <br />
+        <strong>Output:</strong> 6
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= k &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= w &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+        <li>
+          <code>n == profits.length</code>
+        </li>
+        <li>
+          <code>n == capital.length</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= profits[i] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= capital[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
