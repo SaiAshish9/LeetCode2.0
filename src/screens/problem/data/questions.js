2368bf5d73ad882c666f4ad442cd1f6c9075dd29
saiashish9 <saiashish3760@gmail.com>
2025-01-22 23:56:30 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -88402,6 +88402,37 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  625: (
+    <div class="markdown-body div-width">
+      <p>
+        Given a positive integer <code>a</code>, find the smallest positive
+        integer <code>b</code>
+        whose multiplication of each digit equals to <code>a</code>.{" "}
+      </p>
+      <p>
+        If there is no answer or the answer is not fit in 32-bit signed integer,
+        then return 0.
+      </p>
+      <p>
+        <b>Example 1</b>
+        <br />
+        Input:
+      </p>
+      <pre>48 </pre> <br />
+      Output:
+      <pre>68</pre>
+      <p></p>
+      <p>
+        <b>Example 2</b>
+        <br />
+        Input:
+      </p>
+      <pre>15</pre> <br />
+      Output:
+      <pre>35</pre>
+      <p></p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
