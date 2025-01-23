00c555f7b9fe7e3bcb264bb4ccd613d6e83ccab3
saiashish9 <saiashish3760@gmail.com>
2025-01-23 10:40:41 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -89495,6 +89495,78 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1864: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a binary string <code>s</code>, return{" "}
+        <em>
+          the <strong>minimum</strong> number of character swaps to make it{" "}
+          <strong>alternating</strong>, or{" "}
+        </em>
+        <code>-1</code>
+        <em> if it is impossible.</em>
+      </p>
+
+      <p>
+        The string is called <strong>alternating</strong> if no two adjacent
+        characters are equal. For example, the strings <code>"010"</code> and{" "}
+        <code>"1010"</code> are alternating, while the string{" "}
+        <code>"0100"</code> is not.
+      </p>
+
+      <p>
+        Any two characters may be swapped, even if they are&nbsp;
+        <strong>not adjacent</strong>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "111000" <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> Swap positions 1 and 4: "1<u>1</u>10
+        <u>0</u>0" -&gt; "1<u>0</u>10<u>1</u>0" <br />
+        The string is now alternating.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "010" <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> The string is already alternating, no
+        swaps are needed.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "1110" <br />
+        <strong>Output:</strong> -1
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= s.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>s[i]</code> is either <code>'0'</code> or <code>'1'</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
