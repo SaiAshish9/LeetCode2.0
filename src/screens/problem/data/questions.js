71d5b819e2c56e6e236ede6c5c1960b927c21073
saiashish9 <saiashish3760@gmail.com>
2025-01-23 11:14:31 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -90564,6 +90564,72 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  2847: (
+    <article role="main" class="blog-post">
+      <p>
+        Given a <strong>positive</strong> integer <code>n</code>, return{" "}
+        <em>
+          a string representing the <strong>smallest positive</strong> integer
+          such that the product of its digits is equal to
+        </em>{" "}
+        <code>n</code>
+        <em>, or </em>
+        <code>"-1"</code>
+        <em> if no such number exists</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 105 <br />
+        <strong>Output:</strong> "357" <br />
+        <strong>Explanation:</strong> 3 * 5 * 7 = 105. It can be shown that 357
+        is the smallest number with a product of digits equal to 105. So the
+        answer would be "105".
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 7 <br />
+        <strong>Output:</strong> "7" <br />
+        <strong>Explanation:</strong> Since 7 has only one digit, its product of
+        digits would be 7. We will show that 7 is the smallest number with a
+        product of digits equal to 7. Since the product of numbers 1 to 6 is 1
+        to 6 respectively, so "7" would be the answer.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 44 <br />
+        <strong>Output:</strong> "-1" <br />
+        <strong>Explanation:</strong> It can be shown that there is no number
+        such that its product of digits is equal to 44. So the answer would be
+        "-1".
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>18</sup>
+          </code>
+        </li>
+      </ul>
+    </article>
+  ),
 };
 
 export default QUESTIONS;
