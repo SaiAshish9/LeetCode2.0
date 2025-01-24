cfe55feeeee88eb84326e260ba8179bdcf47a9ed
saiashish9 <saiashish3760@gmail.com>
2025-01-25 00:41:32 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -92725,6 +92725,80 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  652: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code>&nbsp;of a binary tree, return all{" "}
+        <strong>duplicate subtrees</strong>.
+      </p>
+
+      <p>
+        For each kind of duplicate subtrees, you only need to return the root
+        node of any <b>one</b> of them.
+      </p>
+
+      <p>
+        Two trees are <strong>duplicate</strong> if they have the{" "}
+        <strong>same structure</strong> with the{" "}
+        <strong>same node values</strong>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/08/16/e1.jpg"
+        style={{ width: 450, height: 354 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [1,2,3,4,null,2,4,null,null,4] <br />
+        <strong>Output:</strong> [[2,4],[4]]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/08/16/e2.jpg"
+        style={{ width: 321, height: 201 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [2,1,1] <br />
+        <strong>Output:</strong> [[1]]
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/08/16/e33.jpg"
+        style={{ width: 450, height: 303 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [2,2,2,3,null,3,null] <br />
+        <strong>Output:</strong> [[2,3],[3]]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of the nodes in the tree will be in the range{" "}
+          <code>[1, 5000]</code>
+        </li>
+        <li>
+          <code>-200 &lt;= Node.val &lt;= 200</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
