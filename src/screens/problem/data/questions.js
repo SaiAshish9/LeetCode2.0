d12560952840e9e7eca1f9cddbb9310e3f0729a9
saiashish9 <saiashish3760@gmail.com>
2025-01-25 00:24:53 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -92655,6 +92655,76 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  572: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the roots of two binary trees <code>root</code> and{" "}
+        <code>subRoot</code>, return <code>true</code> if there is a subtree of{" "}
+        <code>root</code> with the same structure and node values of
+        <code> subRoot</code> and <code>false</code> otherwise.
+      </p>
+
+      <p>
+        A subtree of a binary tree <code>tree</code> is a tree that consists of
+        a node in <code>tree</code> and all of this node's descendants. The tree{" "}
+        <code>tree</code> could also be considered as a subtree of itself.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/04/28/subtree1-tree.jpg"
+        style={{ width: 532, height: 400 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [3,4,5,1,2], subRoot = [4,1,2] <br />
+        <strong>Output:</strong> true <br />
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/04/28/subtree2-tree.jpg"
+        style={{ width: 502, height: 458 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [3,4,5,1,2,null,null,null,null,0],
+        subRoot = [4,1,2] <br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the <code>root</code> tree is in the range{" "}
+          <code>[1, 2000]</code>.
+        </li>
+        <li>
+          The number of nodes in the <code>subRoot</code> tree is in the range{" "}
+          <code>[1, 1000]</code>.
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= root.val &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= subRoot.val &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
