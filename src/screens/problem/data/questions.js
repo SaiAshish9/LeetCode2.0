fe9f60849d1d55dff021f5807404afe859bab034
saiashish9 <saiashish3760@gmail.com>
2025-01-24 19:02:52 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -91616,6 +91616,92 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       <code>O(1)</code> extra space)?
     </div>
   ),
+  116: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <strong>perfect binary tree</strong> where all leaves
+        are on the same level, and every parent has two children. The binary
+        tree has the following definition:
+      </p>
+
+      <pre>
+        {`struct Node {
+      int val;
+      Node *left;
+      Node *right;
+      Node *next;
+    }`}
+      </pre>
+
+      <p>
+        Populate each next pointer to point to its next right node. If there is
+        no next right node, the next pointer should be set to <code>NULL</code>.
+      </p>
+
+      <p>
+        Initially, all next pointers are set to <code>NULL</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2019/02/14/116_sample.png"
+        style={{ width: 500, height: 171 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [1,2,3,4,5,6,7] <br />
+        <strong>Output:</strong> [1,#,2,3,#,4,5,6,7,#] <br />
+        <strong>Explanation: </strong>Given the above perfect binary tree
+        (Figure A), your function should populate each next pointer to point to
+        its next right node, just like in Figure B. The serialized output is in
+        level order as connected by the next pointers, with '#' signifying the
+        end of each level.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [] <br />
+        <strong>Output:</strong> []
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range{" "}
+          <code>
+            [0, 2<sup>12</sup> - 1]
+          </code>
+          .
+        </li>
+        <li>
+          <code>-1000 &lt;= Node.val &lt;= 1000</code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow-up:</strong>
+      </p>
+
+      <ul>
+        <li>You may only use constant extra space.</li>
+        <li>
+          The recursive approach is fine. You may assume implicit stack space
+          does not count as extra space for this problem.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
