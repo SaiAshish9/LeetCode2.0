edf1758d828e1f15f7ad6c03201f0319cf35bc20
saiashish9 <saiashish3760@gmail.com>
2025-01-24 18:27:37 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -91399,6 +91399,77 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       iteratively?
     </div>
   ),
+  112: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary tree and an integer{" "}
+        <code>targetSum</code>, return <code>true</code> if the tree has a{" "}
+        <strong>root-to-leaf</strong> path such that adding up all the values
+        along the path equals <code>targetSum</code>.
+      </p>
+      <p>
+        A <strong>leaf</strong> is a node with no children.
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/01/18/pathsum1.jpg"
+        style={{ width: 500, height: 356 }}
+      />
+      <pre>
+        <strong>Input:</strong> root =
+        [5,4,8,11,null,13,4,7,2,null,null,null,1], targetSum = 22 <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> The root-to-leaf path with the target sum
+        is shown.
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/01/18/pathsum2.jpg"
+      />{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> root = [1,2,3], targetSum = 5 <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> There are two root-to-leaf paths in the
+        tree: <br />
+        (1 --&gt; 2): The sum is 3. <br />
+        (1 --&gt; 3): The sum is 4. <br />
+        There is no root-to-leaf path with sum = 5.
+      </pre>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> root = [], targetSum = 0 <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> Since the tree is empty, there are no
+        root-to-leaf paths.
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range <code>[0, 5000]</code>
+          .
+        </li>
+        <li>
+          <code>-1000 &lt;= Node.val &lt;= 1000</code>
+        </li>
+        <li>
+          <code>-1000 &lt;= targetSum &lt;= 1000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
