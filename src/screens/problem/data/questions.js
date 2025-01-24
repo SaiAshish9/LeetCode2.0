3a8a76b5300156952725b9d1365390f192f433aa
saiashish9 <saiashish3760@gmail.com>
2025-01-25 00:08:35 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -92342,9 +92342,7 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
         <code>3-4-5</code>
         <span>, so return </span>
         <code>3</code>
-        <span>
-          .
-        </span>
+        <span>.</span>
       </pre>
       <br />
       <p>
@@ -92372,14 +92370,64 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
         Longest consecutive sequence path is <code>2-3</code>
         <span>, not </span>
         <code>3-2-1</code>
-        <span>
-          , so return{" "}
-        </span>
+        <span>, so return </span>
         <code>2</code>
-        <span>
-          .
-        </span>
+        <span>.</span>
       </pre>
+    </div>
+  ),
+  404: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary tree, return{" "}
+        <em>the sum of all left leaves.</em>
+      </p>
+
+      <p>
+        A <strong>leaf</strong> is a node with no children. A{" "}
+        <strong>left leaf</strong> is a leaf that is the left child of another
+        node.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/04/08/leftsum-tree.jpg"
+        style={{ width: 277, height: 302 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [3,9,20,null,null,15,7] <br />
+        <strong>Output:</strong> 24 <br />
+        <strong>Explanation:</strong> There are two left leaves in the binary
+        tree, with values 9 and 15 respectively.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [1] <br />
+        <strong>Output:</strong> 0
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range <code>[1, 1000]</code>
+          .
+        </li>
+        <li>
+          <code>-1000 &lt;= Node.val &lt;= 1000</code>
+        </li>
+      </ul>
     </div>
   ),
 };
