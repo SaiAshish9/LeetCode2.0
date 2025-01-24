e72fa08a34cf684532eb8d61c66bb11bb19786e0
saiashish9 <saiashish3760@gmail.com>
2025-01-24 19:32:32 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -92155,6 +92155,65 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       iteratively?
     </div>
   ),
+  156: (
+    <div class="markdown-body div-width">
+      <p>
+        Given a binary tree where all the right nodes are either leaf nodes with
+        a sibling (a left node that shares the same parent node) or empty, flip
+        it upside down and turn it into a tree where the original right nodes
+        turned into left leaf nodes. Return the new root.
+      </p>
+      <p>
+        <strong>Example:</strong>
+      </p>
+      <pre>
+        <strong>Input: </strong>[1,2,3,4,5] <br />
+        {`1
+   / \
+  2   3
+ / \
+4   5`}{" "}
+        <br />
+        <strong>Output:</strong> return the root of the binary tree
+        [4,5,2,#,#,3,1] <br />
+        {`4
+  / \
+ 5   2
+    / \
+   3   1`}
+      </pre>
+      <br />
+      <p>
+        <strong>Clarification:</strong>
+      </p>
+      <p>
+        Confused what{" "}
+        <code>
+          [4,5,2,#,#,3,1<font face="monospace">]</font>
+        </code>
+        &nbsp;means? Read more below on how binary tree is serialized on OJ.
+      </p>
+      <p>
+        The serialization of a binary tree follows a level order traversal,
+        where '#' signifies a path terminator where no node exists below.
+      </p>
+      <p>Here's an example:</p> <br />
+      <pre>
+        {" "}
+        {`1
+  / \
+ 2   3
+    /
+   4
+    \
+     5`}
+      </pre>{" "}
+      <br />
+      <p>
+        The above binary tree is serialized as <code>[1,2,3,#,#,4,#,#,5]</code>.
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
