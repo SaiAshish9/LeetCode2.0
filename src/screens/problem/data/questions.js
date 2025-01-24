b6c9c52c27eb40b441b3e5601850ddc87e12acb1
saiashish9 <saiashish3760@gmail.com>
2025-01-24 18:54:55 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -91545,6 +91545,77 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  114: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary tree, flatten the tree into a
+        "linked list":
+      </p>
+      <ul>
+        <li>
+          The "linked list" should use the same <code>TreeNode</code> class
+          where the <code>right</code> child pointer points to the next node in
+          the list and the <code>left</code> child pointer is always{" "}
+          <code>null</code>.
+        </li>
+        <li>
+          The "linked list" should be in the same order as a{" "}
+          <a
+            href="https://en.wikipedia.org/wiki/Tree_traversal#Pre-order,_NLR"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <strong>pre-order</strong>
+            <strong> traversal</strong>
+          </a>{" "}
+          of the binary tree.
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/01/14/flaten.jpg"
+        style={{ width: 500, height: 226 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [1,2,5,3,4,null,6] <br />
+        <strong>Output:</strong> [1,null,2,null,3,null,4,null,5,null,6]
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> root = [] <br />
+        <strong>Output:</strong> []
+      </pre>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> root = [0] <br />
+        <strong>Output:</strong> [0]
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range <code>[0, 2000]</code>
+          .
+        </li>
+        <li>
+          <code>-100 &lt;= Node.val &lt;= 100</code>
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <strong>Follow up:</strong> Can you flatten the tree in-place (with{" "}
+      <code>O(1)</code> extra space)?
+    </div>
+  ),
 };
 
 export default QUESTIONS;
