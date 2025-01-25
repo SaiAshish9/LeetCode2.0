1e53ee4d20eb228ada1c3452e5b9e361f422109b
saiashish9 <saiashish3760@gmail.com>
2025-01-25 11:43:51 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -93565,6 +93565,132 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </div>
     </div>
   ),
+  1559: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a 2D array of characters <code>grid</code> of size{" "}
+        <code>m x n</code>, you need to find if there exists any cycle
+        consisting of the <strong>same value</strong> in <code>grid</code>.
+      </p>
+
+      <p>
+        A cycle is a path of <strong>length 4 or more</strong> in the grid that
+        starts and ends at the same cell. From a given cell, you can move to one
+        of the cells adjacent to it - in one of the four directions (up, down,
+        left, or right), if it has the <strong>same value</strong> of the
+        current cell.
+      </p>
+
+      <p>
+        Also, you cannot move to the cell that you visited in your last move.
+        For example, the cycle <code>(1, 1) -&gt; (1, 2) -&gt; (1, 1)</code> is
+        invalid because from <code>(1, 2)</code> we visited <code>(1, 1)</code>{" "}
+        which was the last visited cell.
+      </p>
+
+      <p>
+        Return <code>true</code> if any cycle of the same value exists in{" "}
+        <code>grid</code>, otherwise, return <code>false</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <p>
+        <strong>
+          <img
+            alt=""
+            src="https://assets.leetcode.com/uploads/2020/07/15/1.png"
+            style={{ width: 231, height: 152 }}
+          />
+        </strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid =
+        [["a","a","a","a"],["a","b","b","a"],["a","b","b","a"],["a","a","a","a"]]{" "}
+        <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation: </strong>There are two valid cycles shown in
+        different colors in the image below:
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2020/07/15/11.png"
+          style={{ width: 225, height: 163 }}
+        />
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <p>
+        <strong>
+          <img
+            alt=""
+            src="https://assets.leetcode.com/uploads/2020/07/15/22.png"
+            style={{ width: 236, height: 154 }}
+          />
+        </strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid =
+        [["c","c","c","a"],["c","d","c","c"],["c","c","e","c"],["f","c","c","c"]]{" "}
+        <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation: </strong>There is only one valid cycle highlighted
+        in the image below:
+        <img
+          alt=""
+          src="https://assets.leetcode.com/uploads/2020/07/15/2.png"
+          style={{ width: 229, height: 157 }}
+        />
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <p>
+        <strong>
+          <img
+            alt=""
+            src="https://assets.leetcode.com/uploads/2020/07/15/3.png"
+            style={{ width: 183, height: 120 }}
+          />
+        </strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid =
+        [["a","b","b"],["b","z","b"],["b","b","a"]] <br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>m == grid.length</code>
+        </li>
+        <li>
+          <code>n == grid[i].length</code>
+        </li>
+        <li>
+          <code>1 &lt;= m, n &lt;= 500</code>
+        </li>
+        <li>
+          <code>grid</code> consists only of lowercase English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
