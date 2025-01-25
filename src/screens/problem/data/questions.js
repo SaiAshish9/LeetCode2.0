5dd55a65ddf534d0a6b0a3a2cc44b1905cc491a8
saiashish9 <saiashish3760@gmail.com>
2025-01-25 11:14:05 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -92937,6 +92937,99 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </p>
     </div>
   ),
+  865: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary tree, the depth of each node is{" "}
+        <strong>the shortest distance to the root</strong>.
+      </p>
+
+      <p>
+        Return <em>the smallest subtree</em> such that it contains{" "}
+        <strong>all the deepest nodes</strong> in the original tree.
+      </p>
+
+      <p>
+        A node is called <strong>the deepest</strong> if it has the largest
+        depth possible among any node in the entire tree.
+      </p>
+
+      <p>
+        The <strong>subtree</strong> of a node is a tree consisting of that
+        node, plus the set of all descendants of that node.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://s3-lc-upload.s3.amazonaws.com/uploads/2018/07/01/sketch1.png"
+        style={{ width: 600, height: 510 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [3,5,1,6,2,0,8,null,null,7,4] <br />
+        <strong>Output:</strong> [2,7,4] <br />
+        <strong>Explanation:</strong> We return the node with value 2, colored
+        in yellow in the diagram. <br />
+        The nodes coloured in blue are the deepest nodes of the tree. <br />
+        Notice that nodes 5, 3 and 2 contain the deepest nodes in the tree but
+        node 2 is the smallest subtree among them, so we return it.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [1] <br />
+        <strong>Output:</strong> [1] <br />
+        <strong>Explanation:</strong> The root is the deepest node in the tree.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [0,1,3,null,2] <br />
+        <strong>Output:</strong> [2] <br />
+        <strong>Explanation:</strong> The deepest node in the tree is 2, the
+        valid subtrees are the subtrees of nodes 2, 1 and 0 but the subtree of
+        node 2 is the smallest.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree will be in the range{" "}
+          <code>[1, 500]</code>.
+        </li>
+        <li>
+          <code>0 &lt;= Node.val &lt;= 500</code>
+        </li>
+        <li>
+          The values of the nodes in the tree are <strong>unique</strong>.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Note:</strong> This question is the same as 1123:{" "}
+        <a
+          href="https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/"
+          target="_blank"
+        >
+          https://leetcode.com/problems/lowest-common-ancestor-of-deepest-leaves/
+        </a>
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
