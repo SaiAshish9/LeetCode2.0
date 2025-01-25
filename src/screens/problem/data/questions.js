4bb8ee0dcbdea112204ab0cf372bdcc485007d5a
saiashish9 <saiashish3760@gmail.com>
2025-01-25 12:10:04 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -94318,6 +94318,139 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  2867: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        There is an undirected tree with <code>n</code> nodes labeled from{" "}
+        <code>1</code> to <code>n</code>. You are given the integer{" "}
+        <code>n</code> and a 2D integer array <code>edges</code> of length{" "}
+        <code>n - 1</code>, where{" "}
+        <code>
+          edges[i] = [u<sub>i</sub>, v<sub>i</sub>]
+        </code>{" "}
+        indicates that there is an edge between nodes{" "}
+        <code>
+          u<sub>i</sub>
+        </code>{" "}
+        and{" "}
+        <code>
+          v<sub>i</sub>
+        </code>{" "}
+        in the tree.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>number of valid paths</strong> in the tree
+        </em>
+        .
+      </p>
+
+      <p>
+        A path <code>(a, b)</code> is <strong>valid</strong> if there exists{" "}
+        <strong>exactly one</strong> prime number among the node labels in the
+        path from <code>a</code> to <code>b</code>.
+      </p>
+
+      <p>
+        <strong>Note</strong> that:
+      </p>
+
+      <ul>
+        <li>
+          The path <code>(a, b)</code> is a sequence of{" "}
+          <strong>distinct</strong> nodes starting with node <code>a</code> and
+          ending with node <code>b</code> such that every two adjacent nodes in
+          the sequence share an edge in the tree.
+        </li>
+        <li>
+          Path <code>(a, b)</code> and path <code>(b, a)</code> are considered
+          the <strong>same</strong> and counted only <strong>once</strong>.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2023/08/27/example1.png"
+        style={{ width: 440, height: 357 }}
+      />
+      <pre>
+        <strong>Input:</strong> n = 5, edges = [[1,2],[1,3],[2,4],[2,5]]
+        <br />
+        <strong>Output:</strong> 4<br />
+        <strong>Explanation:</strong> The pairs with exactly one prime number on
+        the path between them are: <br />
+        - (1, 2) since the path from 1 to 2 contains prime number 2. <br />
+        - (1, 3) since the path from 1 to 3 contains prime number 3.
+        <br />
+        - (1, 4) since the path from 1 to 4 contains prime number 2.
+        <br />
+        - (2, 4) since the path from 2 to 4 contains prime number 2.
+        <br />
+        It can be shown that there are only 4 valid paths.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2023/08/27/example2.png"
+        style={{ width: 488, height: 384 }}
+      />
+      <pre>
+        <strong>Input:</strong> n = 6, edges = [[1,2],[1,3],[2,4],[3,5],[3,6]]
+        <br />
+        <strong>Output:</strong> 6<br />
+        <strong>Explanation:</strong> The pairs with exactly one prime number on
+        the path between them are: <br />
+        - (1, 2) since the path from 1 to 2 contains prime number 2. <br />
+        - (1, 3) since the path from 1 to 3 contains prime number 3. <br />
+        - (1, 4) since the path from 1 to 4 contains prime number 2.
+        <br />
+        - (1, 6) since the path from 1 to 6 contains prime number 3.
+        <br />
+        - (2, 4) since the path from 2 to 4 contains prime number 2.
+        <br />
+        - (3, 6) since the path from 3 to 6 contains prime number 3.
+        <br />
+        It can be shown that there are only 6 valid paths.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>edges.length == n - 1</code>
+        </li>
+        <li>
+          <code>edges[i].length == 2</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= u<sub>i</sub>, v<sub>i</sub> &lt;= n
+          </code>
+        </li>
+        <li>
+          The input is generated such that <code>edges</code> represent a valid
+          tree.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
