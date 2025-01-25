39fca31148c5e80ff5be76cff0d01edefa4db324
saiashish9 <saiashish3760@gmail.com>
2025-01-25 11:23:53 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -93160,6 +93160,101 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1325: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a binary tree <code>root</code> and an integer <code>target</code>
+        , delete all the <strong>leaf nodes</strong> with value{" "}
+        <code>target</code>.
+      </p>
+
+      <p>
+        Note that once you delete a leaf node with value <code>target</code>
+        <strong>, </strong>if its parent node becomes a leaf node and has the
+        value <code>target</code>, it should also be deleted (you need to
+        continue doing that until you cannot).
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <p>
+        <strong>
+          <img
+            alt=""
+            src="https://assets.leetcode.com/uploads/2020/01/09/sample_1_1684.png"
+            style={{ width: 500, height: 112 }}
+          />
+        </strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [1,2,3,2,null,2,4], target = 2 <br />
+        <strong>Output:</strong> [1,null,3,null,4] <br />
+        <strong>Explanation:</strong> Leaf nodes in green with value (target =
+        2) are removed (Picture in left). <br />
+        After removing, new nodes become leaf nodes with value (target = 2)
+        (Picture in center).
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <p>
+        <strong>
+          <img
+            alt=""
+            src="https://assets.leetcode.com/uploads/2020/01/09/sample_2_1684.png"
+            style={{ width: 400, height: 154 }}
+          />
+        </strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [1,3,3,3,2], target = 3 <br />
+        <strong>Output:</strong> [1,3,null,null,2]
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <p>
+        <strong>
+          <img
+            alt=""
+            src="https://assets.leetcode.com/uploads/2020/01/15/sample_3_1684.png"
+            style={{ width: 500, height: 166 }}
+          />
+        </strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [1,2,null,2,null,2], target = 2 <br />
+        <strong>Output:</strong> [1] <br />
+        <strong>Explanation:</strong> Leaf nodes in green with value (target =
+        2) are removed at each step.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range <code>[1, 3000]</code>
+          .
+        </li>
+        <li>
+          <code>1 &lt;= Node.val, target &lt;= 1000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
