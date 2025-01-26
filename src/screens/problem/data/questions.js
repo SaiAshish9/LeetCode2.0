70c2e03a528f06f33e25e87192d80294efd259c2
saiashish9 <saiashish3760@gmail.com>
2025-01-26 21:00:00 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -99481,6 +99481,114 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  87: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        We can scramble a string s to get a string t using the following
+        algorithm:
+      </p>
+
+      <ul>
+        <li>If the length of the string is 1, stop.</li>
+        <li>
+          If the length of the string is &gt; 1, do the following:
+          <ul>
+            <li>
+              Split the string into two non-empty substrings at a random index,
+              i.e., if the string is <code>s</code>, divide it to <code>x</code>{" "}
+              and <code>y</code> where <code>s = x + y</code>.
+            </li>
+            <li>
+              <strong>Randomly</strong>&nbsp;decide to swap the two substrings
+              or to keep them in the same order. i.e., after this step,{" "}
+              <code>s</code> may become <code>s = x + y</code> or{" "}
+              <code>s = y + x</code>.
+            </li>
+            <li>
+              Apply step 1 recursively on each of the two substrings{" "}
+              <code>x</code> and <code>y</code>.
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+      <p>
+        Given two strings <code>s1</code> and <code>s2</code> of{" "}
+        <strong>the same length</strong>, return <code>true</code> if{" "}
+        <code>s2</code> is a scrambled string of <code>s1</code>, otherwise,
+        return <code>false</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s1 = "great", s2 = "rgeat" <br />
+        <strong>Output:</strong> true
+        <br />
+        <strong>Explanation:</strong> One possible scenario applied on s1 is:
+        <br />
+        "great" --&gt; "gr/eat" // divide at random index.
+        <br />
+        "gr/eat" --&gt; "gr/eat" // random decision is not to swap the two
+        substrings and keep them in order.
+        <br />
+        "gr/eat" --&gt; "g/r / e/at" // apply the same algorithm recursively on
+        both substrings. divide at random index each of them.
+        <br />
+        "g/r / e/at" --&gt; "r/g / e/at" // random decision was to swap the
+        first substring and to keep the second substring in the same order.
+        <br />
+        "r/g / e/at" --&gt; "r/g / e/ a/t" // again apply the algorithm
+        recursively, divide "at" to "a/t".
+        <br />
+        "r/g / e/ a/t" --&gt; "r/g / e/ a/t" // random decision is to keep both
+        substrings in the same order.
+        <br />
+        The algorithm stops now, and the result string is "rgeat" which is s2.
+        <br />
+        As one possible scenario led s1 to be scrambled to s2, we return true.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s1 = "abcde", s2 = "caebd" <br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s1 = "a", s2 = "a" <br />
+        <strong>Output:</strong> true
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>s1.length == s2.length</code>
+        </li>
+        <li>
+          <code>1 &lt;= s1.length &lt;= 30</code>
+        </li>
+        <li>
+          <code>s1</code> and <code>s2</code> consist of lowercase English
+          letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
