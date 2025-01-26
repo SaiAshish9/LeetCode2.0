6746e6876f2d59686680d82ad00291113df9b626
saiashish9 <saiashish3760@gmail.com>
2025-01-26 21:13:19 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -99853,6 +99853,140 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </p>
     </div>
   ),
+  115: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given two strings s and t, return <i>the number of distinct</i>{" "}
+        <b>
+          <i>subsequences</i>
+        </b>
+        <i> of </i>s<i> which equals </i>t.
+      </p>
+
+      <p>
+        The test cases are generated so that the answer fits on a 32-bit signed
+        integer.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "rabbbit", t = "rabbit" <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> <br />
+        As shown below, there are 3 ways you can generate "rabbit" from s.{" "}
+        <br />
+        <code>
+          <strong>
+            <u>rabb</u>
+          </strong>
+          b
+          <strong>
+            <u>it</u>
+          </strong>
+        </code>{" "}
+        <br />
+        <code>
+          <strong>
+            <u>ra</u>
+          </strong>
+          b
+          <strong>
+            <u>bbit</u>
+          </strong>
+        </code>
+        <br />
+        <code>
+          <strong>
+            <u>rab</u>
+          </strong>
+          b
+          <strong>
+            <u>bit</u>
+          </strong>
+        </code>
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "babgbag", t = "bag"
+        <br />
+        <strong>Output:</strong> 5<br />
+        <strong>Explanation:</strong>
+        <br />
+        As shown below, there are 5 ways you can generate "bag" from s.
+        <br />
+        <code>
+          <strong>
+            <u>ba</u>
+          </strong>
+          b
+          <u>
+            <strong>g</strong>
+          </u>
+          bag
+        </code>
+        <br />
+        <code>
+          <strong>
+            <u>ba</u>
+          </strong>
+          bgba
+          <strong>
+            <u>g</u>
+          </strong>
+        </code>
+        <br />
+        <code>
+          <u>
+            <strong>b</strong>
+          </u>
+          abgb
+          <strong>
+            <u>ag</u>
+          </strong>
+        </code>
+        <br />
+        <code>
+          ba
+          <u>
+            <strong>b</strong>
+          </u>
+          gb
+          <u>
+            <strong>ag</strong>
+          </u>
+        </code>
+        <br />
+        <code>
+          babg
+          <strong>
+            <u>bag</u>
+          </strong>
+        </code>
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= s.length, t.length &lt;= 1000</code>
+        </li>
+        <li>
+          <code>s</code> and <code>t</code> consist of English letters.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
