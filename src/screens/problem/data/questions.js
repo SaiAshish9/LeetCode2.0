e8d6d87beff00feb16cd1d2e36c2dd964f3b8466
saiashish9 <saiashish3760@gmail.com>
2025-01-26 23:20:10 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -100932,6 +100932,52 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  265: (
+    <div class="markdown-body div-width" style="margin-top: 20px">
+      <p>
+        There are a row of <i>n</i> houses, each house can be painted with one
+        of the <i>k</i>
+        colors. The cost of paint all the houses such that no two adjacent
+        houses have the same color.
+      </p>
+
+      <p>
+        The cost of painting each house with a certain color is represented by a{" "}
+        <code>
+          <i>n</i> x <i>k</i>
+        </code>
+        cost matrix. For example, <code>costs[0][0]</code> is the cost of
+        painting house 0 with color 0; <code>costs[1][2]</code> is the cost of
+        painting house 1 with color 2, and so on... Find the minimum cost to
+        paint all houses.
+      </p>
+
+      <p>
+        <b>Note:</b>
+        <br />
+        All costs are positive integers.
+      </p>
+
+      <p>
+        <strong>Example:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> [[1,5,3],[2,9,4]] <br />
+        <strong>Output:</strong> 5 <br />
+        <strong>Explanation: </strong>Paint house 0 into color 0, paint house 1
+        into color 2. Minimum cost: 1 + 4 = 5; <br />
+        &nbsp; Or paint house 0 into color 2, paint house 1 into color 0.
+        Minimum cost: 3 + 2 = 5.
+      </pre>
+
+      <p>
+        <b>Follow up:</b>
+        <br />
+        Could you solve it in <i>O</i>(<i>nk</i>) runtime?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
