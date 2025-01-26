562bc1a16d032f64a51e9dc877e0d6845b98106b
saiashish9 <saiashish3760@gmail.com>
2025-01-26 11:33:26 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -94725,6 +94725,73 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       iteratively?
     </div>
   ),
+  105: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given two integer arrays <code>preorder</code> and <code>inorder</code>{" "}
+        where <code>preorder</code> is the preorder traversal of a binary tree
+        and <code>inorder</code> is the inorder traversal of the same tree,
+        construct and return <em>the binary tree</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/02/19/tree.jpg"
+        style={{ width: 277, height: 302 }}
+      />
+      <pre>
+        <strong>Input:</strong> preorder = [3,9,20,15,7], inorder =
+        [9,3,15,20,7] <br />
+        <strong>Output:</strong> [3,9,20,null,null,15,7]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> preorder = [-1], inorder = [-1] <br />
+        <strong>Output:</strong> [-1]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= preorder.length &lt;= 3000</code>
+        </li>
+        <li>
+          <code>inorder.length == preorder.length</code>
+        </li>
+        <li>
+          <code>-3000 &lt;= preorder[i], inorder[i] &lt;= 3000</code>
+        </li>
+        <li>
+          <code>preorder</code> and <code>inorder</code> consist of{" "}
+          <strong>unique</strong> values.
+        </li>
+        <li>
+          Each value of <code>inorder</code> also appears in{" "}
+          <code>preorder</code>.
+        </li>
+        <li>
+          <code>preorder</code> is <strong>guaranteed</strong> to be the
+          preorder traversal of the tree.
+        </li>
+        <li>
+          <code>inorder</code> is <strong>guaranteed</strong> to be the inorder
+          traversal of the tree.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
