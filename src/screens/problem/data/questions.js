5fddc21fd40bda473ee669420e2e92c873d1f389
saiashish9 <saiashish3760@gmail.com>
2025-01-26 21:35:55 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -100090,6 +100090,64 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </p>
     </div>
   ),
+  120: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a <code>triangle</code> array, return{" "}
+        <em>the minimum path sum from top to bottom</em>.
+      </p>
+      <p>
+        For each step, you may move to an adjacent number of the row below. More
+        formally, if you are on index <code>i</code> on the current row, you may
+        move to either index <code>i</code> or index <code>i + 1</code> on the
+        next row.
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> triangle = [[2],[3,4],[6,5,7],[4,1,8,3]] <br />
+        <strong>Output:</strong> 11 <br />
+        <strong>Explanation:</strong> The triangle looks like: <br />
+        <u>2</u> <br />
+        <u>3</u> 4 <br />6 <u>5</u> 7 <br />4 <u>1</u> 8 3 <br />
+        The minimum path sum from top to bottom is 2 + 3 + 5 + 1 = 11
+        (underlined above).
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> triangle = [[-10]] <br />
+        <strong>Output:</strong> -10
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>1 &lt;= triangle.length &lt;= 200</code>
+        </li>
+        <li>
+          <code>triangle[0].length == 1</code>
+        </li>
+        <li>
+          <code>triangle[i].length == triangle[i - 1].length + 1</code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= triangle[i][j] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <strong>Follow up:</strong> Could you&nbsp;do this using only{" "}
+      <code>O(n)</code> extra space, where <code>n</code> is the total number of
+      rows in the triangle?
+    </div>
+  ),
 };
 
 export default QUESTIONS;
