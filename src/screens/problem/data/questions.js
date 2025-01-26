52da4f55e4b31fceca48802b2f3a80e6d479ab22
saiashish9 <saiashish3760@gmail.com>
2025-01-26 18:07:50 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -98863,6 +98863,80 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  44: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an input string (<code>s</code>) and a pattern (<code>p</code>),
+        implement wildcard pattern matching with support for <code>'?'</code>{" "}
+        and <code>'*'</code> where:
+      </p>
+
+      <ul>
+        <li>
+          <code>'?'</code> Matches any single character.
+        </li>
+        <li>
+          <code>'*'</code> Matches any sequence of characters (including the
+          empty sequence).
+        </li>
+      </ul>
+
+      <p>
+        The matching should cover the <strong>entire</strong> input string (not
+        partial).
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "aa", p = "a" <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> "a" does not match the entire string "aa".
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "aa", p = "*" <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong>&nbsp;'*' matches any sequence.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "cb", p = "?a" <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong>&nbsp;'?' matches 'c', but the second
+        letter is 'a', which does not match 'b'.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>0 &lt;= s.length, p.length &lt;= 2000</code>
+        </li>
+        <li>
+          <code>s</code> contains only lowercase English letters.
+        </li>
+        <li>
+          <code>p</code> contains only lowercase English letters,{" "}
+          <code>'?'</code> or <code>'*'</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
