95237edf9585feebfad43120da8422b8eb8ae104
saiashish9 <saiashish3760@gmail.com>
2025-01-26 12:15:39 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -95169,6 +95169,68 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </article>
   ),
+  508: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary tree, return the most frequent{" "}
+        <strong>subtree sum</strong>. If there is a tie, return all the values
+        with the highest frequency in any order.
+      </p>
+
+      <p>
+        The <strong>subtree sum</strong> of a node is defined as the sum of all
+        the node values formed by the subtree rooted at that node (including the
+        node itself).
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/04/24/freq1-tree.jpg"
+        style={{ width: 207, height: 183 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [5,2,-3] <br />
+        <strong>Output:</strong> [2,-3,4]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/04/24/freq2-tree.jpg"
+        style={{ width: 207, height: 183 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [5,2,-5] <br />
+        <strong>Output:</strong> [2]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range{" "}
+          <code>
+            [1, 10<sup>4</sup>]
+          </code>
+          .
+        </li>
+        <li>
+          <code>
+            -10<sup>5</sup> &lt;= Node.val &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
