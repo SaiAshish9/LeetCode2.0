ec7607aef256ee874b0295427a61bc0680576f6c
saiashish9 <saiashish3760@gmail.com>
2025-01-26 14:20:37 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -97221,6 +97221,100 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </div>
     </div>
   ),
+  1367: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a binary tree <code>root</code> and a&nbsp;linked list with&nbsp;
+        <code>head</code>&nbsp;as the first node.&nbsp;
+      </p>
+
+      <p>
+        Return True if all the elements in the linked list starting from the{" "}
+        <code>head</code> correspond to some <em>downward path</em> connected in
+        the binary tree&nbsp;otherwise return False.
+      </p>
+
+      <p>
+        In this context downward path means a path that starts at some node and
+        goes downwards.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <p>
+        <strong>
+          <img
+            alt=""
+            src="https://assets.leetcode.com/uploads/2020/02/12/sample_1_1720.png"
+            style={{ width: 220, height: 280 }}
+          />
+        </strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> head = [4,2,8], root =
+        [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3] <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> Nodes in blue form a subpath in the binary
+        Tree.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <p>
+        <strong>
+          <img
+            alt=""
+            src="https://assets.leetcode.com/uploads/2020/02/12/sample_2_1720.png"
+            style={{ width: 220, height: 280 }}
+          />
+        </strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> head = [1,4,2,6], root =
+        [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3] <br />
+        <strong>Output:</strong> true
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> head = [1,4,2,6,8], root =
+        [1,4,4,null,2,2,null,1,null,6,8,null,null,null,null,1,3] <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> There is no path in the binary tree that
+        contains all the elements of the linked list from <code>head</code>.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree will be in the range{" "}
+          <code>[1, 2500]</code>.
+        </li>
+        <li>
+          The number of nodes in the list will be in the range{" "}
+          <code>[1, 100]</code>.
+        </li>
+        <li>
+          <code>1 &lt;= Node.val&nbsp;&lt;= 100</code>&nbsp;for each node in the
+          linked list and binary tree.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
