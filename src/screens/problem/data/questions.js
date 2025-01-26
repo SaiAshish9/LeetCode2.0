f639b2605a2341fde06d83855c9b04f9613bf67c
saiashish9 <saiashish3760@gmail.com>
2025-01-26 13:39:16 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -96723,6 +96723,75 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1026: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary tree, find the maximum value{" "}
+        <code>v</code> for which there exist <strong>different</strong> nodes{" "}
+        <code>a</code> and <code>b</code> where <code>v = |a.val - b.val|</code>{" "}
+        and <code>a</code> is an ancestor of <code>b</code>.
+      </p>
+
+      <p>
+        A node <code>a</code> is an ancestor of <code>b</code> if either: any
+        child of <code>a</code> is equal to <code>b</code>&nbsp;or any child of{" "}
+        <code>a</code> is an ancestor of <code>b</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/11/09/tmp-tree.jpg"
+        style={{ width: 400, height: 390 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [8,3,10,1,6,null,14,null,null,4,7,13]{" "}
+        <br />
+        <strong>Output:</strong> 7 <br />
+        <strong>Explanation: </strong>We have various ancestor-node differences,
+        some of which are given below : <br />
+        |8 - 3| = 5 <br />
+        |3 - 7| = 4 <br />
+        |8 - 1| = 7 <br />
+        |10 - 13| = 3 <br />
+        Among all possible differences, the maximum value of 7 is obtained by |8
+        - 1| = 7.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/11/09/tmp-tree-1.jpg"
+        style={{ width: 250, height: 349 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [1,null,2,null,0,3] <br />
+        <strong>Output:</strong> 3
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range <code>[2, 5000]</code>
+          .
+        </li>
+        <li>
+          <code>
+            0 &lt;= Node.val &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
