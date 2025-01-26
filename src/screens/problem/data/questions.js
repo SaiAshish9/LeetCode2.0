e11dfdcaae273a8d71cf1d9e9e635e1188d8efcd
saiashish9 <saiashish3760@gmail.com>
2025-01-26 21:16:15 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -99987,6 +99987,50 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  118: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer <code>numRows</code>, return the first numRows of{" "}
+        <strong>Pascal's triangle</strong>.
+      </p>
+
+      <p>
+        In <strong>Pascal's triangle</strong>, each number is the sum of the two
+        numbers directly above it as shown:
+      </p>
+      <img
+        alt=""
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0d/PascalTriangleAnimated2.gif"
+        style={{ height: 240, width: 260 }}
+      />
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> numRows = 5 <br />
+        <strong>Output:</strong> [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]{" "}
+        <br />
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <pre>
+        <strong>Input:</strong> numRows = 1 <br />
+        <strong>Output:</strong> [[1]]
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= numRows &lt;= 30</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
