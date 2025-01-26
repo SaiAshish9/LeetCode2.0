f4037535c07b4bed6075c9d9be8be93ddb9c3456
saiashish9 <saiashish3760@gmail.com>
2025-01-26 23:49:13 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -100978,6 +100978,93 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </p>
     </div>
   ),
+  276: (
+    <article role="main" class="blog-post">
+      <p>
+        You are painting a fence of <code>n</code> posts with <code>k</code>{" "}
+        different colors. You must paint the posts following these rules:
+      </p>
+
+      <ul>
+        <li>
+          Every post must be painted <strong>exactly one</strong> color.
+        </li>
+        <li>
+          There <strong>cannot</strong> be three or more{" "}
+          <strong>consecutive</strong> posts with the same color.
+        </li>
+      </ul>
+
+      <p>
+        Given the two integers <code>n</code> and <code>k</code>, return{" "}
+        <em>
+          the <strong>number of ways</strong> you can paint the fence
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <p>
+        <img
+          alt=""
+          src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/0200-0299/0276.Paint%20Fence/images/paintfenceex1.png"
+          style={{ width: 507, height: 313 }}
+        />
+      </p>
+      <pre>
+        <strong>Input:</strong> n = 3, k = 2 <br />
+        <strong>Output:</strong> 6 <br />
+        <strong>Explanation: </strong>All the possibilities are shown.
+        <br />
+        Note that painting all the posts red or all the posts green is invalid
+        because there cannot be three posts in a row with the same color.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 1, k = 1<br />
+        <strong>Output:</strong> 1
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 7, k = 2<br />
+        <strong>Output:</strong> 42
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= n &lt;= 50</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= k &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          The testcases are generated such that the answer is in the range{" "}
+          <code>
+            [0, 2<sup>31</sup> - 1]
+          </code>{" "}
+          for the given <code>n</code> and <code>k</code>.
+        </li>
+      </ul>
+    </article>
+  ),
 };
 
 export default QUESTIONS;
