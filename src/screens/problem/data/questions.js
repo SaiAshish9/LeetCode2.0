d63d377564dfc9114386c9f2538adec96a3f1b60
saiashish9 <saiashish3760@gmail.com>
2025-01-26 12:22:46 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -95231,6 +95231,58 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  515: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary tree, return{" "}
+        <em>an array of the largest value in each row</em> of the tree{" "}
+        <strong>(0-indexed)</strong>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/08/21/largest_e1.jpg"
+        style={{ width: 300, height: 172 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [1,3,2,5,3,null,9] <br />
+        <strong>Output:</strong> [1,3,9]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [1,2,3] <br />
+        <strong>Output:</strong> [1,3]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree will be in the range{" "}
+          <code>
+            [0, 10<sup>4</sup>]
+          </code>
+          .
+        </li>
+        <li>
+          <code>
+            -2<sup>31</sup> &lt;= Node.val &lt;= 2<sup>31</sup> - 1
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
