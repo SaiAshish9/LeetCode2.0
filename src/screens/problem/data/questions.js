eab469dbbad2bfa2f5dcad98745037e22e1a09ee
saiashish9 <saiashish3760@gmail.com>
2025-01-26 18:31:57 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -99306,6 +99306,65 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  64: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a <code>m x n</code> <code>grid</code> filled with non-negative
+        numbers, find a path from top left to bottom right, which minimizes the
+        sum of all numbers along its path.
+      </p>
+
+      <p>
+        <strong>Note:</strong> You can only move either down or right at any
+        point in time.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/11/05/minpath.jpg"
+        style={{ width: 242, height: 242 }}
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[1,3,1],[1,5,1],[4,2,1]] <br />
+        <strong>Output:</strong> 7 <br />
+        <strong>Explanation:</strong> Because the path 1 → 3 → 1 → 1 → 1
+        minimizes the sum.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid = [[1,2,3],[4,5,6]] <br />
+        <strong>Output:</strong> 12
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>m == grid.length</code>
+        </li>
+        <li>
+          <code>n == grid[i].length</code>
+        </li>
+        <li>
+          <code>1 &lt;= m, n &lt;= 200</code>
+        </li>
+        <li>
+          <code>0 &lt;= grid[i][j] &lt;= 200</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
