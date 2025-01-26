a45e3e03d3ab404aaf4837cea3d62e3bc0779f99
saiashish9 <saiashish3760@gmail.com>
2025-01-26 23:14:35 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -100878,6 +100878,60 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </pre>
     </div>
   ),
+  264: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        An <strong>ugly number</strong> is a positive integer whose prime
+        factors are limited to <code>2</code>, <code>3</code>, and{" "}
+        <code>5</code>.
+      </p>
+
+      <p>
+        Given an integer <code>n</code>, return <em>the</em>{" "}
+        <code>
+          n<sup>th</sup>
+        </code>{" "}
+        <em>
+          <strong>ugly number</strong>
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 10 <br />
+        <strong>Output:</strong> 12 <br />
+        <strong>Explanation:</strong> [1, 2, 3, 4, 5, 6, 8, 9, 10, 12] is the
+        sequence of the first 10 ugly numbers.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 1 <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> 1 has no prime factors, therefore all of
+        its prime factors are limited to 2, 3, and 5.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= n &lt;= 1690</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
