73af5aeb5774cfa4233eab517843962ae1f5d142
saiashish9 <saiashish3760@gmail.com>
2025-01-27 00:53:42 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -101335,6 +101335,85 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  313: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        A <strong>super ugly number</strong> is a positive integer whose prime
+        factors are in the array <code>primes</code>.
+      </p>
+
+      <p>
+        Given an integer <code>n</code> and an array of integers{" "}
+        <code>primes</code>, return <em>the</em>{" "}
+        <code>
+          n<sup>th</sup>
+        </code>{" "}
+        <em>
+          <strong>super ugly number</strong>
+        </em>
+        .
+      </p>
+
+      <p>
+        The{" "}
+        <code>
+          n<sup>th</sup>
+        </code>{" "}
+        <strong>super ugly number</strong> is <strong>guaranteed</strong> to fit
+        in a <strong>32-bit</strong> signed integer.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 12, primes = [2,7,13,19] <br />
+        <strong>Output:</strong> 32 <br />
+        <strong>Explanation:</strong> [1,2,4,7,8,13,14,16,19,26,28,32] is the
+        sequence of the first 12 super ugly numbers given primes = [2,7,13,19].
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 1, primes = [2,3,5] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> 1 has no prime factors, therefore all of
+        its prime factors are in the array primes = [2,3,5].
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>1 &lt;= primes.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>2 &lt;= primes[i] &lt;= 1000</code>
+        </li>
+        <li>
+          <code>primes[i]</code> is <strong>guaranteed</strong> to be a prime
+          number.
+        </li>
+        <li>
+          All the values of <code>primes</code> are <strong>unique</strong> and
+          sorted in <strong>ascending order</strong>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
