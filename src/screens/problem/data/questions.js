e9b59d5021b8273cf9d60289651856701c8c90ce
saiashish9 <saiashish3760@gmail.com>
2025-01-26 22:01:48 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -100592,6 +100592,70 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  198: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are a professional robber planning to rob houses along a street.
+        Each house has a certain amount of money stashed, the only constraint
+        stopping you from robbing each of them is that adjacent houses have
+        security systems connected and{" "}
+        <b>
+          it will automatically contact the police if two adjacent houses were
+          broken into on the same night
+        </b>
+        .
+      </p>
+
+      <p>
+        Given an integer array <code>nums</code> representing the amount of
+        money of each house, return{" "}
+        <em>
+          the maximum amount of money you can rob tonight{" "}
+          <b>without alerting the police</b>
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,1] <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> Rob house 1 (money = 1) and then rob house
+        3 (money = 3). <br />
+        Total amount you can rob = 1 + 3 = 4.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [2,7,9,3,1] <br />
+        <strong>Output:</strong> 12 <br />
+        <strong>Explanation:</strong> Rob house 1 (money = 2), rob house 3
+        (money = 9) and rob house 5 (money = 1). <br />
+        Total amount you can rob = 2 + 9 + 1 = 12.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>0 &lt;= nums[i] &lt;= 400</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
