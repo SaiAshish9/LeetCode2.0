d0d97f23121409ccf37dee693195fcd9b9bfeecb
saiashish9 <saiashish3760@gmail.com>
2025-01-26 11:53:14 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -94938,6 +94938,94 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  222: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a <strong>complete</strong> binary tree,
+        return the number of the nodes in the tree.
+      </p>
+
+      <p>
+        According to{" "}
+        <strong>
+          <a
+            href="http://en.wikipedia.org/wiki/Binary_tree#Types_of_binary_trees"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Wikipedia
+          </a>
+        </strong>
+        , every level, except possibly the last, is completely filled in a
+        complete binary tree, and all nodes in the last level are as far left as
+        possible. It can have between <code>1</code> and{" "}
+        <code>
+          2<sup>h</sup>
+        </code>{" "}
+        nodes inclusive at the last level <code>h</code>.
+      </p>
+
+      <p>
+        Design an algorithm that runs in less than&nbsp;
+        <code data-stringify-type="code">O(n)</code>&nbsp;time complexity.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/01/14/complete.jpg"
+        style={{ width: 372, height: 302 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [1,2,3,4,5,6] <br />
+        <strong>Output:</strong> 6
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [] <br />
+        <strong>Output:</strong> 0
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [1] <br />
+        <strong>Output:</strong> 1
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range{" "}
+          <code>
+            [0, 5 * 10<sup>4</sup>]
+          </code>
+          .
+        </li>
+        <li>
+          <code>
+            0 &lt;= Node.val &lt;= 5 * 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          The tree is guaranteed to be <strong>complete</strong>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
