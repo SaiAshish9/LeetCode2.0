a41cafa49edd681f2fb6b47fea9428c84d7ddaa4
saiashish9 <saiashish3760@gmail.com>
2025-01-26 14:24:04 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -97315,6 +97315,126 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1612: (
+    <div class="markdown-body div-width">
+      <div class="content__u3I1 question-content__JfgR">
+        <div>
+          <p>
+            A{" "}
+            <strong>
+              <a
+                href="https://en.wikipedia.org/wiki/Binary_expression_tree"
+                target="_blank"
+                rel="noreferrer"
+              >
+                binary expression tree
+              </a>
+            </strong>{" "}
+            is a kind of binary tree used to represent arithmetic expressions.
+            Each node of a binary expression tree has either zero or two
+            children. Leaf nodes (nodes with 0 children) correspond to operands
+            (variables), and internal nodes (nodes with two children) correspond
+            to the operators. In this problem, we only consider the{" "}
+            <code>'+'</code> operator (i.e. addition).
+          </p>
+
+          <p>
+            You are given the roots of two binary expression trees,{" "}
+            <code>root1</code> and <code>root2</code>. Return <code>true</code>
+            <em> if the two binary expression trees are equivalent</em>.
+            Otherwise, return <code>false</code>.
+          </p>
+
+          <p>
+            Two binary expression trees are equivalent if they{" "}
+            <strong>evaluate to the same value</strong> regardless of what the
+            variables are set to.
+          </p>
+
+          <p>
+            <strong>Follow up:</strong> What will you change in your solution if
+            the tree also supports the <code>'-'</code> operator (i.e.
+            subtraction)?
+          </p>
+
+          <p>&nbsp;</p>
+          <p>
+            <strong>Example 1:</strong>
+          </p>
+
+          <pre>
+            <strong>Input:</strong> root1 = [x], root2 = [x] <br />
+            <strong>Output:</strong> true
+          </pre>
+
+          <p>
+            <strong>Example 2:</strong>
+          </p>
+
+          <p>
+            <strong>
+              <img
+                alt=""
+                src="https://assets.leetcode.com/uploads/2020/10/04/tree1.png"
+                style={{ width: 211, height: 131 }}
+              />
+            </strong>
+          </p>
+
+          <pre>
+            <strong>Input:</strong> root1 = [+,a,+,null,null,b,c], root2 =
+            [+,+,b,c,a] <br />
+            <strong>Output:</strong> true <br />
+            <strong>Explaination:</strong>{" "}
+            <code>a + (b + c) == (b + c) + a</code>
+          </pre>
+
+          <p>
+            <strong>Example 3:</strong>
+          </p>
+
+          <p>
+            <strong>
+              <img
+                alt=""
+                src="https://assets.leetcode.com/uploads/2020/10/04/tree2.png"
+                style={{ width: 211, height: 131 }}
+              />
+            </strong>
+          </p>
+
+          <pre>
+            <strong>Input:</strong> root1 = [+,a,+,null,null,b,c], root2 =
+            [+,+,b,d,a] <br />
+            <strong>Output:</strong> false
+            <br />
+            <strong>Explaination:</strong>{" "}
+            <code>a + (b + c) != (b + d) + a</code>
+          </pre>
+
+          <p>&nbsp;</p>
+          <p>
+            <strong>Constraints:</strong>
+          </p>
+
+          <ul>
+            <li>
+              The number of nodes in both trees are equal, odd and, in the range{" "}
+              <code>[1, 4999]</code>.
+            </li>
+            <li>
+              <code>Node.val</code> is <code>'+'</code> or a lower-case English
+              letter.
+            </li>
+            <li>
+              It's <strong>guaranteed</strong> that the tree given is a valid
+              binary expression tree.
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
