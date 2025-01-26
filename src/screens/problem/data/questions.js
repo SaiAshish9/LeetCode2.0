c73ae49559a54525d8c3e739a0e1f00a59d15557
saiashish9 <saiashish3760@gmail.com>
2025-01-26 15:21:57 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -98105,6 +98105,169 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  2689: (
+    <article role="main" class="blog-post">
+      <p>
+        You are given the&nbsp;<code>root</code> of a binary tree&nbsp;and an
+        integer <code>k</code>. Besides&nbsp;the left and right children, every
+        node of this tree has two other properties, a&nbsp;
+        <strong>string&nbsp;</strong>
+        <code>node.val</code>&nbsp;containing only lowercase English letters
+        (possibly empty) and a non-negative integer&nbsp;<code>node.len</code>
+        .&nbsp;There are two types of nodes in this tree:
+      </p>
+
+      <ul>
+        <li>
+          <strong>Leaf</strong>: These nodes have no children,{" "}
+          <code>node.len = 0</code>, and <code>node.val</code> is some{" "}
+          <strong>non-empty</strong> string.
+        </li>
+        <li>
+          <strong>Internal</strong>: These nodes have at least one child (also
+          at most two children),&nbsp;<code>node.len&nbsp;&gt; 0</code>
+          ,&nbsp;and <code>node.val</code> is an <strong>empty</strong> string.
+        </li>
+      </ul>
+
+      <p>
+        The tree described above is called a <em>Rope</em> binary tree. Now we
+        define <code>S[node]</code>&nbsp;recursively as follows:
+      </p>
+
+      <ul>
+        <li>
+          If <code>node</code>&nbsp;is some leaf node,{" "}
+          <code>S[node] = node.val</code>,
+        </li>
+        <li>
+          Otherwise if <code>node</code> is some internal node,{" "}
+          <code>S[node] = concat(S[node.left], S[node.right])</code>.
+        </li>
+      </ul>
+
+      <p>
+        &nbsp;Return<em> k-th character of the string&nbsp;</em>
+        <code>S[root]</code>.
+      </p>
+
+      <p>
+        <strong>Note:</strong> If <code>s</code> and <code>p</code> are two
+        strings, <code>concat(s, p)</code> is a string obtained by concatenating{" "}
+        <code>p</code> to&nbsp;<code>s</code>. For example,{" "}
+        <code>concat("ab", "zz") = "abzz"</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [10,4,"abcpoe","g","rta"], k = 6 <br />
+        <strong>Output:</strong> "b" <br />
+        <strong>Explanation:</strong> In the picture below, we put an integer on
+        internal nodes that represents node.len, and a string on leaf nodes that
+        represents node.val. <br />
+        You can see that S[root] = concat(concat("g", "rta"), "abcpoe") =
+        "grtaabcpoe". So S[root][5], which represents 6th character&nbsp;of it,
+        is equal to "b".
+      </pre>
+
+      <p>
+        <img
+          alt=""
+          src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/2600-2699/2689.Extract%20Kth%20Character%20From%20The%20Rope%20Tree/images/example1.png"
+          style={{ width: 300, height: 213, marginLeft: 280, marginRight: 280 }}
+        />
+      </p>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = [12,6,6,"abc","efg","hij","klm"], k = 3
+        <br />
+        <strong>Output:</strong> "c"
+        <br />
+        <strong>Explanation:</strong> In the picture below, we put an integer on
+        internal nodes that represents node.len, and a string on leaf nodes that
+        represents node.val. <br />
+        You can see that S[root] = concat(concat("abc", "efg"), concat("hij",
+        "klm")) = "abcefghijklm". So S[root][2], which represents the 3rd
+        character of it, is equal to "c".
+      </pre>
+
+      <p>
+        <img
+          alt=""
+          src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/2600-2699/2689.Extract%20Kth%20Character%20From%20The%20Rope%20Tree/images/example2.png"
+          style={{ width: 400, height: 232, marginLeft: 255, marginRight: 255 }}
+        />
+      </p>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> root = ["ropetree"], k = 8<br />
+        <strong>Output:</strong> "e"
+        <br />
+        <strong>Explanation:</strong> In the picture below, we put an integer on
+        internal nodes that represents node.len, and a string on leaf nodes that
+        represents node.val.
+        <br />
+        You can see that S[root] = "ropetree". So S[root][7], which represents
+        8th character of it, is equal to "e".
+      </pre>
+
+      <p>
+        <img
+          alt=""
+          src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/2600-2699/2689.Extract%20Kth%20Character%20From%20The%20Rope%20Tree/images/example3.png"
+          style={{ width: 80, height: 78, marginLeft: 400, marginRight: 400 }}
+        />
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range{" "}
+          <code>
+            [1, 10<sup>3</sup>]
+          </code>
+        </li>
+        <li>
+          <code>node.val</code> contains only lowercase English letters
+        </li>
+        <li>
+          <code>0 &lt;= node.val.length &lt;= 50</code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= node.len &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          for leaf nodes, <code>node.len = 0</code> and <code>node.val</code> is
+          non-empty
+        </li>
+        <li>
+          for internal nodes, <code>node.len &gt; 0</code> and{" "}
+          <code>node.val</code> is empty
+        </li>
+        <li>
+          <code>1 &lt;= k &lt;= S[root].length</code>
+        </li>
+      </ul>
+    </article>
+  ),
 };
 
 export default QUESTIONS;
