371a986ca6a805cbcbbb8e914ad54e4e6b184f8f
saiashish9 <saiashish3760@gmail.com>
2025-01-26 18:40:21 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -99365,6 +99365,58 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  70: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are climbing a staircase. It takes <code>n</code> steps to reach the
+        top.
+      </p>
+
+      <p>
+        Each time you can either climb <code>1</code> or <code>2</code> steps.
+        In how many distinct ways can you climb to the top?
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> There are two ways to climb to the top.{" "}
+        <br />
+        1. 1 step + 1 step <br />
+        2. 2 steps
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 3 <br />
+        <strong>Output:</strong> 3 <br />
+        <strong>Explanation:</strong> There are three ways to climb to the top.{" "}
+        <br />
+        1. 1 step + 1 step + 1 step <br />
+        2. 1 step + 2 steps <br />
+        3. 2 steps + 1 step
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= n &lt;= 45</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
