f7e82faad45671776d0c03bc07e7b44b58bbd0a0
saiashish9 <saiashish3760@gmail.com>
2025-01-26 13:43:23 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -96792,6 +96792,89 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1028: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        We run a&nbsp;preorder&nbsp;depth-first search (DFS) on the{" "}
+        <code>root</code> of a binary tree.
+      </p>
+
+      <p>
+        At each node in this traversal, we output <code>D</code> dashes (where{" "}
+        <code>D</code> is the depth of this node), then we output the value of
+        this node.&nbsp; If the depth of a node is <code>D</code>, the depth of
+        its immediate child is <code>D + 1</code>.&nbsp; The depth of the{" "}
+        <code>root</code> node is <code>0</code>.
+      </p>
+
+      <p>
+        If a node has only one child, that child is guaranteed to be{" "}
+        <strong>the left child</strong>.
+      </p>
+
+      <p>
+        Given the output <code>traversal</code> of this traversal, recover the
+        tree and return <em>its</em> <code>root</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2024/09/10/recover_tree_ex1.png"
+        style={{ width: 423, height: 200 }}
+      />
+      <pre>
+        <strong>Input:</strong> traversal = "1-2--3--4-5--6--7" <br />
+        <strong>Output:</strong> [1,2,5,3,4,6,7]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2024/09/10/recover_tree_ex2.png"
+        style={{ width: 432, height: 250 }}
+      />
+      <pre>
+        <strong>Input:</strong> traversal = "1-2--3---4-5--6---7" <br />
+        <strong>Output:</strong> [1,2,5,3,null,6,null,4,null,7]
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2024/09/10/recover_tree_ex3.png"
+        style={{ width: 305, height: 250 }}
+      />
+      <pre>
+        <strong>Input:</strong> traversal = "1-401--349---90--88" <br />
+        <strong>Output:</strong> [1,401,null,349,88,90]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the original tree is in the range{" "}
+          <code>[1, 1000]</code>.
+        </li>
+        <li>
+          <code>
+            1 &lt;= Node.val &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
