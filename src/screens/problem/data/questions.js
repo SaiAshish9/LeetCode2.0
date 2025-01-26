d77e1fe9e3f35a5f47f7c9dc345c324de82c3e80
saiashish9 <saiashish3760@gmail.com>
2025-01-26 13:08:34 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -96066,6 +96066,75 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  687: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>root</code> of a binary tree, return{" "}
+        <em>
+          the length of the longest path, where each node in the path has the
+          same value
+        </em>
+        . This path may or may not pass through the root.
+      </p>
+
+      <p>
+        <strong>The length of the path</strong> between two nodes is represented
+        by the number of edges between them.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/10/13/ex1.jpg"
+        style={{ width: 450, height: 238 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [5,4,5,1,1,null,5] <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> The shown image shows that the longest
+        path of the same value (i.e. 5).
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/10/13/ex2.jpg"
+        style={{ width: 450, height: 238 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [1,4,5,4,4,null,5] <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> The shown image shows that the longest
+        path of the same value (i.e. 4).
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the tree is in the range{" "}
+          <code>
+            [0, 10<sup>4</sup>]
+          </code>
+          .
+        </li>
+        <li>
+          <code>-1000 &lt;= Node.val &lt;= 1000</code>
+        </li>
+        <li>
+          The depth of the tree will not exceed <code>1000</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
