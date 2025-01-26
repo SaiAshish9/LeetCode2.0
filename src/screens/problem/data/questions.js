bd1a569e32b3c94168201f0407ba513449ff123c
saiashish9 <saiashish3760@gmail.com>
2025-01-26 14:52:53 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -97759,6 +97759,73 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </div>
     </div>
   ),
+  2236: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given the <code>root</code> of a <strong>binary tree</strong>{" "}
+        that consists of exactly <code>3</code> nodes: the root, its left child,
+        and its right child.
+      </p>
+
+      <p>
+        Return <code>true</code>{" "}
+        <em>
+          if the value of the root is equal to the <strong>sum</strong> of the
+          values of its two children, or{" "}
+        </em>
+        <code>false</code>
+        <em> otherwise</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2022/04/08/graph3drawio.png"
+        style={{ width: 281, height: 199 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [10,4,6] <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> The values of the root, its left child,
+        and its right child are 10, 4, and 6, respectively. <br />
+        10 is equal to 4 + 6, so we return true.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2022/04/08/graph3drawio-1.png"
+        style={{ width: 281, height: 199 }}
+      />
+      <pre>
+        <strong>Input:</strong> root = [5,3,1] <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> The values of the root, its left child,
+        and its right child are 5, 3, and 1, respectively. <br />5 is not equal
+        to 3 + 1, so we return false.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The tree consists only of the root, its left child, and its right
+          child.
+        </li>
+        <li>
+          <code>-100 &lt;= Node.val &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
