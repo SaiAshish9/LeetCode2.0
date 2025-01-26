45b7333ccd1ae50baee7697c88795a15d0e14ba5
saiashish9 <saiashish3760@gmail.com>
2025-01-27 00:26:29 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -101116,6 +101116,89 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  300: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code>, return{" "}
+        <em>
+          the length of the longest <strong>strictly increasing </strong>
+        </em>
+        <span
+          data-keyword="subsequence-array"
+          class=" cursor-pointer relative text-dark-blue-s text-sm"
+        >
+          <div class="popover-wrapper inline-block" data-headlessui-state="">
+            <div>
+              <div
+                aria-expanded="false"
+                data-headlessui-state=""
+                id="headlessui-popover-button-:r6a:"
+              >
+                <div>
+                  <em>
+                    <strong>subsequence</strong>
+                  </em>
+                </div>
+              </div>
+            </div>
+          </div>
+        </span>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [10,9,2,5,3,7,101,18] <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> The longest increasing subsequence is
+        [2,3,7,101], therefore the length is 4.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [0,1,0,3,2,3] <br />
+        <strong>Output:</strong> 4
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [7,7,7,7,7,7,7] <br />
+        <strong>Output:</strong> 1
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 2500</code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= nums[i] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <b>Follow up:</b>&nbsp;Can you come up with an algorithm that runs
+        in&nbsp;<code>O(n log(n))</code> time complexity?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
