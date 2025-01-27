e64dc6ee3af4dee9aed2b59702cbc1d63dd9f886
saiashish9 <saiashish3760@gmail.com>
2025-01-27 12:17:49 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -102046,6 +102046,82 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  678: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string <code>s</code> containing only three types of characters:{" "}
+        <code>'('</code>, <code>')'</code> and <code>'*'</code>, return{" "}
+        <code>true</code> <em>if</em> <code>s</code>{" "}
+        <em>
+          is <strong>valid</strong>
+        </em>
+        .
+      </p>
+      <p>
+        The following rules define a <strong>valid</strong> string:
+      </p>
+      <ul>
+        <li>
+          Any left parenthesis <code>{"'('"}</code> must have a corresponding
+          right parenthesis <code>{"')'"}</code>.
+        </li>
+        <li>
+          Any right parenthesis <code>{"')'"}</code> must have a corresponding
+          left parenthesis <code>'('</code>.
+        </li>
+        <li>
+          Left parenthesis <code>{"'('"}</code> must go before the corresponding
+          right parenthesis <code>')'</code>.
+        </li>
+        <li>
+          <code>'*'</code> could be treated as a single right parenthesis{" "}
+          <code>{"')'"}</code> or a single left parenthesis <code>{"'('"}</code>{" "}
+          or an empty string <code>""</code>.
+        </li>
+      </ul>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> s = "()" <br />
+        <strong>Output:</strong> true <br />
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> s = "(*)"
+        <br />
+        <strong>Output:</strong> true
+        <br />
+      </pre>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <br />
+      <pre>
+        <strong>Input:</strong> s = {'"(*))"'}
+        <br />
+        <strong>Output:</strong> true
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>1 &lt;= s.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>s[i]</code> is <code>'('</code>, <code>')'</code> or{" "}
+          <code>'*'</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
