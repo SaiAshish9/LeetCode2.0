bb0dc337d0b3ca9de62fa815e6e9628ed0b6ba18
saiashish9 <saiashish3760@gmail.com>
2025-01-27 12:53:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -102539,6 +102539,90 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  887: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given <code>k</code> identical eggs and you have access to a
+        building with <code>n</code> floors labeled from <code>1</code> to{" "}
+        <code>n</code>.
+      </p>
+
+      <p>
+        You know that there exists a floor <code>f</code> where{" "}
+        <code>0 &lt;= f &lt;= n</code> such that any egg dropped at a floor{" "}
+        <strong>higher</strong> than <code>f</code> will <strong>break</strong>,
+        and any egg dropped <strong>at or below</strong> floor <code>f</code>{" "}
+        will <strong>not break</strong>.
+      </p>
+
+      <p>
+        Each move, you may take an unbroken egg and drop it from any floor{" "}
+        <code>x</code> (where <code>1 &lt;= x &lt;= n</code>). If the egg
+        breaks, you can no longer use it. However, if the egg does not break,
+        you may <strong>reuse</strong> it in future moves.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>minimum number of moves</strong> that you need to
+          determine <strong>with certainty</strong> what the value of{" "}
+        </em>
+        <code>f</code> is.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> k = 1, n = 2 <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation: </strong> <br />
+        Drop the egg from floor 1. If it breaks, we know that f = 0. <br />
+        Otherwise, drop the egg from floor 2. If it breaks, we know that f = 1.{" "}
+        <br />
+        If it does not break, then we know f = 2. <br />
+        Hence, we need at minimum 2 moves to determine with certainty what the
+        value of f is.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> k = 2, n = 6 <br />
+        <strong>Output:</strong> 3
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> k = 3, n = 14 <br />
+        <strong>Output:</strong> 4
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= k &lt;= 100</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
