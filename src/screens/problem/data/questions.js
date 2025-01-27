4662122d9bb3de16d28bfa3d29577e8e6b9e59ed
saiashish9 <saiashish3760@gmail.com>
2025-01-27 18:34:27 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -103537,6 +103537,80 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1682: (
+    <article role="main" class="blog-post">
+      <p>
+        A subsequence of a string <code>s</code> is considered a{" "}
+        <strong>good palindromic subsequence</strong> if:
+      </p>
+
+      <ul>
+        <li>
+          It is a subsequence of <code>s</code>.
+        </li>
+        <li>It is a palindrome (has the same value if reversed).</li>
+        <li>
+          It has an <strong>even</strong> length.
+        </li>
+        <li>
+          No two consecutive characters are equal, except the two middle ones.
+        </li>
+      </ul>
+
+      <p>
+        For example, if <code>s = "abcabcabb"</code>, then <code>"abba"</code>{" "}
+        is considered a <strong>good palindromic subsequence</strong>, while{" "}
+        <code>"bcb"</code> (not even length) and <code>"bbbb"</code> (has equal
+        consecutive characters) are not.
+      </p>
+
+      <p>
+        Given a string <code>s</code>, return{" "}
+        <em>
+          the <strong>length</strong> of the{" "}
+          <strong>longest good palindromic subsequence</strong> in{" "}
+        </em>
+        <code>s</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "bbabab" <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> The longest good palindromic subsequence
+        of s is "baab".
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "dcbccacdb" <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> The longest good palindromic subsequence
+        of s is "dccd".
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= s.length &lt;= 250</code>
+        </li>
+        <li>
+          <code>s</code> consists of lowercase English letters.
+        </li>
+      </ul>
+    </article>
+  ),
 };
 
 export default QUESTIONS;
