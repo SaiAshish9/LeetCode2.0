ae6d32d2bd55a948e8b52452dbf717376efee46b
saiashish9 <saiashish3760@gmail.com>
2025-01-27 21:59:55 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -104504,6 +104504,102 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  2297: (
+    <div>
+      <p>
+        You are given a <strong>0-indexed</strong> integer array{" "}
+        <code>nums</code> of length <code>n</code>. You are initially standing
+        at index <code>0</code>. You can jump from index <code>i</code> to index{" "}
+        <code>j</code> where <code>i &lt; j</code> if:
+      </p>
+
+      <ul>
+        <li>
+          <code>nums[i] &lt;= nums[j]</code> and{" "}
+          <code>nums[k] &lt; nums[i]</code> for all indexes <code>k</code> in
+          the range <code>i &lt; k &lt; j</code>, or
+        </li>
+        <li>
+          <code>nums[i] &gt; nums[j]</code> and{" "}
+          <code>nums[k] &gt;= nums[i]</code> for all indexes <code>k</code> in
+          the range <code>i &lt; k &lt; j</code>.
+        </li>
+      </ul>
+
+      <p>
+        You are also given an integer array <code>costs</code> of length{" "}
+        <code>n</code> where <code>costs[i]</code> denotes the cost of jumping{" "}
+        <strong>to</strong> index <code>i</code>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>minimum</strong> cost to jump to the index{" "}
+        </em>
+        <code>n - 1</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [3,2,4,4,1], costs = [3,7,6,4,2] <br />
+        <strong>Output:</strong> 8 <br />
+        <strong>Explanation:</strong> You start at index 0. <br />
+        - Jump to index 2 with a cost of costs[2] = 6. <br />
+        - Jump to index 4 with a cost of costs[4] = 2.
+        <br />
+        The total cost is 8. It can be proven that 8 is the minimum cost needed.
+        <br />
+        Two other possible paths are from index 0 -&gt; 1 -&gt; 4 and index 0
+        -&gt; 2 -&gt; 3 -&gt; 4.
+        <br />
+        These have a total cost of 9 and 12, respectively.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [0,1,2], costs = [1,1,1]
+        <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong> Start at index 0.
+        <br />
+        - Jump to index 1 with a cost of costs[1] = 1.
+        <br />
+        - Jump to index 2 with a cost of costs[2] = 1.
+        <br />
+        The total cost is 2. Note that you cannot jump directly from index 0 to
+        index 2 because nums[0] &lt;= nums[1].
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == nums.length == costs.length</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= nums[i], costs[i] &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
