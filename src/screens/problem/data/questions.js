b89c9992ec4fd5f38da78af6cf99f71e31c1712d
saiashish9 <saiashish3760@gmail.com>
2025-01-27 23:13:44 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -105828,7 +105828,6 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
               >
                 <div>binary array</div>
               </div>
-              <div style="position: fixed; z-index: 40; inset: 0px auto auto 0px; transform: translate(77px, 252px);"></div>
             </div>
           </div>
         </span>{" "}
@@ -105860,7 +105859,6 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
                 >
                   <div>subarray</div>
                 </div>
-                <div style="position: fixed; z-index: 40; inset: 0px auto auto 0px; transform: translate(106px, 355px);"></div>
               </div>
             </div>
           </span>{" "}
