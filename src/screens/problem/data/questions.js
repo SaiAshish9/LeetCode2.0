54b6cb6f98715c7c2168793b67340ac22fa37288
saiashish9 <saiashish3760@gmail.com>
2025-01-27 18:37:00 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -103611,6 +103611,89 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </article>
   ),
+  1749: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an integer array <code>nums</code>. The{" "}
+        <strong>absolute sum</strong> of a subarray{" "}
+        <code>
+          [nums<sub>l</sub>, nums<sub>l+1</sub>, ..., nums<sub>r-1</sub>, nums
+          <sub>r</sub>]
+        </code>{" "}
+        is{" "}
+        <code>
+          abs(nums<sub>l</sub> + nums<sub>l+1</sub> + ... + nums<sub>r-1</sub> +
+          nums<sub>r</sub>)
+        </code>
+        .
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>maximum</strong> absolute sum of any{" "}
+          <strong>(possibly empty)</strong> subarray of{" "}
+        </em>
+        <code>nums</code>.
+      </p>
+
+      <p>
+        Note that <code>abs(x)</code> is defined as follows:
+      </p>
+
+      <ul>
+        <li>
+          If <code>x</code> is a negative integer, then <code>abs(x) = -x</code>
+          .
+        </li>
+        <li>
+          If <code>x</code> is a non-negative integer, then{" "}
+          <code>abs(x) = x</code>.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,-3,2,3,-4] <br />
+        <strong>Output:</strong> 5 <br />
+        <strong>Explanation:</strong> The subarray [2,3] has absolute sum =
+        abs(2+3) = abs(5) = 5.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [2,-5,1,-4,3,-2] <br />
+        <strong>Output:</strong> 8 <br />
+        <strong>Explanation:</strong> The subarray [-5,1,-4] has absolute sum =
+        abs(-5+1-4) = abs(-8) = 8.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= nums[i] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
