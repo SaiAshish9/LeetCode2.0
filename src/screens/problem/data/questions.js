81f4231d6838f64f14ce23220bc46deec29d6364
saiashish9 <saiashish3760@gmail.com>
2025-01-27 11:44:55 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -101652,6 +101652,77 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </div>
     </div>
   ),
+  377: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array of <strong>distinct</strong> integers <code>nums</code>{" "}
+        and a target integer <code>target</code>, return{" "}
+        <em>the number of possible combinations that add up to</em>&nbsp;
+        <code>target</code>.
+      </p>
+
+      <p>
+        The test cases are generated so that the answer can fit in a{" "}
+        <strong>32-bit</strong> integer.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3], target = 4 <br />
+        <strong>Output:</strong> 7 <br />
+        <strong>Explanation:</strong> <br />
+        The possible combination ways are: <br />
+        (1, 1, 1, 1) <br />
+        (1, 1, 2) <br />
+        (1, 2, 1) <br />
+        (1, 3) <br />
+        (2, 1, 1) <br />
+        (2, 2) <br />
+        (3, 1) <br />
+        Note that different sequences are counted as different combinations.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [9], target = 3 <br />
+        <strong>Output:</strong> 0
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 200</code>
+        </li>
+        <li>
+          <code>1 &lt;= nums[i] &lt;= 1000</code>
+        </li>
+        <li>
+          All the elements of <code>nums</code> are <strong>unique</strong>.
+        </li>
+        <li>
+          <code>1 &lt;= target &lt;= 1000</code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong> What if negative numbers are allowed in the
+        given array? How does it change the problem? What limitation we need to
+        add to the question to allow negative numbers?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
