3113d370b99b775e5c7814794cbcff20ddc092a9
saiashish9 <saiashish3760@gmail.com>
2025-01-27 22:33:44 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -104947,6 +104947,92 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  2484: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string of digits <code>s</code>, return{" "}
+        <em>
+          the number of <strong>palindromic subsequences</strong> of
+        </em>{" "}
+        <code>s</code>
+        <em> having length </em>
+        <code>5</code>. Since the answer may be very large, return it{" "}
+        <strong>modulo</strong>{" "}
+        <code>
+          10<sup>9</sup> + 7
+        </code>
+        .
+      </p>
+
+      <p>
+        <strong>Note:</strong>
+      </p>
+
+      <ul>
+        <li>
+          A string is <strong>palindromic</strong> if it reads the same forward
+          and backward.
+        </li>
+        <li>
+          A <strong>subsequence</strong> is a string that can be derived from
+          another string by deleting some or no characters without changing the
+          order of the remaining characters.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "103301" <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong> <br />
+        There are 6 possible subsequences of length 5:
+        "10330","10331","10301","10301","13301","03301". <br />
+        Two of them (both equal to "10301") are palindromic.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "0000000" <br />
+        <strong>Output:</strong> 21 <br />
+        <strong>Explanation:</strong> All 21 subsequences are "00000", which is
+        palindromic.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "9999900000" <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> The only two palindromic subsequences are
+        "99999" and "00000".
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>s</code> consists of digits.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
