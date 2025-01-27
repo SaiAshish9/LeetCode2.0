48bf523e97069cf1490b8d4b1069e0f0130458aa
saiashish9 <saiashish3760@gmail.com>
2025-01-27 23:16:37 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -106005,7 +106005,6 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
               >
                 <div>subsequence</div>
               </div>
-              <div style="position: fixed; z-index: 40; inset: 0px auto auto 0px; transform: translate(381px, 315px);"></div>
             </div>
           </div>
         </span>{" "}
