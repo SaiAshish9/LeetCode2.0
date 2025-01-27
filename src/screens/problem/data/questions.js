c0546c8c5cab61d66c63fdbb85a0486b33990317
saiashish9 <saiashish3760@gmail.com>
2025-01-27 13:12:46 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -102752,6 +102752,88 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1049: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an array of integers <code>stones</code> where{" "}
+        <code>stones[i]</code> is the weight of the{" "}
+        <code>
+          i<sup>th</sup>
+        </code>{" "}
+        stone.
+      </p>
+
+      <p>
+        We are playing a game with the stones. On each turn, we choose any two
+        stones and smash them together. Suppose the stones have weights{" "}
+        <code>x</code> and <code>y</code> with <code>x &lt;= y</code>. The
+        result of this smash is:
+      </p>
+
+      <ul>
+        <li>
+          If <code>x == y</code>, both stones are destroyed, and
+        </li>
+        <li>
+          If <code>x != y</code>, the stone of weight <code>x</code> is
+          destroyed, and the stone of weight <code>y</code> has new weight{" "}
+          <code>y - x</code>.
+        </li>
+      </ul>
+
+      <p>
+        At the end of the game, there is <strong>at most one</strong> stone
+        left.
+      </p>
+
+      <p>
+        Return <em>the smallest possible weight of the left stone</em>. If there
+        are no stones left, return <code>0</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> stones = [2,7,4,1,8,1] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> <br />
+        We can combine 2 and 4 to get 2, so the array converts to [2,7,1,8,1]
+        then, <br />
+        we can combine 7 and 8 to get 1, so the array converts to [2,1,1,1]
+        then, <br />
+        we can combine 2 and 1 to get 1, so the array converts to [1,1,1] then,{" "}
+        <br />
+        we can combine 1 and 1 to get 0, so the array converts to [1], then
+        that's the optimal value.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> stones = [31,26,33,21,40] <br />
+        <strong>Output:</strong> 5
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= stones.length &lt;= 30</code>
+        </li>
+        <li>
+          <code>1 &lt;= stones[i] &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
