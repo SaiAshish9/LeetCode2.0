58601b07460d14c385ede2d5a5ae73a076ce9efa
saiashish9 <saiashish3760@gmail.com>
2025-01-27 11:54:27 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -101792,6 +101792,85 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  474: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an array of binary strings <code>strs</code> and two
+        integers <code>m</code> and <code>n</code>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the size of the largest subset of <code>strs</code> such that there
+          are <strong>at most</strong>{" "}
+        </em>
+        <code>m</code>
+        <em> </em>
+        <code>0</code>
+        <em>'s and </em>
+        <code>n</code>
+        <em> </em>
+        <code>1</code>
+        <em>'s in the subset</em>.
+      </p>
+
+      <p>
+        A set <code>x</code> is a <strong>subset</strong> of a set{" "}
+        <code>y</code> if all elements of <code>x</code> are also elements of{" "}
+        <code>y</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> strs = ["10","0001","111001","1","0"], m = 5, n
+        = 3 <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> The largest subset with at most 5 0's and
+        3 1's is {`{"10", "0001", "1", "0"}`}, so the answer is 4. <br />
+        Other valid but smaller subsets include {`{"0001", "1"}`} and{" "}
+        {`{"10", "1", "0"}`}. <br />
+        {`{"111001"}`} is an invalid subset because it contains 4 1's, greater
+        than the maximum of 3.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> strs = ["10","0","1"], m = 1, n = 1<br />
+        <strong>Output:</strong> 2<br />
+        <b>Explanation:</b> The largest subset is {`{"0", "1"}`}, so the answer
+        is 2.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= strs.length &lt;= 600</code>
+        </li>
+        <li>
+          <code>1 &lt;= strs[i].length &lt;= 100</code>
+        </li>
+        <li>
+          <code>strs[i]</code> consists only of digits <code>'0'</code> and{" "}
+          <code>'1'</code>.
+        </li>
+        <li>
+          <code>1 &lt;= m, n &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
