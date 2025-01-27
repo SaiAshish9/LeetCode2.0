3dc28d31d636b2082fda47f65f10c6d84fd6cb18
saiashish9 <saiashish3760@gmail.com>
2025-01-27 12:31:34 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -102271,6 +102271,90 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  750: (
+    <div class="markdown-body div-width">
+      <p>
+        Given a grid where each entry is only 0 or 1, find the number of corner
+        rectangles.
+      </p>
+
+      <p>
+        A <em>corner rectangle</em> is 4 distinct 1s on the grid that form an
+        axis-aligned rectangle. Note that only the corners need to have the
+        value 1. Also, all four 1s used must be distinct.
+      </p>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid = <br />
+        [[1, 0, 0, 1, 0], <br />
+        [0, 0, 1, 0, 1], <br />
+        [0, 0, 0, 1, 0], <br />
+        [1, 0, 1, 0, 1]] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> There is only one corner rectangle, with
+        corners grid[1][2], grid[1][4], grid[3][2], grid[3][4].
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid = <br />
+        [[1, 1, 1], <br />
+        [1, 1, 1], <br />
+        [1, 1, 1]] <br />
+        <strong>Output:</strong> 9 <br />
+        <strong>Explanation:</strong> There are four 2x2 rectangles, four 2x3
+        and 3x2 rectangles, and one 3x3 rectangle.
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid = <br />
+        [[1, 1, 1, 1]] <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> Rectangles must have four distinct
+        corners.
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <strong>Note:</strong>
+      </p>
+
+      <ol>
+        <li>
+          The number of rows and columns of <code>grid</code> will each be in
+          the range <code>[1, 200]</code>.
+        </li>
+        <li>
+          Each <code>grid[i][j]</code> will be either <code>0</code> or{" "}
+          <code>1</code>.
+        </li>
+        <li>
+          The number of <code>1</code>s in the grid will be at most{" "}
+          <code>6000</code>.
+        </li>
+      </ol>
+
+      <p>&nbsp;</p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
