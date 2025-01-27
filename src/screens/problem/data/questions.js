ac3519ff0ecd23f55646da054f67c52905431e32
saiashish9 <saiashish3760@gmail.com>
2025-01-27 22:29:32 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -104858,6 +104858,95 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  2439: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given a <strong>0-indexed</strong> array <code>nums</code>{" "}
+        comprising of <code>n</code> non-negative integers.
+      </p>
+
+      <p>In one operation, you must:</p>
+
+      <ul>
+        <li>
+          Choose an integer <code>i</code> such that{" "}
+          <code>1 &lt;= i &lt; n</code> and <code>nums[i] &gt; 0</code>.
+        </li>
+        <li>
+          Decrease <code>nums[i]</code> by 1.
+        </li>
+        <li>
+          Increase <code>nums[i - 1]</code> by 1.
+        </li>
+      </ul>
+
+      <p>
+        Return
+        <em>
+          {" "}
+          the <strong>minimum</strong> possible value of the{" "}
+          <strong>maximum</strong> integer of{" "}
+        </em>
+        <code>nums</code>
+        <em>
+          {" "}
+          after performing <strong>any</strong> number of operations
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [3,7,1,6] <br />
+        <strong>Output:</strong> 5 <br />
+        <strong>Explanation:</strong> <br />
+        One set of optimal operations is as follows: <br />
+        1. Choose i = 1, and nums becomes [4,6,1,6]. <br />
+        2. Choose i = 3, and nums becomes [4,6,2,5]. <br />
+        3. Choose i = 1, and nums becomes [5,5,2,5]. <br />
+        The maximum integer of nums is 5. It can be shown that the maximum
+        number cannot be less than 5.
+        <br /> Therefore, we return 5.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [10,1] <br />
+        <strong>Output:</strong> 10 <br />
+        <strong>Explanation:</strong> <br />
+        It is optimal to leave nums as is, and since 10 is the maximum value, we
+        return 10.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == nums.length</code>
+        </li>
+        <li>
+          <code>
+            2 &lt;= n &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= nums[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
