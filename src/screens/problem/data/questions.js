0d6d0b5668d25c4b4025e8b7a543af6e3effab91
saiashish9 <saiashish3760@gmail.com>
2025-01-27 13:07:28 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -102692,6 +102692,66 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1012: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer <code>n</code>, return{" "}
+        <em>the number of positive integers in the range </em>
+        <code>[1, n]</code>
+        <em>
+          {" "}
+          that have <strong>at least one</strong> repeated digit
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 20 <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> The only positive number (&lt;= 20) with
+        at least 1 repeated digit is 11.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 100 <br />
+        <strong>Output:</strong> 10 <br />
+        <strong>Explanation:</strong> The positive numbers (&lt;= 100) with
+        atleast 1 repeated digit are 11, 22, 33, 44, 55, 66, 77, 88, 99, and
+        100.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 1000 <br />
+        <strong>Output:</strong> 262
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
