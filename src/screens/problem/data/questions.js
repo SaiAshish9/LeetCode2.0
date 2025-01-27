34945372a6d0e52a1d4923b8cba4f6cfd7e15a26
saiashish9 <saiashish3760@gmail.com>
2025-01-27 17:40:33 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -103456,6 +103456,87 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  1478: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the array <code>houses</code> where <code>houses[i]</code> is the
+        location of the{" "}
+        <code>
+          i<sup>th</sup>
+        </code>{" "}
+        house along a street and an integer <code>k</code>, allocate{" "}
+        <code>k</code> mailboxes in the street.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>minimum</strong> total distance between each house and its
+          nearest mailbox
+        </em>
+        .
+      </p>
+
+      <p>
+        The test cases are generated so that the answer fits in a 32-bit
+        integer.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/05/07/sample_11_1816.png"
+        style={{ width: 454, height: 154 }}
+      />
+      <pre>
+        <strong>Input:</strong> houses = [1,4,8,10,20], k = 3 <br />
+        <strong>Output:</strong> 5 <br />
+        <strong>Explanation:</strong> Allocate mailboxes in position 3, 9 and
+        20. <br />
+        Minimum total distance from each houses to nearest mailboxes is |3-1| +
+        |4-3| + |9-8| + |10-9| + |20-20| = 5
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2020/05/07/sample_2_1816.png"
+        style={{ width: 433, height: 154 }}
+      />
+      <pre>
+        <strong>Input:</strong> houses = [2,3,5,12,18], k = 2 <br />
+        <strong>Output:</strong> 9 <br />
+        <strong>Explanation:</strong> Allocate mailboxes in position 3 and 14.{" "}
+        <br />
+        Minimum total distance from each houses to nearest mailboxes is |2-3| +
+        |3-3| + |5-3| + |12-14| + |18-14| = 9.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= k &lt;= houses.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= houses[i] &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          All the integers of <code>houses</code> are <strong>unique</strong>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
