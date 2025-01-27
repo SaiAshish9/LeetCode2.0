d3f7f291cb8c4afed00e49cd8482f3fed117bc59
saiashish9 <saiashish3760@gmail.com>
2025-01-27 23:56:43 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -106389,6 +106389,52 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  43: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given two non-negative integers <code>num1</code> and <code>num2</code>{" "}
+        represented as strings, return the product of <code>num1</code> and{" "}
+        <code>num2</code>, also represented as a string.
+      </p>
+      <p>
+        <strong>Note:</strong>&nbsp;You must not use any built-in BigInteger
+        library or convert the inputs to integer directly.
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> num1 = "2", num2 = "3" <br />
+        <strong>Output:</strong> "6" <br />
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> num1 = "123", num2 = "456" <br />
+        <strong>Output:</strong> "56088"
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>1 &lt;= num1.length, num2.length &lt;= 200</code>
+        </li>
+        <li>
+          <code>num1</code> and <code>num2</code> consist of digits only.
+        </li>
+        <li>
+          Both <code>num1</code> and <code>num2</code>&nbsp;do not contain any
+          leading zero, except the number <code>0</code> itself.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
