395bb26522b0b4466c333bdbcc54d30de79709d3
saiashish9 <saiashish3760@gmail.com>
2025-03-06 12:43:14 +0530

    k

--- src/screens/problem/data/htmlTagDescriiption.js
+++ src/screens/problem/data/htmlTagDescriiption.js
@@ -5908,6 +5908,151 @@ class UnionFind:
       </ul>
     </>
   ),
+  "Binary Search": (
+    <>
+      {" "}
+      <h2>Binary Search LeetCode Problems</h2>
+      <p>
+        Binary search is a fundamental algorithm often used in LeetCode problems
+        that involve searching in sorted arrays or optimizing solutions with
+        monotonic properties. Below are different problem variations and
+        techniques related to binary search:
+      </p>
+      <ul>
+        <li>
+          <strong>
+            <a
+              href="https://leetcode.com/problems/binary-search/"
+              target="_blank" rel="noreferrer"
+            >
+              704. Binary Search
+            </a>
+          </strong>
+          <pre>
+            {`def search(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        elif nums[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1`}
+          </pre>
+        </li>
+
+        <li>
+          <strong>
+            <a
+              href="https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/"
+              target="_blank" rel="noreferrer"
+            >
+              34. Find First and Last Position of Element
+            </a>
+          </strong>
+          <pre>
+            {`def searchRange(nums: List[int], target: int) -> List[int]:
+    def findBound(isFirst: bool) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                if isFirst:
+                    if mid == left or nums[mid - 1] != target:
+                        return mid
+                    right = mid - 1
+                else:
+                    if mid == right or nums[mid + 1] != target:
+                        return mid
+                    left = mid + 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+    
+    return [findBound(True), findBound(False)]`}
+          </pre>
+        </li>
+
+        <li>
+          <strong>
+            <a
+              href="https://leetcode.com/problems/search-in-rotated-sorted-array/"
+              target="_blank" rel="noreferrer"
+            >
+              33. Search in Rotated Sorted Array
+            </a>
+          </strong>
+          <pre>
+            {`def search(nums: List[int], target: int) -> int:
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] == target:
+            return mid
+        if nums[left] <= nums[mid]:
+            if nums[left] <= target < nums[mid]:
+                right = mid - 1
+            else:
+                left = mid + 1
+        else:
+            if nums[mid] < target <= nums[right]:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return -1`}
+          </pre>
+        </li>
+
+        <li>
+          <strong>
+            <a
+              href="https://leetcode.com/problems/find-minimum-in-rotated-sorted-array/"
+              target="_blank" rel="noreferrer"
+            >
+              153. Find Minimum in Rotated Sorted Array
+            </a>
+          </strong>
+          <pre>
+            {`def findMin(nums: List[int]) -> int:
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[right]:
+            left = mid + 1
+        else:
+            right = mid
+    return nums[left]`}
+          </pre>
+        </li>
+
+        <li>
+          <strong>
+            <a
+              href="https://leetcode.com/problems/find-peak-element/"
+              target="_blank" rel="noreferrer"
+            >
+              162. Find Peak Element
+            </a>
+          </strong>
+          <pre>
+            {`def findPeakElement(nums: List[int]) -> int:
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if nums[mid] > nums[mid + 1]:
+            right = mid
+        else:
+            left = mid + 1
+    return left`}
+          </pre>
+        </li>
+      </ul>
+    </>
+  ),
 };
 
 export default TAG_DESCRIPTION;
