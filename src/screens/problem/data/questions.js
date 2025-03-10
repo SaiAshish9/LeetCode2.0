2a71f111dd177f1b364e6d6eb0356036bfa9ad74
saiashish9 <saiashish3760@gmail.com>
2025-03-10 21:07:34 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -107062,6 +107062,85 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  147: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>head</code> of a singly linked list, sort the list using{" "}
+        <strong>insertion sort</strong>, and return{" "}
+        <em>the sorted list's head</em>.
+      </p>
+
+      <p>
+        The steps of the <strong>insertion sort</strong> algorithm:
+      </p>
+
+      <ul>
+        <li>
+          Insertion sort iterates, consuming one input element each repetition
+          and growing a sorted output list.
+        </li>
+        <li>
+          At each iteration, insertion sort removes one element from the input
+          data, finds the location it belongs within the sorted list and inserts
+          it there.
+        </li>
+        <li>It repeats until no input elements remain.</li>
+      </ul>
+
+      <p>
+        The following is a graphical example of the insertion sort algorithm.
+        The partially sorted list (black) initially contains only the first
+        element in the list. One element (red) is removed from the input data
+        and inserted in-place into the sorted list with each iteration.
+      </p>
+      <img
+        alt=""
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0f/Insertion-sort-example-300px.gif"
+        style={{ height: 180, width: 300 }}
+      />
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/03/04/sort1linked-list.jpg"
+        style={{ width: 422, height: 222 }}
+      />
+      <pre>
+        <strong>Input:</strong> head = [4,2,1,3] <br />
+        <strong>Output:</strong> [1,2,3,4]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/03/04/sort2linked-list.jpg"
+        style={{ width: 542, height: 222 }}
+      />
+      <pre>
+        <strong>Input:</strong> head = [-1,5,3,4,0] <br />
+        <strong>Output:</strong> [-1,0,3,4,5]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the list is in the range <code>[1, 5000]</code>
+          .
+        </li>
+        <li>
+          <code>-5000 &lt;= Node.val &lt;= 5000</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
