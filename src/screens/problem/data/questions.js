3139349ed03f9f0fada2205107e02ed8bace6525
saiashish9 <saiashish3760@gmail.com>
2025-03-10 23:27:40 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -107141,6 +107141,70 @@ todoList.getAllTasks(1); // return ["Task3", "Task1"]. User 1 now has 2 uncomple
       </ul>
     </div>
   ),
+  149: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array of <code>points</code> where{" "}
+        <code>
+          points[i] = [x<sub>i</sub>, y<sub>i</sub>]
+        </code>{" "}
+        represents a point on the <strong>X-Y</strong> plane, return{" "}
+        <em>the maximum number of points that lie on the same straight line</em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/02/25/plane1.jpg"
+        style={{ width: 300, height: 294 }}
+      />
+      <pre>
+        <strong>Input:</strong> points = [[1,1],[2,2],[3,3]] <br />
+        <strong>Output:</strong> 3
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/02/25/plane2.jpg"
+        style={{ width: 300, height: 294 }}
+      />
+      <pre>
+        <strong>Input:</strong> points = [[1,1],[3,2],[5,3],[4,1],[2,3],[1,4]]{" "}
+        <br />
+        <strong>Output:</strong> 4
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= points.length &lt;= 300</code>
+        </li>
+        <li>
+          <code>points[i].length == 2</code>
+        </li>
+        <li>
+          <code>
+            -10<sup>4</sup> &lt;= x<sub>i</sub>, y<sub>i</sub> &lt;= 10
+            <sup>4</sup>
+          </code>
+        </li>
+        <li>
+          All the <code>points</code> are <strong>unique</strong>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
