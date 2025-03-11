f69ffdde0ebf3af72b627c917651afb02d90dbbd
saiashish9 <saiashish3760@gmail.com>
2025-03-11 15:20:07 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -50774,6 +50774,73 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  275: (
+    <div class="markdown-body div-width">
+      <p>
+        Given an array of citations{" "}
+        <strong>sorted&nbsp;in ascending order </strong>(each citation is a
+        non-negative integer) of a researcher, write a function to compute the
+        researcher's h-index.
+      </p>
+
+      <p>
+        According to the&nbsp;
+        <a
+          href="https://en.wikipedia.org/wiki/H-index"
+          target="_blank"
+          rel="noreferrer"
+        >
+          definition of h-index on Wikipedia
+        </a>
+        : "A scientist has index&nbsp;<i>h</i>&nbsp;if&nbsp;<i>h</i>&nbsp;of
+        his/her&nbsp;<i>N</i>&nbsp;papers have&nbsp;<b>at least</b>&nbsp;
+        <i>h</i>&nbsp;citations each, and the other&nbsp;<i>N − h</i>
+        &nbsp;papers have&nbsp;<b>no more than</b>&nbsp;<i>h&nbsp;</i>citations
+        each."
+      </p>
+
+      <p>
+        <b>Example:</b>
+      </p>
+
+      <pre>
+        <b>Input:</b> <code>citations = [0,1,3,5,6]</code> <br />
+        <b>Output:</b> 3 <br />
+        <strong>Explanation: </strong>
+        <code>[0,1,3,5,6] </code>means the researcher has <code>5</code> papers
+        in total and each of them had received 0<code>, 1, 3, 5, 6</code>{" "}
+        citations respectively. &nbsp; Since the researcher has <code>3</code>{" "}
+        papers with <b>at least</b> <code>3</code> citations each and the
+        remaining &nbsp; two with <b>no more than</b> <code>3</code> citations
+        each, her h-index is <code>3</code>.
+      </pre>
+
+      <p>
+        <strong>Note:</strong>
+      </p>
+
+      <p>
+        If there are several possible values for&nbsp;<em>h</em>, the maximum
+        one is taken as the h-index.
+      </p>
+
+      <p>
+        <strong>Follow up:</strong>
+      </p>
+
+      <ul>
+        <li>
+          This is a follow up problem to&nbsp;
+          <a href="http://206.81.6.248/problems/h-index/description/">
+            H-Index
+          </a>
+          , where <code>citations</code>
+          is now guaranteed to be sorted in ascending order.
+        </li>
+        <li>Could you solve it in logarithmic time complexity?</li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
