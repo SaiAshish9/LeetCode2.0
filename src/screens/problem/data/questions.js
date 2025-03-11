c86b982e81914696072a8e5fb8632fa925da524a
saiashish9 <saiashish3760@gmail.com>
2025-03-11 20:44:31 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -52016,6 +52016,47 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  339: (
+    <div class="markdown-body div-width" style={{ marginTop: 20 }}>
+      <p>
+        Given a nested list of integers, return the sum of all integers in the
+        list weighted by their depth.
+      </p>
+
+      <p>
+        Each element is either an integer, or a list -- whose elements may also
+        be integers or other lists.
+      </p>
+
+      <div>
+        <p>
+          <strong>Example 1:</strong>
+        </p>
+
+        <pre>
+          <strong>Input: </strong>
+          <span id="example-input-1-1">[[1,1],2,[1,1]]</span> <br />
+          <strong>Output: </strong>
+          <span id="example-output-1">10 </span> <br />
+          <strong>Explanation: </strong>Four 1's at depth 2, one 2 at depth 1.
+        </pre>
+      </div>
+      <div>
+        <p>
+          <strong>Example 2:</strong>
+        </p>
+
+        <pre>
+          <strong>Input: </strong>
+          <span id="example-input-2-1">[1,[4,[6]]]</span> <br />
+          <strong>Output: </strong>
+          <span id="example-output-2">27 </span> <br />
+          <strong>Explanation: </strong>One 1 at depth 1, one 4 at depth 2, and
+          one 6 at depth 3; 1 + 4*2 + 6*3 = 27.
+        </pre>
+      </div>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
