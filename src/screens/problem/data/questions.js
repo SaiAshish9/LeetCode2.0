2698808f0299b4f00f8ab91ab3cae5b67719a101
saiashish9 <saiashish3760@gmail.com>
2025-03-11 20:49:41 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -52057,6 +52057,51 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </div>
     </div>
   ),
+  343: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer <code>n</code>, break it into the sum of <code>k</code>{" "}
+        <strong>positive integers</strong>, where <code>k &gt;= 2</code>, and
+        maximize the product of those integers.
+      </p>
+
+      <p>
+        Return <em>the maximum product you can get</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 2 <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> 2 = 1 + 1, 1 × 1 = 1.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> n = 10 <br />
+        <strong>Output:</strong> 36 <br />
+        <strong>Explanation:</strong> 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>2 &lt;= n &lt;= 58</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
