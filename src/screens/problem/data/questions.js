b5c95d34481806624f650befbe900fcec4170f58
saiashish9 <saiashish3760@gmail.com>
2025-03-11 13:12:20 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -50163,6 +50163,44 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </pre>
     </div>
   ),
+  186: (
+    <div class="markdown-body div-width">
+      <p>
+        Given an input string
+        <strong>
+          <em>&nbsp;</em>
+        </strong>
+        , reverse the string word by word.&nbsp;
+      </p>
+
+      <p>
+        <strong>Example:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>["t","h","e"," ","s","k","y",
+        <br />" ","i","s"," ","b","l","u","e"] <br />
+        <strong>Output: </strong>["b","l","u","e"," ","i","s"," ",
+        <br />
+        "s","k","y"," ","t","h","e"]
+      </pre>
+
+      <p>
+        <strong>Note:&nbsp;</strong>
+      </p>
+
+      <ul>
+        <li>A word is defined as a sequence of non-space characters.</li>
+        <li>The input string does not contain leading or trailing spaces.</li>
+        <li>The words are always separated by a single space.</li>
+      </ul>
+
+      <p>
+        <strong>Follow up:&nbsp;</strong>Could you do it <i>in-place</i> without
+        allocating extra space?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
