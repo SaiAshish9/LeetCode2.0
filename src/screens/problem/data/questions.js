5c287991fdf1255e743109b7188e6d6c5356541b
saiashish9 <saiashish3760@gmail.com>
2025-03-11 15:13:11 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -50636,6 +50636,40 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </pre>
     </div>
   ),
+  253: (
+    <div class="markdown-body div-width">
+      <p>
+        Given an array of meeting time intervals consisting of start and end
+        times <code>[[s1,e1],[s2,e2],...]</code>
+        (s<sub>i</sub> &lt; e<sub>i</sub>), find the minimum number of
+        conference rooms required.
+      </p>
+
+      <p>
+        <strong>Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> <code>[[0, 30],[5, 10],[15, 20]]</code> <br />
+        <strong>Output:</strong> 2
+      </pre>
+
+      <p>
+        <b>Example 2:</b>
+      </p>
+
+      <pre>
+        <b>Input:</b> [[7,10],[2,4]] <br />
+        <b>Output:</b> 1
+      </pre>
+
+      <p>
+        <strong>NOTE:</strong>&nbsp;input types have been changed on April 15,
+        2019. Please reset to default code definition to get new method
+        signature.
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
