9f251ee1573c3a68c85c7fb652d34bfcd7989aba
saiashish9 <saiashish3760@gmail.com>
2025-03-11 17:13:44 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -51603,6 +51603,58 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </div>
   ),
+  325: (
+    <div class="markdown-body div-width">
+      <p>
+        Given an array <i>nums</i> and a target value <i>k</i>, find the maximum
+        length of a subarray that sums to <i>k</i>. If there isn't one, return 0
+        instead.
+      </p>
+
+      <p>
+        <b>Note:</b>
+        <br />
+        The sum of the entire <i>nums</i> array is guaranteed to fit within the
+        32-bit signed integer range.
+      </p>
+
+      <p>
+        <b>Example 1:</b>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <i>nums</i> = <code>[1, -1, 5, -2, 3]</code>, <i>k</i> = <code>3</code>{" "}
+        <br />
+        <strong>Output: </strong>4 <br />
+        <strong>Explanation: </strong>The subarray <code>[1, -1, 5, -2]</code>{" "}
+        sums to 3 and is the longest.
+      </pre>
+
+      <p>
+        <b>Example 2:</b>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>
+        <i>nums</i> = <code>[-2, -1, 2, 1]</code>, <i>k</i> = <code>1</code>{" "}
+        <br />
+        <strong>Output: </strong>2{" "}
+        <strong>
+          {" "}
+          <br />
+          Explanation:{" "}
+        </strong>
+        The subarray <code>[-1, 2]</code> sums to 1 and is the longest.
+      </pre>
+
+      <p>
+        <b>Follow Up:</b>
+        <br />
+        Can you do it in O(<i>n</i>) time?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
