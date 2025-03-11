5a84f6ae3216a5b1537644fcc9cdd3d515832d8b
saiashish9 <saiashish3760@gmail.com>
2025-03-11 15:59:06 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -51213,6 +51213,125 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </pre>
     </div>
   ),
+  305: (
+    <article role="main" class="blog-post">
+      <p>
+        You are given an empty 2D binary grid <code>grid</code> of size{" "}
+        <code>m x n</code>. The grid represents a map where <code>0</code>'s
+        represent water and <code>1</code>'s represent land. Initially, all the
+        cells of <code>grid</code> are water cells (i.e., all the cells are{" "}
+        <code>0</code>'s).
+      </p>
+
+      <p>
+        We may perform an add land operation which turns the water at position
+        into a land. You are given an array <code>positions</code> where{" "}
+        <code>
+          positions[i] = [r<sub>i</sub>, c<sub>i</sub>]
+        </code>{" "}
+        is the position{" "}
+        <code>
+          (r<sub>i</sub>, c<sub>i</sub>)
+        </code>{" "}
+        at which we should operate the{" "}
+        <code>
+          i<sup>th</sup>
+        </code>{" "}
+        operation.
+      </p>
+
+      <p>
+        Return <em>an array of integers</em> <code>answer</code> <em>where</em>{" "}
+        <code>answer[i]</code>{" "}
+        <em>is the number of islands after turning the cell</em>{" "}
+        <code>
+          (r<sub>i</sub>, c<sub>i</sub>)
+        </code>{" "}
+        <em>into a land</em>.
+      </p>
+
+      <p>
+        An <strong>island</strong> is surrounded by water and is formed by
+        connecting adjacent lands horizontally or vertically. You may assume all
+        four edges of the grid are all surrounded by water.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <p>
+        <img
+          alt=""
+          src="https://fastly.jsdelivr.net/gh/doocs/leetcode@main/solution/0300-0399/0305.Number%20of%20Islands%20II/images/tmp-grid.jpg"
+          style={{ width: 500, height: 294 }}
+        />
+      </p>
+      <pre>
+        <strong>Input:</strong> m = 3, n = 3, positions =
+        [[0,0],[0,1],[1,2],[2,1]] <br />
+        <strong>Output:</strong> [1,1,2,3] <br />
+        <strong>Explanation:</strong> <br />
+        Initially, the 2d grid is filled with water. <br />
+        - Operation #1: addLand(0, 0) turns the water at grid[0][0] into a land.
+        We have 1 island.
+        <br />
+        - Operation #2: addLand(0, 1) turns the water at grid[0][1] into a land.
+        We still have 1 island.
+        <br />
+        - Operation #3: addLand(1, 2) turns the water at grid[1][2] into a land.
+        We have 2 islands.
+        <br />- Operation #4: addLand(2, 1) turns the water at grid[2][1] into a
+        land. We have 3 islands.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> m = 1, n = 1, positions = [[0,0]] <br />
+        <strong>Output:</strong> [1]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= m, n, positions.length &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= m * n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>positions[i].length == 2</code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= r<sub>i</sub> &lt; m
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= c<sub>i</sub> &lt; n
+          </code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong> Could you solve it in time complexity{" "}
+        <code>O(k log(mn))</code>, where <code>k == positions.length</code>?
+      </p>
+    </article>
+  ),
 };
 
 export default QUESTIONS;
