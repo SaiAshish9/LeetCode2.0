c8e386ebba3d982c923ee35b6755b4a661b39688
saiashish9 <saiashish3760@gmail.com>
2025-03-11 13:35:13 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -50201,6 +50201,68 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </p>
     </div>
   ),
+  203: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given the <code>head</code> of a linked list and an integer{" "}
+        <code>val</code>, remove all the nodes of the linked list that has{" "}
+        <code>Node.val == val</code>, and return <em>the new head</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/03/06/removelinked-list.jpg"
+        style={{ width: 500, height: 142 }}
+      />
+      <pre>
+        <strong>Input:</strong> head = [1,2,6,3,4,5,6], val = 6 <br />
+        <strong>Output:</strong> [1,2,3,4,5]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> head = [], val = 1 <br />
+        <strong>Output:</strong> []
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> head = [7,7,7,7], val = 7 <br />
+        <strong>Output:</strong> []
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          The number of nodes in the list is in the range{" "}
+          <code>
+            [0, 10<sup>4</sup>]
+          </code>
+          .
+        </li>
+        <li>
+          <code>1 &lt;= Node.val &lt;= 50</code>
+        </li>
+        <li>
+          <code>0 &lt;= val &lt;= 50</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
