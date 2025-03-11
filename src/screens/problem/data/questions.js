7a4b3dc8ac8eaab1bc6df6b5fb76a990f207bb5b
saiashish9 <saiashish3760@gmail.com>
2025-03-11 12:47:01 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -108406,6 +108406,54 @@ sol.read(buf, 1); // We have reached the end of file, no more characters can be 
       </pre>
     </div>
   ),
+  179: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a list of non-negative integers <code>nums</code>, arrange them
+        such that they form the largest number and return it.
+      </p>
+
+      <p>
+        Since the result may be very large, so you need to return a string
+        instead of an integer.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [10,2] <br />
+        <strong>Output:</strong> "210"
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [3,30,34,5,9] <br />
+        <strong>Output:</strong> "9534330"
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= nums[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
