ac8c2992482e5e9acac7c5da561bc4f4b026c1aa
saiashish9 <saiashish3760@gmail.com>
2025-03-11 15:36:27 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -51122,6 +51122,58 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </ul>
     </article>
   ),
+  291: (
+    <div class="markdown-body div-width">
+      <p>
+        Given a <code>pattern</code> and a string <code>str</code>, find if{" "}
+        <code>str</code> follows the same pattern.
+      </p>
+
+      <p>
+        Here <b>follow</b> means a full match, such that there is a bijection
+        between a letter in
+        <code>pattern</code> and a <b>non-empty</b> substring in{" "}
+        <code>str</code>.
+      </p>
+
+      <p>
+        <strong>Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>pattern = <code>"abab"</code>, str ={" "}
+        <code>"redblueredblue"</code> <br />
+        <strong>Output:</strong> true
+      </pre>
+
+      <p>
+        <strong>Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input: </strong>pattern = pattern = <code>"aaaa"</code>, str ={" "}
+        <code>"asdasdasdasd"</code> <br />
+        <strong>Output:</strong> true
+      </pre>
+
+      <p>
+        <strong>Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> pattern = <code>"aabb"</code>, str ={" "}
+        <code>"xyzabcxzyabc"</code> <br />
+        <strong>Output:</strong> false
+      </pre>
+
+      <p>
+        <b>Notes:</b>
+        <br />
+        You may assume both <code>pattern</code> and <code>str</code> contains
+        only lowercase letters.
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
