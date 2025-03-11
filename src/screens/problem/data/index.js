3dc3240efc30b4270355d5e8bbbaedebb8b3b7ea
saiashish9 <saiashish3760@gmail.com>
2025-03-11 13:06:48 +0530

    k

--- src/screens/problem/data/index.js
+++ src/screens/problem/data/index.js
@@ -1,7 +1,10 @@
-import QUESTIONS from "./questions";
+import QUESTIONS1 from "./questions";
+import QUESTIONS2 from "./questions1";
 import SOLUTIONING from "./solutioning";
 import PREMIUM from "./premium";
 import TAG_DESCRIPTION from "./htmlTagDescriiption";
 import COMPANIES from "./companies";
+
+const QUESTIONS = { ...QUESTIONS1, ...QUESTIONS2 };
 
 export { QUESTIONS, SOLUTIONING, PREMIUM, TAG_DESCRIPTION, COMPANIES };
