2e03cc23747ed2fd860ee2616d88daf3d6563faf
saiashish9 <saiashish3760@gmail.com>
2025-03-11 17:16:47 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -51655,6 +51655,90 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </p>
     </div>
   ),
+  331: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        One way to serialize a binary tree is to use{" "}
+        <strong>preorder traversal</strong>. When we encounter a non-null node,
+        we record the node's value. If it is a null node, we record using a
+        sentinel value such as <code>'#'</code>.
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2021/03/12/pre-tree.jpg"
+        style={{ width: 362, height: 293 }}
+      />
+      <p>
+        For example, the above binary tree can be serialized to the string{" "}
+        <code>"9,3,4,#,#,1,#,#,2,#,6,#,#"</code>, where <code>'#'</code>{" "}
+        represents a null node.
+      </p>
+      <p>
+        Given a string of comma-separated values <code>preorder</code>, return{" "}
+        <code>true</code> if it is a correct preorder traversal serialization of
+        a binary tree.
+      </p>
+      <p>
+        It is <strong>guaranteed</strong> that each comma-separated value in the
+        string must be either an integer or a character <code>'#'</code>{" "}
+        representing null pointer.
+      </p>
+      <p>You may assume that the input format is always valid.</p>
+      <ul>
+        <li>
+          For example, it could never contain two consecutive commas, such as{" "}
+          <code>"1,,3"</code>.
+        </li>
+      </ul>
+      <p>
+        <strong>Note:&nbsp;</strong>You are not allowed to reconstruct the tree.
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> preorder = "9,3,4,#,#,1,#,#,2,#,6,#,#" <br />
+        <strong>Output:</strong> true <br />
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> preorder = "1,#"
+        <br />
+        <strong>Output:</strong> false
+        <br />
+      </pre>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+      <br />
+      <pre>
+        <strong>Input:</strong> preorder = "9,#,#,1"
+        <br />
+        <strong>Output:</strong> false
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>
+            1 &lt;= preorder.length &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>preorder</code> consist of integers in the range{" "}
+          <code>[0, 100]</code> and <code>'#'</code> separated by commas{" "}
+          <code>','</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
