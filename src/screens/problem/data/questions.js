0dc644bb7b5564337e66d747bdf1c8cb7a00c8e0
saiashish9 <saiashish3760@gmail.com>
2025-03-11 16:03:46 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -51332,6 +51332,79 @@ snake.move("U"); -&gt; Returns -1 (Game over because snake collides with border)
       </p>
     </article>
   ),
+  306: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        An <strong>additive number</strong> is a string whose digits can form an{" "}
+        <strong>additive sequence</strong>.
+      </p>
+
+      <p>
+        A valid <strong>additive sequence</strong> should contain{" "}
+        <strong>at least</strong> three numbers. Except for the first two
+        numbers, each subsequent number in the sequence must be the sum of the
+        preceding two.
+      </p>
+
+      <p>
+        Given a string containing only digits, return <code>true</code> if it is
+        an <strong>additive number</strong> or <code>false</code> otherwise.
+      </p>
+
+      <p>
+        <strong>Note:</strong> Numbers in the additive sequence{" "}
+        <strong>cannot</strong> have leading zeros, so sequence{" "}
+        <code>1, 2, 03</code> or <code>1, 02, 3</code> is invalid.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> "112358" <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> <br />
+        The digits can form an additive sequence: 1, 1, 2, 3, 5, 8. <br />1 + 1
+        = 2, 1 + 2 = 3, 2 + 3 = 5, 3 + 5 = 8
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> "199100199"
+        <br />
+        <strong>Output:</strong> true
+        <br />
+        <strong>Explanation:</strong> <br />
+        The additive sequence is: 1, 99, 100, 199.&nbsp;
+        <br />1 + 99 = 100, 99 + 100 = 199
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= num.length &lt;= 35</code>
+        </li>
+        <li>
+          <code>num</code> consists only of digits.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong> How would you handle overflow for very large
+        input integers?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
