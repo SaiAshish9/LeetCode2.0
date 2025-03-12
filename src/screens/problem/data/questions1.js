0ce3cb7afed18aaa4cffebeb290fcfed3a542ffe
saiashish9 <saiashish3760@gmail.com>
2025-03-12 21:10:27 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -58562,6 +58562,117 @@ const QUESTIONS = {
       </div>
     </div>
   ),
+  352: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a data stream input of non-negative integers{" "}
+        <code>
+          a<sub>1</sub>, a<sub>2</sub>, ..., a<sub>n</sub>
+        </code>
+        , summarize the numbers seen so far as a list of disjoint intervals.
+      </p>
+
+      <p>
+        Implement the <code>SummaryRanges</code> class:
+      </p>
+
+      <ul>
+        <li>
+          <code>SummaryRanges()</code> Initializes the object with an empty
+          stream.
+        </li>
+        <li>
+          <code>void addNum(int value)</code> Adds the integer{" "}
+          <code>value</code> to the stream.
+        </li>
+        <li>
+          <code>int[][] getIntervals()</code> Returns a summary of the integers
+          in the stream currently as a list of disjoint intervals{" "}
+          <code>
+            [start<sub>i</sub>, end<sub>i</sub>]
+          </code>
+          . The answer should be sorted by{" "}
+          <code>
+            start<sub>i</sub>
+          </code>
+          .
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input</strong> <br />
+        ["SummaryRanges", "addNum", "getIntervals", "addNum", "getIntervals",
+        "addNum", "getIntervals", "addNum", "getIntervals", "addNum",
+        "getIntervals"] <br />
+        [[], [1], [], [3], [], [7], [], [2], [], [6], []] <br />
+        <strong>Output</strong> <br />
+        [null, null, [[1, 1]], null, [[1, 1], [3, 3]], null, [[1, 1], [3, 3],
+        [7, 7]], null, [[1, 3], [7, 7]], null, [[1, 3], [6, 7]]]
+        <strong>Explanation</strong> <br />
+        SummaryRanges summaryRanges = new SummaryRanges();
+        <br />
+        summaryRanges.addNum(1); // arr = [1]
+        <br />
+        summaryRanges.getIntervals(); // return [[1, 1]]
+        <br />
+        summaryRanges.addNum(3); // arr = [1, 3]
+        <br />
+        summaryRanges.getIntervals(); // return [[1, 1], [3, 3]]
+        <br />
+        summaryRanges.addNum(7); // arr = [1, 3, 7]
+        <br />
+        summaryRanges.getIntervals(); // return [[1, 1], [3, 3], [7, 7]]
+        <br />
+        summaryRanges.addNum(2); // arr = [1, 2, 3, 7]
+        <br />
+        summaryRanges.getIntervals(); // return [[1, 3], [7, 7]]
+        <br />
+        summaryRanges.addNum(6); // arr = [1, 2, 3, 6, 7]
+        <br />
+        summaryRanges.getIntervals(); // return [[1, 3], [6, 7]]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            0 &lt;= value &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          At most{" "}
+          <code>
+            3 * 10<sup>4</sup>
+          </code>{" "}
+          calls will be made to <code>addNum</code> and{" "}
+          <code>getIntervals</code>.
+        </li>
+        <li>
+          At most{" "}
+          <code>
+            10<sup>2</sup>
+          </code>
+          &nbsp;calls will be made to&nbsp;<code>getIntervals</code>.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Follow up:</strong> What if there are lots of merges and the
+        number of disjoint intervals is small compared to the size of the data
+        stream?
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
