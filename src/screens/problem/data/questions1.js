84d6789b2da8b0a41078bf848a56eb2201464aa0
saiashish9 <saiashish3760@gmail.com>
2025-03-13 07:31:27 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -59003,6 +59003,37 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  369: (
+    <div class="markdown-body div-width">
+      <p>
+        Given a non-negative integer represented as <b>non-empty</b> a singly
+        linked list of digits, plus one to the integer.
+      </p>
+
+      <p>
+        You may assume the integer do not contain any leading zero, except the
+        number 0 itself.
+      </p>
+
+      <p>
+        The digits are stored such that the most significant digit is at the
+        head of the list.
+      </p>
+
+      <div>
+        <p>
+          <strong>Example :</strong>
+        </p>
+
+        <pre>
+          <strong>Input: </strong>
+          <span id="example-input-1-1">[1,2,3]</span>
+          <strong>Output: </strong>
+          <span id="example-output-1">[1,2,4]</span>
+        </pre>
+      </div>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
