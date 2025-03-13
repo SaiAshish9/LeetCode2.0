16e0c0df7b935e05c96980470e30b991a4949368
saiashish9 <saiashish3760@gmail.com>
2025-03-13 11:07:20 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -60206,7 +60206,8 @@ const QUESTIONS = {
         negative integers,{" "}
         <a
           href="https://en.wikipedia.org/wiki/Two%27s_complement"
-          target="_blank" rel="noreferrer"
+          target="_blank"
+          rel="noreferrer"
         >
           two’s complement
         </a>{" "}
@@ -60246,6 +60247,93 @@ const QUESTIONS = {
           <code>
             -2<sup>31</sup> &lt;= num &lt;= 2<sup>31</sup> - 1
           </code>
+        </li>
+      </ul>
+    </div>
+  ),
+  495: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Our hero Teemo is attacking an enemy Ashe with poison attacks! When
+        Teemo attacks Ashe, Ashe gets poisoned for a exactly{" "}
+        <code>duration</code> seconds. More formally, an attack at second{" "}
+        <code>t</code> will mean Ashe is poisoned during the{" "}
+        <strong>inclusive</strong> time interval{" "}
+        <code>[t, t + duration - 1]</code>. If Teemo attacks again{" "}
+        <strong>before</strong> the poison effect ends, the timer for it is{" "}
+        <strong>reset</strong>, and the poison effect will end{" "}
+        <code>duration</code> seconds after the new attack.
+      </p>
+
+      <p>
+        You are given a <strong>non-decreasing</strong> integer array{" "}
+        <code>timeSeries</code>, where <code>timeSeries[i]</code> denotes that
+        Teemo attacks Ashe at second <code>timeSeries[i]</code>, and an integer{" "}
+        <code>duration</code>.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the <strong>total</strong> number of seconds that Ashe is poisoned
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> timeSeries = [1,4], duration = 2 <br />
+        <strong>Output:</strong> 4 <br />
+        <strong>Explanation:</strong> Teemo's attacks on Ashe go as follows:{" "}
+        <br />
+        - At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+        <br />
+        - At second 4, Teemo attacks, and Ashe is poisoned for seconds 4 and 5.
+        <br />
+        Ashe is poisoned for seconds 1, 2, 4, and 5, which is 4 seconds in
+        total.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> timeSeries = [1,2], duration = 2<br />
+        <strong>Output:</strong> 3<br />
+        <strong>Explanation:</strong> Teemo's attacks on Ashe go as follows:
+        <br />
+        - At second 1, Teemo attacks, and Ashe is poisoned for seconds 1 and 2.
+        <br />
+        - At second 2 however, Teemo attacks again and resets the poison timer.
+        Ashe is poisoned for seconds 2 and 3.
+        <br />
+        Ashe is poisoned for seconds 1, 2, and 3, which is 3 seconds in total.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= timeSeries.length &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            0 &lt;= timeSeries[i], duration &lt;= 10<sup>7</sup>
+          </code>
+        </li>
+        <li>
+          <code>timeSeries</code> is sorted in <strong>non-decreasing</strong>{" "}
+          order.
         </li>
       </ul>
     </div>
