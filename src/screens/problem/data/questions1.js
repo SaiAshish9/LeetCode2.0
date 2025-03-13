b60583caa553b076ab28c16d7c59cbc660077e67
saiashish9 <saiashish3760@gmail.com>
2025-03-13 07:56:00 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -59618,6 +59618,80 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  385: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string s represents the serialization of a nested list,
+        implement a parser to deserialize it and return{" "}
+        <em>the deserialized</em> <code>NestedInteger</code>.
+      </p>
+
+      <p>
+        Each element is either an integer or a list whose elements may also be
+        integers or other lists.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "324" <br />
+        <strong>Output:</strong> 324 <br />
+        <strong>Explanation:</strong> You should return a NestedInteger object
+        which contains a single integer 324.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "[123,[456,[789]]]" <br />
+        <strong>Output:</strong> [123,[456,[789]]] <br />
+        <strong>Explanation:</strong> Return a NestedInteger object containing a
+        nested list with 2 elements: <br />
+        1. An integer containing value 123.
+        <br />
+        2. A nested list containing two elements:
+        <br />
+        i. An integer containing value 456.
+        <br />
+        ii. A nested list with one element:
+        <br />
+        a. An integer containing value 789
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 5 * 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>s</code> consists of digits, square brackets <code>"[]"</code>,
+          negative sign <code>'-'</code>, and commas <code>','</code>.
+        </li>
+        <li>
+          <code>s</code> is the serialization of valid{" "}
+          <code>NestedInteger</code>.
+        </li>
+        <li>
+          All the values in the input are in the range{" "}
+          <code>
+            [-10<sup>6</sup>, 10<sup>6</sup>]
+          </code>
+          .
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
