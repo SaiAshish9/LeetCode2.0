f57b08afdecee5d85768d0a63b63509c85a43cf4
saiashish9 <saiashish3760@gmail.com>
2025-03-13 14:15:17 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -62853,6 +62853,56 @@ l a d y`}
       <p></p>
     </div>
   ),
+  423: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string <code>s</code> containing an out-of-order English
+        representation of digits <code>0-9</code>, return{" "}
+        <em>
+          the digits in <strong>ascending</strong> order
+        </em>
+        .
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> s = "owoztneoer" <br />
+        <strong>Output:</strong> "012" <br />
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> s = "fviefuro" <br />
+        <strong>Output:</strong> "45"
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>
+            1 &lt;= s.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>s[i]</code> is one of the characters{" "}
+          <code>
+            ["e","g","f","i","h","o","n","s","r","u","t","w","v","x","z"]
+          </code>
+          .
+        </li>
+        <li>
+          <code>s</code> is <strong>guaranteed</strong> to be valid.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
