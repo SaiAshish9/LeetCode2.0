14f07a1762da0369b9a7d1a4bab65d85d6a5ba19
saiashish9 <saiashish3760@gmail.com>
2025-03-13 07:38:08 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -59093,6 +59093,69 @@ const QUESTIONS = {
       </pre>
     </div>
   ),
+  372: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Your task is to calculate{" "}
+        <code>
+          a<sup>b</sup>
+        </code>{" "}
+        mod <code>1337</code> where <code>a</code> is a positive integer and{" "}
+        <code>b</code> is an extremely large positive integer given in the form
+        of an array.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> a = 2, b = [3] <br />
+        <strong>Output:</strong> 8
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> a = 2, b = [1,0] <br />
+        <strong>Output:</strong> 1024
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> a = 1, b = [4,3,3,8,5,2] <br />
+        <strong>Output:</strong> 1
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= a &lt;= 2<sup>31</sup> - 1
+          </code>
+        </li>
+        <li>
+          <code>1 &lt;= b.length &lt;= 2000</code>
+        </li>
+        <li>
+          <code>0 &lt;= b[i] &lt;= 9</code>
+        </li>
+        <li>
+          <code>b</code> does not contain leading zeros.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
