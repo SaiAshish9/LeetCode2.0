68ff5cb41e10e3c45be308f30d6cd3bbe24a80d5
saiashish9 <saiashish3760@gmail.com>
2025-03-13 14:32:42 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -63202,6 +63202,76 @@ l a d y`}
       </ul>
     </article>
   ),
+  409: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string <code>s</code> which consists of lowercase or uppercase
+        letters, return the length of the{" "}
+        <strong>
+          longest{" "}
+          <span
+            data-keyword="palindrome-string"
+            class=" cursor-pointer relative text-dark-blue-s text-sm"
+          >
+            <button
+              type="button"
+              aria-haspopup="dialog"
+              aria-expanded="false"
+              aria-controls="radix-:rck:"
+              data-state="closed"
+              class=""
+            >
+              palindrome
+            </button>
+          </span>
+        </strong>
+        &nbsp;that can be built with those letters.
+      </p>
+
+      <p>
+        Letters are <strong>case sensitive</strong>, for example,{" "}
+        <code>"Aa"</code> is not considered a palindrome.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "abccccdd" <br />
+        <strong>Output:</strong> 7 <br />
+        <strong>Explanation:</strong> One longest palindrome that can be built
+        is "dccaccd", whose length is 7.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> s = "a" <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> The longest palindrome that can be built
+        is "a", whose length is 1.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= s.length &lt;= 2000</code>
+        </li>
+        <li>
+          <code>s</code> consists of lowercase <strong>and/or</strong> uppercase
+          English&nbsp;letters only.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
