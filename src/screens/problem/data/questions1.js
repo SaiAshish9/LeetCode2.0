3449be20f33fe57a1be6626ba29847048d0ac1e6
saiashish9 <saiashish3760@gmail.com>
2025-03-13 14:53:32 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -63533,6 +63533,56 @@ l a d y`}
       </ul>
     </div>
   ),
+  416: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code>, return <code>true</code>{" "}
+        <em>
+          if you can partition the array into two subsets such that the sum of
+          the elements in both subsets is equal or{" "}
+        </em>
+        <code>false</code>
+        <em> otherwise</em>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,5,11,5] <br />
+        <strong>Output:</strong> true <br />
+        <strong>Explanation:</strong> The array can be partitioned as [1, 5, 5]
+        and [11].
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3,5] <br />
+        <strong>Output:</strong> false <br />
+        <strong>Explanation:</strong> The array cannot be partitioned into equal
+        sum subsets.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 200</code>
+        </li>
+        <li>
+          <code>1 &lt;= nums[i] &lt;= 100</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
