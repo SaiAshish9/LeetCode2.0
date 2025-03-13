7e3098a0b885d37f7ea5674bb730519a3ac034cf
saiashish9 <saiashish3760@gmail.com>
2025-03-13 12:11:05 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -61029,6 +61029,104 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  471: (
+    <div class="markdown-body div-width">
+      <p>
+        Given a <b>non-empty</b> string, encode the string such that its encoded
+        length is the shortest.
+      </p>
+
+      <p>
+        The encoding rule is: <code>k[encoded_string]</code>, where the{" "}
+        <i>encoded_string</i> inside the square brackets is being repeated
+        exactly <i>k</i> times.
+      </p>
+
+      <p>
+        <b>Note:</b>
+      </p>
+
+      <ul>
+        <li>
+          <i>k</i> will be a positive integer and encoded string will not be
+          empty or have extra space.
+        </li>
+        <li>
+          You may assume that the input string contains only lowercase English
+          letters. The string's length is at most 160.
+        </li>
+        <li>
+          If an encoding process does not make the string shorter, then do not
+          encode it. If there are several solutions, return any of them is fine.
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <b>Example 1:</b>
+      </p>
+
+      <pre>
+        Input: "aaa" <br />
+        Output: "aaa" <br />
+        Explanation: There is no way to encode it such that it is shorter than
+        the input string, so we do not encode it.
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <b>Example 2:</b>
+      </p>
+
+      <pre>
+        Input: "aaaaa" <br />
+        Output: "5[a]" <br />
+        Explanation: "5[a]" is shorter than "aaaaa" by 1 character.
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <b>Example 3:</b>
+      </p>
+
+      <pre>
+        Input: "aaaaaaaaaa" <br />
+        Output: "10[a]" <br />
+        Explanation: "a9[a]" or "9[a]a" are also valid solutions, both of them
+        have the same length = 5, which is the same as "10[a]".
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <b>Example 4:</b>
+      </p>
+
+      <pre>
+        Input: "aabcaabcd" <br />
+        Output: "2[aabc]d" <br />
+        Explanation: "aabc" occurs twice, so one answer can be "2[aabc]d".
+      </pre>
+
+      <p>&nbsp;</p>
+
+      <p>
+        <b>Example 5:</b>
+      </p>
+
+      <pre>
+        Input: "abbbabbbcabbbabbbc" <br />
+        Output: "2[2[abbb]c]" <br />
+        Explanation: "abbbabbbc" occurs twice, but "abbbabbbc" can also be
+        encoded to "2[abbb]c", so one answer can be "2[2[abbb]c]".
+      </pre>
+
+      <p>&nbsp;</p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
