8495a5fcbb4a2a72f913a8c3885c18bf902f6c2f
saiashish9 <saiashish3760@gmail.com>
2025-03-13 12:26:42 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -61412,6 +61412,71 @@ const QUESTIONS = {
       <p></p>
     </div>
   ),
+  462: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code> of size <code>n</code>, return{" "}
+        <em>
+          the minimum number of moves required to make all array elements equal
+        </em>
+        .
+      </p>
+
+      <p>
+        In one move, you can increment or decrement an element of the array by{" "}
+        <code>1</code>.
+      </p>
+
+      <p>
+        Test cases are designed so that the answer will fit in a{" "}
+        <strong>32-bit</strong> integer.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3] <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong>
+        <br />
+        Only two moves are needed (remember each move increments or decrements
+        one element): <br />[<u>1</u>,2,3] =&gt; [2,2,<u>3</u>] =&gt; [2,2,2]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,10,2,9] <br />
+        <strong>Output:</strong> 16
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>n == nums.length</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= nums.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            -10<sup>9</sup> &lt;= nums[i] &lt;= 10<sup>9</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
