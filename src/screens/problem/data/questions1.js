2243e5e55235dfb6c5f2d546714262bc573ee414
saiashish9 <saiashish3760@gmail.com>
2025-03-13 12:28:53 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -61477,6 +61477,84 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  461: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        The{" "}
+        <a
+          href="https://en.wikipedia.org/wiki/Hamming_distance"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Hamming distance
+        </a>{" "}
+        between two integers is the number of positions at which the
+        corresponding bits are different.
+      </p>
+
+      <p>
+        Given two integers <code>x</code> and <code>y</code>, return{" "}
+        <em>
+          the <strong>Hamming distance</strong> between them
+        </em>
+        .
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> x = 1, y = 4 <br />
+        <strong>Output:</strong> 2<br />
+        <strong>Explanation:</strong>
+        <br />
+        1 (0 0 0 1)
+        <br />
+        4 (0 1 0 0)
+        <br />
+        ↑ ↑<br />
+        The above arrows point to positions where the corresponding bits are
+        different.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> x = 3, y = 1 <br />
+        <strong>Output:</strong> 1
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            0 &lt;=&nbsp;x, y &lt;= 2<sup>31</sup> - 1
+          </code>
+        </li>
+      </ul>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Note:</strong> This question is the same as{" "}
+        <a
+          href="https://leetcode.com/problems/minimum-bit-flips-to-convert-number/description/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          2220: Minimum Bit Flips to Convert Number.
+        </a>
+      </p>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
