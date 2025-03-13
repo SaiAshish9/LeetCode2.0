6e2699f813a22dd546742638b3f449667bca135e
saiashish9 <saiashish3760@gmail.com>
2025-03-13 12:00:31 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -60814,7 +60814,8 @@ const QUESTIONS = {
         The{" "}
         <a
           href="https://en.wikipedia.org/wiki/Hamming_distance"
-          target="_blank" rel="noreferrer"
+          target="_blank"
+          rel="noreferrer"
         >
           Hamming distance
         </a>{" "}
@@ -60875,6 +60876,85 @@ const QUESTIONS = {
         <li>
           The answer for the given input will fit in a <strong>32-bit</strong>{" "}
           integer.
+        </li>
+      </ul>
+    </div>
+  ),
+  475: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Winter is coming! During the contest, your first job is to design a
+        standard heater with a fixed warm radius to warm all the houses.
+      </p>
+
+      <p>
+        Every house can be warmed, as long as the house is within the heater's
+        warm radius range.&nbsp;
+      </p>
+
+      <p>
+        Given the positions of <code>houses</code> and <code>heaters</code> on a
+        horizontal line, return{" "}
+        <em>
+          the minimum radius standard of heaters&nbsp;so that those heaters
+          could cover all houses.
+        </em>
+      </p>
+
+      <p>
+        <strong>Notice</strong> that&nbsp;all the <code>heaters</code> follow
+        your radius standard, and the warm radius will the same.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> houses = [1,2,3], heaters = [2] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> The only heater was placed in the position
+        2, and if we use the radius 1 standard, then all the houses can be
+        warmed.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> houses = [1,2,3,4], heaters = [1,4] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> The two heaters were placed at positions 1
+        and 4. We need to use a radius 1 standard, then all the houses can be
+        warmed.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> houses = [1,5], heaters = [2] <br />
+        <strong>Output:</strong> 3
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= houses.length, heaters.length &lt;= 3 * 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= houses[i], heaters[i] &lt;= 10<sup>9</sup>
+          </code>
         </li>
       </ul>
     </div>
