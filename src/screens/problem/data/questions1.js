0b34f9d34ecaebbba8c4d5be600fec8822d4c25d
saiashish9 <saiashish3760@gmail.com>
2025-03-13 13:43:38 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -62370,6 +62370,84 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  435: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an array of intervals <code>intervals</code> where{" "}
+        <code>
+          intervals[i] = [start<sub>i</sub>, end<sub>i</sub>]
+        </code>
+        , return{" "}
+        <em>
+          the minimum number of intervals you need to remove to make the rest of
+          the intervals non-overlapping
+        </em>
+        .
+      </p>
+
+      <p>
+        <strong>Note</strong> that intervals which only touch at a point are{" "}
+        <strong>non-overlapping</strong>. For example, <code>[1, 2]</code> and{" "}
+        <code>[2, 3]</code> are non-overlapping.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> intervals = [[1,2],[2,3],[3,4],[1,3]] <br />
+        <strong>Output:</strong> 1 <br />
+        <strong>Explanation:</strong> [1,3] can be removed and the rest of the
+        intervals are non-overlapping.
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> intervals = [[1,2],[1,2],[1,2]] <br />
+        <strong>Output:</strong> 2 <br />
+        <strong>Explanation:</strong> You need to remove two [1,2] to make the
+        rest of the intervals non-overlapping.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> intervals = [[1,2],[2,3]] <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> You don't need to remove any of the
+        intervals since they're already non-overlapping.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>
+            1 &lt;= intervals.length &lt;= 10<sup>5</sup>
+          </code>
+        </li>
+        <li>
+          <code>intervals[i].length == 2</code>
+        </li>
+        <li>
+          <code>
+            -5 * 10<sup>4</sup> &lt;= start<sub>i</sub> &lt; end<sub>i</sub>{" "}
+            &lt;= 5 * 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
