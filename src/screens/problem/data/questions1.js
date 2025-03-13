889dc838504ba361183841c91886e69183ff9606
saiashish9 <saiashish3760@gmail.com>
2025-03-13 07:58:32 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -59692,6 +59692,46 @@ const QUESTIONS = {
       </ul>
     </div>
   ),
+  386: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer <code>n</code>, return all the numbers in the range{" "}
+        <code>[1, n]</code> sorted in lexicographical order.
+      </p>
+      <p>
+        You must write an algorithm that runs in&nbsp;<code>O(n)</code>
+        &nbsp;time and uses <code>O(1)</code> extra space.&nbsp;
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <pre>
+        <strong>Input:</strong> n = 13 <br />
+        <strong>Output:</strong> [1,10,11,12,13,2,3,4,5,6,7,8,9] <br />
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+      <br />
+      <pre>
+        <strong>Input:</strong> n = 2<br />
+        <strong>Output:</strong> [1,2]
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 5 * 10<sup>4</sup>
+          </code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
