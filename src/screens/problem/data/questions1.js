af957fad6834f5cc34df1b624ca07d83b4a899b7
saiashish9 <saiashish3760@gmail.com>
2025-03-18 15:31:55 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -63991,6 +63991,85 @@ l a d y`}
       </ul>
     </div>
   ),
+  1361: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You have <code>n</code> binary tree nodes numbered from <code>0</code>{" "}
+        to <code>n - 1</code> where node <code>i</code> has two children{" "}
+        <code>leftChild[i]</code> and <code>rightChild[i]</code>, return{" "}
+        <code>true</code> if and only if <strong>all</strong> the given nodes
+        form <strong>exactly one</strong> valid binary tree.
+      </p>
+      <p>
+        If node <code>i</code> has no left child then <code>leftChild[i]</code>{" "}
+        will equal <code>-1</code>, similarly for the right child.
+      </p>
+      <p>
+        Note that the nodes have no values and that we only use the node numbers
+        in this problem.
+      </p>
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>{" "}
+      <br />
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2019/08/23/1503_ex1.png"
+        style={{ width: 195, height: 287 }}
+      />
+      <pre>
+        <strong>Input:</strong> n = 4, leftChild = [1,-1,3,-1], rightChild =
+        [2,-1,-1,-1] <br />
+        <strong>Output:</strong> true
+      </pre>
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>{" "}
+      <br />
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2019/08/23/1503_ex2.png"
+        style={{ width: 183, height: 272 }}
+      />
+      <pre>
+        <strong>Input:</strong> n = 4, leftChild = [1,-1,3,-1], rightChild =
+        [2,3,-1,-1] <br />
+        <strong>Output:</strong> false
+      </pre>
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>{" "}
+      <br />
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2019/08/23/1503_ex3.png"
+        style={{ width: 82, height: 174 }}
+      />
+      <pre>
+        <strong>Input:</strong> n = 2, leftChild = [1,0], rightChild = [-1,-1]{" "}
+        <br />
+        <strong>Output:</strong> false
+      </pre>
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+      <ul>
+        <li>
+          <code>n == leftChild.length == rightChild.length</code>
+        </li>
+        <li>
+          <code>
+            1 &lt;= n &lt;= 10<sup>4</sup>
+          </code>
+        </li>
+        <li>
+          <code>-1 &lt;= leftChild[i], rightChild[i] &lt;= n - 1</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
