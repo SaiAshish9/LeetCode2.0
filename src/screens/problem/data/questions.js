d8049a07044a654e5adcc96dccb917ecdb5b1235
saiashish9 <saiashish3760@gmail.com>
2025-03-26 16:52:25 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -52124,6 +52124,67 @@ m.next(5) = (10 + 3 + 5) / 3`}
       <p>&nbsp;</p>
     </div>
   ),
+  17: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a string containing digits from <code>2-9</code> inclusive, return
+        all possible letter combinations that the number could represent. Return
+        the answer in <strong>any order</strong>.
+      </p>
+
+      <p>
+        A mapping of digits to letters (just like on the telephone buttons) is
+        given below. Note that 1 does not map to any letters.
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2022/03/15/1200px-telephone-keypad2svg.png"
+        style={{ width: 300, height: 243 }}
+      />
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> digits = "23" <br />
+        <strong>Output:</strong> ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> digits = "" <br />
+        <strong>Output:</strong> []
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> digits = "2" <br />
+        <strong>Output:</strong> ["a","b","c"]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>0 &lt;= digits.length &lt;= 4</code>
+        </li>
+        <li>
+          <code>digits[i]</code> is a digit in the range <code>['2', '9']</code>
+          .
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
