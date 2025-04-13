520edfeaef57ceb3cd770095efd060d1444f50a0
saiashish9 <saiashish3760@gmail.com>
2025-04-14 05:07:00 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -64171,6 +64171,72 @@ l a d y`}
       </ul>
     </div>
   ),
+  40: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given a collection of candidate numbers (<code>candidates</code>) and a
+        target number (<code>target</code>), find all unique combinations in{" "}
+        <code>candidates</code>&nbsp;where the candidate numbers sum to{" "}
+        <code>target</code>.
+      </p>
+
+      <p>
+        Each number in <code>candidates</code>&nbsp;may only be used{" "}
+        <strong>once</strong> in the combination.
+      </p>
+
+      <p>
+        <strong>Note:</strong>&nbsp;The solution set must not contain duplicate
+        combinations.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> candidates = [10,1,2,7,6,1,5], target = 8 <br />
+        <strong>Output:</strong> <br />
+        {`[
+  [1,1,6],
+  [1,2,5],
+  [1,7],
+  [2,6]
+  ]`}
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> candidates = [2,5,2,1,2], target = 5 <br />
+        <strong>Output:</strong> <br />
+        {`[
+  [1,2,2],
+  [5]
+  ]`}
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;=&nbsp;candidates.length &lt;= 100</code>
+        </li>
+        <li>
+          <code>1 &lt;=&nbsp;candidates[i] &lt;= 50</code>
+        </li>
+        <li>
+          <code>1 &lt;= target &lt;= 30</code>
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
