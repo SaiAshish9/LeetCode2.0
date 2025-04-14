a1f1813f85542fad23a03372b7d4f5807ac8dccc
saiashish9 <saiashish3760@gmail.com>
2025-04-14 10:11:09 +0530

    k

--- src/screens/problem/data/questions1.js
+++ src/screens/problem/data/questions1.js
@@ -64474,6 +64474,72 @@ l a d y`}
       </div>
     </>
   ),
+  78: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        Given an integer array <code>nums</code> of <strong>unique</strong>{" "}
+        elements, return <em>all possible</em>{" "}
+        <span
+          data-keyword="subset"
+          class=" cursor-pointer relative text-dark-blue-s text-sm"
+        >
+          <button
+            type="button"
+            aria-haspopup="dialog"
+            aria-expanded="false"
+            aria-controls="radix-:r6f:"
+            data-state="closed"
+            class=""
+          >
+            <em>subsets</em>
+          </button>
+        </span>{" "}
+        <em>(the power set)</em>.
+      </p>
+
+      <p>
+        The solution set <strong>must not</strong> contain duplicate subsets.
+        Return the solution in <strong>any order</strong>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [1,2,3]
+        <strong>Output:</strong> [[],[1],[2],[1,2],[3],[1,3],[2,3],[1,2,3]]{" "}
+        <br />
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> nums = [0] <br />
+        <strong>Output:</strong> [[],[0]]
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>1 &lt;= nums.length &lt;= 10</code>
+        </li>
+        <li>
+          <code>-10 &lt;= nums[i] &lt;= 10</code>
+        </li>
+        <li>
+          All the numbers of&nbsp;<code>nums</code> are <strong>unique</strong>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
