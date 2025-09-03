e0e9166792c42a8d422ee0cf906c2691f63da051
saiashish9 <saiashish3760@gmail.com>
2025-09-03 15:18:03 +0530

    k

--- src/screens/problem/index.js
+++ src/screens/problem/index.js
@@ -308,6 +308,7 @@ const Problem = () => {
           <LogoImg
             style={{ paddingRight: "0.5rem" }}
             alt="img"
+            onClick={() => navigate("/")}
             src="https://leetcode.com/_next/static/images/logo-dark-c96c407d175e36c81e236fcfdd682a0b.png"
           />
           <MiddleLeftHeaderContainer>
