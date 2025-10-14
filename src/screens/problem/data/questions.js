89f4df6c2d3b961ebee90ca5c24b017ec5115fcf
saiashish9 <saiashish3760@gmail.com>
2025-10-14 15:32:31 +0530

    k

--- src/screens/problem/data/questions.js
+++ src/screens/problem/data/questions.js
@@ -52117,6 +52117,99 @@ m.next(5) = (10 + 3 + 5) / 3`}
       </ul>
     </div>
   ),
+  994: (
+    <div class="elfjS" data-track-load="description_content">
+      <p>
+        You are given an <code>m x n</code> <code>grid</code> where each cell
+        can have one of three values:
+      </p>
+
+      <ul>
+        <li>
+          <code>0</code> representing an empty cell,
+        </li>
+        <li>
+          <code>1</code> representing a fresh orange, or
+        </li>
+        <li>
+          <code>2</code> representing a rotten orange.
+        </li>
+      </ul>
+
+      <p>
+        Every minute, any fresh orange that is{" "}
+        <strong>4-directionally adjacent</strong> to a rotten orange becomes
+        rotten.
+      </p>
+
+      <p>
+        Return{" "}
+        <em>
+          the minimum number of minutes that must elapse until no cell has a
+          fresh orange
+        </em>
+        . If <em>this is impossible, return</em> <code>-1</code>.
+      </p>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong class="example">Example 1:</strong>
+      </p>
+      <img
+        alt=""
+        src="https://assets.leetcode.com/uploads/2019/02/16/oranges.png"
+        style={{ width: 650, height: 137 }}
+      />
+      <pre>
+        <strong>Input:</strong> grid = [[2,1,1],[1,1,0],[0,1,1]] <br />
+        <strong>Output:</strong> 4 <br />
+      </pre>
+
+      <p>
+        <strong class="example">Example 2:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid = [[2,1,1],[0,1,1],[1,0,1]] <br />
+        <strong>Output:</strong> -1 <br />
+        <strong>Explanation:</strong> The orange in the bottom left corner (row
+        2, column 0) is never rotten, because rotting only happens
+        4-directionally.
+      </pre>
+
+      <p>
+        <strong class="example">Example 3:</strong>
+      </p>
+
+      <pre>
+        <strong>Input:</strong> grid = [[0,2]] <br />
+        <strong>Output:</strong> 0 <br />
+        <strong>Explanation:</strong> Since there are already no fresh oranges
+        at minute 0, the answer is just 0.
+      </pre>
+
+      <p>&nbsp;</p>
+      <p>
+        <strong>Constraints:</strong>
+      </p>
+
+      <ul>
+        <li>
+          <code>m == grid.length</code>
+        </li>
+        <li>
+          <code>n == grid[i].length</code>
+        </li>
+        <li>
+          <code>1 &lt;= m, n &lt;= 10</code>
+        </li>
+        <li>
+          <code>grid[i][j]</code> is <code>0</code>, <code>1</code>, or{" "}
+          <code>2</code>.
+        </li>
+      </ul>
+    </div>
+  ),
 };
 
 export default QUESTIONS;
